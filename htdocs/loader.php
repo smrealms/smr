3578233575079452b5346f5e6937d007512680ca
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2008-12-18 23:03:14 +0000

    Remove magic quotes.

--- htdocs/loader.php
+++ htdocs/loader.php
@@ -1,4 +1,17 @@
 <?
+if (get_magic_quotes_gpc())
+{
+    function stripslashes_array($array)
+    {
+        return is_array($array) ? array_map('stripslashes_array', $array) : stripslashes($array);
+    }
+
+    $_COOKIE = stripslashes_array($_COOKIE);
+    $_FILES = stripslashes_array($_FILES);
+    $_GET = stripslashes_array($_GET);
+    $_POST = stripslashes_array($_POST);
+    $_REQUEST = stripslashes_array($_REQUEST);
+}
 
 //xdebug_start_profiling();
 
