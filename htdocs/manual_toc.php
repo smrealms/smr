145e088871ce92673f5b6fe319be06227513da87
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2008-12-18 22:23:50 +0000

    .

--- htdocs/manual_toc.php
+++ htdocs/manual_toc.php
@@ -5,70 +5,70 @@ require_once($LIB . 'global/smr_db.inc');
 include(ENGINE . 'Old_School/smr.inc');
 include(ENGINE . 'Old_School/help.inc');
 
-$PHP_OUTPUT.=('<!doctype html public "-//W3C//DTD HTML 4.0 Transitional//EN">');
+echo ('<!doctype html public "-//W3C//DTD HTML 4.0 Transitional//EN">');
 
-$PHP_OUTPUT.=('<html>');
-$PHP_OUTPUT.=('<head>');
-$PHP_OUTPUT.=('<link rel="stylesheet" type="text/css" href="'.$URL.'/default.css">');
-$PHP_OUTPUT.=('<title>Space Merchant Realms - Manual</title>');
-$PHP_OUTPUT.=('<meta http-equiv="pragma" content="no-cache">');
-$PHP_OUTPUT.=('</head>');
+echo ('<html>');
+echo ('<head>');
+echo ('<link rel="stylesheet" type="text/css" href="'.$URL.'/default.css">');
+echo ('<title>Space Merchant Realms - Manual</title>');
+echo ('<meta http-equiv="pragma" content="no-cache">');
+echo ('</head>');
 
-$PHP_OUTPUT.=('<body>');
+echo ('<body>');
 
-$PHP_OUTPUT.=('<table width="100%" border="0">');
+echo ('<table width="100%" border="0">');
 
-$PHP_OUTPUT.=('<tr>');
-$PHP_OUTPUT.=('<td>');
-$PHP_OUTPUT.=('<table>');
-$PHP_OUTPUT.=('<tr>');
-$PHP_OUTPUT.=('<th width="32">');
-$PHP_OUTPUT.=('<img src="'.$URL.'/images/help/empty.jpg" width="32" height="32">');
-$PHP_OUTPUT.=('</th>');
-$PHP_OUTPUT.=('<th width="32">');
-$PHP_OUTPUT.=('<img src="'.$URL.'/images/help/empty.jpg" width="32" height="32">');
-$PHP_OUTPUT.=('</th>');
-$PHP_OUTPUT.=('<th width="32">');
-$PHP_OUTPUT.=('<img src="'.$URL.'/images/help/empty.jpg" width="32" height="32">');
-$PHP_OUTPUT.=('</th>');
-$PHP_OUTPUT.=('<th width="100%" align="center" validn="middle" style="font-size:18pt;font-weight:bold;">Table of Content</th>');
-$PHP_OUTPUT.=('<th width="32"><a href="'.$URL.'/manual_toc.php"><img src="'.$URL.'/images/help/contents.jpg" width="32" height="32" border="0"></a></th>');
-$PHP_OUTPUT.=('</tr>');
-$PHP_OUTPUT.=('</table>');
-$PHP_OUTPUT.=('</td>');
-$PHP_OUTPUT.=('</tr>');
+echo ('<tr>');
+echo ('<td>');
+echo ('<table>');
+echo ('<tr>');
+echo ('<th width="32">');
+echo ('<img src="'.$URL.'/images/help/empty.jpg" width="32" height="32">');
+echo ('</th>');
+echo ('<th width="32">');
+echo ('<img src="'.$URL.'/images/help/empty.jpg" width="32" height="32">');
+echo ('</th>');
+echo ('<th width="32">');
+echo ('<img src="'.$URL.'/images/help/empty.jpg" width="32" height="32">');
+echo ('</th>');
+echo ('<th width="100%" align="center" validn="middle" style="font-size:18pt;font-weight:bold;">Table of Content</th>');
+echo ('<th width="32"><a href="'.$URL.'/manual_toc.php"><img src="'.$URL.'/images/help/contents.jpg" width="32" height="32" border="0"></a></th>');
+echo ('</tr>');
+echo ('</table>');
+echo ('</td>');
+echo ('</tr>');
 
-$PHP_OUTPUT.=('<tr>');
-$PHP_OUTPUT.=('<td>');
+echo ('<tr>');
+echo ('<td>');
 
 echo_menu(0);
 
-$PHP_OUTPUT.=('</td>');
-$PHP_OUTPUT.=('</tr>');
+echo ('</td>');
+echo ('</tr>');
 
-$PHP_OUTPUT.=('<tr>');
-$PHP_OUTPUT.=('<td>');
-$PHP_OUTPUT.=('<table>');
-$PHP_OUTPUT.=('<tr>');
-$PHP_OUTPUT.=('<th width="32">');
-$PHP_OUTPUT.=('<img src="'.$URL.'/images/help/empty.jpg" width="32" height="32">');
-$PHP_OUTPUT.=('</th>');
-$PHP_OUTPUT.=('<th width="32">');
-$PHP_OUTPUT.=('<img src="'.$URL.'/images/help/empty.jpg" width="32" height="32">');
-$PHP_OUTPUT.=('</th>');
-$PHP_OUTPUT.=('<th width="32">');
-$PHP_OUTPUT.=('<img src="'.$URL.'/images/help/empty.jpg" width="32" height="32">');
-$PHP_OUTPUT.=('</th>');
-$PHP_OUTPUT.=('<th width="100%" align="center" validn="middle" style="font-size:18pt;font-weight:bold;">Table of Content</th>');
-$PHP_OUTPUT.=('<th width="32"><a href="'.$URL.'/manual_toc.php"><img src="'.$URL.'/images/help/contents.jpg" width="32" height="32" border="0"></a></th>');
-$PHP_OUTPUT.=('</tr>');
-$PHP_OUTPUT.=('</table>');
-$PHP_OUTPUT.=('</td>');
-$PHP_OUTPUT.=('</tr>');
+echo ('<tr>');
+echo ('<td>');
+echo ('<table>');
+echo ('<tr>');
+echo ('<th width="32">');
+echo ('<img src="'.$URL.'/images/help/empty.jpg" width="32" height="32">');
+echo ('</th>');
+echo ('<th width="32">');
+echo ('<img src="'.$URL.'/images/help/empty.jpg" width="32" height="32">');
+echo ('</th>');
+echo ('<th width="32">');
+echo ('<img src="'.$URL.'/images/help/empty.jpg" width="32" height="32">');
+echo ('</th>');
+echo ('<th width="100%" align="center" validn="middle" style="font-size:18pt;font-weight:bold;">Table of Content</th>');
+echo ('<th width="32"><a href="'.$URL.'/manual_toc.php"><img src="'.$URL.'/images/help/contents.jpg" width="32" height="32" border="0"></a></th>');
+echo ('</tr>');
+echo ('</table>');
+echo ('</td>');
+echo ('</tr>');
 
-$PHP_OUTPUT.=('</table>');
+echo ('</table>');
 
-$PHP_OUTPUT.=('</body>');
-$PHP_OUTPUT.=('</html>');
+echo ('</body>');
+echo ('</html>');
 
 ?>
