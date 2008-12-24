0a7f97091000ed13cf8aea2ad5778b9e3eacf691
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2008-12-24 16:23:44 +0000

    .

--- htdocs/weapon_list.php
+++ htdocs/weapon_list.php
@@ -12,16 +12,16 @@ require_once(LIB . 'global/smr_db.inc');
 
 $db = new SMR_DB();
 
-$PHP_OUTPUT.=('<!doctype html public "-//W3C//DTD HTML 4.0 Transitional//EN">');
+echo ('<!doctype html public "-//W3C//DTD HTML 4.0 Transitional//EN">');
 
-$PHP_OUTPUT.=('<html>');
-$PHP_OUTPUT.=('<head>');
-$PHP_OUTPUT.=('<link rel="stylesheet" type="text/css" href="default.css">');
-$PHP_OUTPUT.=('<title>Weapon List</title>');
-$PHP_OUTPUT.=('<meta http-equiv="pragma" content="no-cache">');
-$PHP_OUTPUT.=('</head>');
+echo ('<html>');
+echo ('<head>');
+echo ('<link rel="stylesheet" type="text/css" href="default.css">');
+echo ('<title>Weapon List</title>');
+echo ('<meta http-equiv="pragma" content="no-cache">');
+echo ('</head>');
 
-$PHP_OUTPUT.=('<body>');
+echo ('<body>');
 $seq = $_REQUEST['seq'];
 $order = $_REQUEST['order'];
 if (empty($seq))
@@ -36,18 +36,18 @@ if (isset($order))
 else
 	$order_by = 'weapon_type_id';
 
-$db->query('SELECT * FROM weapon_type, race WHERE weapon_type.race_id = race.race_id ORDER BY $order_by '.$seq);
-$PHP_OUTPUT.=('<table class="standard" cellspacing="0">');
-$PHP_OUTPUT.=('<tr>');
-$PHP_OUTPUT.=('<th align="center"><a href="$site?order=weapon_name&seq='.$seq.'"><span style=color:#80C870;>Weapon Name</span></a></th>');
-$PHP_OUTPUT.=('<th align="center"><a href="$site?order=race_name&seq='.$seq.'"><span style=color:#80C870;>Race</span></a></th>');
-$PHP_OUTPUT.=('<th align="center"><a href="$site?order=cost&seq='.$seq.'"><span style=color:#80C870;>Cost</span></a></th>');
-$PHP_OUTPUT.=('<th align="center"><a href="$site?order=shield_damage&seq='.$seq.'"><span style=color:#80C870;>Shield Damage</span></a></th>');
-$PHP_OUTPUT.=('<th align="center"><a href="$site?order=armor_damage&seq='.$seq.'"><span style=color:#80C870;>Armor Damage</span></a></th>');
-$PHP_OUTPUT.=('<th align="center"><a href="$site?order=accuracy&seq='.$seq.'"><span style=color:#80C870;>Accuracy</span></a></th>');
-$PHP_OUTPUT.=('<th align="center"><a href="$site?order=power_level&seq='.$seq.'"><span style=color:#80C870;>Power Level</span></a></th>');
-$PHP_OUTPUT.=('<th align="center"><a href="$site?order=buyer_restriction&seq='.$seq.'"><span style=color:#80C870;>Restriction</span></a></th>');
-$PHP_OUTPUT.=('</tr>');
+$db->query('SELECT * FROM weapon_type, race WHERE weapon_type.race_id = race.race_id ORDER BY '.$order_by.' '.$seq);
+echo ('<table class="standard" cellspacing="0">');
+echo ('<tr>');
+echo ('<th align="center"><a href="'.$site.'?order=weapon_name&seq='.$seq.'"><span style=color:#80C870;>Weapon Name</span></a></th>');
+echo ('<th align="center"><a href="'.$site.'?order=race_name&seq='.$seq.'"><span style=color:#80C870;>Race</span></a></th>');
+echo ('<th align="center"><a href="'.$site.'?order=cost&seq='.$seq.'"><span style=color:#80C870;>Cost</span></a></th>');
+echo ('<th align="center"><a href="'.$site.'?order=shield_damage&seq='.$seq.'"><span style=color:#80C870;>Shield Damage</span></a></th>');
+echo ('<th align="center"><a href="'.$site.'?order=armor_damage&seq='.$seq.'"><span style=color:#80C870;>Armor Damage</span></a></th>');
+echo ('<th align="center"><a href="'.$site.'?order=accuracy&seq='.$seq.'"><span style=color:#80C870;>Accuracy</span></a></th>');
+echo ('<th align="center"><a href="'.$site.'?order=power_level&seq='.$seq.'"><span style=color:#80C870;>Power Level</span></a></th>');
+echo ('<th align="center"><a href="'.$site.'?order=buyer_restriction&seq='.$seq.'"><span style=color:#80C870;>Restriction</span></a></th>');
+echo ('</tr>');
 while ($db->next_record()) {
 
 	//we need an array so we dont have 8 td rows
@@ -67,12 +67,12 @@ while ($db->next_record()) {
     	$restriction = '&nbsp;';
     $stat[] = $restriction;
 
-    $PHP_OUTPUT.=('<tr>');
+    echo ('<tr>');
     foreach ($stat as $value)
-	    $PHP_OUTPUT.=('<td align="center">$value</td>');
+	    echo ('<td align="center">'.$value.'</td>');
 
-    $PHP_OUTPUT.=('</tr>');
+    echo ('</tr>');
 
 }
-$PHP_OUTPUT.=('</table>');
+echo ('</table>');
 ?>
