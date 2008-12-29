626b6b967439eaa782c0e37436233b41a594476a
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2008-12-29 20:19:31 +0000

    .

--- engine/Old_School/edit_dummys.php
+++ engine/Old_School/edit_dummys.php
@@ -5,6 +5,7 @@ require_once(get_file_loc('DummyPlayer.class.inc'));
 $smarty->assign('EditDummysLink',SmrSession::get_new_href(create_container('skeleton.php','edit_dummys.php')));
 
 $dummyPlayer =& DummyPlayer::getCachedDummyPlayer($_REQUEST['dummy_name']);
+$dummyShip =& $dummyPlayer->getShip();
 
 if(isset($_REQUEST['save_dummy']))
 {
@@ -16,6 +17,7 @@ if(isset($_REQUEST['save_dummy']))
 
 $smarty->assign('DummyNames', DummyPlayer::getDummyPlayerNames());
 $smarty->assign_by_ref('DummyPlayer',$dummyPlayer);
+$smarty->assign_by_ref('DummyShip',$dummyShip);
 $smarty->assign_by_ref('Levels',Globals::getLevelRequirements());
 
 //TODO add game type id
