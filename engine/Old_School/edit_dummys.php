02b8b763a82ceec8b2c51d1cf4b7beb21361cbb7
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2008-12-29 20:47:54 +0000

    .

--- engine/Old_School/edit_dummys.php
+++ engine/Old_School/edit_dummys.php
@@ -10,7 +10,6 @@ $smarty->assign('DummyNames', DummyPlayer::getDummyPlayerNames());
 $smarty->assign('EditDummysLink',SmrSession::get_new_href(create_container('skeleton.php','edit_dummys.php')));
 
 $dummyPlayer =& DummyPlayer::getCachedDummyPlayer($_REQUEST['dummy_name']);
-$dummyShip =& $dummyPlayer->getShip();
 
 if(isset($_REQUEST['save_dummy']))
 {
@@ -18,6 +17,8 @@ if(isset($_REQUEST['save_dummy']))
 	$dummyPlayer->setExperience($_REQUEST['level']);
 	$dummyPlayer->setShipTypeID($_REQUEST['ship_id']);
 }
+
+$dummyShip =& $dummyPlayer->getShip();
 
 $smarty->assign_by_ref('DummyPlayer',$dummyPlayer);
 $smarty->assign_by_ref('DummyShip',$dummyShip);
