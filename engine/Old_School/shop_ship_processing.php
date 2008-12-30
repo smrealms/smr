53328b7eac5f1897d25c75f1444c1f9a96779279
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2008-12-30 17:19:41 +0000

    .

--- engine/Old_School/shop_ship_processing.php
+++ engine/Old_School/shop_ship_processing.php
@@ -1,7 +1,7 @@
 <?
 
 $speed		= $var['speed'];
-$cost		= $var['cost'] - $ship->cost / 2;
+$cost		= $var['cost'] - $ship->getCost() / 2;
 $race_id	= $var['race_id'];
 
 // trade master 33
@@ -29,47 +29,23 @@ if ($player->getCredits() < $cost)
 	create_error('You do not have enough cash to purchase this ship!');
 
 // adapt turns
-$player->setTurns($player->getTurns() * $speed / $ship->speed);
+$player->setTurns($player->getTurns() * $speed / $ship->getSpeed());
 
 // take the money from the user
 $player->decreaseCredits($cost);
 $player->update();
 
 // assign the new ship
+$ship->decloak();
+$ship->disableIllusion();
 $ship->setShipTypeID($var['ship_id']);
 
 // delete cargo
 $ship->removeAllCargo();
 
-// update
-$ship->update();
-
-// adapt hardware
-$db->query('SELECT * FROM hardware_type');
-while ($db->next_record()) {
-
-	$hardware_type_id = $db->f('hardware_type_id');
-
-	// take hardware we don't support
-	if ($ship->getHardware($hardware_type_id) > $ship->getMaxHardware($hardware_type_id))
-		$ship->setHardware($hardware_type_id,$ship->getMaxHardware($hardware_type_id));
-}
-
-// take weapons that we can't carry
-$ship->weapon = array_slice($ship->weapon, 0, $ship->getHardpoints());
-
-// disable hardware
-$db->query('DELETE FROM ship_is_cloaked WHERE account_id = '.$player->getAccountID().' AND ' .
-											 'game_id = '.$player->getGameID());
-$db->query('DELETE FROM ship_has_illusion WHERE account_id = '.$player->getAccountID().' AND ' .
-											   'game_id = '.$player->getGameID());
-
-// if we changed max hardware we need to compensate this.
-$ship->mark_seen();
 
 // update again
-$ship->update_hardware();
-$ship->update_weapon();
+$ship->update();
 $ship->mark_seen();
 
 $account->log(10, 'Buys a '.$ship->getName().' for '.$cost.' credits', $player->getSectorID());
