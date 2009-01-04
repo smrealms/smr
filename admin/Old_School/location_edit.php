1293107cef67bde648c51d1619cf24e1622bc842
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-01-04 19:42:58 +0000

    .

--- admin/Old_School/location_edit.php
+++ admin/Old_School/location_edit.php
@@ -15,6 +15,16 @@ if(isset($var['location_type_id']))
 			$location->addWeaponSold($_REQUEST['add_weapon_id']);
 		if($_REQUEST['add_hardware_id']!=0)
 			$location->addHardwareSold($_REQUEST['add_hardware_id']);
+		if(isset($_REQUEST['remove_ships']) && is_array($_REQUEST['remove_ships']))
+			foreach($_REQUEST['remove_ships'] as $shipTypeID)
+				$location->removeShipSold($shipTypeID);
+		if(isset($_REQUEST['remove_weapons']) && is_array($_REQUEST['remove_weapons']))
+			foreach($_REQUEST['remove_weapons'] as $weaponTypeID)
+				$location->removeWeaponSold($weaponTypeID);
+		if(isset($_REQUEST['remove_hardware']) && is_array($_REQUEST['remove_hardware']))
+			foreach($_REQUEST['remove_hardware'] as $hardwareTypeID)
+				$location->removeHardwareSold($hardwareTypeID);
+
 		$location->setFed(isset($_REQUEST['fed']));
 		$location->setBar(isset($_REQUEST['bar']));
 		$location->setBank(isset($_REQUEST['bank']));
