7d4eec39b823e31f1372ec5569d587ef52e77b03
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-01-07 21:48:07 +0000

    .

--- engine/Default/alliance_stat_processing.php
+++ engine/Default/alliance_stat_processing.php
@@ -3,18 +3,22 @@ if (isset($var['alliance_id'])) $alliance_id = $var['alliance_id'];
 else $alliance_id = $player->getAllianceID();
 require_once(LIB . 'global/smr_alliance.inc');
 if (isset($_REQUEST['password']))
-	$password = $_REQUEST['password'];
+	$password = trim($_REQUEST['password']);
 if (isset($_REQUEST['description']))
-	$description = $_REQUEST['description'];
+	$description = trim($_REQUEST['description']);
 if (isset($_REQUEST['mod']))
-	$mod = $_REQUEST['mod'];
+{
+	if(preg_match('/"/',$_REQUEST['mod']))
+		create_error('You cannot use a " in the image link!');
+	$mod = trim($_REQUEST['mod']);	
+}
 if (isset($_REQUEST['url']))
 	$url = $_REQUEST['url'];
 if (isset($password) && $password == '')
-	create_error('I advice you set a non empty password!');
+	create_error('You cannot set an empty password!');
 
 if (isset($description) && $description == '')
-	create_error('Please enter a description for your alliance');
+	create_error('Please enter a description for your alliance.');
 
 $alliance = new SMR_ALLIANCE($alliance_id, $player->getGameID());
 if (isset($password))
