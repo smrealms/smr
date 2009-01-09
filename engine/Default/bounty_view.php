8b908359b879f9f70d3992b1a39c3cde6ece4d7a
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-01-09 15:18:37 +0000

    .

--- engine/Default/bounty_view.php
+++ engine/Default/bounty_view.php
@@ -4,44 +4,39 @@ $bounties = 0;
 $id = $var['id'];
 $curr_player =& SmrPlayer::getPlayer($id, $player->getGameID());
 $smarty->assign('PageTopic','Viewing '.$curr_player->getPlayerName());
-$db->query('SELECT * FROM bounty WHERE account_id = '.$id.' AND game_id = '.$player->getGameID().' AND type = ' . $db->escape_string('HQ'));
-while ($db->next_record()) {
 
-    $claimer = $db->f('claimer_id');
-	//$days = (TIME - $db->f('time')) / 60 / 60 / 24;
-    //$amount = round($db->f('amount') * pow(1.05,$days));
-    $amount = $db->f('amount');
-    $PHP_OUTPUT.=('The <font color=green>Federal Government</font> is offering a bounty on '.$curr_player->getPlayerName().' worth <font color=yellow>'.$amount.'</font> credits.<br>  ');
-    if ($claimer != 0) {
-
-        $claiming_player =& SmrPlayer::getPlayer($claimer, $player->getGameID());
-        $PHP_OUTPUT.=('This bounty can be claimed by '.$claiming_player->getPlayerName().'<br>');
-
-    }
-    $bounties += 1;
-	
+if($curr_player->hasBounties())
+{
+	$bounties = $curr_player->getBounties();
+	$hasBounty = false;
+	foreach($bounties as $bounty)
+	{
+		 if($bounty['Type'] == 'HQ')
+		 {
+		 	$PHP_OUTPUT.=('The <font color=green>Federal Government</font> is offering a bounty on '.$curr_player->getPlayerName().' worth <font color=yellow>'.$bounty['Amount'].'</font> credits.<br />');
+		 	if ($bounty['Claimer'] != 0)
+		 	{
+			 	$claiming_player =& SmrPlayer::getPlayer($bounty['Claimer'], $player->getGameID());
+			 	$PHP_OUTPUT.=('This bounty can be claimed by '.$claiming_player->getPlayerName().'<br />');
+			 	$hasBounty = true;
+		 	}
+		 }
+	}
+	if($hasBounty)
+		$PHP_OUTPUT.=('<br /><br /><br />');
+	foreach($bounties as $bounty)
+	{
+		 if($bounty['Type'] == 'UG')
+		 {
+			$PHP_OUTPUT.=('The <font color=red>Underground</font> is offering a bounty on '.$curr_player->getPlayerName().' worth <font color=yellow>'.$bounty['Amount'].'</font> credits.<br />');
+		 	if ($bounty['Claimer'] != 0)
+		 	{
+			 	$claiming_player =& SmrPlayer::getPlayer($bounty['Claimer'], $player->getGameID());
+			 	$PHP_OUTPUT.=('This bounty can be claimed by '.$claiming_player->getPlayerName().'<br />');
+		 	}
+		 }
+	}
 }
-if ($bounties > 0) $PHP_OUTPUT.=('<br><br><br>');
-
-
-$db->query('SELECT * FROM bounty WHERE account_id = '.$id.' AND game_id = '.$player->getGameID().' AND type = ' . $db->escape_string('UG'));
-
-while ($db->next_record()) {
-
-    $claimer = $db->f('claimer_id');
-	//$days = (TIME - $db->f('time')) / 60 / 60 / 24;
-    //$amount = round($db->f('amount') * pow(1.05,$days));
-    $amount = $db->f('amount');
-    $PHP_OUTPUT.=('The <font color=red>Underground</font> is offering a bounty on '.$curr_player->getPlayerName().' worth <font color=yellow>'.$amount.'</font> credits.<br>');
-    if ($claimer != 0) {
-
-        $claiming_player =& SmrPlayer::getPlayer($claimer, $player->getGameID());
-        $PHP_OUTPUT.=('This bounty can be claimed by '.$claiming_player->getPlayerName().'<br>');
-
-    }
-	$bounties += 1;
-}
-
-if ($bounties == 0)
-	$PHP_OUTPUT.=('This player has no bounties<br>');
+else
+	$PHP_OUTPUT.=('This player has no bounties<br />');
 ?>
