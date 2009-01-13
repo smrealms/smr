07dfb12b7883cd0b4e58ced6c972eae152a7640b
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-01-13 09:37:32 +0000

    .

--- engine/Classic/bar_ship_name.php
+++ engine/Classic/bar_ship_name.php
@@ -58,7 +58,7 @@ if ($action == "Paint a logo (3 SMR Credits)") {
 		$db->query("REPLACE INTO ship_has_name (game_id, account_id, ship_name) VALUES (" .
 				"$player->game_id, $player->account_id, " . format_string($orig_name, FALSE) . ")");
 		
-		move_uploaded_file($_FILES['photo']['tmp_name'], "UPLOAD" . SmrSession::$account_id . "logo");
+		move_uploaded_file($_FILES['photo']['tmp_name'], UPLOAD . SmrSession::$account_id . "logo");
 		print("<div align=center>Your logo was successfully painted!</div>");
 		include(get_file_loc("bar_opening.php"));
 		return;
