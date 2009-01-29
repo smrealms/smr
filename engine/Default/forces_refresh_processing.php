6cd616aa0abbfbb62435153a8debfed48e361bc2
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-01-29 11:30:47 +0000

    Change force expiry time calculations, and all are now handled in SmrForce class.

--- engine/Default/forces_refresh_processing.php
+++ engine/Default/forces_refresh_processing.php
@@ -2,15 +2,7 @@
 require_once(get_file_loc('SmrForce.class.inc'));
 $forces =& SmrForce::getForce($player->getGameID(), $player->getSectorID(), $var['owner_id']);
 
-if($forces->getCDs() == 0 && $forces->getMines() == 0 && $forces->getSDs() == 1) {
-	$days = 2;
-}
-else {
-	$days = ceil(($forces->getCDs() + $forces->getSDs() + $forces->getMines()) / 10);
-}
-if ($days > 5) $days = 5;
-$forces->setExpire(TIME + ($days * 86400));
-$forces->update();
+$forces->updateExpire();
 
 forward(create_container('skeleton.php', 'current_sector.php'));
 
