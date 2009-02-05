492794727092905dd5b84a39ae3f313475ed87ce
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-02-05 19:45:51 +0000

    #165

--- admin/Default/anon_acc_view.php
+++ admin/Default/anon_acc_view.php
@@ -8,36 +8,39 @@ if (empty($_REQUEST['acct_game'])) {
 	$container = array();
 	$container['url'] = 'skeleton.php';
 	$container['body'] = 'anon_acc_view.php';
-	$PHP_OUTPUT.=('What account would you like to view? (account_id/game_id)<br />');
+	$PHP_OUTPUT.=('What account would you like to view?<br />');
 	$PHP_OUTPUT.=create_echo_form($container);
-	$PHP_OUTPUT.=('<input type=text name=acct_game value="120/5">');
+	$PHP_OUTPUT.=('Account ID: <input type="text" name="anon_account" /><br />');
+	$PHP_OUTPUT.=('Game ID: <input type="text" name="game_id" /><br />');
 	$PHP_OUTPUT.=create_submit('Continue');
 	$PHP_OUTPUT.=('</form>');
 	
-} else {
-
+}
+else
+{
 	//db object
 	$db2 = new SmrMySqlDatabase();
 	//split the name
-	list ($acc, $game) = split('/',$_REQUEST['acct_game']);
+	$acc = $_REQUEST['anon_account'];
+	$game = $_REQUEST['game_id'];
 	//get account info
 	$query = 'SELECT * FROM anon_bank_transactions WHERE anon_id = '.$acc.' AND game_id = '.$game.' ORDER BY transaction_id';
 	$db->query($query);
-	if ($db->getNumRows() > 0) {
-		
+	if ($db->getNumRows() > 0)
+	{
 		$smarty->assign('PageTopic','ANON ACCOUNT '.$acc);
 		$PHP_OUTPUT.= create_table();
 		$PHP_OUTPUT.=('<tr><th align=center>Player Name</th><th align=center>Type</th><th align=center>Amount</th></tr>');
-		while ($db->nextRecord()) {
-			
+		while ($db->nextRecord())
+		{
 			$db2->query('SELECT * FROM player WHERE account_id = ' . $db->getField('account_id'));
 			$db2->nextRecord();
 			$PHP_OUTPUT.=('<tr><td align=center>');
-			$db2->getField('player_name');
+			$PHP_OUTPUT.=$db2->getField('player_name');
 			$PHP_OUTPUT.=('</td><td align=center>');
-			$db->getField('transaction');
+			$PHP_OUTPUT.=$db->getField('transaction');
 			$PHP_OUTPUT.=('</td><td align=center>');
-			$db->getField('amount');
+			$PHP_OUTPUT.=$db->getField('amount');
 			$PHP_OUTPUT.=('</td></tr>');
 			
 		}
