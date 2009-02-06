3630cd7e513e6ba1ea45ec73b9876fe77eb6bf9a
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-02-06 00:13:25 +0000

    #172 Prettify offline screen.

--- htdocs/offline.php
+++ htdocs/offline.php
@@ -1,60 +1,20 @@
-<?
+<?php
 
+// includes
 require_once('config.inc');
+require_once(ENGINE . 'Default/smr.inc');
 require_once(LIB . 'Default/SmrMySqlDatabase.class.inc');
+require_once(get_file_loc('SmrAccount.class.inc'));
+require_once(get_file_loc('SmrSession.class.inc'));
 
-// new db object
 $db = new SmrMySqlDatabase();
 
 $db->query('SELECT * FROM game_disable');
-if ($db->nextRecord()) {
+if ($db->nextRecord())
+{
+	$smarty->assign('Message','Space Merchant Realms is currently <strong>OFF-LINE</strong>.<br />'.$db->getField('reason'));
+}
+else header('Location: '.URL);
 
-	$reason = stripslashes($db->getField('reason'));
-
-} else header('Location: '.URL);
-
+require_once(LIB . 'Login/loginSmarty.php');
 ?>
-
-<!doctype html public '-//W3C//DTD HTML 4.0 Transitional//EN'>
-
-<html>
-
-<head>
-	<link rel='stylesheet' type='text/css' href='default.css'>
-	<meta http-equiv='Content-Type' content='text/html; charset=windows-1252'>
-	<meta http-equiv='Content-Language' content='en-us'>
-	<title>Space Merchant</title>
-</head>
-
-<body>
-
-<?php include('menu.html'); ?>
-
-<table border='0' cellpadding='0' cellspacing='1' width='85%'>
-<tr>
-	<td align='center'><b style='color:red;font-size:125%;'><br /><br /></b>
-	</td>
-</tr>
-<tr>
-	<td bgcolor='#0B8D35'>
-		<table border='0' cellpadding='3' cellspacing='2' width='100%'>
-		<tr bgcolor='#0B2121'>
-			<td>
-				<table border='0' cellpadding='3' cellspacing='2' width='100%'>
-				<tr bgcolor='#0B8D35'>
-					<td align='center'>
-						<p><font face='Times New Roman' size='+2' color='#FFFFFF'>Space Merchant Realms is currently <strong>OFF-LINE</strong>.</font></p>
-						<p><? echo ($reason); ?></p>
-					</td>
-				</tr>
-				</table>
-			</td>
-		</tr>
-		</table>
-	</td>
-</tr>
-</table>
-
-</body>
-
-</html>
