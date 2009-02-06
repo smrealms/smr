c21e30c6b0cc5fec102e3132d5746192ea254f3e
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-02-06 00:38:55 +0000

    .

--- engine/Default/alliance_detail_old.php
+++ engine/Default/alliance_detail_old.php
@@ -13,13 +13,12 @@ $container['game_name'] = $db->getField('game_name');
 
 //get alliance members
 $id = $var['alliance_id'];
-$PHP_OUTPUT.=($game_id.','. $id);
+//$PHP_OUTPUT.=($game_id.','. $id);
 $db->query('SELECT * FROM alliance WHERE alliance_id = '.$id.' AND game_id = '.$game_id);
 $db->nextRecord();
 $PHP_OUTPUT.=('<div align=center>');
 $PHP_OUTPUT.=create_link($container, '<b>&lt;&lt;Back</b>');
 $smarty->assign('PageTopic','Alliance Roster - ' . stripslashes($db->getField('alliance_name')));
-$db->query('SELECT * FROM player WHERE alliance_id = '.$id.' AND game_id = '.$game_id.' ORDER BY experience DESC');
 
 $PHP_OUTPUT.= '
 <table cellspacing="0" cellpadding="0" class="standard">
@@ -34,7 +33,9 @@ $PHP_OUTPUT.= '
 	</tr>
 ';
 
-while ($db->nextRecord()) {
+$db->query('SELECT * FROM player WHERE alliance_id = '.$id.' AND game_id = '.$game_id.' ORDER BY experience DESC');
+while ($db->nextRecord())
+{
 	
 	$PHP_OUTPUT.=('<tr>');
 	$PHP_OUTPUT.=('<td align=center>' . stripslashes($db->getField('player_name')) . '</td>');
