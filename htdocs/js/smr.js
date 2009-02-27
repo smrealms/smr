4523bb69bc83e218197e84075ab7fd88e7e9b950
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-02-27 19:28:36 +0000

    Some changes to optimise client-side performance, including moving inclusion of javascript files lower in the page (rendering is stalled until they are interpreted), and running all the javascript code through jslint.

--- htdocs/js/smr.js
+++ htdocs/js/smr.js
@@ -1,27 +1,34 @@
-function OpenWindow(linkName) {
-var popUpWindow = window.open(linkName, 'chat', 'resizeable, toolbar=no, location=no, directories=no, status=no, menubar=no, copyhistory=no, width=575, height=425')
-popUpWindow.focus(); } 
+function OpenWindow(linkName)
+{
+	var popUpWindow = window.open(linkName, 'chat', 'resizeable, toolbar=no, location=no, directories=no, status=no, menubar=no, copyhistory=no, width=575, height=425');
+	popUpWindow.focus();
+} 
 
-var AttackToggle = 0;
+var AttackToggle = false;
 
-function AttackBlink() {
+function AttackBlink()
+{
 
-	ele = document.getElementById('attack_warning');
+	var ele = document.getElementById('attack_warning');
 
-	AttackToggle = 1 - AttackToggle;
-	if (AttackToggle == 0) {
+	AttackToggle = !AttackToggle;
+	if (AttackToggle === false)
+	{
 		ele.style.color = '#ff0000';
 	}
-	else if (AttackToggle == 1) {
+	else if (AttackToggle === true)
+	{
 		ele.style.color = '#aa0000';
 	}
 }
 
-function SetBlink() {
-	setInterval('AttackBlink()',250);
+function SetBlink()
+{
+	setInterval(AttackBlink,250);
 }
 
-function DoSubmit(action,form_id) {
+function DoSubmit(action,form_id)
+{
 	var form = document.forms[form_id];
 	var input = document.createElement('input');
 	input.setAttribute('type', 'hidden');
@@ -30,21 +37,3 @@ function DoSubmit(action,form_id) {
 	form.appendChild(input);
 	form.submit();
 }
-
-// function DoOnload() {
-// return;
-	// var buttons = document.getElementsByName('action');
-	// //var num_buttons = buttons.length;
-	// while(buttons.length) {
-		// var text = document.createTextNode(buttons[0].value)
-		// var button = document.createElement('div');
-		// button.id = 'buttonA';
-		// button.innerHTML = '<a href="javascript:DoSubmit('' + buttons[0].value + '','' + buttons[0].parentNode.id + '')">' + buttons[0].value + '</a>';
-		// buttons[0].parentNode.insertBefore(button,buttons[0]);
-		// buttons[0].parentNode.removeChild(buttons[0]);
-
-	// }
-	// document.cookie='Legit=0';
-// }
-
-// window.onload = DoOnload;
