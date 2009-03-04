5a61d7d52700023f337cc53454f49b40b5935d41
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-03-04 16:20:00 +0000

    Stop an error when trying to kick no one.

--- engine/Default/alliance_remove_member_processing.php
+++ engine/Default/alliance_remove_member_processing.php
@@ -2,7 +2,8 @@
 require_once(LIB . 'global/smr_alliance.inc');
 $alliance = new SMR_ALLIANCE($player->getAllianceID(), SmrSession::$game_id);
 $account_id = $_REQUEST['account_id'];
-foreach ($account_id as $id) {
+foreach ($account_id as $id)
+{
 	
 	if ($id == $alliance->leader_id)
 		create_error('You can\'t remove the leader!');
@@ -16,6 +17,10 @@ foreach ($account_id as $id) {
 
 }
 
+if(empty($account_id_list))
+{
+	create_error('You have to choose someone to remove them!');
+}
 $db->query('UPDATE player SET alliance_id = 0 WHERE account_id IN ('.$account_id_list.') AND ' .
 												   'game_id = '.$player->getGameID());
 $db->query('DELETE FROM player_has_alliance_role WHERE game_id = '.$player->getGameID().' AND account_id IN ('.$account_id_list.')');
