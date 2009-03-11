7db12446f5dedfb5e18dea4450431ccd7218c59c
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-03-11 15:35:39 +0000

    Remove more unnecessary HTML.

--- lib/templates/engine/Default/skeleton.php
+++ lib/templates/engine/Default/skeleton.php
@@ -1,37 +1,29 @@
 <?php $this->includeTemplate('includes/header.inc'); ?>
-<table class="m" align="center" cellspacing="0" cellpadding="0">
+<table class="m" align="center">
 	<tr>
 		<td class="l0" rowspan="2">
 			<div class="l1">
-				<div class="l2">
-					<?php $this->includeTemplate('includes/leftPanel.inc'); ?>
-				</div>
+				<?php $this->includeTemplate('includes/leftPanel.inc'); ?>
 			</div>
 		</td>
 		<td class="m0" colspan="2">
-			<div class="m1">
-				<div class="m2">
-					<div id="middle_panel">
-						<?php
-							if($PageTopic)
-							{
-								?><h1><?php echo $PageTopic ?></h1><br /><?php
-							}
-							if($MenuBar)
-							{
-								echo $MenuBar;
-							}
-							$this->includeTemplate($TemplateBody);
-						?>
-					</div>
-				</div>
+			<div id="middle_panel">
+				<?php
+					if($PageTopic)
+					{
+						?><h1><?php echo $PageTopic ?></h1><br /><?php
+					}
+					if($MenuBar)
+					{
+						echo $MenuBar;
+					}
+					$this->includeTemplate($TemplateBody);
+				?>
 			</div>
 		</td>
 		<td class="r0">
-			<div class="r1">
-				<div class="r2" id="right_panel">
-					<?php $this->includeTemplate('includes/rightPanel.inc'); ?>
-				</div>
+			<div id="right_panel">
+				<?php $this->includeTemplate('includes/rightPanel.inc'); ?>
 			</div>
 		</td>
 	</tr>
