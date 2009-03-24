e080aa99f843036ef958008b773eefb8cae4806b
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-03-24 11:17:29 +0000

    Stop multiple action errors after getting a database error, make under attack warning stay when AJAX/user refreshes.

--- engine/Default/beta_func_processing.php
+++ engine/Default/beta_func_processing.php
@@ -26,7 +26,8 @@ elseif ($var['func'] == 'PageNewb')
 	$page =& SmrPlayer::getPlayer(ACCOUNT_PAGE,$player->getGameID());
 	$page->setNewbieTurns(0);
 }
-elseif ($var['func'] == 'Ship' && $_REQUEST['ship_id'] <= 75 && $_REQUEST['ship_id'] != 68) {
+elseif ($var['func'] == 'Ship' && $_REQUEST['ship_id'] <= 75 && $_REQUEST['ship_id'] != 68)
+{
 	$ship_id = $_REQUEST['ship_id'];
 	
 	$speed = $ship->getSpeed();
