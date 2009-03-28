f3ae8948083aab2e9dd92cbbb32554659834a0c0
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-03-28 10:20:52 +0000

    Add a quick link to blacklist players when viewing messages, also add Manage Blacklist to "menue"

--- engine/Default/message_blacklist_add.php
+++ engine/Default/message_blacklist_add.php
@@ -4,25 +4,32 @@ $container = array();
 $container['url'] = 'skeleton.php';
 $container['body'] = 'message_blacklist.php';
 
-if(!isset($_REQUEST['PlayerName'])) {
+if(!isset($_REQUEST['PlayerName']) && !isset($var['account_id']))
+{
 	$container['error'] = 1;	
 	forward($container);
 	exit;
 }
 
-$player_name = mysql_real_escape_string($_REQUEST['PlayerName']);
-
-$db = new SmrMySqlDatabase();
-
-$db->query('SELECT account_id FROM player WHERE player_name=\'' . $player_name . '\' AND game_id=' . SmrSession::$game_id . ' LIMIT 1');
-
-if(!$db->nextRecord()) {
-	$container['error'] = 1;	
-	forward($container);
-	exit;
+if(isset($var['account_id']))
+{
+	$blacklisted_id = $var['account_id'];
 }
-
-$blacklisted_id = $db->getField('account_id');
+else
+{
+	$player_name = mysql_real_escape_string($_REQUEST['PlayerName']);
+	
+	$db = new SmrMySqlDatabase();
+	
+	$db->query('SELECT account_id FROM player WHERE player_name=\'' . $player_name . '\' AND game_id=' . SmrSession::$game_id . ' LIMIT 1');
+	
+	if(!$db->nextRecord()) {
+		$container['error'] = 1;	
+		forward($container);
+		exit;
+	}
+	$blacklisted_id = $db->getField('account_id');
+}
 
 $db->query('SELECT account_id FROM message_blacklist WHERE account_id=' . SmrSession::$account_id . ' AND blacklisted_id=' . $blacklisted_id . ' AND game_id=' . SmrSession::$game_id . ' LIMIT 1');
 
