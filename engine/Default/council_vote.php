656f87f59e14996c0fcd9209e24c1047f4523f31
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-04-02 16:56:02 +0000

    Fix council vote.

--- engine/Default/council_vote.php
+++ engine/Default/council_vote.php
@@ -125,7 +125,7 @@ if ($db->getNumRows() > 0) {
 			$PHP_OUTPUT.=create_submit_style('No', 'background-color:green;');
 		else
 			$PHP_OUTPUT.=create_submit('No');
-		if ($player->equals($president))
+		if ($president !== false && $player->equals($president))
 		{
 			$PHP_OUTPUT.=('&nbsp;');
 			$PHP_OUTPUT.=create_submit('Veto');
