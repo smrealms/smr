cc2b932c5b8edf471c826671b805403211cd8c7b
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-05-03 15:30:51 +0000

    Fix viewing claimable bounty list ( Trader -> Bounties )

--- engine/Default/trader_bounties.php
+++ engine/Default/trader_bounties.php
@@ -10,22 +10,22 @@ $PHP_OUTPUT.= 'Bounties awaiting collection.<br /><br />';
 $PHP_OUTPUT.= '<table class="standard fullwidth"><tr><th>Federal</th><th>Underground</th></tr><tr>';
 
 $db->query('SELECT * FROM bounty WHERE claimer_id=' . $player->getAccountID() . ' AND game_id=' . $player->getGameID() .' AND type=\'HQ\'');
-doBountyList(&$PHP_OUTPUT,&$db);
+doBountyList($PHP_OUTPUT,$db,$player);
 $db->query('SELECT * FROM bounty WHERE claimer_id=' . $player->getAccountID() . ' AND game_id=' . $player->getGameID() .' AND type=\'UG\'');
-doBountyList(&$PHP_OUTPUT,&$db);
+doBountyList($PHP_OUTPUT,$db,$player);
 $PHP_OUTPUT.= '</tr></table>';
 
-function doBountyList(&$PHP_OUTPUT,&$db)
+function doBountyList(&$PHP_OUTPUT,&$db,&$player)
 {
 	$PHP_OUTPUT.='<td style="width:50%" class="top">';
 	$any=false;
 	while($db->nextRecord())
 	{
 		$any=true;
-		$bountyPlayer =& SmrPlayer::getPlayer($player->getGameID());
+		$bountyPlayer =& SmrPlayer::getPlayer($db->getField('account_id'),$player->getGameID());
 		$PHP_OUTPUT.= $bountyPlayer->getDisplayName()
 						.' : <span class="yellow">'.number_format($db->getField('amount')).'</span> credits and'
-						. '<span class="yellow">'.number_format($db->getField('smr_credits')). '</span> SMR credits<br />';
+						. ' <span class="yellow">'.number_format($db->getField('smr_credits')). '</span> SMR credits<br />';
 	}
 	if(!$any)
 		$PHP_OUTPUT.='None';
