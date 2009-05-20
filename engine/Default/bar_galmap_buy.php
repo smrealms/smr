1a9bbd745208818afa4ed8df52f3381770bc90e8
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-05-20 15:00:57 +0000

    Switch to using OO methods for accessing galaxy info for galaxy map buying.  This will fix a possible error with galaxy names (related to local/galaxy map error)

--- engine/Default/bar_galmap_buy.php
+++ engine/Default/bar_galmap_buy.php
@@ -1,27 +1,18 @@
 <?
-require_once(get_file_loc('SmrSector.class.inc'));
-$sector =& SmrSector::getSector(SmrSession::$game_id, $player->getSectorID());
-
 $gameInfo =& Globals::getGameInfo($player->getGameID());
 if($gameInfo['StartDate']+86400*3 > TIME)
 	create_error('You cannot buy maps within the first 3 days');
 
-if ($account->getTotalSmrCredits() < 2) {
-	
+if ($account->getTotalSmrCredits() < 2)
 	create_error('You don\'t have enough SMR Credits.  Donate money to SMR to gain SMR Credits!');
-	return;
-}
 
 //gal map buy
 if (isset($var['process']))
 {
 	$gal_id = $_REQUEST['gal_id'];
-	if ($gal_id == 0) {
-		
+	if ($gal_id == 0)
 		create_error('You must select a galaxy to buy the map of!');
-		return;
-		
-	}
+
 	$player->increaseHOF(1,array('Bar','Maps Bought'));
 	//take money
 	$account->decreaseTotalSmrCredits(2);
@@ -29,20 +20,15 @@ if (isset($var['process']))
 	$account_id = $player->getAccountID();
 	$game_id = $player->getGameID();
 	//get start sector
-	$db->query('SELECT * FROM sector WHERE galaxy_id = '.$gal_id.' AND game_id = '.$player->getGameID().' ORDER BY sector_id LIMIT 1');
-	$db->nextRecord();
-	$low = $db->getField('sector_id');
+	$galaxy =& SmrGalaxy::getGalaxy($player->getGameID(),$gal_id);
+	$low = $galaxy->getStartSector();
 	//get end sector
-	$db->query('SELECT * FROM sector WHERE galaxy_id = '.$gal_id.' AND game_id = '.$player->getGameID().' ORDER BY sector_id DESC LIMIT 1');
-	$db->nextRecord();
-	$high = $db->getField('sector_id');
+	$high = $galaxy->getEndSector();
 
 	// Have they already got this map? (Are there any unexplored sectors?
 	$db->query('SELECT * FROM player_visited_sector WHERE sector_id >= '.$low.' AND sector_id <= '.$high.' AND account_id = '.$account_id.' AND game_id = '.$game_id.' LIMIT 1');
-	if(!$db->nextRecord()) {
+	if(!$db->nextRecord())
 		create_error('You already have maps of this galaxy!');
-		return;
-	}
 	
 	// delete all entries from the player_visited_sector/port table
 	$db->query('DELETE FROM player_visited_sector WHERE sector_id >= '.$low.' AND sector_id <= '.$high.' AND account_id = '.$account_id.' AND game_id = '.$game_id);
@@ -59,9 +45,9 @@ if (isset($var['process']))
 	//offer another drink and such
 	$PHP_OUTPUT.=('<div align=center>Galaxy Info has been added.  Enjoy!</div><br />');
 	include(get_file_loc('bar_opening.php'));
-	
-} else {
-	
+}
+else
+{
 	//find what gal they want
 	$container = array();
 	$container['url'] = 'skeleton.php';
@@ -72,19 +58,14 @@ if (isset($var['process']))
 	$PHP_OUTPUT.=create_echo_form($container);
 	$PHP_OUTPUT.=('<select type=select name=gal_id>');
 	$PHP_OUTPUT.=('<option value=0>[Select a galaxy]</option>');
-	$db->query('SELECT galaxy_id FROM sector WHERE game_id = '.$player->getGameID().' GROUP BY galaxy_id ORDER BY galaxy_id ASC');
-	$db2 = new SmrMySqlDatabase();
-	while ($db->nextRecord()) {
-		
-		$gal_id = $db->getField('galaxy_id');
-		$db2->query('SELECT * FROM galaxy WHERE galaxy_id = '.$gal_id);
-		if ($db2->nextRecord()) $PHP_OUTPUT.=('<option value='.$gal_id.'>' . $db2->getField('galaxy_name') . '</option>');
-		
-	}
+	$gameGalaxies =& SmrGalaxy::getGameGalaxies($player->getGameID());
+	foreach ($gameGalaxies as &$galaxy)
+	{	
+		$PHP_OUTPUT.=('<option value='.$galaxy->getGalaxyID().'>' . $galaxy->getName() . '</option>');
+	} unset($galaxy);
 	$PHP_OUTPUT.=('</select><br />');
 	$PHP_OUTPUT.=create_submit('Buy the map');
 	$PHP_OUTPUT.=('</form></div>');
-	
 }
 
 ?>
