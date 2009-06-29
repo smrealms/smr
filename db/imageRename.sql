c443b27a1be2c321e075c9704d7b8a647e22fd8c
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-06-29 13:23:36 +0000

    Some more compressed images and a database script to update some image links in database.

---
+++ db/imageRename.sql
@@ -0,0 +1,3 @@
+UPDATE `location_type` SET `location_image`='images/hardware.png' WHERE `location_image`='images/hardware.gif';
+UPDATE `location_type` SET `location_image`='images/bank.png' WHERE `location_image`='images/bank.gif';
+UPDATE `location_type` SET `location_image`='images/bar.png' WHERE `location_image`='images/bar.gif';
