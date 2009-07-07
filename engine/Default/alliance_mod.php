b65dc123643ca6a3c2ae1a1964acffdbdcb9c430
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-07-07 19:24:54 +0000

    Allow bbcode in MOTD.

--- engine/Default/alliance_mod.php
+++ engine/Default/alliance_mod.php
@@ -18,7 +18,7 @@ if (strlen($db->getField('img_src')) && $db->getField('img_src') != 'http://') {
 }
 
 $PHP_OUTPUT.= '<span class="yellow">Message from your leader</span><br /><br />';
-$PHP_OUTPUT.= $db->getField('mod');
+$PHP_OUTPUT.= bbifyMessage($db->getField('mod'));
 
 $db->query('SELECT * FROM player_has_alliance_role WHERE account_id = '.$player->getAccountID().' AND game_id = '.$player->getGameID().' AND alliance_id='.$alliance_id);
 if ($db->nextRecord()) $role_id = $db->getField('role_id');
