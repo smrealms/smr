87a59219c68b414795c15a62d2f36d7c44d6dc38
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-07-26 12:45:38 +0000

    Add ability to retrieve game info for all games at once using the Globals::getGameInfo method. Separate admin_message_send out into a template. Add a preview option to admin messages.

--- admin/Default/admin_message_send.php
+++ admin/Default/admin_message_send.php
@@ -1,49 +1,32 @@
-<?
+<?php
 
 $template->assign('PageTopic','Send Message');
-$game_id = $_REQUEST['game_id'];
+$gameID = isset($_REQUEST['game_id'])?$_REQUEST['game_id'] : (isset($var['game_id']) ? $var['game_id'] : null);
 // check if we know the game yet
-if (empty($game_id)) {
+if (empty($gameID))
+{
+	$template->assign('AdminMessageChooseGameFormHref',SmrSession::get_new_href(create_container('skeleton.php', 'admin_message_send.php')));
+	$template->assignByRef('Games',Globals::getGameInfo());
+}
+else
+{
+	$container =create_container('admin_message_send_processing.php');
+	$container['game_id']	= $gameID;
+	$template->assign('AdminMessageSendFormHref',SmrSession::get_new_href($container));
+	$template->assign('MessageGameID',$gameID);
 
-	$PHP_OUTPUT.=create_echo_form(create_container('skeleton.php', 'admin_message_send.php'));
-	$PHP_OUTPUT.=('<p>Please select a game:</p>');
-	$PHP_OUTPUT.=('<select name="game_id" size="1" id="InputFields">');
-	$PHP_OUTPUT.=('<option value="20000">Send to All Players</option>');
-
-	$db->query('SELECT * FROM game ORDER BY game_id');
-
-	while ($db->nextRecord())
-		$PHP_OUTPUT.=('<option value="' . $db->getField('game_id') . '">' . $db->getField('game_name') . '</option>');
-
-	$PHP_OUTPUT.=('</select>&nbsp;&nbsp;');
-	$PHP_OUTPUT.=create_submit('Select');
-	$PHP_OUTPUT.=('</form>');
-
-} else {
-
-	$container = array();
-	$container['url']		= 'admin_message_send_processing.php';
-	$container['game_id']	= $game_id;
-
-	$PHP_OUTPUT.=create_echo_form($container);
-	if ($game_id != 20000) {
-		
+	if ($gameID != 20000)
+	{
 		$PHP_OUTPUT.=('<select name="account_id" size="1" id="InputFields">');
 		$PHP_OUTPUT.=('<option value="0">[Please Select]</option>');
 	
-		$db->query('SELECT * FROM player WHERE game_id = '.$game_id.' ORDER BY player_id');
-	
+		$gamePlayers = array();
+		$db->query('SELECT * FROM player WHERE game_id = '.$gameID.' ORDER BY player_id');
 		while ($db->nextRecord())
-			$PHP_OUTPUT.=('<option value="' . $db->getField('account_id') . '">' . stripslashes($db->getField('player_name')) . ' (' . $db->getField('player_id') . ')</option>');
-	
-		$PHP_OUTPUT.=('</select><br /><br />');
-		
+			$gamePlayers[]= array('AccountID' => $db->getField('account_id'), 'PlayerID' => $db->getField('player_id'), 'Name' => $db->getField('player_name'));
+		$template->assignByRef('GamePlayers',$gamePlayers);
 	}
-	$PHP_OUTPUT.=('<textarea name="message" id="InputFields" style="width:350px;height:100px;"></textarea><br />');
-	$PHP_OUTPUT.=('Hours Till Expire: <input type=text name=expire value=1 size=2 id=InputFields> (0 = never expire)<br /><br />');
-	$PHP_OUTPUT.=create_submit('Send message');
-	$PHP_OUTPUT.=('</form>');
-
+	if(isset($var['preview']))
+		$template->assign('Preview', $var['preview']);
 }
-
 ?>
--- admin/Default/admin_message_send_processing.php
+++ admin/Default/admin_message_send_processing.php
@@ -1,35 +1,43 @@
 <?
+
+$message = htmlentities(trim($_POST['message']));
+if($_REQUEST['action'] == 'Preview message')
+{
+	$container = create_container('skeleton.php','admin_message_send.php');
+	transfer('game_id');
+	$container['preview'] = $message;
+	forward($container);
+}
+
 $account_id = $_REQUEST['account_id'];
 $game_id = $var['game_id'];
-if (!empty($account_id) || $game_id == 20000) {
-
+if (!empty($account_id) || $game_id == 20000)
+{
 	$message = $_REQUEST['message'];
 	$message = $db->escape_string($message, false);
 	$expire = $_REQUEST['expire'];
 	if ($expire > 0) $expire = ($expire * 3600) + TIME;
-	if ($game_id != 20000) {
-		
+	if ($game_id != 20000)
+	{
 		$db->query('INSERT INTO message (account_id, game_id, message_type_id, message_text, sender_id, send_time, expire_time)
-					VALUES('.$account_id.', '.$game_id.', '.MSG_ADMIN.', '.$message.', 0, '.TIME.', '.$expire.')');
+					VALUES('.$account_id.', '.$game_id.', '.MSG_ADMIN.', '.$message.', '.ACCOUNT_ID_ADMIN.', '.TIME.', '.$expire.')');
 	
 		// give him the message icon
 		$db->query('REPLACE INTO player_has_unread_messages (game_id, account_id, message_type_id) VALUES
 					('.$game_id.', '.$account_id.', '.MSG_ADMIN.')');
-					
-	} else {
-		
+	}
+	else
+	{
 		//send to all players
 		$db2 = new SmrMySqlDatabase();
 		$db->query('SELECT * FROM player');
-		while ($db->nextRecord()) {
-			
+		while ($db->nextRecord())
+		{
 			$db2->query('INSERT INTO message (account_id, game_id, message_type_id, message_text, sender_id, send_time, expire_time)' .
-					'VALUES(' . $db->getField('account_id') . ', ' . $db->getField('game_id') . ', '.MSG_ADMIN.', '.$message.', 0, '.TIME.', '.$expire.')');
+					'VALUES(' . $db->getField('account_id') . ', ' . $db->getField('game_id') . ', '.MSG_ADMIN.', '.$message.', '.ACCOUNT_ID_ADMIN.', '.TIME.', '.$expire.')');
 			$db2->query('REPLACE INTO player_has_unread_messages (game_id, account_id, message_type_id) VALUES' .
 					'(' . $db->getField('game_id') . ', ' . $db->getField('account_id') . ', '.MSG_ADMIN.')');
-					
 		}
-		
 	}
 }
 
