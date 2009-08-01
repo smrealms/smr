1a39f367d547c13e251859752a81ac70a28e86c1
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-08-01 16:03:16 +0000

    Separate message_view into a template.

--- engine/Default/message_view.php
+++ engine/Default/message_view.php
@@ -1,21 +1,12 @@
-<?
+<?php
 
-if (!isset($var['folder_id'])) {
+require_once(get_file_loc('menue.inc'));
+create_message_menue();
 
+if (!isset($var['folder_id']))
+{
 	$template->assign('PageTopic','VIEW MESSAGES');
 
-	include(get_file_loc('menue.inc'));
-	$PHP_OUTPUT.=create_message_menue();
-
-	$PHP_OUTPUT.=('<p>Please choose your Message folder!</p>');
-
-	$PHP_OUTPUT.=('<p>');
-	$PHP_OUTPUT.=('<table class="standard">');
-	$PHP_OUTPUT.=('<tr>');
-	$PHP_OUTPUT.=('<th>Folder</th>');
-	$PHP_OUTPUT.=('<th>Messages</th>');
-	$PHP_OUTPUT.=('<th>&nbsp;</th>');
-	$PHP_OUTPUT.=('</tr>');
 
 	$db2 = new SmrMySqlDatabase();
 	
@@ -30,237 +21,166 @@ if (!isset($var['folder_id'])) {
 		$db->query('SELECT * FROM message_type ' .
 					'WHERE message_type_id != 5 ' .
 				   'ORDER BY message_type_id');
-
+	$messageBoxes = array();
 	while ($db->nextRecord())
 	{
 		$message_type_id = $db->getField('message_type_id');
-		$message_type_name = $db->getField('message_type_name');
+		$messageBox['Name'] = $db->getField('message_type_name');
 
 		// do we have unread msges in that folder?
-		$db2->query('SELECT * FROM message ' .
+		$db2->query('SELECT message_id FROM message ' .
 						'WHERE account_id = '.SmrSession::$account_id.' AND ' .
 							  'game_id = '.SmrSession::$game_id.' AND ' .
 							  'message_type_id = '.$message_type_id.' AND ' .
 							  'msg_read = \'FALSE\' AND ' .
-							  'reciever_delete = \'FALSE\''
+							  'reciever_delete = \'FALSE\' LIMIT 1'
 					);
-		$msg_read = $db2->getNumRows();
+		$messageBox['HasUnread'] = $db2->getNumRows() != 0;
 
+		$messageBox['MessageCount'] = 0;
 		// get number of msges
 		$db2->query('SELECT count(message_id) as message_count FROM message ' .
 						'WHERE account_id = '.SmrSession::$account_id.' AND ' .
 							  'game_id = '.SmrSession::$game_id.' AND ' .
 							  'message_type_id = '.$message_type_id.' AND reciever_delete = \'FALSE\'');
 		if ($db2->nextRecord())
-			$message_count = $db2->getField('message_count');
+			$messageBox['MessageCount'] = $db2->getField('message_count');
 
-		$PHP_OUTPUT.=('<tr>');
-		$PHP_OUTPUT.=('<td');
-		if ($msg_read) $PHP_OUTPUT.=(' style="font-weight:bold;"');
-		$PHP_OUTPUT.=('>');
 		$container = array();
 		$container['url'] = 'skeleton.php';
 		$container['body'] = 'message_view.php';
 		$container['folder_id'] = $message_type_id;
-		$PHP_OUTPUT.=create_link($container, $message_type_name);
-		$PHP_OUTPUT.=('</td>');
-		$PHP_OUTPUT.=('<td align="center" style="color:yellow;');
-		if ($msg_read) $PHP_OUTPUT.=('font-weight:bold;');
-		$PHP_OUTPUT.=('">'.$message_count.'</td>');
-		$PHP_OUTPUT.=('<td');
-		if ($msg_read) $PHP_OUTPUT.=(' style="font-weight:bold;"');
-		$PHP_OUTPUT.=('>');
+		$messageBox['ViewHref'] = SmrSession::get_new_href($container);
+		
 		$container = array();
 		$container['url'] = 'message_delete_processing.php';
 		$container['folder_id'] = $message_type_id;
-		$PHP_OUTPUT.=create_link($container, 'Empty');
-		$PHP_OUTPUT.=('</td>');
-		$PHP_OUTPUT.=('</tr>');
-
+		$messageBox['DeleteHref'] = SmrSession::get_new_href($container);
+		$messageBoxes[] = $messageBox;
 	}
-
-	$PHP_OUTPUT.=('</table>');
-	$PHP_OUTPUT.=('</p><p>');
-	
-	$container = array();
-	$container['url'] = 'skeleton.php';
-	$container['body'] = 'message_blacklist.php';
-	$PHP_OUTPUT.=create_link($container,'Manage Player Blacklist');
-	
-	$PHP_OUTPUT.= '</p>';
-
-} else {
-	
-	$db->query('SELECT * FROM message ' .
+	$template->assignByRef('MessageBoxes',$messageBoxes);
+}
+else
+{
+	$db->query('SELECT count(*) as count FROM message ' .
 						'WHERE account_id = '.SmrSession::$account_id.' AND ' .
 							  'game_id = '.SmrSession::$game_id.' AND ' .
 							  'message_type_id = ' . $var['folder_id'] . ' AND ' .
 							  'msg_read = \'FALSE\' AND reciever_delete = \'FALSE\'');
-	$unread_messages = $db->getNumRows();
+	$db->nextRecord();
+	$messageBox['UnreadMessages'] = $db->getField('count');
+	$messageBox['Type'] = $var['folder_id'];
 	// remove entry for this folder from unread msg table
-	$player->setMessagesRead($var['folder_id']);
+	$player->setMessagesRead($messageBox['Type']);
 
 	$db->query('SELECT * FROM message_type WHERE message_type_id = ' . $var['folder_id']);
 	if ($db->nextRecord())
-		$template->assign('PageTopic','VIEW ' . $db->getField('message_type_name'));
+		$messageBox['Name'] = $db->getField('message_type_name');
+	$template->assign('PageTopic','Viewing ' . $messageBox['Name']);
 
-	include(get_file_loc('menue.inc'));
-	$PHP_OUTPUT.=create_message_menue();
-
-	if ($var['folder_id'] == MSG_GLOBAL) {
-
-		$PHP_OUTPUT.=create_echo_form(create_container('message_global_ignore.php', ''));
-		$PHP_OUTPUT.=('<div align="center">Ignore global messages?&nbsp;&nbsp;');
-
-		if ($player->isIgnoreGlobals())
-			$PHP_OUTPUT.=create_submit_style('Yes', 'background-color:green;');
-		else
-			$PHP_OUTPUT.=create_submit('Yes');
-		$PHP_OUTPUT.=('&nbsp;');
-		if (!$player->isIgnoreGlobals())
-			$PHP_OUTPUT.=create_submit_style('No', 'background-color:green;');
-		else
-			$PHP_OUTPUT.=create_submit('No');
-		$PHP_OUTPUT.=('</div></form>');
-
+	if ($messageBox['Type'] == MSG_GLOBAL)
+	{
+		$template->assign('IgnoreGlobalsFormHref', SmrSession::get_new_href(create_container('message_global_ignore.php')));
 	}
 
-	$PHP_OUTPUT.=('<br />');
-	$container = array();
-	$container['url'] = 'message_delete_processing.php';
+	$container = create_container('message_delete_processing.php');
 	transfer('folder_id');
-
-	$PHP_OUTPUT.=create_echo_form($container);
-	$PHP_OUTPUT.=create_submit('Delete');
-	$PHP_OUTPUT.=('&nbsp;');
-	$PHP_OUTPUT.=('<select name="action" size="1" id="InputFields">');
-	$PHP_OUTPUT.=('<option>Marked Messages</option>');
-	$PHP_OUTPUT.=('<option>All Messages</option>');
-	$PHP_OUTPUT.=('</select>');
+	$messageBox['DeleteFormHref'] = SmrSession::get_new_href($container);
+	
 	$db->query('SELECT * FROM message ' .
 						'WHERE account_id = '.$player->getAccountID().' AND ' .
 							  'game_id = '.$player->getGameID().' AND ' .
 							  'message_type_id = ' . $var['folder_id'].' AND reciever_delete = \'FALSE\'' .
 						' ORDER BY send_time DESC');
-	$message_count = $db->getNumRows();
+	$messageBox['NumberMessages'] = $db->getNumRows();
+	$messageBox['Messages'] = array();
 
-	$PHP_OUTPUT.=('<p>You have <span style="color:yellow;">'.$message_count.'</span> message');
-	if ($message_count != 1)
-		$PHP_OUTPUT.=('s');
-	$PHP_OUTPUT.=('.</p>');
-	$PHP_OUTPUT.=('<table width="100%" class="standard">');
 	if ($var['folder_id'] == MSG_SCOUT && !isset($var['show_all']))
 	{
 		// get rid of all old scout messages (>48h)
 		$db2 = new SmrMySqlDatabase();
 		$db2->query('DELETE FROM message WHERE send_time < '.(TIME - 172800).' AND message_type_id = '.MSG_SCOUT);
 		
-		$dispContainer = array();
-		$dispContainer['url'] = 'skeleton.php';
-		$dispContainer['body'] = 'message_view.php';
-		$dispContainer['folder_id'] = MSG_SCOUT;
-		$dispContainer['show_all'] = TRUE;
-		if ($unread_messages > 25 || $message_count - $unread_messages > 25) {
-			$PHP_OUTPUT.=create_button($dispContainer, 'Show all Messages');
-			$PHP_OUTPUT.=('<br />');
+		if ($messageBox['UnreadMessages'] > 25 || $messageBox['NumberMessages'] - $messageBox['UnreadMessages'] > 25)
+		{
+			$dispContainer = create_container('skeleton.php','message_view.php');
+			$dispContainer['folder_id'] = MSG_SCOUT;
+			$dispContainer['show_all'] = true;
+			$messageBox['ShowAllHref'] = SmrSession::get_new_href($dispContainer);
 		}
-		if ($unread_messages > 25) {
+		if ($messageBox['UnreadMessages'] > 25 || $messageBox['NumberMessages'] - $messageBox['UnreadMessages'] > 25)
+		{
 			//here we group new messages
-			$query = 'SELECT alignment, player_id, sender_id, player_name AS sender, count( message_id ) AS number, min( send_time ) as first, max( send_time) as last 
+			$query = 'SELECT alignment, player_id, sender_id, player_name AS sender, count( message_id ) AS number, min( send_time ) as first, max( send_time) as last, msg_read
 					FROM message, player 
 					WHERE player.account_id = message.sender_id 
 					AND message.account_id = ' . $player->getAccountID() . '
 					AND message.game_id = ' . $player->getGameID()  . '
 					AND player.game_id = ' . $player->getGameID() . '
 					AND message_type_id = ' . $var['folder_id'] . '
-					AND msg_read = \'FALSE\'  AND reciever_delete = \'FALSE\'
-					GROUP BY sender_id 
+					AND reciever_delete = \'FALSE\'
+					GROUP BY sender_id, msg_read
 					ORDER BY send_time DESC';
 			$db->query($query);
-			while ($db->nextRecord()) {
+			while ($db->nextRecord())
+			{
 				//display grouped stuff (allow for deletion)
 				$playerName = get_colored_text($db->getField('alignment'), stripslashes($db->getField('sender')) . ' (' . $db->getField('player_id') . ')');
 				$message = 'Your forces have spotted ' . $playerName . ' passing your forces ' . $db->getField('number') . ' times.';
-				$PHP_OUTPUT.=displayGrouped($playerName, $db->getField('player_id'), $db->getField('sender_id'), $message, $db->getField('first'), $db->getField('last'), TRUE);
+				displayGrouped($messageBox,$playerName, $db->getField('player_id'), $db->getField('sender_id'), $message, $db->getField('first'), $db->getField('last'), $db->getField('msg_read') == 'FALSE');
 			}
-		} else {
+		}
+		else
+		{
 			//not enough to group, display separatly
 			$query = 'SELECT message_id, sender_id, message_text, send_time, msg_read
 					FROM message
 					WHERE account_id = ' . $player->getAccountID() . '
 					AND game_id = ' . $player->getGameID() . '
 					AND message_type_id = ' . $var['folder_id'] . '
-					AND msg_read = \'FALSE\'  AND reciever_delete = \'FALSE\'
+					AND reciever_delete = \'FALSE\'
 					ORDER BY send_time DESC';
 			$db->query($query);
 			while ($db->nextRecord())
-				$PHP_OUTPUT.=displayMessage($db->getField('message_id'), $db->getField('sender_id'), stripslashes($db->getField('message_text')), $db->getField('send_time'), $db->getField('msg_read'), $var['folder_id']);
-		}
-		if ($message_count - $unread_messages > 25) {
-			$query = 'SELECT alignment, player_id, sender_id, player_name AS sender, count( message_id ) AS number, min( send_time ) as first, max( send_time) as last 
-					FROM message, player 
-					WHERE player.account_id = message.sender_id 
-					AND message.account_id = ' . $player->getAccountID()  . '
-					AND message.game_id = ' . $player->getGameID() . '
-					AND player.game_id = ' . $player->getGameID() . '
-					AND message_type_id = ' . $var['folder_id'] . '
-					AND msg_read = \'TRUE\'  AND reciever_delete = \'FALSE\'
-					GROUP BY sender_id 
-					ORDER BY send_time DESC';
-			$db->query($query);
-			while ($db->nextRecord()) {
-				$playerName = get_colored_text($db->getField('alignment'), stripslashes($db->getField('sender')) . ' (' . $db->getField('player_id') . ')');
-				$message = 'Your forces have spotted ' . $playerName . ' passing your forces ' . $db->getField('number') . ' times.';
-				$PHP_OUTPUT.=displayGrouped($playerName, $db->getField('player_id'), $db->getField('sender_id'), $message, $db->getField('first'), $db->getField('last'), FALSE);
-			}
-		} else {
-			$query = 'SELECT * FROM message 
-					WHERE account_id = ' . $player->getAccountID() . ' AND
-					game_id = ' . $player->getGameID() . '
-					AND message_type_id = ' . $var['folder_id'] . '
-					AND msg_read = \'TRUE\' AND reciever_delete = \'FALSE\'
-					ORDER BY send_time DESC';
-			$db->query($query);
-			while ($db->nextRecord())
-				$PHP_OUTPUT.=displayMessage($db->getField('message_id'), $db->getField('sender_id'), stripslashes($db->getField('message_text')), $db->getField('send_time'), $db->getField('msg_read'),$var['folder_id']);
+				displayMessage($messageBox,$db->getField('message_id'), $db->getField('sender_id'), stripslashes($db->getField('message_text')), $db->getField('send_time'), $db->getField('msg_read'), $var['folder_id']);
 		}
 		$db->query('UPDATE message SET msg_read = \'TRUE\' WHERE message_type_id = '.MSG_SCOUT.' AND game_id = '.$player->getGameID().' AND account_id = '.$player->getAccountID());
-	} else {
-		while ($db->nextRecord()) {
+	}
+	else
+	{
+		while ($db->nextRecord())
+		{
 			$message_id = $db->getField('message_id');
 			$sender_id = $db->getField('sender_id');
 			$message_text = stripslashes($db->getField('message_text'));
 			$send_time = $db->getField('send_time');
 			$msg_read = $db->getField('msg_read');
-			$PHP_OUTPUT.=displayMessage($message_id, $sender_id, $message_text, $send_time, $msg_read, $var['folder_id']);
+			displayMessage($messageBox,$message_id, $sender_id, $message_text, $send_time, $msg_read, $var['folder_id']);
 		}
 		$db->query('UPDATE message SET msg_read = \'TRUE\' WHERE message_type_id = '.$var['folder_id'].' AND game_id = '.$player->getGameID().' AND account_id = '.$player->getAccountID());
 	}
-	$PHP_OUTPUT.=('</table></form>');
-
+	$template->assignByRef('MessageBox',$messageBox);
 }
-function displayGrouped($playerName, $player_id, $sender_id, $message_text, $first, $last, $star) {
+function displayGrouped(&$messageBox,$playerName, $player_id, $sender_id, $message_text, $first, $last, $star)
+{
 	$array = array($sender_id, $first, $last);
 
-	$return= ('<tr><td width="10"><input type="checkbox" name="message_id[]" value="' . base64_encode(serialize($array)) . '">');
-	if ($star) $return.= ('*');
-	$return.= ('</td>');
-	$return.= ('<td nowrap="nowrap" width="100%">From: ');
+	$message = array();	
+	$message['ID'] = base64_encode(serialize($array));
+	$message['Unread'] = $star;
 	$container = create_container('skeleton.php','trader_search_result.php');
 	$container['player_id'] = $player_id;
-	$return.= create_link($container, $playerName);
-	$return.= ('</td>');
-	$return.= ('<td nowrap="nowrap" colspan="2">Date: ' . date(DATE_FULL_SHORT, $first) . ' - ' . date(DATE_FULL_SHORT, $last) . '</td></tr>');
-	$return.= ('<tr>');
-	$return.= ('<td colspan="4">');
-	//insert link to expand them.
-	$return.= ($message_text);
-	$return.= ('</td>');
-	$return.= ('</tr>');
-	return $return;
+	$message['SenderDisplayName'] = create_link($container, $playerName);
+	$message['FirstSendTime'] = $first;
+	$message['LastSendTime'] = $last;
+	$message['Text'] = $message_text;
+	$messageBox['GroupedMessages'][] = $message;
 }
-function displayMessage($message_id, $sender_id, $message_text, $send_time, $msg_read, $type) {
+function displayMessage(&$messageBox,$message_id, $sender_id, $message_text, $send_time, $msg_read, $type)
+{
 	global $player, $account;
+	
+	$message = array();
 	
 	$replace = explode('!', $message_text);
 	foreach ($replace as $key => $timea)
@@ -284,70 +204,60 @@ function displayMessage($message_id, $sender_id, $message_text, $send_time, $msg
 	$sender = false;
 	if (!empty($sender_id) && $sender_id!=ACCOUNT_ID_PORT&&$sender_id!=ACCOUNT_ID_ADMIN&&$sender_id!=ACCOUNT_ID_PLANET)
 		$sender =& SmrPlayer::getPlayer($sender_id, $player->getGameID());
-	$return= ('<tr>');
-	$return.= ('<td width="10"><input type="checkbox" name="message_id[]" value="'.$message_id.'">');
-	// remember id for marking as read
-	if ($msg_read == 'FALSE') $return.= ('*');
-	$return.= ('</td>');
-	$return.= ('<td nowrap="nowrap" width="100%">From: ');
+	
+	$senderName = '';
 	if($sender_id==ACCOUNT_ID_PORT)
 	{
-		$return.= '<span class="yellow">Port Defenses</span>';
+		$senderName.= '<span class="yellow">Port Defenses</span>';
 	}
 	else if($sender_id==ACCOUNT_ID_ADMIN)
 	{
-		$return.= '<span style="font:small-caps bold;color:blue;">Administrator</span>';
+		$senderName.= '<span style="font:small-caps bold;color:blue;">Administrator</span>';
 	}
 	else if($sender_id==ACCOUNT_ID_PLANET)
 	{
-		$return.= '<span class="yellow">Planetary Defenses</span>';
+		$senderName.= '<span class="yellow">Planetary Defenses</span>';
 	}
 	else if (is_object($sender))
 	{
 		$container = create_container('skeleton.php','trader_search_result.php');
 		$container['player_id'] = $sender->getPlayerID();
-		$return.= create_link($container, $sender->getDisplayName());
+		$senderName.= create_link($container, $sender->getDisplayName());
 	}
 	else
 	{
 		if ($type == 7)
-			$return.= ('<span style="font:small-caps bold;color:blue;">Administrator</span>');
+			$senderName.= ('<span style="font:small-caps bold;color:blue;">Administrator</span>');
 		elseif ($type == 6)
 		{
-			$return.= ('<span class="green">');
-			$return.= ('Alliance Ambassador');
-			$return.= ('</span>');
+			$senderName.= ('<span class="green">Alliance Ambassador</span>');
 		}
-		elseif ($type == 2) $return.= ('<span class="yellow">Port Defenses</span>');
-		else $return.= ('Unknown');
+		elseif ($type == 2) $senderName.= ('<span class="yellow">Port Defenses</span>');
+		else $senderName.= ('Unknown');
 	}
-	$return.= ('</td>');
-	$return.= ('<td nowrap="nowrap"'.(!is_object($sender)?' colspan="3"':'').'>Date: ' . date(DATE_FULL_SHORT, $send_time) . '</td>');
-	$return.= ('<td>');
 	$container = create_container('skeleton.php','message_notify_confirm.php');
 	$container['message_id'] = $message_id;
 	$container['sent_time'] = $send_time;
 	$container['notified_time'] = TIME;
-	$return.= create_link($container, '<img src="images/notify.gif" border="0" align="right"title="Report this message to an admin">');
-	$return.= ('</td>');
+	$message['ReportHref'] = SmrSession::get_new_href($container);
 	if (is_object($sender))
 	{
-		$return.= ('<td>');
 		$container = create_container('skeleton.php','message_blacklist_add.php');
 		$container['account_id'] = $sender_id;
-		$return.= create_link($container, 'Blacklist Player');
-		$return.= ('</td>');	
-		$return.= ('<td>');
+		$message['BlacklistHref'] = SmrSession::get_new_href($container);
+		
 		$container = create_container('skeleton.php','message_send.php');
 		$container['receiver']	= $sender->getAccountID();
-		$return.=create_link($container, 'Reply');
-		$return.= ('</td>');
+		$message['ReplyHref'] = SmrSession::get_new_href($container);
 	}
 	
-	$return.= ('</tr>');
-	$return.= ('<tr>');
-	$return.= ('<td colspan="6">'.bbifyMessage($message_text).'</td>');
-	$return.= ('</tr>');
-	return $return;
+	$message['ID'] = $message_id;
+	$message['Text'] = $message_text;
+	$message['SenderDisplayName'] = $senderName;
+	if(is_object($sender))
+		$message['Sender'] =& $sender;
+	$message['Unread'] = $msg_read == 'FALSE';
+	$message['SendTime'] = $send_time;
+	$messageBox['Messages'][] =& $message;
 }
 ?>
---
+++ lib/templates/engine/Default/message_view.php
@@ -0,0 +1,95 @@
+<?php $this->includeTemplate('includes/menu.inc',array('MenuItems' => array(
+					array('Link'=>$ViewMessagesLink,'Text'=>'View Messages'),
+					array('Link'=>$SendCouncilMessageLink,'Text'=>'Send Council Message'),
+					array('Link'=>$SendGlobalMessageLink,'Text'=>'Send Global Message'),
+					array('Link'=>$ManageBlacklistLink,'Text'=>'Manage Blacklist'))));
+if (isset($MessageBoxes))
+{ ?>
+	<p>Please choose your Message folder!</p>
+
+	<table class="standard">
+		<tr>
+			<th>Folder</th>
+			<th>Messages</th>
+			<th>&nbsp;</th>
+		</tr><?php
+		foreach ($MessageBoxes as $MessageBox)
+		{ ?>
+			<tr<?php if($MessageBox['HasUnread']) { ?>  style="font-weight:bold;"<?php } ?>>
+				<td>
+					<a href="<?php echo $MessageBox['ViewHref']; ?>"><?php echo $MessageBox['Name']; ?></a>
+				</td>
+				<td align="center" style="color:yellow;"><?php echo $MessageBox['MessageCount']; ?></td>
+				<td><a href="<?php echo $MessageBox['DeleteHref']; ?>">Empty</a></td>
+			</tr><?php
+		} ?>
+	</table>
+	<p><a href="<?php echo $ManageBlacklistLink; ?>">Manage Player Blacklist</a></p><?php
+}
+else
+{
+	if ($MessageBox['Type'] == MSG_GLOBAL)
+	{ ?>
+		<form name="IgnoreGlobalsForm" method="POST" action="<?php echo $IgnoreGlobalsFormHref; ?>">
+			<div align="center">Ignore global messages?&nbsp;&nbsp;
+				<input type="submit" name="action" value="Yes" id="InputFields"<?php if ($ThisPlayer->isIgnoreGlobals()) { ?> style="background-color:green;"<?php } ?> />&nbsp;<input type="submit" name="action" value="No" id="InputFields"<?php if (!$ThisPlayer->isIgnoreGlobals()) { ?> style="background-color:green;"<?php } ?> />
+			</div>
+		</form><?php
+	} ?>
+	<br />
+	<form name="MessageDeleteForm" method="POST" action="<?php echo $MessageBox['DeleteFormHref']; ?>">
+		<input type="submit" name="action" value="Delete" id="InputFields" />&nbsp;<select name="action" size="1" id="InputFields">
+																						<option>Marked Messages</option>
+																						<option>All Messages</option>
+																					</select>
+		<p>You have <span style="color:yellow;"><?php echo $MessageBox['NumberMessages']; ?></span> message<?php if($MessageBox['NumberMessages']!=1) { ?>s<?php } ?>.</p><?php
+		if (isset($MessageBox['ShowAllHref']))
+		{
+			?><div class="buttonA"><a class="buttonA" href="<?php echo $MessageBox['ShowAllHref'] ?>">&nbsp;Show all Messages&nbsp;</a></div><br /><br /><?php
+		} ?>
+		<table width="100%" class="standard"><?php
+			if(isset($MessageBox['Messages']))
+			{
+				foreach($MessageBox['Messages'] as &$Message)
+				{ ?>
+					<tr>
+						<td width="10"><input type="checkbox" name="message_id[]" value="<?php echo $Message['ID']; ?>" /><?php if($Message['Unread']) { ?>*<?php } ?></td>
+						<td nowrap="nowrap" width="100%">From: <?php echo $Message['SenderDisplayName']; ?></td>
+						<td nowrap="nowrap"<?php if(!isset($Message['Sender'])) { ?> colspan="3"<?php } ?>>Date: <?php echo date(DATE_FULL_SHORT, $Message['SendTime']); ?></td>
+						<td>
+							<a href="<?php echo $Message['ReportHref']; ?>"><img src="images/notify.gif" border="0" align="right"title="Report this message to an admin"</a>
+						</td><?php
+						if (isset($Message['Sender']))
+						{ ?>
+							<td>
+								<a href="<?php echo $Message['BlacklistHref']; ?>">Blacklist Player</a>
+							</td>	
+							<td>
+								<a href="<?php echo $Message['ReplyHref']; ?>">Reply</a>
+							</td><?php
+						} ?>
+					</tr>
+					<tr>
+						<td colspan="6"><?php echo bbifyMessage($Message['Text']); ?></td>
+					</tr>
+					<?php
+				} unset($Message);
+			}
+			if(isset($MessageBox['GroupedMessages']))
+			{
+				foreach($MessageBox['GroupedMessages'] as &$Message)
+				{ ?>
+					<tr>
+						<td width="10"><input type="checkbox" name="message_id[]" value="<?php echo $Message['ID'] ?>" /><?php if($Message['Unread']) { ?>*<?php } ?></td>
+						<td nowrap="nowrap" width="100%">From: <?php echo $Message['SenderDisplayName']; ?></td>
+						<td nowrap="nowrap">Date: <?php echo date(DATE_FULL_SHORT, $Message['FirstSendTime']); ?> - <?php echo date(DATE_FULL_SHORT, $Message['LastSendTime']); ?></td>
+					</tr>
+					<tr>
+						<td colspan="4"><?php echo bbifyMessage($Message['Text']); ?></td>
+					</tr>
+					<?php
+				} unset($Message);
+			} ?>
+		</table>
+	</form><?php
+} ?>
