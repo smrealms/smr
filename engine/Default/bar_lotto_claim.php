9f785ec0f0284bbbe1f172c2acf91f1d65331131
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2009-08-10 00:04:20 +0000

    Persist claim message on refresh.

--- engine/Default/bar_lotto_claim.php
+++ engine/Default/bar_lotto_claim.php
@@ -3,7 +3,6 @@
 //check if we really are a winner
 $db->query('SELECT * FROM player_has_ticket WHERE game_id = '.$player->getGameID().' AND ' . 
 			'account_id = '.$player->getAccountID().' AND time = 0');
-$PHP_OUTPUT.=('<div align=center>');
 while ($db->nextRecord())
 {
 	$prize = $db->getField('prize');
@@ -13,13 +12,12 @@ while ($db->nextRecord())
 	$player->update();
 	$player->increaseHOF($prize,array('Bar','Lotto','Money','Winnings'));
 	$player->increaseHOF(1,array('Bar','Lotto','Results','Wins'));
-	$PHP_OUTPUT.=('You have claimed <font color=red>$' . number_format($prize) . '</font>!<br />');
+	SmrSession::updateVar('message','<div align=center>You have claimed <font color=red>$' . number_format($prize) . '</font>!<br /></div><br />');
 	$db->query('DELETE FROM player_has_ticket WHERE game_id = '.$player->getGameID().' AND ' . 
 			'account_id = '.$player->getAccountID().' AND prize = '.$prize.' AND time = 0 LIMIT 1');
 	$news_message = '<font color=yellow>'.$player->getPlayerName().'</font> has won the lotto!  The jackpot was ' . number_format($prize) . '.  <font color=yellow>'.$player->getPlayerName().'</font> can report to any bar to claim his prize!';
 	$db->query('DELETE FROM news WHERE news_message = '.$db->escapeString($news_message).' AND game_id = '.$player->getGameID());
 }
-$PHP_OUTPUT.=('</div><br />');
 //offer another drink and such
 include(get_file_loc('bar_opening.php'));
 
