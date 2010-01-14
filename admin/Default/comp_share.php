864749e49161c12518ca711ac5ef98e82beb50a4
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2010-01-14 14:05:09 +0000

    Minor fixes of last commit

--- admin/Default/comp_share.php
+++ admin/Default/comp_share.php
@@ -64,7 +64,7 @@ while ($db->nextRecord())
 			$PHP_OUTPUT.=('<tr align="center">');
 			//if ($echoMainAcc) $PHP_OUTPUT.=('<td rowspan='.$rows.' align=center>'.$currTabAccLogin.' ('.$currTabAccId.')</td>');
 			$PHP_OUTPUT.=('<td>'.$currLinkAccLogin.' ('.$currLinkAccId.')</td>';
-			$PHP_OUTPUT.='<td'.($db2->getBoolean('validated')?'':'style="text-decoration:line-through;"').'').$db2->getField('email').' ('.($db2->getBoolean('validated')?'Valid':'Invalid').')</td><td>';
+			$PHP_OUTPUT.='<td'.($db2->getBoolean('validated')?'':' style="text-decoration:line-through;"').'').'>'.$db2->getField('email').' ('.($db2->getBoolean('validated')?'Valid':'Invalid').')</td><td>';
 			$db2->query('SELECT * FROM account_exceptions WHERE account_id = '.$currLinkAccId);
 			if ($db2->nextRecord()) $PHP_OUTPUT.=$db2->getField('reason');
 			else $PHP_OUTPUT.=('&nbsp;');
