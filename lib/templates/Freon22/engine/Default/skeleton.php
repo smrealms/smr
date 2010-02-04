1b430e15428714280877af77537662e8bfc2e8d1
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2010-02-04 22:24:03 +0000

    Layout improvements

--- lib/templates/Freon22/engine/Default/skeleton.php
+++ lib/templates/Freon22/engine/Default/skeleton.php
@@ -30,10 +30,10 @@
 						<br />
 						<span id="tod"><?php echo $timeDisplay; ?></span>
 					</td>
-					<td class="topcenterCell">
-						<div class="TopInfor"><?php
-							if(isset($ThisPlayer))
-							{ ?>
+					<td class="topcenterCell"><?php
+						if(isset($ThisPlayer))
+						{ ?>
+							<div class="TopInfor">
 								<div class="name">
 									<?php echo $ThisPlayer->getLevelName(); ?>
 									<br />
@@ -100,170 +100,169 @@
 									<a class="Nav" href="htcRound.htm">Plot A Cource</a> | 
 									<a class="Nav" href="htcRound.htm">Local Map</a> | 
 									<a class="Nav" href="htcRound.htm">Galaxy Map</a>
-								</div--><?php
-							} ?>
-						</div>
-					</td>
-					<td rowspan="2" class="rightCell">
-						<div class="RightInfor"><?php
-							if(isset($ThisPlayer))
-							{ ?>
-								<div class="messages">
-								<span id="message_area"><?php
-									if(isset($MessageGlobalLink))
-									{ ?>
-										<a class="message" title="Global Messages" href="<?php echo $MessageGlobalLink; ?>"><img src="images/global_msg.png" alt="Global Messages"/></a>
-										<span class="small"><?php echo $MessageGlobalNum; ?></span><?php
-									}
-									if(isset($MessagePersonalLink))
-									{ ?>
-										<a class="message" title="Personal Messages" href="<?php echo $MessagePersonalLink; ?>"><img src="images/personal_msg.png" alt="Personal Messages"/></a>
-										<span class="small"><?php echo $MessagePersonalNum; ?></span><?php
-									}
-									if(isset($MessageScoutLink))
-									{ ?>
-										<a class="message" title="Scout Messages" href="<?php echo $MessageScoutLink; ?>"><img src="images/scout_msg.png" alt="Scout Messages"/></a>
-										<span class="small"><?php echo $MessageScoutNum; ?></span><?php
-									}
-									if(isset($MessagePoliticalLink))
-									{ ?>
-										<a class="message" title="Political Messages" href="<?php echo $MessagePoliticalLink; ?>"><img src="images/council_msg.png" alt="Political Messages"/></a>
-										<span class="small"><?php echo $MessagePoliticalNum; ?></span><?php
-									}
-									if(isset($MessageAllianceLink))
-									{ ?>
-										<a class="message" title="Alliance Messages" href="<?php echo $MessageAllianceLink; ?>"><img src="images/alliance_msg.png" alt="Alliance Messages"/></a>
-										<span class="small"><?php echo $MessageAllianceNum; ?></span><?php
-									}
-									if(isset($MessageAdminLink))
-									{ ?>
-										<a class="message" title="Admin Messages" href="<?php echo $MessageAdminLink; ?>"><img src="images/admin_msg.png" alt="Admin Messages"/></a>
-										<span class="small"><?php echo $MessageAdminNum; ?></span><?php
-									}
-									if(isset($MessagePlanetLink))
-									{ ?>
-										<a class="message" title="Planet Messages" href="<?php echo $MessagePlanetLink ?>"><img src="images/planet_msg.png" alt="Planet Messages"/></a>
-										<span class="small"><?php echo $MessagePlanetNum; ?></span><?php
-									} 
-									if(isset($MessageGlobalLink) || isset($MessagePersonalLink) || isset($MessageScoutLink) || isset($MessagePoliticalLink) || isset($MessageAllianceLink) || isset($MessageAdminLink) || isset($MessagePlanetLink))
-									{
-										?><br /><?php
-									} ?>
-									</span>
-								</div>
-							
-								<div class="RightInforShip">
-								<a href="<?php echo URL; ?>/ship_list.php" target="shipList"><span id="ship_name"><?php echo $ThisShip->getName(); ?></span></a><br /><?php
-								if($ThisPlayer->hasCustomShipName())
-								{
-									echo $ThisPlayer->getCustomShipName(); ?><br /><?php
-								} ?>
-								Rating: <span id="offense"><?php echo number_format($ThisShip->getAttackRating()); ?></span>/<span id="defense"><?php echo number_format($ThisShip->getDefenseRating()); ?></span><br />
-		
-								Shields: <span id="shields"><?php echo $ThisShip->getShields(); ?>/<?php echo $ThisShip->getMaxShields(); ?></span><br />
-		
-								Armor: <span id="armour"><?php echo $ThisShip->getArmour(); ?>/<?php echo $ThisShip->getMaxArmour(); ?></span><br />
-								<br />
-								<a class="Nav" href="<?php echo $HardwareLink; ?>">CIJSD: <?php
-									if($ThisShip->hasCloak()) echo '*';
-									else echo '-';
-									if($ThisShip->hasIllusion()) echo '*';
-									else echo '-';
-									if($ThisShip->hasJump()) echo '*';
-									else echo '-';
-									if($ThisShip->hasScanner()) echo '*';
-									else echo '-';
-									if($ThisShip->hasDCS()) echo '*';
-									else echo '-';
-								?></a><br /><?php
-								if($ThisShip->hasCloak())
-								{
-									if($ThisShip->isCloaked())
-									{
-										?><strong style="color:lime;">*** Cloak active ***</strong><?php
-									}
-									else
-									{
-										?><strong style="color:red;">*** Cloak inactive ***</strong><?php
-									} ?>
-									<br /><br /><?php
-								}
-								if($ThisShip->hasActiveIllusion())
-								{ ?>
-									<span class="npcColor"><?php echo $ThisShip->getIllusionShipName(); ?></span><br />
-									IG Rating : <?php echo $ThisShip->getIllusionAttack(); ?>/<?php echo $ThisShip->getIllusionDefense(); ?><br />
-									<br /><?php
-								} ?>
-								<br /><br />
-								<a class="Nav big" href="<?php echo $ForceDropLink; ?>">Forces</a><br /><?php
-								
-								if(isset($DropMineLink))
-								{ ?>
-									<a class="Nav" href="<?php echo $DropMineLink; ?>">
-										<span class="bold">[X]</span>
-									</a><?php
-								} ?>Mines : <?php echo $ThisShip->getMines(); ?>/<?php echo $ThisShip->getMaxMines(); ?><br /><?php
-			
-								if(isset($DropCDLink))
-								{ ?>
-									<a class="Nav" href="<?php echo $DropCDLink; ?>">
-										<span class="bold">[X]</span>
-									</a><?php
-								} ?>Combat : <?php echo $ThisShip->getCDs(); ?>/<?php echo $ThisShip->getMaxCDs(); ?><br /><?php
-			
-								if(isset($DropSDLink))
-								{ ?>
-									<a class="Nav" href="<?php echo $DropSDLink; ?>">
-										<span class="bold">[X]</span>
-									</a><?php
-								} ?>Scout : <?php echo $ThisShip->getSDs(); ?>/<?php echo $ThisShip->getMaxSDs(); ?><br />
-								<br />
-								
-								<a class="Nav" href="<?php echo $CargoJettisonLink; ?>">Cargo Holds (<?php echo $ThisShip->getCargoHolds(); ?>/<?php echo $ThisShip->getMaxCargoHolds(); ?>)</a><br /><?php
-								
-								if($ThisShip->hasCargo())
-								{
-									foreach($ThisShip->getCargo() as $GoodID => $GoodAmount)
-									{
-										$Good =& Globals::getGood($GoodID); ?>
-										<img src="<?php echo $Good['ImageLink']; ?>" alt="<?php echo $Good['Name']; ?>"> : <?php echo $GoodAmount; ?><br /><?php
-									}
-								} ?>
-								
-								Empty : <?php echo $ThisShip->getEmptyHolds(); ?><br />
-								<br />
-								<a class="Nav big" href="<?php echo $WeaponReorderLink; ?>">Weapons</a><br /><?php
-								if($ThisShip->hasWeapons())
-								{ ?>
-									<div class="wep_drop1" id="hide-show" onclick="toggleWepD(<?php echo $ThisShip->getNumWeapons(); ?>,'<?php echo $ThisPlayer->getToggleWeaponHidingHREF(); ?>');">
-										<noscript><a href="<?php echo $ThisPlayer->getToggleWeaponHidingHREF(); ?>"></noscript>
-											Show/Hide (<?php echo $ThisShip->getNumWeapons(); ?>)<br /><?php
-												$i=0;
-												$Weapons =& $ThisShip->getWeapons();
-												foreach($Weapons as &$Weapon)
-												{
-													$i++; ?>
-													<span class="wep1" <?php
-														if(!$ThisPlayer->isDisplayWeapons()){ ?>style="display: none;"<?php } ?> id="wep_item<?php echo $i; ?>"><?php echo $Weapon->getName(); ?><br />
-													</span><?php
-												} unset($Weapon); unset($Weapons); ?>
-										<noscript></a></noscript>
-									</div><?php
-								} ?>
-								Open : <?php echo $ThisShip->getOpenWeaponSlots(); ?><br />
-								<span class="noWrap">Total Damage: (<?php echo $ThisShip->getTotalShieldDamage(); ?>/<?php echo $ThisShip->getTotalArmourDamage(); ?>)</span><br />
-								Power Used: <?php echo $ThisShip->getPowerUsed(); ?>/<?php echo $ThisShip->getMaxPower();
-								if(ENABLE_BETA)
-								{
-									?><br /><br /><a class="Nav" href="<?php echo Globals::getBetaFunctionsHREF(); ?>">Beta Functions</a><?php
-								} ?>
+								</div-->
 							</div><?php
-							} ?>
-						</div>
+						} ?>
+					</td>
+					<td rowspan="2" class="rightCell"><?php
+						if(isset($ThisPlayer))
+						{ ?>
+							<div class="RightInfor">
+								<div class="messages">
+									<span id="message_area"><?php
+										if(isset($MessageGlobalLink))
+										{ ?>
+											<a class="message" title="Global Messages" href="<?php echo $MessageGlobalLink; ?>"><img src="images/global_msg.png" alt="Global Messages"/></a>
+											<span class="small"><?php echo $MessageGlobalNum; ?></span><?php
+										}
+										if(isset($MessagePersonalLink))
+										{ ?>
+											<a class="message" title="Personal Messages" href="<?php echo $MessagePersonalLink; ?>"><img src="images/personal_msg.png" alt="Personal Messages"/></a>
+											<span class="small"><?php echo $MessagePersonalNum; ?></span><?php
+										}
+										if(isset($MessageScoutLink))
+										{ ?>
+											<a class="message" title="Scout Messages" href="<?php echo $MessageScoutLink; ?>"><img src="images/scout_msg.png" alt="Scout Messages"/></a>
+											<span class="small"><?php echo $MessageScoutNum; ?></span><?php
+										}
+										if(isset($MessagePoliticalLink))
+										{ ?>
+											<a class="message" title="Political Messages" href="<?php echo $MessagePoliticalLink; ?>"><img src="images/council_msg.png" alt="Political Messages"/></a>
+											<span class="small"><?php echo $MessagePoliticalNum; ?></span><?php
+										}
+										if(isset($MessageAllianceLink))
+										{ ?>
+											<a class="message" title="Alliance Messages" href="<?php echo $MessageAllianceLink; ?>"><img src="images/alliance_msg.png" alt="Alliance Messages"/></a>
+											<span class="small"><?php echo $MessageAllianceNum; ?></span><?php
+										}
+										if(isset($MessageAdminLink))
+										{ ?>
+											<a class="message" title="Admin Messages" href="<?php echo $MessageAdminLink; ?>"><img src="images/admin_msg.png" alt="Admin Messages"/></a>
+											<span class="small"><?php echo $MessageAdminNum; ?></span><?php
+										}
+										if(isset($MessagePlanetLink))
+										{ ?>
+											<a class="message" title="Planet Messages" href="<?php echo $MessagePlanetLink ?>"><img src="images/planet_msg.png" alt="Planet Messages"/></a>
+											<span class="small"><?php echo $MessagePlanetNum; ?></span><?php
+										} 
+										if(isset($MessageGlobalLink) || isset($MessagePersonalLink) || isset($MessageScoutLink) || isset($MessagePoliticalLink) || isset($MessageAllianceLink) || isset($MessageAdminLink) || isset($MessagePlanetLink))
+										{
+											?><br /><?php
+										} ?>
+										</span>
+									</div>
+							
+									<div class="RightInforShip">
+									<a href="<?php echo URL; ?>/ship_list.php" target="shipList"><span id="ship_name"><?php echo $ThisShip->getName(); ?></span></a><br /><?php
+									if($ThisPlayer->hasCustomShipName())
+									{
+										echo $ThisPlayer->getCustomShipName(); ?><br /><?php
+									} ?>
+									Rating: <span id="offense"><?php echo number_format($ThisShip->getAttackRating()); ?></span>/<span id="defense"><?php echo number_format($ThisShip->getDefenseRating()); ?></span><br />
+			
+									Shields: <span id="shields"><?php echo $ThisShip->getShields(); ?>/<?php echo $ThisShip->getMaxShields(); ?></span><br />
+			
+									Armor: <span id="armour"><?php echo $ThisShip->getArmour(); ?>/<?php echo $ThisShip->getMaxArmour(); ?></span><br />
+									<br />
+									<a class="Nav" href="<?php echo $HardwareLink; ?>">CIJSD: <?php
+										if($ThisShip->hasCloak()) echo '*';
+										else echo '-';
+										if($ThisShip->hasIllusion()) echo '*';
+										else echo '-';
+										if($ThisShip->hasJump()) echo '*';
+										else echo '-';
+										if($ThisShip->hasScanner()) echo '*';
+										else echo '-';
+										if($ThisShip->hasDCS()) echo '*';
+										else echo '-';
+									?></a><br /><?php
+									if($ThisShip->hasCloak())
+									{
+										if($ThisShip->isCloaked())
+										{
+											?><strong style="color:lime;">*** Cloak active ***</strong><?php
+										}
+										else
+										{
+											?><strong style="color:red;">*** Cloak inactive ***</strong><?php
+										} ?>
+										<br /><br /><?php
+									}
+									if($ThisShip->hasActiveIllusion())
+									{ ?>
+										<span class="npcColor"><?php echo $ThisShip->getIllusionShipName(); ?></span><br />
+										IG Rating : <?php echo $ThisShip->getIllusionAttack(); ?>/<?php echo $ThisShip->getIllusionDefense(); ?><br />
+										<br /><?php
+									} ?>
+									<br /><br />
+									<a class="Nav big" href="<?php echo $ForceDropLink; ?>">Forces</a><br /><?php
+									
+									if(isset($DropMineLink))
+									{ ?>
+										<a class="Nav" href="<?php echo $DropMineLink; ?>">
+											<span class="bold">[X]</span>
+										</a><?php
+									} ?>Mines : <?php echo $ThisShip->getMines(); ?>/<?php echo $ThisShip->getMaxMines(); ?><br /><?php
+				
+									if(isset($DropCDLink))
+									{ ?>
+										<a class="Nav" href="<?php echo $DropCDLink; ?>">
+											<span class="bold">[X]</span>
+										</a><?php
+									} ?>Combat : <?php echo $ThisShip->getCDs(); ?>/<?php echo $ThisShip->getMaxCDs(); ?><br /><?php
+				
+									if(isset($DropSDLink))
+									{ ?>
+										<a class="Nav" href="<?php echo $DropSDLink; ?>">
+											<span class="bold">[X]</span>
+										</a><?php
+									} ?>Scout : <?php echo $ThisShip->getSDs(); ?>/<?php echo $ThisShip->getMaxSDs(); ?><br />
+									<br />
+									
+									<a class="Nav" href="<?php echo $CargoJettisonLink; ?>">Cargo Holds (<?php echo $ThisShip->getCargoHolds(); ?>/<?php echo $ThisShip->getMaxCargoHolds(); ?>)</a><br /><?php
+									
+									if($ThisShip->hasCargo())
+									{
+										foreach($ThisShip->getCargo() as $GoodID => $GoodAmount)
+										{
+											$Good =& Globals::getGood($GoodID); ?>
+											<img src="<?php echo $Good['ImageLink']; ?>" alt="<?php echo $Good['Name']; ?>"> : <?php echo $GoodAmount; ?><br /><?php
+										}
+									} ?>
+									
+									Empty : <?php echo $ThisShip->getEmptyHolds(); ?><br />
+									<br />
+									<a class="Nav big" href="<?php echo $WeaponReorderLink; ?>">Weapons</a><br /><?php
+									if($ThisShip->hasWeapons())
+									{ ?>
+										<div class="wep_drop1" id="hide-show" onclick="toggleWepD(<?php echo $ThisShip->getNumWeapons(); ?>,'<?php echo $ThisPlayer->getToggleWeaponHidingHREF(); ?>');">
+											<noscript><a href="<?php echo $ThisPlayer->getToggleWeaponHidingHREF(); ?>"></noscript>
+												Show/Hide (<?php echo $ThisShip->getNumWeapons(); ?>)<br /><?php
+													$i=0;
+													$Weapons =& $ThisShip->getWeapons();
+													foreach($Weapons as &$Weapon)
+													{
+														$i++; ?>
+														<span class="wep1" <?php
+															if(!$ThisPlayer->isDisplayWeapons()){ ?>style="display: none;"<?php } ?> id="wep_item<?php echo $i; ?>"><?php echo $Weapon->getName(); ?><br />
+														</span><?php
+													} unset($Weapon); unset($Weapons); ?>
+											<noscript></a></noscript>
+										</div><?php
+									} ?>
+									Open : <?php echo $ThisShip->getOpenWeaponSlots(); ?><br />
+									<span class="noWrap">Total Damage: (<?php echo $ThisShip->getTotalShieldDamage(); ?>/<?php echo $ThisShip->getTotalArmourDamage(); ?>)</span><br />
+									Power Used: <?php echo $ThisShip->getPowerUsed(); ?>/<?php echo $ThisShip->getMaxPower();
+									if(ENABLE_BETA)
+									{
+										?><br /><br /><a class="Nav" href="<?php echo Globals::getBetaFunctionsHREF(); ?>">Beta Functions</a><?php
+									} ?>
+								</div>
+							</div><?php
+						} ?>
 					</td>
 				</tr>
 				<tr>
-	
 					<td class="leftCell">
 						<?php $this->includeTemplate('includes/LeftPanel.inc'); ?>
 					</td>
