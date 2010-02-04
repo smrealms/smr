c34df722737b42a8447eaf252401ade058792b49
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2010-02-04 14:22:31 +0000

    Only include css file once and update htc file to work with resizing

--- lib/templates/Freon22/engine/Default/skeleton.php
+++ lib/templates/Freon22/engine/Default/skeleton.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<link href="<?php echo URL; ?>/css/htcCSS2.css" rel="stylesheet" type="text/css"/>
 		<title><?php echo $Title; ?> <?php if(isset($GameName)) echo $GameName; ?></title>
 		<meta http-equiv="pragma" content="no-cache"><?php
 		if(is_object($ThisAccount) && $ThisAccount->isDefaultCSSEnabled())
