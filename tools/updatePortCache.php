5357f7047a2d3ed216bd6b9987a44c49d2826527
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2010-03-20 19:43:43 +0000

    Consolidation of port cache info to greatly reduce storage space used.

---
+++ tools/updatePortCache.php
@@ -0,0 +1,15 @@
+<?php
+require_once('/home/page/SMR/htdocs/config.inc');
+require_once(LIB . 'Default/SmrMySqlDatabase.class.inc');
+require_once(LIB . 'Default/Globals.class.inc');
+require_once(LIB . 'Default/SmrPort.class.inc');
+
+$db = new SmrMySqlDatabase();
+$db->query('SELECT account_id,sector_id,game_id FROM player_visited_port');
+while ($db->nextRecord())
+{
+	SmrPort::getCachedPort($db->getInt('game_id'),$db->getInt('sector_id'),$db->getInt('account_id'))->addCachePort($db->getInt('account_id'));
+	SmrPort::clearCache();
+}
+
+?>
