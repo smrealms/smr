e63e379122d09c46a25e7398303b0601e67e0ac6
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2010-03-23 21:41:25 +0000

    Randomly choose a default race to select each time the game join page is loaded.

--- engine/Default/game_join.php
+++ engine/Default/game_join.php
@@ -60,6 +60,8 @@ $db->query('SELECT location_name, location.location_type_id as loc_id
 				  game_id = ' . $var['game_id'] . '
 			ORDER BY location.location_type_id');
 $races = array();
+$selectedRace = mt_rand(1,$db->getNumRows());
+$i=1;
 while ($db->nextRecord())
 {
 	// get the name for this race
@@ -76,7 +78,8 @@ while ($db->nextRecord())
 	$races[$curr_race_id]['ID'] = $curr_race_id;
 	$races[$curr_race_id]['Name'] = $race_name;
 	$races[$curr_race_id]['NumberOfPlayers'] = $db2->getField('number_of_race')>0?$db2->getField('number_of_race'):0;
-	
+	$races[$curr_race_id]['Selected'] = $i==$selectedRace;
+	$i++;
 }
 $template->assign('Races',$races);
 
