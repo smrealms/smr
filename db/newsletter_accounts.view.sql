8f7f2661624261e449ae1958161ea1eb2db36158
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2010-03-27 13:11:21 +0000

    newsletter_accounts view.

---
+++ db/newsletter_accounts.view.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE VIEW newsletter_accounts AS
+SELECT account_id, email, first_name, last_name FROM smr_new.account a
+WHERE validated = 'TRUE' AND NOT EXISTS(SELECT account_id FROM smr_new.account_is_closed WHERE account_id = a.account_id)
+UNION
+SELECT 100000+account_id account_id, email, first_name, last_name FROM smr_classic.account b
+WHERE validated = 'TRUE' AND NOT EXISTS(SELECT account_id FROM smr_classic.account_is_closed WHERE account_id = b.account_id) AND NOT EXISTS(SELECT email FROM smr_new.account WHERE email=b.email)
+UNION
+SELECT 200000+account_id account_id, email, first_name, last_name FROM smr_12.account c
+WHERE validated = 'TRUE' AND NOT EXISTS(SELECT account_id FROM smr_12.account_is_closed WHERE account_id = c.account_id) AND NOT EXISTS(SELECT email FROM smr_new.account WHERE email=c.email) AND NOT EXISTS(SELECT email FROM smr_classic.account WHERE email=c.email)
