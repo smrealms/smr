88432fed7b0931e4ee41ec667ddedc3e15d5a87e
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2010-06-07 12:51:41 +0000

    Store posted variables in $var

--- engine/Default/bank_alliance.php
+++ engine/Default/bank_alliance.php
@@ -7,10 +7,9 @@
 // ********************************
 
 // is account validated?
-if ($account->validated == 'FALSE') {
-	$PHP_OUTPUT.=create_error('You are not validated so you cannot use banks.');
-	return;
-}
+if ($account->validated == 'FALSE')
+	create_error('You are not validated so you cannot use banks.');
+
 if (isset($var['alliance_id'])) $alliance_id = $var['alliance_id'];
 else $alliance_id = $player->getAllianceID();
 $template->assign('PageTopic','Bank');
@@ -31,23 +30,27 @@ $db->query('SELECT * FROM alliance_treaties WHERE game_id = '.$player->getGameID
 			' AND (alliance_id_1 = '.$player->getAllianceID().' OR alliance_id_2 = '.$player->getAllianceID().')'.
 			' AND aa_access = 1 AND official = \'TRUE\'');
 $temp=array();
-while ($db->nextRecord()) {
+while ($db->nextRecord())
+{
 	if ($db->getField('alliance_id_1') == $player->getAllianceID())
 		$temp[$db->getField('alliance_id_2')] = $db->getField('alliance_id_1');
 	else $temp[$db->getField('alliance_id_1')] = $db->getField('alliance_id_2');
 }
 $tempAllIDs = array_keys($temp);
-if (sizeof($tempAllIDs)) {
+if (sizeof($tempAllIDs))
+{
 	$tempAllIDs[] = $player->getAllianceID();
 	$temp[$player->getAllianceID()] = $player->getAllianceID();
 	$db->query('SELECT alliance_name, alliance_id FROM alliance WHERE alliance_id IN (' . implode(',',$tempAllIDs) . ') AND game_id = '.$player->getGameID());
 	while ($db->nextRecord()) $alliances[$db->getField('alliance_id')] = stripslashes($db->getField('alliance_name'));
-	if (sizeof($temp) > 0) {
+	if (sizeof($temp) > 0)
+	{
 		$container=array();
 		$container['url'] = 'skeleton.php';
 		$container['body'] = 'bank_alliance.php';
 		$PHP_OUTPUT.=('<ul>');
-		foreach ($temp as $alliedID => $myID) {
+		foreach ($temp as $alliedID => $myID)
+		{
 			$container['alliance_id'] = $alliedID;
 			$PHP_OUTPUT.=('<li>');
 			$PHP_OUTPUT.=create_link($container,'<span class="bold">' . $alliances[$alliedID] . '\'s Account</span>');
@@ -59,7 +62,8 @@ if (sizeof($tempAllIDs)) {
 $PHP_OUTPUT.= 'Hello ';
 $PHP_OUTPUT.= $player->getPlayerName();
 $PHP_OUTPUT.= ',<br />';
-if ($alliance_id == $player->getAllianceID()) {
+if ($alliance_id == $player->getAllianceID())
+{
 	$db->query('SELECT * FROM player_has_alliance_role WHERE account_id = '.$player->getAccountID().' AND game_id = '.$player->getGameID().' AND alliance_id='.$alliance_id);
 	if ($db->nextRecord()) $role_id = $db->getField('role_id');
 	else $role_id = 0;
@@ -71,49 +75,60 @@ $db->nextRecord();
 $exempt = $db->getField('exempt_with') == 'TRUE';
 if ($db->getField('with_per_day') == -2) $PHP_OUTPUT.=('You can withdraw an unlimited amount from this account. <br />');
 elseif ($db->getField('with_per_day') == -1) $PHP_OUTPUT.=('You can only withdraw ' . number_format($differential) . ' more credits based on your deposits.<br />');
-else {
+else
+{
 	$perDay = $db->getField('with_per_day');
 	$PHP_OUTPUT.=('You can withdraw up to ' . number_format($db->getField('with_per_day')) . ' credits per 24 hours.<br />');
 	$db->query('SELECT sum(amount) as total FROM alliance_bank_transactions WHERE alliance_id = '.$alliance_id.' AND game_id = '.$player->getGameID().' AND ' . 
 			'payee_id = '.$player->getAccountID().' AND transaction = \'Payment\' AND exempt = 0 AND time > ' . (TIME - 24 * 60 * 60));
 	$PHP_OUTPUT.=('So far you have withdrawn ');
 	$remaining = $perDay;
-	if ($db->nextRecord() && !is_null($db->getField('total'))) {
+	if ($db->nextRecord() && !is_null($db->getField('total')))
+	{
 		$PHP_OUTPUT.=(number_format($db->getField('total')));
 		$remaining -= $db->getField('total');
 	} else $PHP_OUTPUT.=('0');
 	$PHP_OUTPUT.=(' credits in the past 24 hours.  You can withdraw ' . number_format($remaining) . ' more credits.<br />');
 }
-if (isset($_REQUEST['maxValue'])
-	&& is_numeric($_REQUEST['maxValue'])
-	&& $_REQUEST['maxValue'] > 0
-) {
-	$maxValue = $_REQUEST['maxValue'];
+if (isset($_REQUEST['maxValue']))
+	SmrSession::updateVar('maxValue',$_REQUEST['maxValue']);
+if (isset($_REQUEST['minValue']))
+	SmrSession::updateVar('minValue',$_REQUEST['minValue']);
+
+if (isset($var['maxValue'])
+	&& is_numeric($var['maxValue'])
+	&& $var['maxValue'] > 0)
+{
+	$maxValue = $var['maxValue'];
 }
-else {
+else
+{
 	$db->query('SELECT MAX(transaction_id) FROM alliance_bank_transactions
 				WHERE game_id=' . $player->getGameID() . '
 				AND alliance_id=' . $alliance_id
 				);
-	if($db->nextRecord()) {
+	if($db->nextRecord())
+	{
 		$maxValue = $db->getField('MAX(transaction_id)');
 		$minValue = $maxValue - 5;
-		if($minValue < 1) {
+		if($minValue < 1)
+		{
 			$minValue = 1;
 		}
 	}
-	else{
+	else
+	{
 		$minValue = 1;
 		$maxValue = 5;
 	}
 }
 
-if(isset($_REQUEST['minValue'])
-	&& $_REQUEST['minValue'] <= $maxValue
-	&& $_REQUEST['minValue'] > 0
-	&& is_numeric($_REQUEST['maxValue'])
+if(isset($var['minValue'])
+	&& $var['minValue'] <= $maxValue
+	&& $var['minValue'] > 0
+	&& is_numeric($var['maxValue'])
 ) {
-	$minValue = $_REQUEST['minValue'];
+	$minValue = $var['minValue'];
 }
 
 $query = '
