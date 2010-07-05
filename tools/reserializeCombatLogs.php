fd3dfd6ba1ab8df6a649d21482f6eb27bd4adc31
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2010-07-05 12:58:43 +0000

    Only serialise the minimum possible for SmrPlayer and add a script to reserialise all combat logs.

---
+++ tools/reserializeCombatLogs.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once('../htdocs/config.inc');
+require_once(LIB . 'Default/SmrMySqlDatabase.class.inc');
+require_once(LIB . 'Default/Globals.class.inc');
+require_once(LIB . 'Default/SmrPlayer.class.inc');
+
+$db = new SmrMySqlDatabase();
+$db2 = new SmrMySqlDatabase();
+
+//$db->query('SELECT * FROM combat_logs WHERE type=\'PLAYER\' ORDER BY OCTET_LENGTH(result) DESC LIMIT 1');
+//if($db->nextRecord())
+//{
+//	$x = $db->getField('result');
+//	$y = gzuncompress($x);
+//	var_dump($y);
+//	
+//	$z = serialize(unserialize($y));
+//	var_dump($z);
+//	
+//	var_dump(strlen($x));
+//	var_dump(strlen(gzcompress($z)));
+//	var_dump(strlen(bzcompress($z)));
+//}
+
+$db->query('SELECT result,log_id FROM combat_logs');
+while($db->nextRecord())
+{
+	$db2->query('UPDATE combat_logs SET result='.$db2->escapeBinary(gzcompress(serialize(unserialize(gzuncompress($db->getField('result')))))).' WHERE log_id='.$db->getField('log_id'));
+}
+
+?>
