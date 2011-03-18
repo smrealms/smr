d1321d9e5bf34533369c35e5a77883b8de1d4234
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-03-18 20:02:51 +0000

    Add checks to make sure that the leader and new member roles cannot be deleted. Switch to using db->getBoolean in multiple places. Fix a potential display bug if a role name were to contain ".

--- engine/Default/alliance_roles_processing.php
+++ engine/Default/alliance_roles_processing.php
@@ -1,12 +1,14 @@
 <?php
-if (isset($var['alliance_id'])) $alliance_id = $var['alliance_id'];
-else $alliance_id = $player->getAllianceID();
-// with empty role the user wants to create a new entry
-if (!isset($var['role_id'])) {
+$alliance_id = isset($var['alliance_id']) ? $var['alliance_id'] : $player->getAllianceID();
 
+// with empty role the user wants to create a new entry
+if (!isset($var['role_id']))
+{
 	// role empty too? that doesn't make sence
 	if (empty($_POST['role']))
+	{
 		create_error('You must enter a role if you want to create a new one.');
+	}
 
 	$db->lockTable('alliance_has_roles');
 
@@ -16,81 +18,76 @@ if (!isset($var['role_id'])) {
 				WHERE game_id = '.$player->getGameID().' AND
 					  alliance_id = '.$alliance_id);
 	if ($db->nextRecord())
+	{
 		$role_id = $db->getField('MAX(role_id)') + 1;
+	}
 	else
+	{
 		$role_id = 1;
+	}
 	if ($_REQUEST['unlimited']) $withPerDay = -2;
 	elseif ($_REQUEST['positive']) $withPerDay = -1;
 	else $withPerDay = $_REQUEST['maxWith'];
 	if (!is_numeric($withPerDay) || $withPerDay < -2) create_error('You must enter a number for max withdrawals per 24 hours.');
-	if ($_REQUEST['changePW']) $changePass = TRUE;
-	else $changePass = FALSE;
-	if ($_REQUEST['removeMember']) $removeMember = TRUE;
-	else $removeMember = FALSE;
-	if ($_REQUEST['changeMoD']) $changeMOD = TRUE;
-	else $changeMOD = FALSE;
-	if ($_REQUEST['changeRoles']) $changeRoles = TRUE;
-	else $changeRoles = FALSE;
-	if ($_REQUEST['planets']) $planetAccess = TRUE;
-	else $planetAccess = FALSE;
-	if ($_REQUEST['mbMessages']) $mbMessages = TRUE;
-	else $mbMessages = FALSE;
-	if ($_REQUEST['exemptWithdrawls']) $exemptWith = TRUE;
-	else $exemptWith = FALSE;
-	if ($_REQUEST['sendAllMsg']) $sendAllMsg = TRUE;
-	else $sendAllMsg = FALSE;
+	$changePass = (bool)$_REQUEST['changePW'];
+	$removeMember = (bool)$_REQUEST['removeMember'];
+	$changeMOD = (bool)$_REQUEST['changeMoD'];
+	$changeRoles = (bool)$_REQUEST['changeRoles'];
+	$planetAccess = (bool)$_REQUEST['planets'];
+	$mbMessages = (bool)$_REQUEST['mbMessages'];
+	$exemptWith = (bool)$_REQUEST['exemptWithdrawls'];
+	$sendAllMsg = (bool)$_REQUEST['sendAllMsg'];
 
 	$db->query('INSERT INTO alliance_has_roles
 				(alliance_id, game_id, role_id, role, with_per_day, remove_member, change_pass, change_mod, change_roles, planet_access, exempt_with, mb_messages, send_alliance_msg)
 				VALUES ('.$alliance_id.', '.$player->getGameID().', '.$role_id.', ' . $db->escape_string($_POST['role'], true) . ', '.$withPerDay.', '.$db->escapeString($removeMember).', '.$db->escapeString($changePass).', '.$db->escapeString($changeMOD).', '.$db->escapeString($changeRoles).', '.$db->escapeString($planetAccess).', '.$db->escapeString($exemptWith).', '.$db->escapeString($mbMessages).', '.$db->escapeString($sendAllMsg).')');
 
 	$db->unlock();
-
-} else {
-
+}
+else
+{
 	// if no role is given we delete that entry
-	if (empty($_POST['role'])) {
-
-			$db->query('DELETE FROM alliance_has_roles
-						WHERE game_id = '.$player->getGameID().' AND
-							  alliance_id = '.$alliance_id.' AND
-							  role_id = ' . $var['role_id']);
-
+	if (empty($_POST['role']))
+	{
+		if($var['role_id']==1)
+		{
+			create_error('You cannot delete the leader role.');
+		}
+		else if($var['role_id']==2)
+		{
+			create_error('You cannot delete the new member role.');
+		}
+		$db->query('DELETE FROM alliance_has_roles
+					WHERE game_id = '.$player->getGameID().' AND
+						  alliance_id = '.$alliance_id.' AND
+						  role_id = ' . $var['role_id']);
 	// otherwise we update it
-	} else {
-
+	}
+	else
+	{
 		if ($_REQUEST['unlimited']) $withPerDay = -2;
 		elseif ($_REQUEST['positive']) $withPerDay = -1;
 		else $withPerDay = $_REQUEST['maxWith'];
 		if (!is_numeric($withPerDay)) create_error('You must enter a number for max withdrawls per 24 hours.');
-		if ($_REQUEST['changePW']) $changePass = TRUE;
-		else $changePass = FALSE;
-		if ($_REQUEST['removeMember']) $removeMember = TRUE;
-		else $removeMember = FALSE;
-		if ($_REQUEST['changeMoD']) $changeMOD = TRUE;
-		else $changeMOD = FALSE;
-		if ($_REQUEST['changeRoles']) $changeRoles = TRUE;
-		else $changeRoles = FALSE;
-		if ($_REQUEST['planets']) $planetAccess = TRUE;
-		else $planetAccess = FALSE;
-		if ($_REQUEST['mbMessages']) $mbMessages = TRUE;
-		else $mbMessages = FALSE;
-		if ($_REQUEST['exemptWithdrawls']) $exemptWith = TRUE;
-		else $exemptWith = FALSE;
-		if ($_REQUEST['sendAllMsg']) $sendAllMsg = TRUE;
-		else $sendAllMsg = FALSE;
-		if ($var['role_id'] == 1) $changeRoles = TRUE;
+		$changePass = (bool)$_REQUEST['changePW'];
+		$removeMember = (bool)$_REQUEST['removeMember'];
+		$changeMOD = (bool)$_REQUEST['changeMoD'];
+		$changeRoles = (bool)($var['role_id'] == 1 || $_REQUEST['changeRoles']); //Leader can always change roles.
+		$planetAccess = (bool)$_REQUEST['planets'];
+		$mbMessages = (bool)$_REQUEST['mbMessages'];
+		$exemptWith = (bool)$_REQUEST['exemptWithdrawls'];
+		$sendAllMsg = (bool)$_REQUEST['sendAllMsg'];
 		$db->query('UPDATE alliance_has_roles
 					SET role = ' . $db->escape_string($_POST['role'], true) . ',
 					with_per_day = '.$withPerDay.',
-					remove_member = '.$db->escapeString($removeMember).',
-					change_pass = '.$db->escapeString($changePass).',
-					change_mod = '.$db->escapeString($changeMOD).',
-					change_roles = '.$db->escapeString($changeRoles).',
-					planet_access = '.$db->escapeString($planetAccess).',
-					exempt_with = '.$db->escapeString($exemptWith).',
-					mb_messages = '.$db->escapeString($mbMessages).',
-					send_alliance_msg = '.$db->escapeString($sendAllMsg).' 
+					remove_member = '.$db->escapeBoolean($removeMember).',
+					change_pass = '.$db->escapeBoolean($changePass).',
+					change_mod = '.$db->escapeBoolean($changeMOD).',
+					change_roles = '.$db->escapeBoolean($changeRoles).',
+					planet_access = '.$db->escapeBoolean($planetAccess).',
+					exempt_with = '.$db->escapeBoolean($exemptWith).',
+					mb_messages = '.$db->escapeBoolean($mbMessages).',
+					send_alliance_msg = '.$db->escapeBoolean($sendAllMsg).' 
 					WHERE game_id = '.$player->getGameID().' AND
 						  alliance_id = '.$alliance_id.' AND
 						  role_id = ' . $var['role_id']);
