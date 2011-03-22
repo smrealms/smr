2ad2c1e469d261006cc5db870eefc89516448e42
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-03-22 01:15:18 +0000

    Hopefully GP will now play nicely with smart quotes.

--- engine/Default/galactic_post_write_article_processing.php
+++ engine/Default/galactic_post_write_article_processing.php
@@ -1,16 +1,19 @@
 <?php
 $title = $_REQUEST['title'];
 $message = $_REQUEST['message'];
-if (empty($title) || empty($message))
-    create_error('You must enter some text or a title');
-$db->query('SELECT * FROM galactic_post_article WHERE game_id = '.SmrSession::$game_id.' ORDER BY article_id DESC LIMIT 1');
+if (empty($title))
+{
+    create_error('You must enter a title');
+}
+if(empty($message))
+{
+    create_error('You must enter some text');
+}
+$db->query('SELECT MAX(article_id) article_id FROM galactic_post_article WHERE game_id = '.$player->getGameID().' LIMIT 1');
 $db->nextRecord();
-$num = $db->getField('article_id') +1;
-$db->query('INSERT INTO galactic_post_article (game_id, article_id, writer_id, title, text, last_modified) VALUES ('.$player->getGameID().', '.$num.', '.$player->getAccountID().', ' . $db->escape_string($title, FALSE) . ' , ' . $db->escape_string($message, false) . ' , ' . TIME . ')');
-$db->query('UPDATE galactic_post_writer SET last_wrote = ' . TIME . ' WHERE account_id = '.$account->account_id);
-$container = array();
-$container['url'] = 'skeleton.php';
-$container['body'] = 'galactic_post_read.php';
-forward($container);
+$num = $db->getField('article_id') + 1;
+$db->query('INSERT INTO galactic_post_article (game_id, article_id, writer_id, title, text, last_modified) VALUES ('.$player->getGameID().', '.$num.', '.$player->getAccountID().', ' . $db->escapeString($title) . ' , ' . $db->escapeString($message) . ' , ' . TIME . ')');
+$db->query('UPDATE galactic_post_writer SET last_wrote = ' . TIME . ' WHERE account_id = '.$account->getAccountID());
+forward(create_container('skeleton.php', 'galactic_post_read.php'));
 
 ?>
