54610159160ef7f1a5904a265cfa844b5441dc91
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-04-16 03:09:02 +0000

    AJAXify the interesting parts of port scan.

--- engine/Default/port_attack_warning.php
+++ engine/Default/port_attack_warning.php
@@ -1,41 +1,28 @@
 <?php
 require_once(get_file_loc('SmrPort.class.inc'));
 $template->assign('PageTopic','Port Raid');
-$PHP_OUTPUT.=('<span class="red">WARNING WARNING</span> port assault about to commence!!<br />');
-$PHP_OUTPUT.=('Are you sure you want to attack this port?<br /><br />');
+$PHP_OUTPUT.='<span class="red">WARNING WARNING</span> port assault about to commence!!<br />';
+$PHP_OUTPUT.='Are you sure you want to attack this port?<br /><br />';
 $port =& SmrPort::getPort($player->getGameID(),$player->getSectorID());
-$time = TIME;
 
-if ($ship->hasScanner() == 1)
+if ($ship->hasScanner())
 {
 	//they can scan the port
-   $PHP_OUTPUT.=('Your scanners detect that there ');
-   if ($port->getShields() == 1)
-   	$PHP_OUTPUT.=('is 1 shield, ');
-   else
-   	$PHP_OUTPUT.=('are '.$port->getShields().' shields, ');
-   if ($port->getCDs() == 1)
-   	$PHP_OUTPUT.=('1 combat drone, ');
-   else
-   	$PHP_OUTPUT.=($port->getCDs().' combat drones, ');
-   if ($port->getArmour() == 1)
-   	$PHP_OUTPUT.=('and 1 plate of armour.');
-   else
-   	$PHP_OUTPUT.=('and '.$port->getArmour().' plates of armour.');
-
+	$PHP_OUTPUT.='Your scanners detect that there ';
+   	$PHP_OUTPUT.='are '.$port->getShields().' shields, ';
+	$PHP_OUTPUT.=($port->getShields()==1?'is':'are').' <span id="portShields">'.$port->getShields().'</span> shield'.($port->getShields()!=1?'s':'').', ';
+	$PHP_OUTPUT.='and <span id="portCDs">'.$port->getCDs().'</span> combat drone'.($port->getCDs()!=1?'s':'').', ';
+	$PHP_OUTPUT.='and <span id="portArmour">'.$port->getArmour().'</span> plate'.($port->getArmour()!=1?'s':'').' of armour.';
 }
 
-$container = array();
-$container['url'] = 'port_attack_processing.php';
+$container = create_container('port_attack_processing.php');
 $PHP_OUTPUT.=create_echo_form($container);
 $PHP_OUTPUT.=create_submit('Yes');
-$PHP_OUTPUT.=('</form>');
-$PHP_OUTPUT.=('  ');
-$container = array();
-$container['url'] = 'skeleton.php';
-$container['body'] = 'current_sector.php';
+$PHP_OUTPUT.='</form>';
+$PHP_OUTPUT.='  ';
+$container = create_container('skeleton.php', 'current_sector.php');
 $PHP_OUTPUT.=create_echo_form($container);
 $PHP_OUTPUT.=create_submit('No');
-$PHP_OUTPUT.=('</form>');
+$PHP_OUTPUT.='</form>';
 
 ?>
