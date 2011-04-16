fbf3ecb5b880a213e10ae862f162ec003d5ddc26
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-04-16 23:27:48 +0000

    More port attack warning screen to a template.

--- engine/Default/port_attack_warning.php
+++ engine/Default/port_attack_warning.php
@@ -1,28 +1,12 @@
 <?php
-require_once(get_file_loc('SmrPort.class.inc'));
-$template->assign('PageTopic','Port Raid');
-$PHP_OUTPUT.='<span class="red">WARNING WARNING</span> port assault about to commence!!<br />';
-$PHP_OUTPUT.='Are you sure you want to attack this port?<br /><br />';
-$port =& SmrPort::getPort($player->getGameID(),$player->getSectorID());
 
-if ($ship->hasScanner())
+if(!$sector->hasPort())
 {
-	//they can scan the port
-	$PHP_OUTPUT.='Your scanners detect that there ';
-   	$PHP_OUTPUT.='are '.$port->getShields().' shields, ';
-	$PHP_OUTPUT.=($port->getShields()==1?'is':'are').' <span id="portShields">'.$port->getShields().'</span> shield'.($port->getShields()!=1?'s':'').', ';
-	$PHP_OUTPUT.='and <span id="portCDs">'.$port->getCDs().'</span> combat drone'.($port->getCDs()!=1?'s':'').', ';
-	$PHP_OUTPUT.='and <span id="portArmour">'.$port->getArmour().'</span> plate'.($port->getArmour()!=1?'s':'').' of armour.';
+	create_error('This sector does not have a port.');
 }
 
-$container = create_container('port_attack_processing.php');
-$PHP_OUTPUT.=create_echo_form($container);
-$PHP_OUTPUT.=create_submit('Yes');
-$PHP_OUTPUT.='</form>';
-$PHP_OUTPUT.='  ';
-$container = create_container('skeleton.php', 'current_sector.php');
-$PHP_OUTPUT.=create_echo_form($container);
-$PHP_OUTPUT.=create_submit('No');
-$PHP_OUTPUT.='</form>';
+$template->assign('PageTopic','Port Raid');
+
+$template->assign('PortAttackHREF',SmrSession::get_new_href(create_container('port_attack_processing.php')));
 
 ?>
---
+++ lib/templates/Default/engine/Default/port_attack_warning.php
@@ -0,0 +1,17 @@
+<span class="red">WARNING WARNING</span> port assault about to commence!!<br />
+Are you sure you want to attack this port?<br /><br />
+<?php
+if ($ThisShip->hasScanner())
+{ 
+	$Port = $ThisSector->getPort(); ?>
+	Your scanners detect that there <?php if($Port->getShields()==1){ ?>is<?php }else{ ?>are<?php } ?> <span id="portShields"><?php echo $Port->getShields(); ?></span> shield<?php if($Port->getShields()!=1){ ?>s<?php } ?>, 
+	and <span id="portCDs"><?php echo $Port->getCDs(); ?></span> combat drone<?php if($Port->getCDs()!=1){ ?>s<?php } ?>, 
+	and <span id="portArmour"><?php echo $Port->getArmour(); ?></span> plate<?php if($Port->getArmour()!=1){ ?>s<?php } ?> of armour.<?php
+} ?>
+
+<div class="buttonA">
+	<a class="buttonA" href="<?php echo $PortAttackHREF; ?>">&nbsp;Yes&nbsp;</a>
+</div>&nbsp;
+<div class="buttonA">
+	<a class="buttonA" href="<?php echo Globals::getCurrentSectorHREF(); ?>">&nbsp;No&nbsp;</a>
+</div>
