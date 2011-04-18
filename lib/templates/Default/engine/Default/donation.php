0db9d5f4355278d969ba9f2b6788b25794dd14c3
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-04-18 05:05:36 +0000

    Move donation screen to a template and make the donations per week slightly more accurate.

---
+++ lib/templates/Default/engine/Default/donation.php
@@ -0,0 +1,22 @@
+<p style="width:60%; text-align:justify;">Do you enjoy Space Merchant Realms? Would you like to see the game grow? If your answer is yes, then consider making a donation! Your donation will translate into SMR credits that you can use in game to get nifty items, and will also help the game improve.</p>
+Current donation rate is: $<?php echo number_format($TotalDonation / 90*7, 2); ?> per week (within last 3 months).
+
+<br /><br /><br />
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+	<input type="hidden" name="cmd" value="_xclick">
+	<input type="hidden" name="business" value="paypal@chaos-inc.de">
+	<input type="hidden" name="item_name" value="Support development with money.">
+	<input type="hidden" name="item_number" value="<?php echo $ThisAccount->getAccountID(); ?>">
+	<input type="hidden" name="no_shipping" value="1">
+	<input type="hidden" name="no_note" value="1">
+	<input type="hidden" name="currency_code" value="USD">
+	<input type="hidden" name="tax" value="0">
+	<input type="hidden" name="lc" value="US">
+	<input type="hidden" name="bn" value="PP-DonationsBF">
+	<input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-but21.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+	<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+<p>&nbsp;</p>
+<p>Thank you for your donation.<br /></p>
