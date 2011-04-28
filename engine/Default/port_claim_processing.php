7255f78aad7890c5dafec2ca004d61ab69e0a434
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-04-28 09:06:14 +0000

    Clear route_cache on port claim.

--- engine/Default/port_claim_processing.php
+++ engine/Default/port_claim_processing.php
@@ -1,10 +1,8 @@
 <?php
+require_once(get_file_loc('SmrPort.class.inc'));
+$port =& SmrPort::getPort(SmrSession::$game_id,$player->getSectorID());
+$port->setRaceID($player->getRaceID());
 
-$db->query('UPDATE port SET race_id = '.$player->getRaceID().' WHERE game_id = '.$player->getGameID().' AND sector_id = '.$player->getSectorID());
-
-$container = array();
-$container['url'] = 'skeleton.php';
-$container['body'] = 'port_loot.php';
-forward($container);
+forward($port->getPortLootHref());
 
 ?>
