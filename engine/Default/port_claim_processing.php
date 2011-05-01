edcd1f344556a062aa01fa7733c1517df0a73f18
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-05-01 01:20:29 +0000

    Fix port claiming.

--- engine/Default/port_claim_processing.php
+++ engine/Default/port_claim_processing.php
@@ -3,6 +3,6 @@ require_once(get_file_loc('SmrPort.class.inc'));
 $port =& SmrPort::getPort(SmrSession::$game_id,$player->getSectorID());
 $port->setRaceID($player->getRaceID());
 
-forward($port->getPortLootHref());
+forward($port->getLootHREF(true));
 
 ?>
