c91785bf4f2e60fdd48533451ae4429a2268b676
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-05-02 17:15:39 +0000

    Change the Planet Main page to use the same display of players as the Current Sector page.

--- engine/Default/planet_main.php
+++ engine/Default/planet_main.php
@@ -20,35 +20,5 @@ $template->assignByRef('ThisPlanet',$planet);
 
 doTickerAssigns($template, $player, $db);
 
-$db->query('SELECT * FROM player WHERE sector_id = '.$player->getSectorID().' AND ' .
-									'game_id = '.SmrSession::$game_id.' AND ' .
-									'account_id != '.SmrSession::$account_id.' AND ' .
-									'land_on_planet = \'TRUE\' ' .
-								'ORDER BY last_cpl_action DESC');
-if($db->getNumRows() > 0 )
-{
-	$planetPlayers = array();
-	while ($db->nextRecord())
-	{
-		$planetPlayerAccountID = $db->getField('account_id');
-		$planetPlayers[$planetPlayerAccountID]['Player'] =& SmrPlayer::getPlayer($planetPlayerAccountID, SmrSession::$game_id);
-	
-		if ($planet->getOwnerID() == $player->getAccountID())
-		{
-			$container = array();
-			$container['url']			= 'planet_kick_processing.php';
-			$container['account_id']	= $planetPlayerAccountID;
-			$planetPlayers[$planetPlayerAccountID]['KickFormLink'] = SmrSession::get_new_href($container);
-		}
-	
-		$container = array();
-		$container['url']		= 'skeleton.php';
-		$container['body']		= 'trader_search_result.php';
-		$container['player_id']	= $planetPlayers[$planetPlayerAccountID]['Player']->getPlayerID();
-		$planetPlayers[$planetPlayerAccountID]['SearchLink'] = SmrSession::get_new_href($container);
-	}
-	$template->assignByRef('PlanetPlayers',$planetPlayers);
-}
-
 $template->assign('LaunchFormLink',SmrSession::get_new_href(create_container('planet_launch_processing.php', '')));
 ?>
