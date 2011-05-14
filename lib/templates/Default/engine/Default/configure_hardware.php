8ccedb273323436335d1b658af46b52271e17686
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-05-14 13:21:22 +0000

    Use a template for configure hardware page and add the ability to use the jump drive from that page.

---
+++ lib/templates/Default/engine/Default/configure_hardware.php
@@ -0,0 +1,45 @@
+<?php
+if (!$ThisShip->hasCloak() && !$ThisShip->hasIllusion() && !$ThisShip->hasJump())
+{
+	?>You have no configurable hardware!<?php
+}
+else
+{ ?>
+	<?php
+	if ($ThisShip->hasCloak())
+	{ ?>
+		<b>Cloaking Device:</b>&nbsp;&nbsp;&nbsp;&nbsp;<div class="buttonA"><a class="buttonA" href="<?php echo $ToggleCloakHREF; ?>">&nbsp;<?php if ($ThisShip->isCloaked()){ ?>Disable<?php }else{ ?>Enable(<?php echo TURNS_TO_CLOAK; ?>)<?php } ?>&nbsp;</a></div>
+		<br /><br />
+		<?php
+	}
+	
+	if ($ThisShip->hasIllusion())
+	{ ?>
+		<form id="SetIllusionForm" method="POST" action="<?php echo $SetIllusionFormHREF; ?>">
+			<b>Illusion Generator:</b><br /><br />
+			<table cellspacing="0" class="nobord">
+				<tr>
+					<td>Ship:</td>
+					<td>
+						<select name="ship_id" size="1" id="InputFields"><?php
+							$CurrentShipID = $ThisShip->hasActiveIllusion() ? $ThisShip->getIllusionShipID() : $ThisShip->getShipTypeID();
+							foreach($IllusionShips as $ShipTypeID => $ShipName)
+							{
+								?><option value="<?php echo $ShipTypeID; ?>"<?php if($CurrentShipID==$ShipTypeID){ ?> selected="selected"<?php } ?>><?php echo $ShipName; ?></option><?php
+							} ?>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>Attack/Defense</td>
+					<td><input type="text" id="InputFields" name="attack" value="<?php if($ThisShip->hasActiveIllusion()){ echo $ThisShip->getIllusionAttack(); }else{ ?>0<?php } ?>" size="4" class="center">&nbsp;/&nbsp;<input type="text" id="InputFields" name="defense" value="<?php if($ThisShip->hasActiveIllusion()){ echo $ThisShip->getIllusionDefense(); }else{ ?>0<?php } ?>" size="4" class="center"></td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td><input type="submit" name="action" value="Set Illusion" id="InputFields" />&nbsp;&nbsp;&nbsp;&nbsp;<div class="buttonA"><a class="buttonA" href="<?php echo $DisableIllusionHref;?>">&nbsp;Disable Illusion&nbsp;</a></div></td>
+				</tr>
+			</table>
+		</form><?php
+	}
+	$this->includeTemplate('includes/JumpDrive.inc');
+} ?>
--- lib/templates/Default/engine/Default/course_plot.php
+++ lib/templates/Default/engine/Default/course_plot.php
@@ -10,20 +10,9 @@
 		</tr>
 	</table>
 </form><?php
-if ($ThisShip->hasJump())
-{ ?>
-	<br />
-	<form class="standard" id="JumpDriveForm" method="POST" action="<?php echo $JumpDriveFormLink; ?>">
-		<h2>Jumpdrive</h2><br />
-		<table cellspacing="0" cellpadding="0" class="nobord nohpad">
-			<tr>
-				<td>Jump To:&nbsp;</td>
-				<td><input type="text" size="5" name="target" maxlength="5" class="center"></td>
-				<td>&nbsp;&nbsp;&nbsp;&nbsp;<input class="submit" type="submit" name="action" value="Engage Jump (15)"></td>
-			</tr>
-		</table>
-	</form><?php
-} ?>
+
+$this->includeTemplate('includes/JumpDrive.inc'); ?>
+
 <br />
 <h2>Plot To Nearest</h2><br />
 <form class="standard" id="SelectXTypeForm" method="POST" action="">
