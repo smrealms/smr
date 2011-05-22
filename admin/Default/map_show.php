005f910b31d6c6e73ec2c93357e24057ed5470a2
mkunze <mkunze@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-05-22 15:10:38 +0000

    fix for not working with ajax enabled

--- admin/Default/map_show.php
+++ admin/Default/map_show.php
@@ -1,43 +1,44 @@
 <?php
 
-$template->assign('PageTopic','Show Map');
-$game_id = $_REQUEST['game_id'];
-// check if we know the game yet
-if (empty($game_id)) {
+$template->assign('PageTopic', 'Show Map');
 
-	$PHP_OUTPUT.=create_echo_form(create_container('skeleton.php', 'map_show.php'));
-	$PHP_OUTPUT.=('<p>Please select a game:</p>');
-	$PHP_OUTPUT.=('<select name="game_id" size="1" id="InputFields">');
-	$PHP_OUTPUT.=('<option value="0">[Please Select]</option>');
+if (isset($_REQUEST['game_id'])) {
 
-	$db->query('SELECT * FROM game ORDER BY game_id');
+    SmrSession::updateVar('game_id', $_REQUEST['game_id']);
 
-	while ($db->nextRecord())
-		$PHP_OUTPUT.=('<option value="' . $db->getField('game_id') . '">' . $db->getField('game_name') . '</option>');
+    $container = array();
+    $container['url'] = 'map_show_processing.php';
+    transfer('game_id');
 
-	$PHP_OUTPUT.=('</select>&nbsp;&nbsp;');
-	$PHP_OUTPUT.=create_submit('Select');
-	$PHP_OUTPUT.=('</form>');
+    $PHP_OUTPUT .= create_echo_form($container);
+    $PHP_OUTPUT .= ('<select name="account_id" size="1" id="InputFields">');
+    $PHP_OUTPUT .= ('<option value="0">[Please Select]</option>');
+    $PHP_OUTPUT .= ('<option value="all">All Players</option>');
+
+    $db->query('SELECT * FROM player WHERE game_id = ' . $var['game_id'] . ' ORDER BY player_id');
+
+    while ($db->nextRecord())
+        $PHP_OUTPUT .= ('<option value="' . $db->getField('account_id') . '">' . stripslashes($db->getField('player_name')) . ' (' . $db->getField('player_id') . ')</option>');
+
+    $PHP_OUTPUT .= ('</select>&nbsp;&nbsp;');
+    $PHP_OUTPUT .= create_submit('Reveal Map');
+    $PHP_OUTPUT .= ('</form>');
 
 } else {
 
-	$container = array();
-	$container['url']		= 'map_show_processing.php';
-	$container['game_id']	= $game_id;
+    $PHP_OUTPUT .= create_echo_form(create_container('skeleton.php', 'map_show.php'));
+    $PHP_OUTPUT .= ('<p>Please select a game:</p>');
+    $PHP_OUTPUT .= ('<select name="game_id" size="1" id="InputFields">');
+    $PHP_OUTPUT .= ('<option value="0">[Please Select]</option>');
 
-	$PHP_OUTPUT.=create_echo_form($container);
-	$PHP_OUTPUT.=('<select name="account_id" size="1" id="InputFields">');
-	$PHP_OUTPUT.=('<option value="0">[Please Select]</option>');
-	$PHP_OUTPUT.=('<option value="all">All Players</option>');
+    $db->query('SELECT * FROM game ORDER BY game_id');
 
-	$db->query('SELECT * FROM player WHERE game_id = '.$game_id.' ORDER BY player_id');
+    while ($db->nextRecord())
+        $PHP_OUTPUT .= ('<option value="' . $db->getField('game_id') . '">' . $db->getField('game_name') . '</option>');
 
-	while ($db->nextRecord())
-		$PHP_OUTPUT.=('<option value="' . $db->getField('account_id') . '">' . stripslashes($db->getField('player_name')) . ' (' . $db->getField('player_id') . ')</option>');
-
-	$PHP_OUTPUT.=('</select>&nbsp;&nbsp;');
-	$PHP_OUTPUT.=create_submit('Reveal Map');
-	$PHP_OUTPUT.=('</form>');
+    $PHP_OUTPUT .= ('</select>&nbsp;&nbsp;');
+    $PHP_OUTPUT .= create_submit('Select');
+    $PHP_OUTPUT .= ('</form>');
 
 }
 
