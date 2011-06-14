8b56c927805a8b32deaf8de711b162354fd749cd
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-06-14 08:38:01 +0000

    Fix table update script.

--- db/account_has_logs.microtime.col.sql
+++ db/account_has_logs.microtime.col.sql
@@ -1,2 +1,2 @@
-ALTER TABLE account_has_logs CHANGE time microtime INT(16) UNSIGNED NOT NULL;
+ALTER TABLE account_has_logs CHANGE time microtime BIGINT(16) UNSIGNED NOT NULL;
 UPDATE account_has_logs SET microtime = microtime * 1000000;
