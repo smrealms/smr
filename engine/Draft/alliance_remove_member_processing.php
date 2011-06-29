8325edd569a321382439ab28a5cc5bc798548f34
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-06-29 17:44:31 +0000

    Port the changes to kicking players into the draft game version as well.

--- engine/Draft/alliance_remove_member_processing.php
+++ engine/Draft/alliance_remove_member_processing.php
@@ -1,30 +1,30 @@
 <?php
 require_once(get_file_loc('smr_alliance.inc'));
 $alliance = new SMR_ALLIANCE($player->getAllianceID(), SmrSession::$game_id);
-$account_id = $_REQUEST['account_id'];
-foreach ($account_id as $id)
-{
-	
-	if ($id == $alliance->leader_id)
-		create_error('You can\'t remove the leader!');
-	// generate list of messages that should be deleted
-    if ($account_id_list) $account_id_list .= ',';
-    $account_id_list .= $id;
+$accountIDs = $_REQUEST['account_id'];
 
-	$player->sendMessage($id, 2, 'You were kicked out of the alliance!', false);
-	$curr_acc =& SmrAccount::getAccount($id);
-	$curr_acc->log(3, 'kicked from alliance: '.$alliance->alliance_name.' by leader', 0);
-
-}
-
-if(empty($account_id_list))
+if(empty($account_id))
 {
 	create_error('You have to choose someone to remove them!');
 }
-$db->query('UPDATE player SET alliance_id = 0, sector_id = 1, newbie_turns = GREATEST(1,newbie_turns) WHERE account_id IN ('.$account_id_list.') AND ' .
-												   'game_id = '.$player->getGameID());
-$db->query('DELETE FROM player_has_alliance_role WHERE game_id = '.$player->getGameID().' AND account_id IN ('.$account_id_list.')');
 
+foreach ($account_id as $id)
+{
+	if ($id == $alliance->leader_id)
+		create_error('You can\'t remove the leader!');
+}
+
+foreach ($account_id as $id)
+{
+	$currPlayer =& SmrPlayer::getPlayer($id, $player->getGameID());
+	$currPlayer->leaveAlliance($player);
+	$currPlayer->setSectorID(1);
+	$currPlayer->setNewbieTurns(max(1,$currPlayer->getNewbieTurns()));
+}
+
+forward(create_container('skeleton.php', 'alliance_roster.php'));
+
+?>
 forward(create_container('skeleton.php', 'alliance_roster.php'));
 
 ?>
