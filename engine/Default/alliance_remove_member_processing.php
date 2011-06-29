e1579a93702d16f7f5edb0c110dfd74fd4f0132b
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-06-29 16:03:19 +0000

    Kicking players now uses the SmrPlayer->leaveAlliance code, meaning the alliance joinable time is set. Also added a log entry for each player you kick.

--- engine/Default/alliance_remove_member_processing.php
+++ engine/Default/alliance_remove_member_processing.php
@@ -1,29 +1,23 @@
 <?php
 require_once(get_file_loc('smr_alliance.inc'));
 $alliance = new SMR_ALLIANCE($player->getAllianceID(), SmrSession::$game_id);
-$account_id = $_REQUEST['account_id'];
-foreach ($account_id as $id)
-{
-	
-	if ($id == $alliance->leader_id)
-		create_error('You can\'t remove the leader!');
-	// generate list of messages that should be deleted
-    if ($account_id_list) $account_id_list .= ',';
-    $account_id_list .= $id;
+$accountIDs = $_REQUEST['account_id'];
 
-	$player->sendMessage($id, 2, 'You were kicked out of the alliance!', false);
-	$curr_acc =& SmrAccount::getAccount($id);
-	$curr_acc->log(3, 'kicked from alliance: '.$alliance->alliance_name.' by leader', 0);
-
-}
-
-if(empty($account_id_list))
+if(empty($account_id))
 {
 	create_error('You have to choose someone to remove them!');
 }
-$db->query('UPDATE player SET alliance_id = 0 WHERE account_id IN ('.$account_id_list.') AND ' .
-												   'game_id = '.$player->getGameID());
-$db->query('DELETE FROM player_has_alliance_role WHERE game_id = '.$player->getGameID().' AND account_id IN ('.$account_id_list.')');
+
+foreach ($account_id as $id)
+{
+	if ($id == $alliance->leader_id)
+		create_error('You can\'t remove the leader!');
+}
+
+foreach ($account_id as $id)
+{
+	SmrPlayer::getPlayer($id, $player->getGameID())->leaveAlliance($player);
+}
 
 forward(create_container('skeleton.php', 'alliance_roster.php'));
 
