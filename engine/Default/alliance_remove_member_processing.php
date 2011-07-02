bbd459d3cec31588ba36802b62acb77c700fdfca
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-07-02 02:00:02 +0000

    Fix variable names.

--- engine/Default/alliance_remove_member_processing.php
+++ engine/Default/alliance_remove_member_processing.php
@@ -3,20 +3,20 @@ require_once(get_file_loc('SmrAlliance.class.inc'));
 $alliance =& SmrAlliance::getAlliance($player->getAllianceID(), SmrSession::$game_id);
 $accountIDs = $_REQUEST['account_id'];
 
-if(empty($account_id))
+if(empty($accountIDs))
 {
 	create_error('You have to choose someone to remove them!');
 }
 
-foreach ($account_id as $id)
+foreach ($accountIDs as $accountID)
 {
-	if ($id == $alliance->getLeaderID())
+	if ($accountID == $alliance->getLeaderID())
 		create_error('You can\'t remove the leader!');
 }
 
-foreach ($account_id as $id)
+foreach ($accountIDs as $accountID)
 {
-	SmrPlayer::getPlayer($id, $player->getGameID())->leaveAlliance($player);
+	SmrPlayer::getPlayer($accountID, $player->getGameID())->leaveAlliance($player);
 }
 
 forward(create_container('skeleton.php', 'alliance_roster.php'));
