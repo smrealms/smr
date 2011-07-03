61723f653f278b818e0964256005091f97a3bc4f
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-07-03 14:39:13 +0000

    Add new version when setting version live.

--- admin/Default/changelog_set_live_processing.php
+++ admin/Default/changelog_set_live_processing.php
@@ -5,6 +5,11 @@ $db->query('UPDATE version
 			WHERE version_id = ' . $var['version_id']
 		   );
 
+$db->query('SELECT * FROM version WHERE version_id = ' . $var['version_id']);
+$db->nextRecord();
+$db->query('INSERT IGNORE INTO version (version_id, major_version, minor_version, patch_level, went_live) VALUES
+			('.$db->getInt('version_id').','.$db->getInt('major_version').','.$db->getInt('minor_version').','.($db->getInt('patch_level')+1).',0);');
+
 forward(create_container('skeleton.php', 'changelog.php'));
 
 ?>
