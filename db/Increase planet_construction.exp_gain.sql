2d100c6ff4e13f035317f428485432e0c7a5f434
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-07-08 20:06:15 +0000

    Increase experience from planet building by 1.5x

---
+++ db/Increase planet_construction.exp_gain.sql
@@ -0,0 +1,3 @@
+UPDATE smr_new.planet_construction SET exp_gain = 45 WHERE construction_id = 1;
+UPDATE smr_new.planet_construction SET exp_gain = 90 WHERE construction_id = 2;
+UPDATE smr_new.planet_construction SET exp_gain = 270 WHERE construction_id = 3;
