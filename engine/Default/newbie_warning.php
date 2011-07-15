3d16d0cfd86412004c4c60f62aa9953ec46e2a85
mkunze <mkunze@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-07-15 20:54:19 +0000

    typo fix

--- engine/Default/newbie_warning.php
+++ engine/Default/newbie_warning.php
@@ -4,7 +4,7 @@ $player->setNewbieWarning(false);
 
 $template->assign('PageTopic','Warning!');
 
-$PHP_OUTPUT.=('<p>You have gotten this page because you are almost out of newbie turns.');
+$PHP_OUTPUT.=('<p>You have received this page because you are almost out of newbie turns.');
 $PHP_OUTPUT.=('What does this mean? You can now do many things that you couldn\'t do while in newbie turns.</p>');
 
 $PHP_OUTPUT.=('<ol>You can...');
