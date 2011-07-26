285e53d67abae75caf4bf85deec0eb98fc96f1a8
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-07-26 11:26:58 +0000

    Add a check for numeric account id and get HoF name using OO.

--- engine/Default/preferences_confirm.php
+++ engine/Default/preferences_confirm.php
@@ -8,6 +8,8 @@ $amount = $var['amount'];
 $account_id = $var['account_id'];
 if (!is_numeric($amount))
 	create_error('Numbers only please');
+if (!is_numeric($account_id))
+	create_error('Invalid player selected');
 $amount = round($amount);
 if ($amount <= 0)
 	create_error('You can only tranfer a positive amount');
@@ -19,27 +21,7 @@ $template->assign('PageTopic','Confirmation');
 
 $PHP_OUTPUT.=('Are you sure you want to transfer '.$amount.' credits to<br />');
 
-$db->query('SELECT hof_name FROM account WHERE account_id = '.$account_id);
-if ($db->nextRecord())
-	$hofName = $db->getField('hof_name');
-
-//$db->query('SELECT * FROM player WHERE account_id = '.$account_id);
-//if ($db->getNumRows())
-//{
-//	while ($db->nextRecord())
-//	{
-//	    $player_name = stripslashes($db->getField('player_name'));
-//    	$game_id = $db->getField('game_id');
-//
-//	    $db2->query('SELECT * FROM game WHERE game_id = '.$game_id);
-//    	if ($db2->nextRecord())
-//			$game_name = $db2->getField('game_name');
-//
-//		$PHP_OUTPUT.=($player_name.' in game '.$game_name.'('.$game_id.')<br />');
-//	}
-//}
-//else
-	$PHP_OUTPUT.=('Player with HoF name '.$hofName.'?<br />');
+$PHP_OUTPUT.=('Player with HoF name '.SmrAccount::getAccount($account_id)->getHofName().'?<br />');
 
 $PHP_OUTPUT.=('<br/><h3>Please make sure this is definitely the correct person before confirming.</h3>');
 $PHP_OUTPUT.=('<p>&nbsp;</p>');
