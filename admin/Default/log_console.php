7e5ff72d5cd4f7575120262d55f7d7df0d694f9c
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-08-05 18:46:46 +0000

    Stop using NATURAL JOINs in log console to fix a bug with count being incorrect (and potentially hidden due) and remove player name column. Also split log console out to a template.

--- admin/Default/log_console.php
+++ admin/Default/log_console.php
@@ -2,84 +2,38 @@
 
 $template->assign('PageTopic','Log Console');
 
-$PHP_OUTPUT.=('<p>Choose the log files you wish to view or delete!<br />Don\'t keep unnecessary data!</p>');
+$loggedAccounts = array();
 
-$db->query('SELECT account_has_logs.account_id as account_id, login, player_name, count(account_has_logs.account_id) as number_of_entries
+$db->query('SELECT account_id as account_id, login, count(*) as number_of_entries
 			FROM account_has_logs
-			NATURAL JOIN account
-			NATURAL JOIN player
-			GROUP BY account_has_logs.account_id');
-if ($db->getNumRows()) {
-
-	// a second db object
+			JOIN account USING(account_id)
+			GROUP BY account_id');
+if ($db->getNumRows())
+{
 	$db2 = new SmrMySqlDatabase();
+	while ($db->nextRecord())
+	{
+		$accountID = $db->getInt('account_id');
+		$loggedAccounts[$accountID] = array('AccountID' => $accountID,
+								'Login' => $db->getField('login'),
+								'TotalEntries' => $db->getField('number_of_entries'),
+								'Checked' => is_array($var['account_ids']) && in_array($account_id, $var['account_ids']),
+								'Notes' => '');
 
-	$PHP_OUTPUT.=create_echo_form(create_container('skeleton.php', 'log_console_detail.php'));
-
-	$PHP_OUTPUT.=('<table class="standard">');
-	$PHP_OUTPUT.=('<tr>');
-	$PHP_OUTPUT.=('<th>Login</th>');
-	$PHP_OUTPUT.=('<th>Player Name</th>');
-	$PHP_OUTPUT.=('<th>Entries</th>');
-	$PHP_OUTPUT.=('<th>Action</th>');
-	$PHP_OUTPUT.=('<th>Notes</th>');
-	$PHP_OUTPUT.=('</tr>');
-
-	while ($db->nextRecord()) {
-
-		$account_id			= $db->getField('account_id');
-		$login				= $db->getField('login');
-		$player_name		= stripslashes($db->getField('player_name'));
-		$number_of_entries	= $db->getField('number_of_entries');
-
-		if (is_array($var['account_ids']) && in_array($account_id, $var['account_ids']))
-			$checked = ' checked';
-		else
-			$checked = '';
-
-		// put hidden fields in for log type to have all fields selected on next page.
-		$db2->query('SELECT * FROM log_type');
-		while ($db2->nextRecord())
-			$PHP_OUTPUT.=('<input type="hidden" name="log_type_ids[' . $db2->getField('log_type_id') . ']" value="1">');
-
-		$PHP_OUTPUT.=('<tr>');
-		$PHP_OUTPUT.=('<td valign="top">'.$login.'</td>');
-		$PHP_OUTPUT.=('<td valign="top">'.$player_name.'</td>');
-		$PHP_OUTPUT.=('<td valign="top" align="center">'.$number_of_entries.'</td>');
-		$PHP_OUTPUT.=('<td valign="middle" align="center"><input type="checkbox" name="account_ids[]" value="'.$account_id.'"'.$checked.'></td>');
-
-		$db2->query('SELECT * FROM log_has_notes WHERE account_id = '.$account_id);
+		$db2->query('SELECT notes FROM log_has_notes WHERE account_id = '.$account_id);
 		if ($db2->nextRecord())
-			$PHP_OUTPUT.=('<td>' . nl2br($db2->getField('notes')) . '</td>');
-		else
-			$PHP_OUTPUT.=('<td>&nbsp;</td>');
-		$PHP_OUTPUT.=('</tr>');
-
+			$loggedAccounts[$accountID]['Notes'] = nl2br($db2->getField('notes'));
 	}
-
-	$PHP_OUTPUT.=('<tr>');
-	$PHP_OUTPUT.=('<td colspan="3">&nbsp;</td>');
-	$PHP_OUTPUT.=('<td>');
-	$PHP_OUTPUT.=create_submit('View');
-	$PHP_OUTPUT.=('&nbsp;&nbsp;');
-	$PHP_OUTPUT.=create_submit('Delete');
-	$PHP_OUTPUT.=('</td>');
-	$PHP_OUTPUT.=('<td>&nbsp;</td>');
-	$PHP_OUTPUT.=('</tr>');
-	$PHP_OUTPUT.=('</table>');
-
-	$PHP_OUTPUT.=('</form>');
-
-	$PHP_OUTPUT.=('<p>&nbsp;</p>');
-
-	$PHP_OUTPUT.=('<p>');
-	$PHP_OUTPUT.=('Check for:');
-	$PHP_OUTPUT.=('<ul>');
-	$PHP_OUTPUT.=create_link(create_container('skeleton.php', 'log_anonymous_account.php'), '<li>Anonymous Account access</li>');
-	$PHP_OUTPUT.=('</ul>');
-	$PHP_OUTPUT.=('</p>');
-
-} else
-	$PHP_OUTPUT.=('There are no log entries at all!');
-
+	
+	// put hidden fields in for log type to have all fields selected on next page.
+	$logTypes = array();
+	$db->query('SELECT log_type_id FROM log_type');
+	while ($db->nextRecord())
+		$logTypes[] = $db->getInt('log_type_id');
+	$template->assignByRef('LogTypes', $logTypes);
+	
+	$template->assign('LogConsoleFormHREF', SmrSession::get_new_href(create_container('skeleton.php', 'log_console_detail.php')));
+	$template->assign('AnonAccessHRE', SmrSession::get_new_href(create_container('skeleton.php', 'log_anonymous_account.php')));
+}
+$template->assignByRef('LoggedAccounts',$loggedAccounts);
 ?>
---
+++ templates/Default/admin/Default/log_console.php
@@ -0,0 +1,56 @@
+<p>
+	Choose the log files you wish to view or delete!<br />
+	Don't keep unnecessary data!
+</p><?php
+
+if (count($LoggedAccounts)>0)
+{ ?>
+	<form method="POST" action="<?php echo $LogConsoleFormHREF; ?>"><?php
+
+		// put hidden fields in for log type to have all fields selected on next page.
+		foreach($LogTypes as $LogType)
+		{ ?>
+			<input type="hidden" name="log_type_ids[<?php echo $LogType; ?>]" value="1"><?php
+		} ?>
+		
+		<table class="standard">
+			<tr>
+				<th>Login</th>
+				<th>Entries</th>
+				<th>Action</th>
+				<th>Notes</th>
+			</tr><?php
+	
+			foreach($LoggedAccounts as &$LoggedAccount)
+			{ ?>
+				<tr>
+					<td valign="top"><?php echo $LoggedAccount['Login']; ?></td>
+					<td valign="top" align="center"><?php echo $LoggedAccount['TotalEntries']; ?></td>
+					<td valign="middle" align="center"><input type="checkbox" name="account_ids[]" value="<?php echo $LoggedAccount['AccountID']; ?>"<?php if($LoggedAccount['Checked']){ ?> checked="checked"<?php } ?>></td>
+					<td><?php echo $LoggedAccount['Notes']; ?></td>
+				</tr><?php
+			} ?>
+		
+			<tr>
+				<td colspan="3">&nbsp;</td>
+				<td>
+					<input type="submit" name="action" value="View" id="InputFields" />	&nbsp;&nbsp;<input type="submit" name="action" value="Delete" id="InputFields" />
+				</td>
+				<td>&nbsp;</td>
+			</tr>
+		</table>
+
+	</form>
+
+	<p>&nbsp;</p>
+
+	<p>Check for:</p>
+	<ul>
+		<li><a href="<?php echo $AnonAccessHREF; ?>">Anonymous Account access</a></li>
+	</ul><?php
+}
+else
+{ ?>
+	There are no log entries at all!<?php
+}
+?>
