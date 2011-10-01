0658c682abc485118e988970c50ffdaa8d8eac35
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-10-01 12:09:56 +0000

    Use a template for escape pod screen.

--- engine/Default/death.php
+++ engine/Default/death.php
@@ -1,8 +1,3 @@
 <?php
 $template->assign('PageTopic','Death');
-
-$PHP_OUTPUT.=('<p>As the hull of your ship collapses, you quickly launch out in your escape pod. ');
-$PHP_OUTPUT.=('Activating the emergency warp system, your stomach turns as you are hurled through hyperspace back to a safe destination.</p>');
-
-$PHP_OUTPUT.=('<p><img src="images/escape_pod.jpg"></p>');
 ?>
---
+++ templates/Default/engine/Default/death.php
@@ -0,0 +1,5 @@
+<p>
+	As the hull of your ship collapses, you quickly launch out in your escape pod. 
+	Activating the emergency warp system, your stomach turns as you are hurled through hyperspace back to a safe destination.
+</p>
+<p><img src="images/escape_pod.jpg"></p>
