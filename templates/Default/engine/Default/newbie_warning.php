3a5c4928d9d8465dcecfc24011d50087af31e012
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-10-01 15:40:14 +0000

    Use a template for newbie warning.

---
+++ templates/Default/engine/Default/newbie_warning.php
@@ -0,0 +1,23 @@
+<p>
+	You have received this page because you are almost out of newbie turns.
+	What does this mean? You can now do many things that you couldn't do while in newbie turns.
+</p>
+
+<ol>You can...
+	<li>attack other players</li>
+	<li>attack enemy planet\'s</li>
+	<li>Port raid</li>
+	<li>Place forces</li>
+</ol>
+
+<p>But remember, with the good comes the bad. In addition to being able to do the above, they can happen to you.</p>
+
+<ol>You can...
+	<li>Be Attacked</li>
+	<li>Hit enemy forces and take damage</li>
+	<li>die...</li>
+</ol>
+
+<p>Plan for your safety now. Remember to use federal protection (Must have an acceptable attack rating, this increases/decreases with your align so it is safest to have an attack rating of 0 if unsure, also make sure to stick to the federal space of friendly races), know where your alliances strongest planets are, and watch out for the people looking for you.</p>
+
+<p>For more information visit the <a href="<?php echo URL; ?>/manual.php">help files</a></p>
