99cff0d584823dcbfc8cb50b870618f7bebe3729
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-10-03 02:04:06 +0000

    Don't allow challenging the same person twice.

--- engine/Default/chess.php
+++ engine/Default/chess.php
@@ -2,10 +2,17 @@
 require_once(get_file_loc('ChessGame.class.inc'));
 //$chessGame = new ChessGame(0);
 //$template->assignByRef('Board',$chessGame->getBoard());
+$chessGames =& ChessGame::getAccountGames($player->getAccountID());
+$template->assign('ChessGames', $chessGames);
 
-$template->assign('ChessGames', ChessGame::getAccountGames($player->getAccountID()));
+$playersChallenged = array($player->getAccountID() => true);
+foreach($chessGames as $chessGame)
+{
+	$playersChallenged[$chessGame->getWhiteID()] = true;
+	$playersChallenged[$chessGame->getBlackID()] = true;
+}
 
-$db->query('SELECT player_id, player.player_name FROM player JOIN account USING(account_id) LEFT OUTER JOIN npc_logins USING(login) WHERE working IS NULL AND validated = ' . $db->escapeBoolean(true) . ' AND game_id = ' . $db->escapeString($player->getGameID()) . ' AND account_id != ' . $db->escapeNumber($player->getAccountID()) . ' ORDER BY player_name');
+$db->query('SELECT player_id, player.player_name FROM player JOIN account USING(account_id) LEFT OUTER JOIN npc_logins USING(login) WHERE working IS NULL AND validated = ' . $db->escapeBoolean(true) . ' AND game_id = ' . $db->escapeString($player->getGameID()) . ' AND account_id NOT IN ' . $db->escapeArray(array_keys($playersChallenged)) . ' ORDER BY player_name');
 while ($db->nextRecord())
 {
 	$players[$db->getField('player_id')] = $db->getField('player_name');
