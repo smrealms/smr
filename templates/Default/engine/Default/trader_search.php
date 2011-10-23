b80140ceec15410dbdaa0f3c3238616be9fc380d
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-10-23 15:43:22 +0000

    Move trader_search to a template.

---
+++ templates/Default/engine/Default/trader_search.php
@@ -0,0 +1,12 @@
+<p>&nbsp;</p>
+
+<form method="POST" action="<?php echo $TraderSearchHREF; ?>">
+	<span style="font-size:75%;">Player name:</span><br />
+	<input type="text" name="player_name" id="InputFields" style="width:150px">&nbsp;<input type="submit" name="action" value="Search" id="InputFields" />
+
+	<p>&nbsp;</p>
+
+	<span style="font-size:75%;">Player ID:</span><br />
+	<input type="text" name="player_id" id="InputFields" style="width:50px">&nbsp;<input type="submit" name="action" value="Search" id="InputFields" />
+
+</form>
