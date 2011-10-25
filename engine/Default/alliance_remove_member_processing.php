92425e39fd55288d3a370ada158f1ff309cc46e1
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-10-25 22:22:45 +0000

    Tidy up kicking script and fix it for draft mode.

--- engine/Default/alliance_remove_member_processing.php
+++ engine/Default/alliance_remove_member_processing.php
@@ -1,18 +1,11 @@
 <?php
-require_once(get_file_loc('SmrAlliance.class.inc'));
-$alliance =& SmrAlliance::getAlliance($player->getAllianceID(), SmrSession::$game_id);
 $accountIDs = $_REQUEST['account_id'];
 
 if(empty($accountIDs))
-{
 	create_error('You have to choose someone to remove them!');
-}
 
-foreach ($accountIDs as $accountID)
-{
-	if ($accountID == $alliance->getLeaderID())
-		create_error('You can\'t remove the leader!');
-}
+if(in_array($player->getAlliance()->getLeaderID(), $accountIDs))
+	create_error('You can\'t remove the leader!');
 
 foreach ($accountIDs as $accountID)
 {
--- engine/Draft/alliance_remove_member_processing.php
+++ engine/Draft/alliance_remove_member_processing.php
@@ -1,22 +1,15 @@
 <?php
-require_once(get_file_loc('SmrAlliance.class.inc'));
-$alliance =& SmrAlliance::getAlliance($player->getAllianceID(), SmrSession::$game_id);
 $accountIDs = $_REQUEST['account_id'];
 
-if(empty($account_id))
-{
+if(empty($accountIDs))
 	create_error('You have to choose someone to remove them!');
-}
 
-foreach ($account_id as $id)
-{
-	if ($id == $alliance->getLeaderID())
-		create_error('You can\'t remove the leader!');
-}
+if(in_array($player->getAlliance()->getLeaderID(), $accountIDs))
+	create_error('You can\'t remove the leader!');
 
-foreach ($account_id as $id)
+foreach ($accountIDs as $accountID)
 {
-	$currPlayer =& SmrPlayer::getPlayer($id, $player->getGameID());
+	$currPlayer =& SmrPlayer::getPlayer($accountID, $player->getGameID());
 	$currPlayer->leaveAlliance($player);
 	$currPlayer->setSectorID(1);
 	$currPlayer->setNewbieTurns(max(1,$currPlayer->getNewbieTurns()));
