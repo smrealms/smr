eb9f75ffdee76940ae370402c9c99d7cd45bb499
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-10-26 20:22:40 +0000

    Tidy up.

--- engine/Default/game_play_processing.php
+++ engine/Default/game_play_processing.php
@@ -3,18 +3,12 @@
 // register game_id
 SmrSession::$game_id = $var['game_id'];
 
-// check if hof entry is there
-$db->query('SELECT * FROM account_has_stats WHERE account_id = '.SmrSession::$account_id);
-if (!$db->getNumRows())
-	$db->query('INSERT INTO account_has_stats (account_id, HoF_name, games_joined) VALUES ('.$account->getAccountID().', ' . $db->escape_string($account->getLogin(), true) . ', 1)');
-
 $player =& SmrPlayer::getPlayer(SmrSession::$account_id, $var['game_id']);
-$player->setLastSectorID(0);
 $player->updateLastCPLAction();
-$player->update();
 
 // get rid of old plotted course
 $player->deletePlottedCourse();
+$player->update();
 
 // log
 $account->log(2, 'Player entered game '.SmrSession::$game_id, $player->getSectorID());
@@ -25,31 +19,5 @@ if ($player->isLandedOnPlanet())
 else
     $container['body'] = 'current_sector.php';
 
-$player	=& SmrPlayer::getPlayer(SmrSession::$account_id, SmrSession::$game_id);
-$ship	=& $player->getShip();
-
-// update turns on that player
-$player->updateTurns();
-
-// we cant move if we are dead
-//check if we are in kill db...if we are we don't do anything
-if (!isset($var['ahhh'])) {
-
-	$db->query('SELECT * FROM kills WHERE dead_id = '.$player->getAccountID().' AND processed = \'TRUE\' AND game_id = '.$player->getGameID());
-	if ($db->nextRecord() && $var['body'] != 'trader_attack.php') {
-		$container = create_container('death_processing.php');
-		$container['ahhh'] = 'Yes';
-	}
-}
-
-if ($player->getNewbieTurns() <= 20 &&
-	$player->getNewbieWarning() &&
-	$var['url'] != 'newbie_warning_processing.php')
-	$container = create_container('newbie_warning_processing.php');
-
-$url = SmrSession::get_new_href($container);
-SmrSession::update();
-header('Location: '.$url);
-exit;
-
+forward($container);
 ?>
