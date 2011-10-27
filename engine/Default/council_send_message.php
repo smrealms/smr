34c0da95d54942e37362d2c3b6383478784968fc
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-10-27 22:55:45 +0000

    Use OO methods.

--- engine/Default/council_send_message.php
+++ engine/Default/council_send_message.php
@@ -1,24 +1,21 @@
 <?php
 
-$db->query('SELECT * FROM race WHERE race_id = ' . $var['race_id']);
-if ($db->nextRecord())
-	$race_name = $db->getField('race_name');
+$raceName = Globals::getRaceName($var['race_id']);
 
-$template->assign('PageTopic','Send message to ruling council of the '.$race_name);
+$template->assign('PageTopic','Send message to ruling council of the '.$raceName);
 
 require_once(get_file_loc('menu.inc'));
 create_message_menu();
 
 $PHP_OUTPUT.=('<p>');
 
-$container = array();
-$container['url'] = 'council_send_message_processing.php';
+$container = create_container('council_send_message_processing.php');
 transfer('race_id');
 
 $PHP_OUTPUT.=create_echo_form($container);
 $PHP_OUTPUT.=('<p><small><b>From:</b> '.$player->getPlayerName().' ('.$player->getPlayerID().')<br />');
 
-$PHP_OUTPUT.=('<b>To:</b> Ruling Council of '.$race_name.'</small></p>');
+$PHP_OUTPUT.=('<b>To:</b> Ruling Council of '.$raceName.'</small></p>');
 
 $PHP_OUTPUT.=('<textarea name="message" id="InputFields"></textarea><br /><br />');
 $PHP_OUTPUT.=create_submit('Send message');
