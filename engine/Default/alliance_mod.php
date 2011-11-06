0c258a574766e7b0d331b342c6ed4f22b6b57f7d
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-11-06 15:05:01 +0000

    Fix alliance motd page not showing image/motd.

--- engine/Default/alliance_mod.php
+++ engine/Default/alliance_mod.php
@@ -13,14 +13,12 @@ create_alliance_menu($alliance->getAllianceID(),$alliance->getLeaderID());
 
 $PHP_OUTPUT.= '<div align="center">';
 
-if (strlen($db->getField('img_src')) && $db->getField('img_src') != 'http://') {
-	$PHP_OUTPUT.= '<img class="alliance" src="';
-	$PHP_OUTPUT.= $db->getField('img_src');
-	$PHP_OUTPUT.= '" alt="' . htmlspecialchars($db->getField('alliance_name')) . ' Banner"><br /><br />';
+if ($alliance->hasImageURL()) {
+	$PHP_OUTPUT.= '<img class="alliance" src="' . $alliance->getImageURL() . '" alt="' . htmlspecialchars($alliance->getAllianceName()) . ' Banner"><br /><br />';
 }
 
 $PHP_OUTPUT.= '<span class="yellow">Message from your leader</span><br /><br />';
-$PHP_OUTPUT.= bbifyMessage($db->getField('mod'));
+$PHP_OUTPUT.= bbifyMessage($alliance->getMotD());
 
 $db->query('SELECT * FROM player_has_alliance_role WHERE account_id = '.$player->getAccountID().' AND game_id = '.$player->getGameID().' AND alliance_id='.$alliance->getAllianceID());
 if ($db->nextRecord()) $role_id = $db->getField('role_id');
