82f2d7d2e74b5e4d9e126dbef97e9976823e4ada
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-11-10 01:58:22 +0000

    Fix a bug with disbanding alliance.

--- engine/Default/alliance_leave_processing.php
+++ engine/Default/alliance_leave_processing.php
@@ -2,39 +2,31 @@
 $action = $var['action'];
 if ($action == 'YES')
 {
-	$db->query('SELECT count(*) FROM player WHERE alliance_id = '.$player->getAllianceID().' AND ' .
-										  'game_id = '.$player->getGameID());
-
+	$alliance =& $player->getAlliance();
 	// will this alliance be empty if we leave? (means one member right now)
-	if ($db->nextRecord() && $db->getInt('count(*)') == 1)
-	{
-		//$db->query('DELETE FROM alliance WHERE alliance_id = '.$player->getAllianceID().' AND ' .
-											  //'game_id = '.$player->getGameID());
-		$db->query('DELETE FROM alliance_bank_transactions ' .
-				   'WHERE alliance_id = '.$player->getAllianceID().' AND ' .
-						 'game_id = '.$player->getGameID());
-		$db->query('DELETE FROM alliance_thread ' .
-				   'WHERE alliance_id = '.$player->getAllianceID().' AND ' .
-						 'game_id = '.$player->getGameID());
-		$db->query('DELETE FROM alliance_thread_topic ' .
-				   'WHERE alliance_id = '.$player->getAllianceID().' AND ' .
-						 'game_id = '.$player->getGameID());
-		$db->query('DELETE FROM alliance_has_roles
-					WHERE alliance_id = '.$player->getAllianceID().' AND
-						  game_id = '.$player->getGameID());
-		$db->query('UPDATE alliance SET leader_id=0 WHERE alliance_id = '.$player->getAllianceID().' AND ' .
-						 'game_id = '.$player->getGameID());
-	}
-	elseif ($player->isAllianceLeader())
+	if ($player->isAllianceLeader() && $alliance->getNumMembers() > 1)
 		create_error('You are the leader! You must hand over leadership first!');
-
 	$player->leaveAlliance();
-	$player->update();
+	//$db->query('DELETE FROM alliance WHERE alliance_id = '.$player->getAllianceID().' AND ' .
+										  //'game_id = '.$player->getGameID());
+	$db->query('DELETE FROM alliance_bank_transactions ' .
+			   'WHERE alliance_id = '.$player->getAllianceID().' AND ' .
+					 'game_id = '.$player->getGameID());
+	$db->query('DELETE FROM alliance_thread ' .
+			   'WHERE alliance_id = '.$player->getAllianceID().' AND ' .
+					 'game_id = '.$player->getGameID());
+	$db->query('DELETE FROM alliance_thread_topic ' .
+			   'WHERE alliance_id = '.$player->getAllianceID().' AND ' .
+					 'game_id = '.$player->getGameID());
+	$db->query('DELETE FROM alliance_has_roles
+				WHERE alliance_id = '.$player->getAllianceID().' AND
+					  game_id = '.$player->getGameID());
+	$db->query('UPDATE alliance SET leader_id=0 WHERE alliance_id = '.$player->getAllianceID().' AND ' .
+					 'game_id = '.$player->getGameID());
 
 }
 
-$container = array();
-$container['url'] = 'skeleton.php';
+$container = create_container('skeleton.php');
 if ($player->isLandedOnPlanet())
 	$container['body'] = 'planet_main.php';
 else
