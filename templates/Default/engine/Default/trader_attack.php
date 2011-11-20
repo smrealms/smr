b284aab7bef49a102ef27a4f42494723f3bd3408
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-11-20 21:04:05 +0000

    Remove random position.

--- templates/Default/engine/Default/trader_attack.php
+++ templates/Default/engine/Default/trader_attack.php
@@ -3,11 +3,9 @@
 <div align="center"><?php
 	if(isset($Target))
 	{ ?>
-		<div style="width:50%" align="<?php if($RandomPosition == 0){ ?>center<?php }else if($RandomPosition == 1){ ?>right<?php }else{ ?>left<?php } ?>">
-			<div class="buttonA">
-				<a href="<?php echo $Target->getAttackTraderHREF() ?>" class="buttonA">Continue Attack</a>
-			</div>
-		</div><?php
+		<div class="buttonA">
+			<a href="<?php echo $Target->getAttackTraderHREF(); ?>" class="buttonA">Continue&nbsp;Attack</a>
+		</div>><?php
 	}
 	else
 	{ ?>
@@ -15,11 +13,11 @@
 		<div class="buttonA"><?php
 			if($OverrideDeath)
 			{ ?>
-				<a href="<?php echo Globals::getPodScreenHREF() ?>" class="buttonA">Let there be pod</a><?php
+				<a href="<?php echo Globals::getPodScreenHREF(); ?>" class="buttonA">Let there be pod</a><?php
 			}
 			else
 			{ ?>
-				<a href="<?php echo Globals::getCurrentSectorHREF() ?>" class="buttonA">Current Sector</a><?php
+				<a href="<?php echo Globals::getCurrentSectorHREF(); ?>" class="buttonA">Current Sector</a><?php
 			} ?>
 		</div><?php
 	} ?>
