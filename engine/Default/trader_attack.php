3d542ac516dcd4293a756ca51298966e10ecc347
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-11-20 21:04:05 +0000

    Remove random position.

--- engine/Default/trader_attack.php
+++ engine/Default/trader_attack.php
@@ -7,8 +7,4 @@ if(isset($var['override_death']))
 	$template->assign('OverrideDeath',true);
 else
 	$template->assign('OverrideDeath',false);
-	
-if(!isset($var['RandomPosition']))
-	SmrSession::updateVar('RandomPosition',mt_rand(0,2));
-$template->assign('RandomPosition',$var['RandomPosition']);
 ?>
