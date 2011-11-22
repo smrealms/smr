a507bd7932c65f8bd37aa5dff995c5629c0eac71
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-11-22 00:40:52 +0000

    Fix adding notes.

--- engine/Default/note_add_processing.php
+++ engine/Default/note_add_processing.php
@@ -11,18 +11,18 @@
  * 		Adds a new note into the database
  */
  
-if(isset($_POST['note']))
+if(isset($_REQUEST['note']))
 {
-	if(strlen($_POST['note']) < 1000)
-	{
-		$note = htmlentities($note,ENT_QUOTES,'utf-8');
-		$note = nl2br($note);
-		$db->query('INSERT INTO player_has_notes (account_id,game_id,note) VALUES(' .
-			$player->getAccountID() . ',' .
-			$player->getGameID() . ',' .
-			$db->escapeBinary(gzcompress($note)) .
-			')');
-	}
+	$note = $_REQUEST['note'];
+	if(strlen($note) > 1000)
+		create_error('Note cannot be longer than 1000 characters.');
+
+	$note = htmlentities($note,ENT_QUOTES,'utf-8');
+	$note = nl2br($note);
+	$db->query('INSERT INTO player_has_notes (account_id,game_id,note) VALUES(' .
+		$db->escapeNumber($player->getAccountID()) . ',' .
+		$db->escapeNumber($player->getGameID()) . ',' .
+		$db->escapeBinary(gzcompress($note)) . ')');
 }
 
 forward(create_container('skeleton.php', 'trader_status.php'));
