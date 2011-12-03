62892c6e32a83ba7009743610b8ea869c4c67721
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-12-03 18:18:36 +0000

    Add script to increase planet construction cost as per RaF.

---
+++ db/update construction costs.sql
@@ -0,0 +1,3 @@
+UPDATE smr_new.planet_cost_credits SET amount =  '100000' WHERE planet_cost_credits.construction_id = 1;
+UPDATE smr_new.planet_cost_credits SET amount = '1000000' WHERE planet_cost_credits.construction_id = 3;
+UPDATE smr_new.planet_cost_credits SET amount =  '100000' WHERE planet_cost_credits.construction_id = 2;
