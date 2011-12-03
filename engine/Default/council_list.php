b98585b9108861d494897ce72da4c7a8315d6bc8
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-12-03 21:59:33 +0000

    Remove NPCs from racial councils.

--- engine/Default/council_list.php
+++ engine/Default/council_list.php
@@ -63,15 +63,8 @@ if (is_object($president))
 
 $PHP_OUTPUT.=('<br /><br /><div align="center" class="bold">Member</div>');
 
-$db->query('SELECT account_id FROM player ' .
-		   'WHERE game_id = '.$player->getGameID().' AND ' .
-				 'race_id = '.$race_id.' ' .
-		   'ORDER by experience DESC ' .
-		   'LIMIT ' . MAX_COUNCIL_MEMBERS);
-		   
-if ($db->getNumRows() > 0)
-{
-
+$councilMembers = Council::getRaceCouncil($player->getGameID(), $race_id);
+if(count($councilMembers) > 0) {
 	$PHP_OUTPUT.=('<p><table class="standard" align="center" width="85%">');
 	$PHP_OUTPUT.=('<tr>');
 	$PHP_OUTPUT.=('<th>&nbsp;</th>');
@@ -81,67 +74,65 @@ if ($db->getNumRows() > 0)
 	$PHP_OUTPUT.=('<th>Experience</th>');
 	$PHP_OUTPUT.=('</tr>');
 
-	$count = 0;
-	while ($db->nextRecord()) {
-
-		$council =& SmrPlayer::getPlayer($db->getField('account_id'), $player->getGameID());
-		$count++;
+	foreach($councilMembers as $count => $accountID) {
+		$councilPlayer =& SmrPlayer::getPlayer($accountID, $player->getGameID());
 
 		$PHP_OUTPUT.=('<tr>');
-
 		$PHP_OUTPUT.=('<td align="center"');
-		if ($council->getAccountID() == $player->getAccountID())
+		if ($councilPlayer->getAccountID() == $player->getAccountID()) {
 			$PHP_OUTPUT.=(' class="bold"');
+		}
 		$PHP_OUTPUT.=('>'.$count.'.</td>');
 
 		$PHP_OUTPUT.=('<td valign="middle"');
-		if ($council->getAccountID() == $player->getAccountID())
+		if ($councilPlayer->getAccountID() == $player->getAccountID()) {
 			$PHP_OUTPUT.=(' class="bold"');
-		$PHP_OUTPUT.=('>'.$council->getLevelName().' ');
+		}
+		$PHP_OUTPUT.=('>'.$councilPlayer->getLevelName().' ');
 		$container = array();
 		$container['url']		= 'skeleton.php';
 		$container['body']		= 'trader_search_result.php';
-		$container['player_id']	= $council->getPlayerID();
-		$PHP_OUTPUT.=create_link($container, $council->getDisplayName());
+		$container['player_id']	= $councilPlayer->getPlayerID();
+		$PHP_OUTPUT.=create_link($container, $councilPlayer->getDisplayName());
 		$PHP_OUTPUT.=('</td>');
 
 		$PHP_OUTPUT.=('<td align="center"');
-		if ($council->getAccountID() == $player->getAccountID())
+		if ($councilPlayer->getAccountID() == $player->getAccountID()) {
 			$PHP_OUTPUT.=(' class="bold"');
+		}
 		$PHP_OUTPUT.=('>');
 		$container = array();
 		$container['url'] = 'skeleton.php';
 		$container['body'] = 'council_send_message.php';
-		$container['race_id'] = $council->getRaceID();
-		$PHP_OUTPUT.=create_link($container, $player->getColouredRaceName($council->getRaceID()));
+		$container['race_id'] = $councilPlayer->getRaceID();
+		$PHP_OUTPUT.=create_link($container, $player->getColouredRaceName($councilPlayer->getRaceID()));
 		$PHP_OUTPUT.=('</td>');
 
 		$PHP_OUTPUT.=('<td');
-		if ($council->getAccountID() == $player->getAccountID())
+		if ($councilPlayer->getAccountID() == $player->getAccountID())
 			$PHP_OUTPUT.=(' class="bold"');
 		$PHP_OUTPUT.=('>');
-		if ($council->hasAlliance())
-		{
-			$PHP_OUTPUT.=create_link($council->getAllianceRosterHREF(), $council->getAllianceName());
-		} else
+		if ($councilPlayer->hasAlliance()) {
+			$PHP_OUTPUT.=create_link($councilPlayer->getAllianceRosterHREF(), $councilPlayer->getAllianceName());
+		}
+		else {
 			$PHP_OUTPUT.=('(none)');
+		}
 		$PHP_OUTPUT.=('</td>');
 
 		$PHP_OUTPUT.=('<td align="right"');
-		if ($council->getAccountID() == $player->getAccountID())
+		if ($councilPlayer->getAccountID() == $player->getAccountID()) {
 			$PHP_OUTPUT.=(' class="bold"');
-		$PHP_OUTPUT.=('>'.$council->getExperience().'</td>');
+		}
+		$PHP_OUTPUT.=('>'.$councilPlayer->getExperience().'</td>');
 
 		$PHP_OUTPUT.=('</tr>');
-
 	}
-
 	$PHP_OUTPUT.=('</table></p>');
-
-
-} else
+}
+else {
 	$PHP_OUTPUT.=('<div align="center">This council doesn\'t have any members!</div>');
-
+}
 $PHP_OUTPUT.=('<p>&nbsp;</p>');
 
 $PHP_OUTPUT.=('<b>View Council</b><br />');
@@ -156,7 +147,6 @@ foreach($races as $raceID => $raceInfo)
 
 	$PHP_OUTPUT.=create_link($container, '<span style="font-size:75%;">'.$raceInfo['Race Name'].'</span>');
 	$PHP_OUTPUT.=('<br />');
-
 }
 
 ?>
