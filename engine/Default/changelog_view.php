f0fa74d2fd141ae0af441039f848019a56205954
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-12-20 18:56:47 +0000

    Formatting and query escaping.

--- engine/Default/changelog_view.php
+++ engine/Default/changelog_view.php
@@ -6,19 +6,20 @@ $db2 = new SmrMySqlDatabase();
 
 $db->query('SELECT *
 			FROM version
-			WHERE version_id <= ' . $var['version_id'] . '
+			WHERE version_id <= ' . $db->escapeNumber($var['version_id']) . '
 			ORDER BY version_id DESC');
-while ($db->nextRecord())
-{
-	$version_id = $db->getField('version_id');
-	$version = $db->getField('major_version') . '.' . $db->getField('minor_version') . '.' . $db->getField('patch_level');
-	$went_live = $db->getField('went_live');
+while ($db->nextRecord()) {
+	$version_id = $db->getInt('version_id');
+	$version = $db->getInt('major_version') . '.' . $db->getInt('minor_version') . '.' . $db->getInt('patch_level');
+	$went_live = $db->getInt('went_live');
 
 	// get human readable format for date
-	if ($went_live > 0)
+	if ($went_live > 0) {
 		$went_live = date(DATE_FULL_SHORT, $went_live);
-	else
+	}
+	else {
 		$went_live = 'never';
+	}
 
 	$PHP_OUTPUT.=('<b><small>'.$version.' ('.$went_live.'):</small></b>');
 
@@ -26,13 +27,13 @@ while ($db->nextRecord())
 
 	$db2->query('SELECT *
 				FROM changelog
-				WHERE version_id = '.$version_id.'
+				WHERE version_id = ' . $db->escapeNumber($version_id) . '
 				ORDER BY changelog_id');
-	while ($db2->nextRecord())
-		$PHP_OUTPUT.=('<li>' . stripslashes($db2->getField('change_title')) . '<br /><small>' . stripslashes($db2->getField('change_message')) . '</small></li>');
+	while ($db2->nextRecord()) {
+		$PHP_OUTPUT.=('<li>' . $db2->getField('change_title') . '<br /><small>' . $db2->getField('change_message') . '</small></li>');
+	}
 
 	$PHP_OUTPUT.=('</ul><br />');
-
 }
 
 ?>
