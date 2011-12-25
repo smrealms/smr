6849cba6db6f6dd9605aa494475e999b5e994e20
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2011-12-25 10:55:41 +0000

    Formatting.

--- engine/Default/planet_land_processing.php
+++ engine/Default/planet_land_processing.php
@@ -1,24 +1,30 @@
 <?php
 
 // is account validated?
-if (!$account->isValidated())
+if (!$account->isValidated()) {
 	create_error('You are not validated so you can\'t land on a planet.');
+}
 
 // do we have enough turns?
-if ($player->getTurns() == 0)
+if ($player->getTurns() == 0) {
 	create_error('You don\'t have enough turns to land on planet.');
+}
 	
 if ($player->hasAlliance()) {
 	$db->query('SELECT * FROM player_has_alliance_role WHERE account_id = ' . $db->escapeNumber($player->getAccountID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND alliance_id=' . $db->escapeNumber($player->getAllianceID()));
-	if ($db->nextRecord()) $role_id = $db->getField('role_id');
-	else $role_id = 0;
+	if ($db->nextRecord()) {
+		$role_id = $db->getField('role_id');
+	}
+	else {
+		$role_id = 0;
+	}
 	$db->query('SELECT * FROM alliance_has_roles WHERE alliance_id = ' . $db->escapeNumber($player->getAllianceID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND role_id = '.$role_id);
 	$db->nextRecord();
-	if ($db->getField('planet_access') == 'FALSE') {
+	if (!$db->getBoolean('planet_access')) {
 		$db->query('SELECT owner_id FROM planet WHERE sector_id = ' . $db->escapeNumber($player->getSectorID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()) . ' LIMIT 1');
-		$db->nextRecord();
-		if ($db->getField('owner_id') != 0 && $db->getField('owner_id') != $player->getAccountID())
+		if ($db->nextRecord() && $db->getInt('owner_id') != 0 && $db->getInt('owner_id') != $player->getAccountID()) {
 			create_error('Your alliance doesn\'t allow you to dock at their planet.');
+		}
 	}
 }
 $player->setLandedOnPlanet(true);
