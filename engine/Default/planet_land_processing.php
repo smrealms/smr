4cacbf69424e3f9e0549f2a8c0f986497d7d5046
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2012-01-07 23:44:49 +0000

    Do not allow landing on a planet with newbie turns.

--- engine/Default/planet_land_processing.php
+++ engine/Default/planet_land_processing.php
@@ -9,6 +9,10 @@ if (!$account->isValidated()) {
 if ($player->getTurns() == 0) {
 	create_error('You don\'t have enough turns to land on planet.');
 }
+
+if($player->hasNewbieTurns()) {
+	create_error('You cannot land on a planet whilst under newbie protection.');
+}
 	
 if ($player->hasAlliance()) {
 	$db->query('SELECT * FROM player_has_alliance_role WHERE account_id = ' . $db->escapeNumber($player->getAccountID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND alliance_id=' . $db->escapeNumber($player->getAllianceID()));
