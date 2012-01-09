1d201e0ecafc8b08762452cdebe53559ae3f5bf2
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2012-01-09 12:39:52 +0000

    Add linebreaks.

--- tools/rerunChessGames.php
+++ tools/rerunChessGames.php
@@ -12,11 +12,11 @@ $db->query('SELECT chess_game_id FROM chess_game');
 while($db->nextRecord()) {
 	$chessGameID = $db->getInt('chess_game_id');
 	$game = new ChessGame($chessGameID);
-	echo 'Running game ' . $chessGameID . ' for white id "' . $game->getWhiteID() . '", black id "' . $game->getBlackID() .'", winner "' . $game->getWinner() . '"';
-	echo 'Moves: ' . $game->getMoves();
+	echo 'Running game ' . $chessGameID . ' for white id "' . $game->getWhiteID() . '", black id "' . $game->getBlackID() .'", winner "' . $game->getWinner() . '"' . EOL;
+	echo 'Moves: ' . $game->getMoves() . EOL . EOL;
 	$game->rerunGame(true);
-	echo 'Finished game ' . $chessGameID . ' for white id "' . $game->getWhiteID() . '", black id "' . $game->getBlackID() .'", winner "' . $game->getWinner() . '"';
-	echo 'Moves: ' . $game->getMoves();
+	echo 'Finished game ' . $chessGameID . ' for white id "' . $game->getWhiteID() . '", black id "' . $game->getBlackID() .'", winner "' . $game->getWinner() . '"' . EOL;
+	echo 'Moves: ' . $game->getMoves() . EOL . EOL;
 }
 
 ?>
