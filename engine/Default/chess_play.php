95927e25633f006b1f958c9b71d0447a979837de
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2012-01-14 18:11:57 +0000

    Refresh chess after a move submit using inbuilt SMR refresh, moves chess.js into ajax.js in order to support this.

--- engine/Default/chess_play.php
+++ engine/Default/chess_play.php
@@ -1,5 +1,5 @@
 <?php
 require_once(get_file_loc('ChessGame.class.inc'));
 $template->assign('ChessGame',new ChessGame($var['ChessGameID']));
-$template->assign('ChessMoveHREF',SmrSession::get_new_href(create_container('chess_move_processing.php','chess_move.php',array('AJAX' => true, 'ChessGameID' => $var['ChessGameID']))));
+$template->assign('ChessMoveHREF',SmrSession::get_new_href(create_container('chess_move_processing.php','',array('AJAX' => true, 'ChessGameID' => $var['ChessGameID']))));
 ?>
