1832e9ca99fd520ffd9e4af49af9eda8afb22783
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2012-01-14 01:10:38 +0000

    Arg, this is php not javascript damnit, we use .= here.

--- engine/Default/chat_rules.php
+++ engine/Default/chat_rules.php
@@ -6,7 +6,7 @@ $autoChannels = '#SMR';
 if($player->hasAlliance()) {
 	$allianceChan = $player->getAlliance()->getIrcChannel();
 	if($allianceChan != '') {
-		$autoChannels += ',' . urlencode($allianceChan);
+		$autoChannels .= ',' . urlencode($allianceChan);
 	}
 }
 $template->assign('AutoChannels', $autoChannels);
