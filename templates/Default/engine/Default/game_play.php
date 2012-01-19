a2970dc4f43959a6def56264e3418ca231719cc5
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2012-01-19 23:13:35 +0000

    Add show/hide button for previous games list.

--- templates/Default/engine/Default/game_play.php
+++ templates/Default/engine/Default/game_play.php
@@ -11,44 +11,46 @@ if(isset($Message))
 <a href="<?php echo $ThisAccount->getUserRankingHREF(); ?>"><b class="yellow">Rankings</b></a>
 <br />You are ranked as <?php $this->doAn($ThisAccount->getRankName()); ?> <span style="font-size:125%;color:greenyellow;"><?php echo $UserRankName ?></span> player.<br /><br />
 
-<?php
-if(isset($Games['Play']))
-{ ?>
-	<table class="standard">
-		<tr>
-			<th align="center">&nbsp;</th>
-			<th align="center">Game Name</th>
-			<th align="center">Turns</th>
-			<th align="center">Playing</th>
-			<th align="center">Last Active</th>
-			<th align="center">Last Movement</th>
-			<th align="center">End Date</th>
-			<th align="center">Game Type</th>
-			<th align="center">Game Speed</th>
-		</tr><?php
-		foreach($Games['Play'] as $Game)
-		{ ?>
+<div id="playGames" class="ajax"><?php
+	if(isset($Games['Play']))
+	{ ?>
+		<table class="standard">
 			<tr>
-				<td>
-					<div class="buttonA">
-						<a id="game_play_<?php echo $Game['ID']; ?>" class="buttonA" href="<?php echo $Game['PlayGameLink']; ?>">&nbsp;Play Game&nbsp;</a>
-					</div>
-				</td>
-				<td width="35%"><a href="<?php echo $Game['GameStatsLink']; ?>"><?php echo $Game['Name']; ?> (<?php echo $Game['ID']; ?>)</a></td>
-				
-				<td><?php echo $Game['Maintenance']; ?></td>
-				<td><?php echo $Game['NumberPlaying']; ?></td>
-				<td><?php echo $Game['LastActive']; ?></td>
-				<td><?php echo $Game['LastMovement']; ?></td>
-				<td class="noWrap"><?php echo $Game['EndDate']; ?></td>
-				<td><?php echo $Game['Type']; ?></td>
-				<td><?php echo $Game['Speed']; ?></td>
+				<th align="center">&nbsp;</th>
+				<th align="center">Game Name</th>
+				<th align="center">Turns</th>
+				<th align="center">Playing</th>
+				<th align="center">Last Active</th>
+				<th align="center">Last Movement</th>
+				<th align="center">End Date</th>
+				<th align="center">Game Type</th>
+				<th align="center">Game Speed</th>
 			</tr><?php
-		} ?>
-	</table><br />
-	<br /><?php
-}
-?><h1><a href="<?php echo $VotingHref; ?>">Voting</a></h1><?php
+			foreach($Games['Play'] as $Game)
+			{ ?>
+				<tr>
+					<td>
+						<div class="buttonA">
+							<a id="game_play_<?php echo $Game['ID']; ?>" class="buttonA" href="<?php echo $Game['PlayGameLink']; ?>">&nbsp;Play Game&nbsp;</a>
+						</div>
+					</td>
+					<td width="35%"><a href="<?php echo $Game['GameStatsLink']; ?>"><?php echo $Game['Name']; ?> (<?php echo $Game['ID']; ?>)</a></td>
+
+					<td><?php echo $Game['Maintenance']; ?></td>
+					<td><?php echo $Game['NumberPlaying']; ?></td>
+					<td><?php echo $Game['LastActive']; ?></td>
+					<td><?php echo $Game['LastMovement']; ?></td>
+					<td class="noWrap"><?php echo $Game['EndDate']; ?></td>
+					<td><?php echo $Game['Type']; ?></td>
+					<td><?php echo $Game['Speed']; ?></td>
+				</tr><?php
+			} ?>
+		</table><br />
+		<br /><?php
+	} ?>
+</div>
+		
+<h1><a href="<?php echo $VotingHref; ?>">Voting</a></h1><?php
 if (isset($Voting))
 {
 	?>Please take a couple of seconds to answer the following question(s) for the SMR Admin team. Thanks!<?php
@@ -65,41 +67,43 @@ if (isset($Voting))
 		</form><?php
 	}
 } ?><br />
-<h1>Join Game</h1><?php
-if(isset($Games['Join']))
-{ ?>
-	<table class="standard">
-		<tr>
-			<th align="center">&nbsp;</th>
-			<th width="150">Game Name</th>
-			<th>Start Date</th>
-			<th>End Date</th>
-			<th>Max Players</th>
-			<th>Type</th>
-			<th>Game Speed</th>
-			<th>Credits Needed</th>
-		</tr><?php
-		foreach($Games['Join'] as $Game)
-		{ ?>
+<h1>Join Game</h1>
+<div id="joinGames" class="ajax"><?php
+	if(isset($Games['Join']))
+	{ ?>
+		<table class="standard">
 			<tr>
-				<td>
-					<div class="buttonA"><a id="game_join_<?php echo $Game['ID']; ?>" class="buttonA" href="<?php echo $Game['JoinGameLink']; ?>">&nbsp;<?php if(TIME < $Game['StartDate']) {?>View Info<?php }else{ ?>Join Game<?php } ?>&nbsp;</a></div>
-				</td>
-				<td width="35%"><?php echo $Game['Name']; ?> (<?php echo $Game['ID']; ?>)</td>
-				<td class="noWrap"><?php echo $Game['StartDate']; ?></td>
-				<td class="noWrap"><?php echo $Game['EndDate']; ?></td>
-				<td><?php echo $Game['MaxPlayers']; ?></td>
-				<td><?php echo $Game['Type']; ?></td>
-				<td><?php echo $Game['Speed']; ?></td>
-				<td><?php echo $Game['Credits']; ?></td>
-			</tr>
-		<?php } ?>
-	</table><?php
-}
-else
-{
-	?><p>You have joined all open games.</p><?php
-} ?>
+				<th align="center">&nbsp;</th>
+				<th width="150">Game Name</th>
+				<th>Start Date</th>
+				<th>End Date</th>
+				<th>Max Players</th>
+				<th>Type</th>
+				<th>Game Speed</th>
+				<th>Credits Needed</th>
+			</tr><?php
+			foreach($Games['Join'] as $Game)
+			{ ?>
+				<tr>
+					<td>
+						<div class="buttonA"><a id="game_join_<?php echo $Game['ID']; ?>" class="buttonA" href="<?php echo $Game['JoinGameLink']; ?>">&nbsp;<?php if(TIME < $Game['StartDate']) {?>View Info<?php }else{ ?>Join Game<?php } ?>&nbsp;</a></div>
+					</td>
+					<td width="35%"><?php echo $Game['Name']; ?> (<?php echo $Game['ID']; ?>)</td>
+					<td class="noWrap"><?php echo $Game['StartDate']; ?></td>
+					<td class="noWrap"><?php echo $Game['EndDate']; ?></td>
+					<td><?php echo $Game['MaxPlayers']; ?></td>
+					<td><?php echo $Game['Type']; ?></td>
+					<td><?php echo $Game['Speed']; ?></td>
+					<td><?php echo $Game['Credits']; ?></td>
+				</tr>
+			<?php } ?>
+		</table><?php
+	}
+	else
+	{
+		?><p>You have joined all open games.</p><?php
+	} ?>
+</div>
 <br />
 <br />
 <h1>Donate Money</h1>
@@ -134,32 +138,35 @@ if(isset($AdminPermissions))
 	<br />
 	<br /><?php
 } ?>
-<h1>Previous Games</h1><?php
-if(isset($Games['Previous']))
-{ ?>
-	<table class="standard">
-		<tr>
-			<th width="150">Game Name</th>
-			<th>Start Date</th>
-			<th>End Date</th>
-			<th>Game Speed</th>
-			<th colspan="3">Options</th>
-		</tr><?php
-		foreach($Games['Previous'] as $Game)
-		{ ?>
+<h1>Previous Games</h1>
+<a onclick="$('#prevGames').slideToggle(600);">Show/Hide</a>
+<div id="prevGames" class="ajax" style="display:none;"><?php
+	if(isset($Games['Previous']))
+	{ ?>
+		<table class="standard">
 			<tr>
-				<td width="35%"><?php if(isset($Game['PreviousGameLink'])){ ?><a href="<?php echo $Game['PreviousGameLink']; ?>"><?php } echo $Game['Name']; ?> (<?php echo $Game['ID']; ?>)<?php if(isset($Game['PreviousGameLink'])){ ?></a><?php } ?></td>
-				<td><?php echo $Game['StartDate'] ?></td>
-				<td><?php echo $Game['EndDate'] ?></td>
-				<td><?php echo $Game['Speed'] ?></td>
-				<td><a href="<?php echo $Game['PreviousGameHOFLink']; ?>">Hall Of Fame</a></td>
-				<td><a href="<?php echo $Game['PreviousGameNewsLink']; ?>">Game News</a></td>
-				<td><?php if(isset($Game['PreviousGameStatsLink'])){ ?><a href="<?php echo $Game['PreviousGameStatsLink']; ?>">Game Stats</a><?php } ?></td>
-			</tr>
-		<?php } ?>
-	</table><?php
-}
-else
-{
-	?><p>There are no previous games.</p><?php
-} ?>
+				<th width="150">Game Name</th>
+				<th>Start Date</th>
+				<th>End Date</th>
+				<th>Game Speed</th>
+				<th colspan="3">Options</th>
+			</tr><?php
+			foreach($Games['Previous'] as $Game)
+			{ ?>
+				<tr>
+					<td width="35%"><?php if(isset($Game['PreviousGameLink'])){ ?><a href="<?php echo $Game['PreviousGameLink']; ?>"><?php } echo $Game['Name']; ?> (<?php echo $Game['ID']; ?>)<?php if(isset($Game['PreviousGameLink'])){ ?></a><?php } ?></td>
+					<td><?php echo $Game['StartDate'] ?></td>
+					<td><?php echo $Game['EndDate'] ?></td>
+					<td><?php echo $Game['Speed'] ?></td>
+					<td><a href="<?php echo $Game['PreviousGameHOFLink']; ?>">Hall Of Fame</a></td>
+					<td><a href="<?php echo $Game['PreviousGameNewsLink']; ?>">Game News</a></td>
+					<td><?php if(isset($Game['PreviousGameStatsLink'])){ ?><a href="<?php echo $Game['PreviousGameStatsLink']; ?>">Game Stats</a><?php } ?></td>
+				</tr>
+			<?php } ?>
+		</table><?php
+	}
+	else
+	{
+		?><p>There are no previous games.</p><?php
+	} ?>
+</div>
