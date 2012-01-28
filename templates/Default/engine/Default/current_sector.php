0cd8b47c9d818ff8e4a90ce7bc15bab08906fb96
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2012-01-28 21:52:34 +0000

    Add a new line after combat results.

--- templates/Default/engine/Default/current_sector.php
+++ templates/Default/engine/Default/current_sector.php
@@ -1,55 +1,44 @@
 <table class="fullwidth" style="border:none">
 	<tr>
-		<td style="padding:0px;vertical-align:top">
-			<?php $this->includeTemplate('includes/SectorNavigation.inc'); ?>
+		<td style="padding:0px;vertical-align:top"><?php
+			$this->includeTemplate('includes/SectorNavigation.inc'); ?>
 		</td>
 		<td style="padding:0px;vertical-align:top;width:32em;"><?php
 			$this->includeTemplate('includes/PlottedCourse.inc');
 			$this->includeTemplate('includes/Ticker.inc');
 			$this->includeTemplate('includes/Missions.inc'); ?>
 			<span id="secmess"><?php
-				if(isset($ErrorMessage))
-				{
-					echo $ErrorMessage ?><br /><?php
+				if(isset($ErrorMessage)) {
+					echo $ErrorMessage; ?><br /><?php
 				}
-				if(isset($ProtectionMessage))
-				{
-					echo $ProtectionMessage ?><br /><?php
+				if(isset($ProtectionMessage)) {
+					echo $ProtectionMessage; ?><br /><?php
 				}
-				if(isset($TurnsMessage))
-				{
-					echo $TurnsMessage ?><br /><?php
+				if(isset($TurnsMessage)) {
+					echo $TurnsMessage; ?><br /><?php
 				}
-				if(isset($TradeMessage))
-				{
-					echo $TradeMessage ?><br /><?php
+				if(isset($TradeMessage)) {
+					echo $TradeMessage; ?><br /><?php
 				}
-				if(isset($ForceRefreshMessage))
-				{
-					echo $ForceRefreshMessage ?><br /><?php
+				if(isset($ForceRefreshMessage)) {
+					echo $ForceRefreshMessage; ?><br /><?php
 				}
-				if(isset($AttackResults))
-				{
-					if($AttackResultsType=='PLAYER')
-					{
+				if(isset($AttackResults)) {
+					if($AttackResultsType=='PLAYER') {
 						$this->includeTemplate('includes/TraderFullCombatResults.inc',array('TraderCombatResults'=>$AttackResults,'MinimalDisplay'=>true));
 					}
-					else if($AttackResultsType=='FORCE')
-					{
+					else if($AttackResultsType=='FORCE') {
 						$this->includeTemplate('includes/ForceFullCombatResults.inc',array('FullForceCombatResults'=>$AttackResults,'MinimalDisplay'=>true));
 					}
-					else if($AttackResultsType=='PORT')
-					{
+					else if($AttackResultsType=='PORT') {
 						$this->includeTemplate('includes/PortFullCombatResults.inc',array('FullPortCombatResults'=>$AttackResults,'MinimalDisplay'=>true));
 					}
-					else if($AttackResultsType=='PLANET')
-					{
+					else if($AttackResultsType=='PLANET') {
 						$this->includeTemplate('includes/PlanetFullCombatResults.inc',array('FullPlanetCombatResults'=>$AttackResults,'MinimalDisplay'=>true));
-					}
+					} ?><br /><?php
 				}
-				if(isset($VarMessage))
-				{
-					echo $VarMessage ?><br /><?php
+				if(isset($VarMessage)) {
+					echo $VarMessage; ?><br /><?php
 				} ?>
 			</span>
 		</td>
