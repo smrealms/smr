edb47f52f1012d307ae9411fa0ff7bedb33ac46d
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2012-03-10 16:47:12 +0000

    Add the ship changes script for current round.

---
+++ db/archive/ship_changes_4.sql
@@ -0,0 +1,4 @@
+UPDATE smr_new.ship_type SET cost = 12026598 WHERE ship_type_id = 21; -- Federal Warrant to 12,026,598 credits
+UPDATE smr_new.ship_type SET cost = 23675738 WHERE ship_type_id = 22; -- Federal Ultimatum to 23,675,738 credits
+UPDATE smr_new.ship_type SET cost = 7483452 WHERE ship_type_id = 24; -- Assasin to 7,483,452 credits
+UPDATE smr_new.ship_type SET cost = 19890100 WHERE ship_type_id = 25; -- Death Cruiser to 19,890,100 credits
