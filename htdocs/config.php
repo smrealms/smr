1d2c40c32e8cdfda1baf11cc6c5edd68ed0178c6
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2012-04-21 22:21:56 +0000

    Make vets only start with 250 newbie turns at the start of a round.

--- htdocs/config.php
+++ htdocs/config.php
@@ -246,6 +246,9 @@ define('BOND_TIME',172800);
  * Miscellaneous definitions
  */
 
+define('STARTING_NEWBIE_TURNS_NEWBIE', 500);
+define('STARTING_NEWBIE_TURNS_VET', 250);
+
 define('NEWBIE', 1);
 define('BEGINNER', 2);
 define('FLEDGLING', 3);
