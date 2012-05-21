c5dcaedb37888b9d58e2fd55384d00c226c1c7a2
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2012-05-21 13:51:27 +0000

    Move reusable functions into a lib file.

--- engine/Default/rankings_player_experience.php
+++ engine/Default/rankings_player_experience.php
@@ -2,6 +2,7 @@
 
 $template->assign('PageTopic','Experience Rankings');
 
+require_once(get_file_loc('Rankings.inc'));
 require_once(get_file_loc('menu.inc'));
 create_ranking_menu(0, 0);
 
@@ -34,57 +35,4 @@ $template->assign('FilterRankingsHREF', SmrSession::getNewHREF($container));
 $lowerLimit = $var['MinRank'] - 1;
 $db->query('SELECT account_id, experience value FROM player WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' ORDER BY experience DESC, player_name LIMIT ' . $lowerLimit . ', ' . ($var['MaxRank'] - $lowerLimit));
 $template->assignByRef('FilteredRankings', collectRankings($db, $lowerLimit));
-
-function collectRankings(&$db, $player, $rank) {
-	$rankings = array();
-	while ($db->nextRecord()) {
-		// increase rank counter
-		$rank++;
-		$currentPlayer =& SmrPlayer::getPlayer($db->getInt('account_id'), $player->getGameID());
-
-		$class='';
-		if ($player->equals($currentPlayer)) {
-			$class .= 'bold';
-		}
-		if($currentPlayer->getAccount()->isNewbie()) {
-			$class.= ' newbie';
-		}
-		if($class!='') {
-			$class = ' class="'.trim($class).'"';
-		}
-
-		$rankings[$rank] = array(
-			'Rank' => $rank,
-			'Player' => &$currentPlayer,
-			'Class' => $class,
-			'Value' => number_format($db->getInt('value'))
-		);
-	}
-	return $rankings;
-}
-
-function calculateMinMaxRanks($ourRank, $totalPlayers) {
-	if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'Show' && is_numeric($_REQUEST['min_rank']) && is_numeric($_REQUEST['max_rank'])) {
-		$minRank = min($_REQUEST['min_rank'], $_REQUEST['max_rank']);
-		$maxRank = max($_REQUEST['min_rank'], $_REQUEST['max_rank']);
-	}
-	elseif(isset($var['MinRank']) && isset($var['MaxRank'])) {
-		$minRank = $var['MinRank'];
-		$maxRank = $var['MaxRank'];
-	}
-	else {
-		$minRank = $ourRank - 5;
-		$maxRank = $ourRank + 5;
-	}
-
-	if ($minRank <= 0 || $minRank > $totalPlayers) {
-		$minRank = 1;
-		$maxRank = 10;
-	}
-
-	$maxRank = min($maxRank, $totalPlayers);
-
-	SmrSession::updateVar('MinRank',$minRank);
-	SmrSession::updateVar('MaxRank',$maxRank);
-}
 ?>
