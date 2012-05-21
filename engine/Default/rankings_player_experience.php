4fc5a12a8fb21e7298ae905c18f146ed80b8cdbd
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2012-05-21 13:53:36 +0000

    Update the reference to the functions.

--- engine/Default/rankings_player_experience.php
+++ engine/Default/rankings_player_experience.php
@@ -23,9 +23,9 @@ $totalPlayers = $player->getGame()->getTotalPlayers();
 $template->assign('TotalPlayers', $totalPlayers);
 
 $db->query('SELECT account_id, experience value FROM player WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' ORDER BY experience DESC, player_name LIMIT 10');
-$template->assignByRef('Rankings', collectRankings($db, 0));
+$template->assignByRef('Rankings', Rankings::collectRankings($db, 0));
 
-calculateMinMaxRanks($ourRank, $totalPlayers);
+Rankings::calculateMinMaxRanks($ourRank, $totalPlayers);
 $template->assign('MaxRank', $var['MaxRank']);
 $template->assign('MinRank', $var['MinRank']);
 
@@ -34,5 +34,5 @@ $template->assign('FilterRankingsHREF', SmrSession::getNewHREF($container));
 
 $lowerLimit = $var['MinRank'] - 1;
 $db->query('SELECT account_id, experience value FROM player WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' ORDER BY experience DESC, player_name LIMIT ' . $lowerLimit . ', ' . ($var['MaxRank'] - $lowerLimit));
-$template->assignByRef('FilteredRankings', collectRankings($db, $lowerLimit));
+$template->assignByRef('FilteredRankings', Rankings::collectRankings($db, $lowerLimit));
 ?>
