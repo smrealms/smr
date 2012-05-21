90236596345921fef48f060ba1d2c20202168b67
page <page@6baec04b-8328-0410-a317-b1918b2a28f2>
2012-05-21 14:21:00 +0000

    Assign template variables within the shared function.

--- engine/Default/rankings_player_experience.php
+++ engine/Default/rankings_player_experience.php
@@ -23,16 +23,13 @@ $totalPlayers = $player->getGame()->getTotalPlayers();
 $template->assign('TotalPlayers', $totalPlayers);
 
 $db->query('SELECT account_id, experience value FROM player WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' ORDER BY experience DESC, player_name LIMIT 10');
-$template->assignByRef('Rankings', Rankings::collectRankings($db, 0));
+$template->assign('Rankings', Rankings::collectRankings($db, 0));
 
 Rankings::calculateMinMaxRanks($ourRank, $totalPlayers);
-$template->assign('MaxRank', $var['MaxRank']);
-$template->assign('MinRank', $var['MinRank']);
 
-$container = create_container('skeleton.php', 'rankings_player_experience.php');
-$template->assign('FilterRankingsHREF', SmrSession::getNewHREF($container));
+$template->assign('FilterRankingsHREF', SmrSession::getNewHREF(create_container('skeleton.php', 'rankings_player_experience.php')));
 
 $lowerLimit = $var['MinRank'] - 1;
 $db->query('SELECT account_id, experience value FROM player WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' ORDER BY experience DESC, player_name LIMIT ' . $lowerLimit . ', ' . ($var['MaxRank'] - $lowerLimit));
-$template->assignByRef('FilteredRankings', Rankings::collectRankings($db, $lowerLimit));
+$template->assign('FilteredRankings', Rankings::collectRankings($db, $lowerLimit));
 ?>
