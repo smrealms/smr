03c118b56b81ddc52147201bd57f0e984de6cfea
Page <pjgazzard@googlemail.com>
2012-06-02 22:07:34 +0100

    No need to set a variable for these hardcoded values.

--- admin/Default/box_reply_processing.php
+++ admin/Default/box_reply_processing.php
@@ -14,10 +14,9 @@ if(!empty($message)) {
 	SmrPlayer::sendMessageFromAdmin($var['game_id'], $var['sender_id'], $_REQUEST['message']);
 	//do we have points?
 	if ($_REQUEST['BanPoints']) {
-		$reasonID = 7;
 		$suspicion = 'Inappropriate Actions';
 		$senderAccount =& SmrAccount::getAccount($var['sender_id']);
-		$senderAccount->addPoints($_REQUEST['BanPoints'],$account,$reasonID,$suspicion);
+		$senderAccount->addPoints($_REQUEST['BanPoints'],$account,7,$suspicion);
 	}
 }
 forward(create_container('skeleton.php', 'box_view.php'));
