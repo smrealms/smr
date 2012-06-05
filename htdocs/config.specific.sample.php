673e2b37416296b4ae1d01fecca42c055605a9f1
Page <pjgazzard@googlemail.com>
2012-06-05 15:33:30 +0100

    Move the 3rd party config stuff into main config file.

--- htdocs/config.specific.sample.php
+++ htdocs/config.specific.sample.php
@@ -18,6 +18,12 @@ define('RECAPTCHA_PRIVATE','');
 define('SMS_DEBUG', '1');
 define('SMS_GATEWAY_KEY','');
 
+define('FACEBOOK_APP_ID','');
+define('FACEBOOK_APP_SECRET','');
+
+define('TWITTER_CONSUMER_KEY','');
+define('TWITTER_CONSUMER_SECRET','');
+
 $COMPATIBILITY_DATABASES = array('Game' => array('SmrClassicMySqlDatabase'=>array('GameType'=>'1.2','Column'=>'old_account_id'),'Smr12MySqlDatabase'=>array('GameType'=>'1.2','Column'=>'old_account_id2')),
 													'History' => array('SmrClassicHistoryMySqlDatabase'=>array('GameType'=>'1.2'),'Smr12HistoryMySqlDatabase'=>array('GameType'=>'1.2')));
 ?>
