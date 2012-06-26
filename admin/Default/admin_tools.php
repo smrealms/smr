4fed6beeb626914f5dd2d472209f365efba18fa2
Ben C <ben@corkumnet.com>
2012-06-26 09:29:25 -0400

    Moved admin tools link to left panel.

---
+++ admin/Default/admin_tools.php
@@ -0,0 +1,17 @@
+<?php
+
+// ***************************************
+// ** Admin Functions
+// ***************************************
+$db->query('SELECT * FROM account_has_permission JOIN permission USING (permission_id) WHERE account_id = '.$db->escapeNumber($account->getAccountID()));
+
+if ($db->getNumRows()) {
+	$adminPermissions = array();
+	while ($db->nextRecord()) {
+		$adminPermissions[] = array( 'PermissionLink' => $db->getField('link_to')?SmrSession::getNewHREF(create_container('skeleton.php',$db->getField('link_to'))):false,
+					'Name' => $db->getField('permission_name'));
+	}
+	$template->assign('AdminPermissions',$adminPermissions);
+}
+
+?>
