0e1ea763ce692431ac97b22eb34e0a22f26b5f3b
Page <pjgazzard@googlemail.com>
2012-06-27 19:47:41 +0100

    Safety checks for becoming a deputy/gang member.
    Add/improve checks that the HQ/UG location exists when entering and make
    sure you can only become a deputy at a HQ and a gang member at a UG.

--- engine/Default/government.php
+++ engine/Default/government.php
@@ -1,46 +1,38 @@
 <?php
 
 // check if our alignment is high enough
-if ($player->getAlignment() <= -100)
+if ($player->getAlignment() <= -100) {
 	create_error('You are not allowed to enter our Government HQ!');
-
-// get the name of this facility
-$db->query('SELECT 1 FROM location JOIN location_type USING(location_type_id) JOIN location_is_hq USING(location_type_id)
-			WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . '
-				AND sector_id = ' . $db->escapeNumber($player->getSectorID()) . '
-				AND location_type_id = '.$db->escapeNumber($var['LocationID']));
-if ($db->nextRecord()) {
-	$location =& SmrLocation::getLocation($var['LocationID']);
-	$raceID = $location->getRaceID();
 }
-else {
-	create_error('There is no headquarter. Obviously.');
-//	throw new Exception('Unable to find that hq.');
+if(!$player->getSector()->hasLocation($var['LocationID'])) {
+	create_error('That location does not exist in this sector');
 }
+$location =& SmrLocation::getLocation($var['LocationID']);
+if(!$location->isHQ()) {
+	create_error('There is no headquarters. Obviously.');
+}
+$raceID = $location->getRaceID();
 
 // are we at war?
-if ($player->getRelation($raceID) <= -300)
+if ($player->getRelation($raceID) <= -300) {
 	create_error('We are at WAR with your race! Get outta here before I call the guards!');
+}
 
-// topic
-if (isset($location))
-	$template->assign('PageTopic',$location->getName());
-else
-	$template->assign('PageTopic','Federal Headquarters');
+$template->assign('PageTopic','Federal Headquarters');
 
 // header menu
 require_once(get_file_loc('menu.inc'));
 create_hq_menu();
 
 $PHP_OUTPUT.='<div align="center">';
-if (isset($location_type_id)) {
+if ($raceID != RACE_NEUTRAL) {
 	$races =& Globals::getRaces();
 	$raceRelations =& Globals::getRaceRelations($player->getGameID(), $raceID);
 	$PHP_OUTPUT.=('We are at WAR with<br /><br />');
 	foreach($raceRelations as $otherRaceID => $relation) {
-		if ($relation <= -300)
+		if ($relation <= -300) {
 			$PHP_OUTPUT.=('<span class="red">The '.$races[$otherRaceID]['Race Name'].'<br /></span>');
-
+		}
 	}
 	$PHP_OUTPUT.=('<br />The government will PAY for the destruction of their ships!');
 }
