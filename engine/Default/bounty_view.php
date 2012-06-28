7d9f2629d1c65f65dbf5bd7f861294d46c4f9707
Page <pjgazzard@googlemail.com>
2012-06-28 23:41:16 +0100

    Split bounty view into a template.

--- engine/Default/bounty_view.php
+++ engine/Default/bounty_view.php
@@ -1,37 +1,5 @@
 <?php
-
-$bounties = 0;
-$id = $var['id'];
-$curr_player =& SmrPlayer::getPlayer($id, $player->getGameID());
-$template->assign('PageTopic','Viewing '.$curr_player->getPlayerName());
-
-if($curr_player->hasBounties()) {
-	$bounties = $curr_player->getBounties();
-	$hasBounty = false;
-	foreach($bounties as $bounty) {
-		if($bounty['Type'] == 'HQ') {
-			$PHP_OUTPUT.=('The <span class="green">Federal Government</span> is offering a bounty on '.$curr_player->getPlayerName().' worth <span class="creds">'.$bounty['Amount'].'</span> credits and <span class="yellow">'.$bounty['SmrCredits'].'</span> SMR credits.<br />');
-			if ($bounty['Claimer'] != 0) {
-				$claiming_player =& SmrPlayer::getPlayer($bounty['Claimer'], $player->getGameID());
-				$PHP_OUTPUT.=('This bounty can be claimed by '.$claiming_player->getPlayerName().'<br />');
-				$hasBounty = true;
-			}
-		}
-	}
-	if($hasBounty) {
-		$PHP_OUTPUT.=('<br /><br /><br />');
-	}
-	foreach($bounties as $bounty) {
-		if($bounty['Type'] == 'UG') {
-			$PHP_OUTPUT.=('The <span class="red">Underground</span> is offering a bounty on '.$curr_player->getPlayerName().' worth <span class="creds">'.$bounty['Amount'].'</span> credits and <span class="yellow">'.$bounty['SmrCredits'].'</span> SMR credits.<br />');
-			if ($bounty['Claimer'] != 0) {
-				$claiming_player =& SmrPlayer::getPlayer($bounty['Claimer'], $player->getGameID());
-				$PHP_OUTPUT.=('This bounty can be claimed by '.$claiming_player->getPlayerName().'<br />');
-			}
-		}
-	}
-}
-else {
-	$PHP_OUTPUT.=('This player has no bounties<br />');
-}
+$bountyPlayer =& SmrPlayer::getPlayer($var['id'], $player->getGameID());
+$template->assign('PageTopic', 'Viewing ' . $bountyPlayer->getPlayerName());
+$template->assignByRef('BountyPlayer', $bountyPlayer);
 ?>
---
+++ templates/Default/engine/Default/bounty_view.php
@@ -0,0 +1,28 @@
+<?php
+if($BountyPlayer->hasBounties()) {
+	$Bounties = $BountyPlayer->getBounties();
+	$HasHQBounty = false;
+	foreach($Bounties as $Bounty) {
+		if($Bounty['Type'] == 'HQ') { ?>
+			The <span class="green">Federal Government</span> is offering a bounty on <?php echo $BountyPlayer->getPlayerName(); ?> worth <span class="creds"><?php echo $Bounty['Amount']; ?></span> credits and <span class="yellow"><?php echo $Bounty['SmrCredits']; ?></span> SMR credits.<br /><?php
+			if ($Bounty['Claimer'] != 0) { ?>
+				This bounty can be claimed by <?php echo SmrPlayer::getPlayer($Bounty['Claimer'], $ThisPlayer->getGameID())->getPlayerName(); ?><br /><?php
+				$HasHQBounty = true;
+			}
+		}
+	}
+	if($HasHQBounty) {
+		?><br /><br /><br /><?php
+	}
+	foreach($Bounties as $Bounty) {
+		if($Bounty['Type'] == 'UG') { ?>
+			The <span class="red">Underground</span> is offering a bounty on <?php echo $BountyPlayer->getPlayerName(); ?> worth <span class="creds"><?php echo $Bounty['Amount']; ?></span> credits and <span class="yellow"><?php echo $Bounty['SmrCredits']; ?></span> SMR credits.<br /><?php
+			if ($Bounty['Claimer'] != 0) {
+				?>This bounty can be claimed by <?php echo SmrPlayer::getPlayer($Bounty['Claimer'], $ThisPlayer->getGameID())->getPlayerName(); ?><br /><?php
+			}
+		}
+	}
+}
+else {
+	?>This player has no bounties<br /><?php
+} ?>
