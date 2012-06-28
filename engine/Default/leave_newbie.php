febca414fe4ed6fb09c4a4f2c421bd2c6a629396
Page <pjgazzard@googlemail.com>
2012-06-28 22:43:26 +0100

    Split leaving newbie protection into a template.

--- engine/Default/leave_newbie.php
+++ engine/Default/leave_newbie.php
@@ -1,12 +1,3 @@
 <?php
-
 $template->assign('PageTopic','Leave Newbie Protection');
-
-$PHP_OUTPUT.=create_echo_form(create_container('leave_newbie_processing.php', ''));
-$PHP_OUTPUT.=('Do you really want to leave Newbie Protection?<br /><br />');
-$PHP_OUTPUT.=create_submit('Yes!');
-$PHP_OUTPUT.=('&nbsp;&nbsp;');
-$PHP_OUTPUT.=create_submit('No!');
-$PHP_OUTPUT.=('</form>');
-
 ?>
