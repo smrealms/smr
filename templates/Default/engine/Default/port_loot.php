7ec96be3132d772b13a86c49f06056cbb025e073
Page <pjgazzard@googlemail.com>
2012-06-28 23:00:35 +0100

    Simplify port looting template.

--- templates/Default/engine/Default/port_loot.php
+++ templates/Default/engine/Default/port_loot.php
@@ -1,25 +1,23 @@
-<div align="center">
-	<table class="standard">
-		<tr>
-			<th align="">Good</th>
-			<th align="center">Supply/Demand</th>
-			<th align="center">Base Price</th>
-			<th align="center">Amount on Ship</th>
-			<th align="center">Amount to Trade</th>
-			<th align="center">Action</th>
-		</tr><?php
-		$BoughtGoods =& $ThisPort->getVisibleGoodsBought($ThisPlayer);
-		foreach($BoughtGoods as &$Good) { ?>
-			<form method="POST" action="<?php echo $ThisPort->getLootGoodHREF($Good); ?>">
-				<tr>
-					<td align="center"><?php echo $Good['Name']; ?></td>
-					<td align="center"><?php echo $Good['Amount']; ?></td>
-					<td align="center"><?php echo $Good['BasePrice']; ?></td>
-					<td align="center"><?php echo $ThisShip->getCargo($Good['ID']); ?></td>
-					<td align="center"><input type="text" name="amount" value="<?php echo min($Good['Amount'], $ThisShip->getCargoHolds()); ?>" size="4" id="InputFields" class="center"></td>
-					<td align="center"><input type="submit" name="action" value="Loot" id="InputFields" /></td>
-				</tr>
-			</form><?php
-		} unset($Good); ?>
-	</table>
-</div>
+<table class="center standard">
+	<tr>
+		<th>Good</th>
+		<th>Supply/Demand</th>
+		<th>Base Price</th>
+		<th>Amount on Ship</th>
+		<th>Amount to Trade</th>
+		<th>Action</th>
+	</tr><?php
+	$BoughtGoods =& $ThisPort->getVisibleGoodsBought($ThisPlayer);
+	foreach($BoughtGoods as &$Good) { ?>
+		<form method="POST" action="<?php echo $ThisPort->getLootGoodHREF($Good); ?>">
+			<tr>
+				<td><?php echo $Good['Name']; ?></td>
+				<td><?php echo $Good['Amount']; ?></td>
+				<td><?php echo $Good['BasePrice']; ?></td>
+				<td><?php echo $ThisShip->getCargo($Good['ID']); ?></td>
+				<td><input type="text" name="amount" value="<?php echo min($Good['Amount'], $ThisShip->getCargoHolds()); ?>" size="4" id="InputFields" class="center"></td>
+				<td><input type="submit" name="action" value="Loot" id="InputFields" /></td>
+			</tr>
+		</form><?php
+	} unset($Good); ?>
+</table>
