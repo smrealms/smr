68d677ee607f1ac6186eb07eebc11a8dce49b548
Michael Kunze <mkunze@chaos-inc.de>
2012-07-01 17:10:55 +0200

    changed to a few more generic example options

--- htdocs/config.specific.sample.php
+++ htdocs/config.specific.sample.php
@@ -1,14 +1,14 @@
 <?php
 define('USE_COMPATIBILITY',false);
 
-define('URL', 'http://localhost/smr1');
-define('ROOT','/home/page/smr/');
-define('LIB', ROOT . 'lib/');
-define('ENGINE', ROOT . 'engine/');
-define('WWW', ROOT . 'htdocs/');
-define('UPLOAD', '/home/page/wwwWrite/upload/');
-define('ADMIN', ROOT . 'admin/');
-define('TOOLS', ROOT . 'tools/');
+define('URL','http://www.yourdomain.com/smr');
+define('ROOT','/usr/share/smr/');
+define('LIB',ROOT.'lib/');
+define('ENGINE',ROOT.'engine/');
+define('WWW',ROOT.'htdocs/');
+define('UPLOAD',WWW.'upload/');
+define('ADMIN',ROOT.'admin/');
+define('TOOLS',ROOT.'tools/');
 
 define('ENABLE_BETA',false);
 define('ACCOUNT_ID_PAGE',1403); //BETA, used for removing newbie turn
