b13fb8635cd15fd0a8d6e4f95af137d32690edca
Page <pjgazzard@googlemail.com>
2012-07-02 00:17:30 +0100

    Tidy political status template.

--- templates/Default/engine/Default/council_politics.php
+++ templates/Default/engine/Default/council_politics.php
@@ -1,35 +1,35 @@
-<div align="center">
+<div class="center">
 	<p>We are at War/Peace<br />with the following races:</p>
-	<table>
+	<table class="center">
 		<tr>
 			<th width="150">Peace</th>
 			<th width="150">Neutral</th>
 			<th width="150">War</th>
 		</tr>
 		<tr>
-			<td align="center" valign="top">
-				<table><?php
+			<td>
+				<table class="center"><?php
 					foreach ($PeaceRaces as $RaceID => $raceInfo) { ?>
 						<tr>
-							<td align="center"><?php echo Globals::getColouredRaceNameForRace($RaceID, $ThisPlayer->getGameID(), $ThisPlayer->getRaceID()); ?></td>
+							<td><?php echo Globals::getColouredRaceNameForRace($RaceID, $ThisPlayer->getGameID(), $ThisPlayer->getRaceID()); ?></td>
 						</tr><?php
 					} ?>
 				</table>
 			</td>
-			<td align="center" valign="top">
-				<table><?php
+			<td>
+				<table class="center"><?php
 					foreach ($NeutralRaces as $RaceID => $raceInfo) { ?>
 						<tr>
-							<td align="center"><?php echo Globals::getColouredRaceNameForRace($RaceID, $ThisPlayer->getGameID(), $ThisPlayer->getRaceID()); ?></td>
+							<td><?php echo Globals::getColouredRaceNameForRace($RaceID, $ThisPlayer->getGameID(), $ThisPlayer->getRaceID()); ?></td>
 						</tr><?php
 					} ?>
 				</table>
 			</td>
-			<td align="center" valign="top">
-				<table><?php
+			<td>
+				<table class="center"><?php
 					foreach ($WarRaces as $RaceID => $raceInfo) { ?>
 						<tr>
-							<td align="center"><?php echo Globals::getColouredRaceNameForRace($RaceID, $ThisPlayer->getGameID(), $ThisPlayer->getRaceID()); ?></td>
+							<td><?php echo Globals::getColouredRaceNameForRace($RaceID, $ThisPlayer->getGameID(), $ThisPlayer->getRaceID()); ?></td>
 						</tr><?php
 					} ?>
 				</table>
