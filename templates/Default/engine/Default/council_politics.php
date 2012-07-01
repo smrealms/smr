e6b9e82b36fc7b8ee9041dceea6907813526516a
Page <pjgazzard@googlemail.com>
2012-07-02 00:06:34 +0100

    Split political status page into a template.

---
+++ templates/Default/engine/Default/council_politics.php
@@ -0,0 +1,29 @@
+<div align="center">
+	<p>We are at War/Peace<br />with the following races:</p>
+	<table>
+		<tr>
+			<th width="150">Peace</th>
+			<th width="150">War</th>
+		</tr>
+		<tr>
+			<td align="center" valign="top">
+				<table><?php
+					foreach ($PeaceRaces as $RaceID => $raceInfo) { ?>
+						<tr>
+							<td align="center"><?php echo Globals::getColouredRaceNameForRace($RaceID, $ThisPlayer->getGameID(), $ThisPlayer->getRaceID()); ?></td>
+						</tr><?php
+					} ?>
+				</table>
+			</td>
+			<td align="center" valign="top">
+				<table><?php
+					foreach ($WarRaces as $RaceID => $raceInfo) { ?>
+						<tr>
+							<td align="center"><?php echo Globals::getColouredRaceNameForRace($RaceID, $ThisPlayer->getGameID(), $ThisPlayer->getRaceID()); ?></td>
+						</tr><?php
+					} ?>
+				</table>
+			</td>
+		</tr>
+	</table>
+</div>
