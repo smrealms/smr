f0bd180b9ed21515d72fe18df320dbdae8e288dc
Page <pjgazzard@googlemail.com>
2012-07-03 13:11:29 +0100

    Make UCI hash size configurable.

--- tools/npc/config.specific.sample.php
+++ tools/npc/config.specific.sample.php
@@ -12,4 +12,5 @@ define('MAX_SLEEP_TIME',1100000);
 define('UCI_CHESS_ENGINE', '/path/to/UCI_CHESS_ENGINE'); // Stockfish works: http://www.stockfishchess.com
 define('UCI_TIME_PER_MOVE_MS', 2000);
 define('UCI_SLEEP_BETWEEN_CYCLES_US', 2000000);
+define('UCI_HASH_SIZE_MB', 512);
 ?>
