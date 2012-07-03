009f09729561ed52e2805d39ca78f92e8c12717e
Page <pjgazzard@googlemail.com>
2012-07-03 22:51:43 +0100

    Show the updates since last login on login.

--- engine/Default/login_check_processing.php
+++ engine/Default/login_check_processing.php
@@ -8,19 +8,27 @@ if(!isset($var['CheckType']) || $var['CheckType'] == 'Validate') {
 		$var['CheckType'] = 'Announcements';
 	}
 }
-
+$db = new SmrMySqlDatabase();
 if($var['CheckType'] == 'Announcements') {
-	$db = new SmrMySqlDatabase();
 	$db->query('SELECT 1 FROM account JOIN announcement ON last_login < time
 				WHERE account_id = ' . $db->escapeNumber(SmrSession::$account_id) . ' LIMIT 1');
 	// do we have announcements?
-	if ($db->getNumRows() != 0) {
-		$container = create_container('skeleton.php', 'announcements.php');
+	if ($db->nextRecord()) {
+		forward(create_container('skeleton.php', 'announcements.php'));
 	}
 	else {
-		$container = create_container('logged_in.php');
+		$var['CheckType'] = 'Updates';
 	}
-	forward($container);
 }
 
+if($var['CheckType'] == 'Updates') {
+	$db->query('SELECT last_login FROM account JOIN version ON last_login < went_live
+				WHERE account_id = ' . $db->escapeNumber(SmrSession::$account_id) . ' LIMIT 1');
+	// do we have updates?
+	if ($db->nextRecord()) {
+		forward(create_container('skeleton.php', 'changelog_view.php', array('Since' => $db->getInt('last_login'))));
+	}
+}
+
+forward(create_container('logged_in.php'));
 ?>
