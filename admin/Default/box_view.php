e81f5e1d1d664d725c3d4f0761e0c8cde53f31f6
Page <pjgazzard@googlemail.com>
2012-07-07 23:15:38 +0100

    Split box view into a template.

--- admin/Default/box_view.php
+++ admin/Default/box_view.php
@@ -3,88 +3,62 @@
 $template->assign('PageTopic','Viewing Message Boxes');
 
 if(!isset($var['box_type_id'])) {
-	$db->query('SELECT count(message_id),box_type_name,box_type_id FROM message_box_types LEFT JOIN message_boxes USING(box_type_id) GROUP BY box_type_id');
+	$db->query('SELECT count(message_id), box_type_name, box_type_id
+				FROM message_box_types
+				LEFT JOIN message_boxes USING(box_type_id)
+				GROUP BY box_type_id, box_type_name');
 	$container = create_container('skeleton.php', 'box_view.php');
-
-	$PHP_OUTPUT.=('<table class="standard">');
-	$PHP_OUTPUT.=('<tr>');
-	$PHP_OUTPUT.=('<th>Folder</th>');
-	$PHP_OUTPUT.=('<th>Messages</th>');
-	$PHP_OUTPUT.=('</tr>');
+	$boxes = array();
 	while($db->nextRecord()) {
-		$container['box_type_id'] = $db->getField('box_type_id');
-		$PHP_OUTPUT.='<tr>
-						<td><a href="'.SmrSession::getNewHREF($container).'">'.$db->getField('box_type_name').'</a></td>
-						<td>'.$db->getField('count(message_id)').'</a></td>
-					</tr>';
+		$boxTypeID = $db->getInt('box_type_id');
+		$container['box_type_id'] = $boxTypeID;
+		$boxes[$boxTypeID] = array(
+			'ViewHREF' => SmrSession::getNewHREF($container),
+			'BoxName' => $db->getField('box_type_name'),
+			'TotalMessages' => $db->getField('count(message_id)')
+		);
 	}
-	$PHP_OUTPUT.='</table>';
+	$template->assign('Boxes', $boxes);
 }
 else {
-	$PHP_OUTPUT.=create_link(create_container('skeleton.php','box_view.php'),'Back<br />');
+	$template->assign('BackHREF', SmrSession::getNewHREF(create_container('skeleton.php','box_view.php')));
 	$db->query('SELECT * FROM message_boxes WHERE box_type_id='.$db->escapeNumber($var['box_type_id']).' ORDER BY send_time DESC');
-	$container = create_container('box_delete_processing.php');
-	$container['box_type_id'] = $var['box_type_id'];
+	$messages = array();
 	if ($db->getNumRows()) {
-		$PHP_OUTPUT.=create_echo_form($container);
-		$PHP_OUTPUT.=create_submit('Delete');
-		$PHP_OUTPUT.=('&nbsp;');
-		$PHP_OUTPUT.=('<select name="action" size="1" id="InputFields">');
-		$PHP_OUTPUT.=('<option>Marked Messages</option>');
-		$PHP_OUTPUT.=('<option>All Messages</option>');
-		$PHP_OUTPUT.=('</select>');
-
-		$PHP_OUTPUT.=('<br /><br />');
-		$PHP_OUTPUT.=('Click the name to reply<br />');
-		$PHP_OUTPUT.=('<table width="100%" class="standard">');
-
+		$container = create_container('box_delete_processing.php');
+		$container['box_type_id'] = $var['box_type_id'];
+		$template->assign('DeleteHREF', SmrSession::getNewHREF($container));
 		while($db->nextRecord()) {
-			$gameID = $db->getField('game_id');
+			$gameID = $db->getInt('game_id');
 			$validGame = $gameID > 0 && Globals::isValidGame($gameID);
-			$PHP_OUTPUT.=('<tr>');
-			$PHP_OUTPUT.=('<td><input type="checkbox" name="message_id[]" value="'.$db->getField('message_id').'"></td>');
+			$messageID = $db->getInt('message_id');
+			$messages[$messageID] = array(
+				'ID' => $messageID
+			);
 			$senderAccount =& SmrAccount::getAccount($db->getField('sender_id'));
-			$PHP_OUTPUT.=('<td class="noWrap">');
-
-			$sender = 'From: '.$senderAccount->getLogin().' ('.$senderAccount->getAccountID().')';
+			$senderName = $senderAccount->getLogin().' ('.$senderAccount->getAccountID().')';
 			if ($validGame) {
-				$senderPlayer =& SmrPlayer::getPlayer($senderAccount->getAccountID(), $db->getInt('game_id'));
+				$senderPlayer =& SmrPlayer::getPlayer($senderAccount->getAccountID(), $gameID);
 				if($senderAccount->getLogin() != $senderPlayer->getPlayerName()) {
-					$sender .= ' a.k.a '.$senderPlayer->getPlayerName();
+					$senderName .= ' a.k.a ' . $senderPlayer->getPlayerName();
 				}
-			}
-			if($validGame) {
+				
 				$container = create_container('skeleton.php', 'box_reply.php');
 				$container['sender_id'] = $senderAccount->getAccountID();
 				$container['game_id'] = $gameID;
-				$PHP_OUTPUT.=create_link($container, $sender);
+				$messages[$messageID]['ReplyHREF'] = SmrSession::getNewHREF($container);
 			}
-			else {
-				$PHP_OUTPUT.=$sender;
-			}
-			$PHP_OUTPUT.='</td>';
-			$PHP_OUTPUT.='<td>';
+			$messages[$messageID]['SenderName'] = $senderName;
 			if (!$validGame) {
-				$PHP_OUTPUT.=('Game no longer exists');
+				$messages[$messageID]['GameName'] = 'Game no longer exists';
 			}
 			else {
-				$PHP_OUTPUT.=Globals::getGameName($gameID);
+				$messages[$messageID]['GameName'] = Globals::getGameName($gameID);
 			}
-			$PHP_OUTPUT.=('</td></tr><tr><td colspan="3">');
-			$PHP_OUTPUT.=('Sent at ' . date(DATE_FULL_SHORT, $db->getField('send_time')));
-			$PHP_OUTPUT.='</td>';
-			$PHP_OUTPUT.=('</tr>');
-			$PHP_OUTPUT.=('<tr>');
-			$PHP_OUTPUT.=('<td width="100%" colspan="3">');
-			$PHP_OUTPUT.= bbifyMessage(htmliseMessage($db->getField('message_text')));
-			$PHP_OUTPUT.=('</td></tr>');
-
+			$messages[$messageID]['SendTime'] = date(DATE_FULL_SHORT, $db->getField('send_time'));
+			$messages[$messageID]['Message'] = bbifyMessage(htmliseMessage($db->getField('message_text')));
 		}
-
-		$PHP_OUTPUT.=('</table>');
-		$PHP_OUTPUT.=('</form>');
-
-	} else
-		$PHP_OUTPUT.=('There are currently no messages in this box.');
+		$template->assign('Messages', $messages);
+	}
 }
 ?>
