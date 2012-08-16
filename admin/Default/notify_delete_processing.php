f61d0a1bfff9c84b08fb5cfc6118410195015330
Page <pjgazzard@googlemail.com>
2012-08-16 19:06:36 +0100

    Fix bug with deleting reported messages.

--- admin/Default/notify_delete_processing.php
+++ admin/Default/notify_delete_processing.php
@@ -4,7 +4,7 @@ if (!isset($notify_id)) {
 	create_error('You must choose the messages you want to delete.');
 }
 
-$db->query('DELETE FROM message_notify WHERE notify_id IN '.$db->escapeArray($notify_id));
+$db->query('DELETE FROM message_notify WHERE notify_id IN (' . $db->escapeArray($notify_id) . ')');
 
 forward(create_container('skeleton.php', 'notify_view.php'));
 ?>
