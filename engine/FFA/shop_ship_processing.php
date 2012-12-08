760710e0f55a32cdab46ff963ae1d53d1b2fc872
Page <pjgazzard@googlemail.com>
2012-12-08 13:29:54 +0000

    Ban top racials/DC/FU in FFA game type.

---
+++ engine/FFA/shop_ship_processing.php
@@ -0,0 +1,23 @@
+<?php
+$shipID = $var['ship_id'];
+
+$bannedShips = array(
+	SHIP_TYPE_ASSAULT_CRAFT,
+	SHIP_TYPE_DARK_MIRAGE,
+	SHIP_TYPE_DESTROYER,
+	SHIP_TYPE_DEVASTATOR,
+	SHIP_TYPE_EATER_OF_SOULS,
+	SHIP_TYPE_FURY,
+	SHIP_TYPE_MOTHER_SHIP,
+	
+	SHIP_TYPE_FEDERAL_ULTIMATUM,
+	SHIP_TYPE_DEATH_CRUISER
+);
+
+if(in_array($shipID, $bannedShips)) {
+	create_error('No top racial for you, ah na na na na!');
+}
+
+require(ENGINE.'Default/shop_ship_processing.php');
+
+?>
