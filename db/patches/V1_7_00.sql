a2d5dd32829a31fd854fdcdcaba598dec9f74750
tmaus <thorsten.maus@zalora.com>
2013-12-22 09:58:23 +0100

    bug fixing on research and planet kicking

--- db/patches/V1_7_00.sql
+++ db/patches/V1_7_00.sql
@@ -1,9 +1,9 @@
-
-#INSERT INTO permission (permission_id, permission_name,link_to) VALUES (34, 'Research','research_view.php');
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+# add research permission
+INSERT INTO permission (permission_id, permission_name,link_to) VALUES (34, 'Research','research_view.php');
 
 -- -----------------------------------------------------
 -- Table `game_research`
@@ -14,6 +14,7 @@ CREATE TABLE IF NOT EXISTS `game_research` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `game_id` INT NULL,
   `ship_research` TINYINT(1) NULL,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`))
   ENGINE = InnoDB;
 
@@ -30,6 +31,7 @@ CREATE TABLE IF NOT EXISTS `game_research_ship_certificate` (
   `game_research_certificate_id` INT NOT NULL,
   `ship_type_id` INT NOT NULL,
   `parent_id` INT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`))
   ENGINE = InnoDB;
 
@@ -54,6 +56,7 @@ CREATE TABLE IF NOT EXISTS `game_research_certificate` (
   `parent_id` INT NULL,
   `credits` INT NULL,
   `computer` INT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`))
   ENGINE = InnoDB;
 
@@ -62,7 +65,7 @@ CREATE INDEX `GRC_GAME_RESARCH_IDX` ON `game_research_certificate` (`game_resear
 CREATE INDEX `GRC_PARENT_IDX` ON `game_research_certificate` (`parent_id` ASC);
 
 
------------------------------------------------------
+-- -----------------------------------------------------
 -- Table `game_alliance_certificate`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `game_alliance_certificate` ;
@@ -71,10 +74,11 @@ CREATE TABLE IF NOT EXISTS `game_alliance_certificate` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `game_research_certificate_id` INT NOT NULL,
   `alliance_id` INT NOT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`))
   ENGINE = InnoDB;
 
-CREATE INDEX `GASC_GAME_RESEARCH_CERTIFICATE_IDX` ON `game_alliance_certificate` (`game_research_certificate_id` ASC);
+CREATE INDEX `GASC_GAME_RESEARCH_SHIP_CERTIFICATE_IDX` ON `game_alliance_certificate` (`game_research_certificate_id` ASC);
 
 CREATE INDEX `GASC_ALLIANCE_IDX` ON `game_alliance_certificate` (`alliance_id` ASC);
 
@@ -95,13 +99,11 @@ CREATE TABLE IF NOT EXISTS `game_alliance_research_progress` (
   PRIMARY KEY (`id`))
   ENGINE = InnoDB;
 
-CREATE INDEX `GARP_GAME_RESEARCH_CERTIFICATE_IDX` ON `game_alliance_research_progress` (`game_research_certificate_id` ASC);
+CREATE INDEX `GARP_GAME_RESEARCH_SHIP_CERTIFICATE_IDX` ON `game_alliance_research_progress` (`game_research_certificate_id` ASC);
 
 CREATE INDEX `GARP_ALLIANCE_IDX` ON `game_alliance_research_progress` (`alliance_id` ASC);
 
 CREATE INDEX `GARP_SECTOR_IDX` ON `game_alliance_research_progress` (`sector_id` ASC);
-
-
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
--- engine/Default/planet_kick_processing.php
+++ engine/Default/planet_kick_processing.php
@@ -1,6 +1,7 @@
 <?php
 if (!$player->isLandedOnPlanet())
 	create_error('You are not on a planet!');
+
 $planet =& $player->getSectorPlanet();
 
 $planetPlayer =& SmrPlayer::getPlayer($var['account_id'], $player->getGameID());
@@ -13,6 +14,11 @@ $player->sendMessage($planetPlayer->getAccountID(), 2, $message, false);
 $planetPlayer->setLandedOnPlanet(false);
 $planetPlayer->setKicked(true);
 $planetPlayer->update();
+
+require_once(get_file_loc('Research.class.inc'));
+$research = new Research($player->getGameID());
+cancelUserResearch($player);
+
 
 forward(create_container('skeleton.php', 'planet_main.php'));
 
