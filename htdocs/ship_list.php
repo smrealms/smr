91e01718cf92f3d26719b4b5bfd06de7e0067ced
wcyrek <github@witoldcyrek.com>
2013-12-26 16:34:53 -0500

    update ship_list.php & weapon_list.php
    Added db patch

--- htdocs/ship_list.php
+++ htdocs/ship_list.php
@@ -19,11 +19,210 @@ try {
 	<link rel="stylesheet" type="text/css" href="<?php echo DEFAULT_CSS_COLOUR; ?>">
 		<title>Space Merchant Realms - Ship List</title>
 		<meta http-equiv="pragma" content="no-cache">
+	<style>
+	SELECT {
+		border: solid #80C870 1px;
+		background-color: #0A4E1D;
+		color: #80C870; }
+	OPTGROUP {
+		border: solid #80C870 1px;
+	}
+	</style>
+	<script>
+		//JS code by Astax to foster filtering the results
+		
+		//Use window variable to store filter values, this is kinda like a JS equivellent of global
+		window.filter = new Array("All", "All", "All", "All", "All", "All", "All", "All", "All", "All",
+		"All", "All", "All", "All", "All", "All", "All", "All", "All");
+		function classPickf() {
+			var option 	= document.getElementById("classPick");
+			var selected;
+			
+			for (var i=0; i < option.options.length; i++) {
+				if (option.options[i].selected)
+					selected = option.options[i].value;
+			}
+			if (selected == "null") 
+				window.filter[2] = "All";
+			else
+				window.filter[2] = selected;
+			applyFilter();
+		
+		}
+		function racePickf() {
+			var option 	= document.getElementById("racePick");
+			var selected;
+			
+			for (var i=0; i < option.options.length; i++) {
+				if (option.options[i].selected)
+					selected = option.options[i].value;
+			}
+			if (selected == "null") 
+				window.filter[1] = "All";
+			else
+				window.filter[1] = selected;
+			applyFilter();
+		
+		}
+		function speedPickf() {
+			var option 	= document.getElementById("speedPick");
+			var selected;
+			
+			for (var i=0; i < option.options.length; i++) {
+				if (option.options[i].selected)
+					selected = option.options[i].value;
+			}
+			if (selected == "null") 
+				window.filter[4] = "All";
+			else
+				window.filter[4] = selected;
+			applyFilter();
+		
+		}
+		function hpPickf() {
+			var option 	= document.getElementById("hpPick");
+			var selected;
+			
+			for (var i=0; i < option.options.length; i++) {
+				if (option.options[i].selected)
+					selected = option.options[i].value;
+			}
+			if (selected == "null") 
+				window.filter[5] = "All";
+			else
+				window.filter[5] = selected;
+			applyFilter();
+		
+		}
+		function restrictPickf() {
+			var option 	= document.getElementById("restrictPick");
+			var selected;
+			
+			for (var i=0; i < option.options.length; i++) {
+				if (option.options[i].selected)
+					selected = option.options[i].value;
+			}
+			if (selected == "null") 
+				window.filter[6] = "All";
+			else
+				window.filter[6] = selected;
+			applyFilter();
+		
+		}
+		function scannerPickf() {
+			var option 	= document.getElementById("scannerPick");
+			var selected;
+			
+			for (var i=0; i < option.options.length; i++) {
+				if (option.options[i].selected)
+					selected = option.options[i].value;
+			}
+			if (selected == "null") 
+				window.filter[13] = "All";
+			else
+				window.filter[13] = selected;
+			applyFilter();
+		
+		}
+		function cloakPickf() {
+			var option 	= document.getElementById("cloakPick");
+			var selected;
+			
+			for (var i=0; i < option.options.length; i++) {
+				if (option.options[i].selected)
+					selected = option.options[i].value;
+			}
+			if (selected == "null") 
+				window.filter[14] = "All";
+			else
+				window.filter[14] = selected;
+			applyFilter();
+		
+		}
+		function illusionPickf() {
+			var option 	= document.getElementById("illusionPick");
+			var selected;
+			
+			for (var i=0; i < option.options.length; i++) {
+				if (option.options[i].selected)
+					selected = option.options[i].value;
+			}
+			if (selected == "null") 
+				window.filter[15] = "All";
+			else
+				window.filter[15] = selected;
+			applyFilter();
+		
+		}
+		function jumpPickf() {
+			var option 	= document.getElementById("jumpPick");
+			var selected;
+			
+			for (var i=0; i < option.options.length; i++) {
+				if (option.options[i].selected)
+					selected = option.options[i].value;
+			}
+			if (selected == "null") 
+				window.filter[16] = "All";
+			else
+				window.filter[16] = selected;
+			applyFilter();
+		
+		}
+		function scramblePickf() {
+			var option 	= document.getElementById("scramblePick");
+			var selected;
+			
+			for (var i=0; i < option.options.length; i++) {
+				if (option.options[i].selected)
+					selected = option.options[i].value;
+			}
+			if (selected == "null") 
+				window.filter[17] = "All";
+			else
+				window.filter[17] = selected;
+			applyFilter();
+		
+		}
+		function applyFilter() {
+			var table 	= document.getElementById("table");
+			for (var i=1; i < table.rows.length; i++) {
+					show = true;
+					for (var j=0; j < table.rows[i].cells.length; j++) {
+						if (window.filter[j] == "All")
+							continue;
+						if (table.rows[i].cells[j].innerHTML != window.filter[j]) {
+							show = false;
+							break;
+						}
+					}
+					if (show)
+						table.rows[i].style.display="";
+					else
+						table.rows[i].style.display="none";
+					
+				}
+			
+		}
+	
+	</script>
 	</head><?php
 
 	$seq = @$_REQUEST['seq'];
 	$order = @$_REQUEST['order'];
 	$hardwarea = @$_REQUEST['hardwarea'];
+	
+	$class 		= buildSelector($db, "classPick", "ship_class_name", "ship_class");
+	$race 		= buildSelector($db, "racePick", "race_name", "race");
+	$speed 		= buildSelector($db, "speedPick", "speed", "ship_type");
+	$hardpoint  = buildSelector($db, "hpPick", "hardpoint", "ship_type");
+	$restrict 	= buildRestriction();
+	$scanner 	= buildToggle("scannerPick");
+	$cloak 		= buildToggle("cloakPick");
+	$illusion 	= buildToggle("illusionPick");
+	$jump 		= buildToggle("jumpPick");
+	$scramble 	= buildToggle("scramblePick");
+	
 	echo ('<body>');
 	if (empty($seq)) {
 		$seq = 'ASC';
@@ -33,9 +232,9 @@ try {
 	}
 	else {
 		$seq = 'ASC';
-	}
-
-	$allowedOrders = array('ship_name','race_name','cost','speed','hardpoint','buyer_restriction','lvl_needed');
+	}	
+	
+	$allowedOrders = array('ship_name','race_name','cost','speed','hardpoint','buyer_restriction','lvl_needed','ship_class_name');
 
 	if (!empty($order) && in_array($order,$allowedOrders)) {
 		$order_by = $order .' '. $seq;
@@ -58,6 +257,7 @@ try {
 			$db2->query('SELECT *
 						FROM ship_type
 						JOIN ship_type_support_hardware USING(ship_type_id)
+						JOIN ship_class USING(ship_class_id)
 						JOIN race USING(race_id)
 						WHERE ship_type_id=' . $db->escapeNumber($db->getInt('ship_type_id')) . '
 						ORDER BY hardware_type_id ASC');
@@ -70,6 +270,7 @@ try {
 		$db->query('SELECT *
 					FROM ship_type
 					JOIN ship_type_support_hardware USING(ship_type_id)
+					JOIN ship_class USING(ship_class_id)
 					JOIN race USING(race_id)
 					ORDER BY '.$order_by);
 		while ($db->nextRecord()) {
@@ -77,27 +278,39 @@ try {
 		}
 	}
 	?>
-
-	<table class="standard">
-		<tr>
-			<th align="left"><a href="?order=ship_name&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Ship Name</span></a></th>
-			<th align="center"><a href="?order=race_name&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Ship Race</span></a></th>
-			<th align="center"><a href="?order=cost&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Cost</span></a></th>
-			<th align="center"><a href="?order=speed&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Speed</span></a></th>
-			<th align="center"><a href="?order=hardpoint&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Hardpoints</span></a></th>
-			<th align="center"><a href="?order=buyer_restriction&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Restriction</span></a></th><?php
+	<div id="container" style="padding: 0;">
+	<div style="width:1400px; margin-left:auto; margin-right:auto;">
+	<table id="table" class="standard">
+		<?php /*<tr style="position:fixed">*/ ?>
+		<tr >
+			<th align="center"><a href="?order=ship_name&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Ship Name</span></a></th>
+			<th align="center"><a href="?order=race_name&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Race</span></a>
+				<?php echo $race ?></th>
+			<th align="center">Class<?php echo $class; ?></th>
+			<th align="center"><a href="?order=cost&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Cost</span></a></th>
+			<th align="center"><a href="?order=speed&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Speed</span></a>
+				<?php echo $speed ?></th>
+			<th align="center"><a href="?order=hardpoint&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Hardpoints</span></a>
+				<?php echo $hardpoint ?></th>
+			<th align="center"><a href="?order=buyer_restriction&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Restriction</span></a>
+				<?php echo $restrict ?></th><?php
 /*			<th align="center"><a href="?order=lvl_needed&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Level Needed(Semi War)</span></a></th>*/ ?>
-			<th align="center"><a href="?hardwarea=1&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Shields</span></a></th>
-			<th align="center"><a href="?hardwarea=2&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Armour</span></a></th>
-			<th align="center"><a href="?hardwarea=3&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Cargo</span></a></th>
-			<th align="center"><a href="?hardwarea=4&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Combat Drones</span></a></th>
-			<th align="center"><a href="?hardwarea=5&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Scout Drones</span></a></th>
-			<th align="center"><a href="?hardwarea=6&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Mines</span></a></th>
-			<th align="center"><a href="?hardwarea=7&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Scanner</span></a></th>
-			<th align="center"><a href="?hardwarea=8&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Cloak</span></a></th>
-			<th align="center"><a href="?hardwarea=9&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Illusion</span></a></th>
-			<th align="center"><a href="?hardwarea=10&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Jump</span></a></th>
-			<th align="center"><a href="?hardwarea=11&seq=<?php echo $seq; ?>"><span style="color:#80C870;">Drone Scrambler</span></a></th>
+			<th align="center"><a href="?hardwarea=1&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Shields</span></a></th>
+			<th align="center"><a href="?hardwarea=2&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Armour</span></a></th>
+			<th align="center"><a href="?hardwarea=3&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Cargo</span></a></th>
+			<th align="center"><a href="?hardwarea=4&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Drones</span></a></th>
+			<th align="center"><a href="?hardwarea=5&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Scouts</span></a></th>
+			<th align="center"><a href="?hardwarea=6&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Mines</span></a></th>
+			<th align="center"><a href="?hardwarea=7&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Scanner</span></a>
+				<?php echo $scanner ?></th>
+			<th align="center"><a href="?hardwarea=8&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Cloak</span></a>
+				<?php echo $cloak ?></th>
+			<th align="center"><a href="?hardwarea=9&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Illusion</span></a>
+				<?php echo $illusion ?></th>
+			<th align="center"><a href="?hardwarea=10&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Jump</span></a>
+				<?php echo $jump ?></th>
+			<th align="center"><a href="?hardwarea=11&amp;seq=<?php echo $seq; ?>"><span style="color:#80C870;">Scrambler</span></a>
+				<?php echo $scramble ?></th>
 		</tr><?php
 
 	foreach($shipArray as $stat) {
@@ -106,17 +319,54 @@ try {
 			echo ('<td align="center">'.$value.'</td>');
 		echo ('</tr>');
 	} ?>
-	</table><?php
+	</table></div></div><?php
 }
 catch(Exception $e) {
 	handleException($e);
+}
+
+function buildSelector($db,  $id, $name, $table) {
+	$selector = '<br><form class="selector" action="" method="get">';
+	$selector .= '<select id="'.$id.'" name="'.$name.'" onchange="'.$id.'f()"><option value=null>All</option>';
+		$db->query("select distinct ".$name." from ".$table." order by ".$name);
+	while ($db->nextRecord()) {
+		$selector .= '<option value="'.$db->getField($name).'">'
+		.$db->getField($name).'</option>';
+	}
+	$selector .= '</select></form>';
+	return $selector;
+}
+
+function buildRestriction() {
+	$restrict = '<br><form class="selector" action="" method="get">'
+	.'<select id="restrictPick" name="restrict" onchange="restrictPickf()">'
+	.'<option value=null>All</option>'
+	.'<option value="">None</option>'
+	."<option value='<font color=\"green\">Good</font>'>Good</option>"
+	."<option value='<font color=\"red\">Evil</font>' style=\"color: red;\">Evil</option></select></form>";
+	
+	return $restrict;
+
+}
+
+function buildToggle($id) {
+	$toggle = '<br><form class="selector" action="" method="get">'
+	.'<select id="'.$id.'" name="'.$id.'" onchange="'.$id.'f()">'
+	.'<option value=null>All</option>'
+	.'<option value="Yes">Yes</option>'
+	.'<option value="">No</option></select></form>';
+	
+	return $toggle;
+
 }
 
 function buildShipStats($db) {
 	//we want to put them all in an array so we dont have to have 15 td rows
 	$stat = array();
     $stat[] = str_replace(' ','&nbsp;',$db->getField('ship_name'));
-	$stat[] = str_replace(' ','&nbsp;',$db->getField('race_name'));
+	//$stat[] = str_replace(' ','&nbsp;',$db->getField('race_name'));
+	$stat[] = $db->getField('race_name');
+	$stat[] = str_replace(' ','&nbsp;',$db->getField('ship_class_name'));
 	$stat[] = number_format($db->getInt('cost'));
     $stat[] = $db->getInt('speed');
 	$stat[] = $db->getInt('hardpoint');
@@ -125,10 +375,10 @@ function buildShipStats($db) {
     elseif ($db->getField('buyer_restriction') == 2)
 		$restriction = '<font color="red">Evil</font>';
 	else
-		$restriction = '&nbsp;';
+		$restriction = '';
     $stat[] = $restriction;
 //    $stat[] = $db->getInt('lvl_needed');
-	$stat[] = number_format($db->getInt('max_amount'));
+	$stat[] = $db->getInt('max_amount');
 	$hardware_id = 2;
     //get our hardware
     while ($hardware_id <= 11)
@@ -140,7 +390,7 @@ function buildShipStats($db) {
     		elseif ($db->getInt('max_amount') == 1)
         		$stat[] = 'Yes';
             else
-	        	$stat[] = '&nbsp;';
+	        	$stat[] = '';
 	    }
         $hardware_id++;
 	}
--- htdocs/weapon_list.php
+++ htdocs/weapon_list.php
@@ -18,10 +18,128 @@ try {
 	echo ('<link rel="stylesheet" type="text/css" href="css/Default.css">');
 	echo ('<link rel="stylesheet" type="text/css" href="css/Default/Default.css">');
 	echo ('<title>Weapon List</title>');
-	echo ('<meta http-equiv="pragma" content="no-cache">');
+	echo ('<meta http-equiv="pragma" content="no-cache">');?>
+	<style>
+	SELECT {
+		border: solid #80C870 1px;
+		background-color: #0A4E1D;
+		color: #80C870; }
+	OPTGROUP {
+		border: solid #80C870 1px;
+	}
+	</style>
+	<script>
+		// Benoit Asselin - http://www.ab-d.fr
+		Array.prototype.in_array = function(p_val) {
+			for(var i = 0, l = this.length; i < l; i++) {
+				if(this[i] == p_val) {
+					return true;
+				}
+			}
+			return false;
+		}
+		//JS code by Astax to foster filtering the results
+		
+		//Use window variable to store filter values, this is kinda like a JS equivellent of global
+		window.filter = new Array("All", "All", "All", "All", "All", "All", "All", "All", "All", "All");
+		
+		//reset all check boxes
+		function resetBoxes() {
+			var toggle = document.getElementById("raceform");
+			for (i = 0; i < toggle.races.length; i++) {
+					toggle.races[i].checked = true;
+			}
+		}
+		function racePickf() {
+			var option 	= document.getElementById("racePick");
+			var selected;
+			
+			for (var i=0; i < option.options.length; i++) {
+				if (option.options[i].selected)
+					selected = option.options[i].value;
+			}
+			if (selected == "null") 
+				window.filter[1] = "All";
+			else
+				window.filter[1] = selected;
+			applyFilter();
+		
+		}
+		function powerPickf() {
+			var option 	= document.getElementById("powerPick");
+			var selected;
+			
+			for (var i=0; i < option.options.length; i++) {
+				if (option.options[i].selected)
+					selected = option.options[i].value;
+			}
+			if (selected == "null") 
+				window.filter[6] = "All";
+			else
+				window.filter[6] = selected;
+			applyFilter();
+		
+		}
+		function restrictPickf() {
+			var option 	= document.getElementById("restrictPick");
+			var selected;
+			
+			for (var i=0; i < option.options.length; i++) {
+				if (option.options[i].selected)
+					selected = option.options[i].value;
+			}
+			if (selected == "null") 
+				window.filter[7] = "All";
+			else
+				window.filter[7] = selected;
+			applyFilter();
+		
+		}
+		
+		function raceToggle() {
+			var toggle = document.getElementById("raceform");
+			window.filter[1] = new Array();
+			for (i = 0; i < toggle.races.length; i++) {
+				if (toggle.races[i].checked) {
+					window.filter[1].push(toggle.races[i].value);
+				}
+			}
+			applyFilter();
+		}
+		
+		function applyFilter() {
+			var table 	= document.getElementById("table");
+			for (var i=1; i < table.rows.length; i++) {
+					show = true;
+					for (var j=0; j < table.rows[i].cells.length; j++) {
+						if (window.filter[j] == "All")
+							continue;
+						if( Object.prototype.toString.call( window.filter[j] ) === '[object Array]' ) {
+							if (!window.filter[j].in_array(table.rows[i].cells[j].innerHTML)) {
+								show = false;
+								break;
+							}
+						} else {
+							if (table.rows[i].cells[j].innerHTML != window.filter[j]) {
+								show = false;
+								break;
+							}
+						}
+					}
+					if (show)
+						table.rows[i].style.display="";
+					else
+						table.rows[i].style.display="none";
+					
+				}
+			
+		}
+	
+	</script>
+	<?php
 	echo ('</head>');
 	
-	echo ('<body>');
+	echo ('<body onload="resetBoxes()">');
 	$seq = isset($_REQUEST['seq']) ? $_REQUEST['seq'] : '';
 	if (empty($seq))
 		$seq = 'ASC';
@@ -36,17 +154,25 @@ try {
 	else
 		$order_by = 'weapon_type_id';
 	
+	//$race 		= buildSelector($db, "racePick", "race_name", "race");
+	$race = "";
+	$power 		= buildSelector($db, "powerPick", "power_level", "weapon_type");
+	$restrict 	= buildRestriction();
+	
+	echo ('<div id="container" style="padding: 0;">');
+	echo ('<div style="width:800px; margin-left:auto; margin-right:auto;">');
+	echo (buildRaceBox($db));	
 	$db->query('SELECT * FROM weapon_type, race WHERE weapon_type.race_id = race.race_id ORDER BY '.$order_by.' '.$seq);
-	echo ('<table class="standard">');
+	echo ('<table id="table" class="standard">');
 	echo ('<tr>');
-	echo ('<th align="center"><a href="?order=weapon_name&seq='.$seq.'"><span style=color:#80C870;>Weapon Name</span></a></th>');
-	echo ('<th align="center"><a href="?order=race_name&seq='.$seq.'"><span style=color:#80C870;>Race</span></a></th>');
-	echo ('<th align="center"><a href="?order=cost&seq='.$seq.'"><span style=color:#80C870;>Cost</span></a></th>');
-	echo ('<th align="center"><a href="?order=shield_damage&seq='.$seq.'"><span style=color:#80C870;>Shield Damage</span></a></th>');
-	echo ('<th align="center"><a href="?order=armour_damage&seq='.$seq.'"><span style=color:#80C870;>Armour Damage</span></a></th>');
-	echo ('<th align="center"><a href="?order=accuracy&seq='.$seq.'"><span style=color:#80C870;>Accuracy</span></a></th>');
-	echo ('<th align="center"><a href="?order=power_level&seq='.$seq.'"><span style=color:#80C870;>Power Level</span></a></th>');
-	echo ('<th align="center"><a href="?order=buyer_restriction&seq='.$seq.'"><span style=color:#80C870;>Restriction</span></a></th>');
+	echo ('<th align="center"><a href="?order=weapon_name&amp;seq='.$seq.'"><span style=color:#80C870;>Weapon Name</span></a></th>');
+	echo ('<th align="center"><a href="?order=race_name&amp;seq='.$seq.'"><span style=color:#80C870;>Race</span></a>'.$race.'</th>');
+	echo ('<th align="center"><a href="?order=cost&amp;seq='.$seq.'"><span style=color:#80C870;>Cost</span></a></th>');
+	echo ('<th align="center"><a href="?order=shield_damage&amp;seq='.$seq.'"><span style=color:#80C870;>Shield<br>Damage</span></a></th>');
+	echo ('<th align="center"><a href="?order=armour_damage&amp;seq='.$seq.'"><span style=color:#80C870;>Armour<br>Damage</span></a></th>');
+	echo ('<th align="center"><a href="?order=accuracy&amp;seq='.$seq.'"><span style=color:#80C870;>Accuracy<br>%</span></a></th>');
+	echo ('<th align="center"><a href="?order=power_level&amp;seq='.$seq.'"><span style=color:#80C870;>Level</span></a>'.$power.'</th>');
+	echo ('<th align="center"><a href="?order=buyer_restriction&amp;seq='.$seq.'"><span style=color:#80C870;>Restriction</span></a>'.$restrict.'</th>');
 	echo ('</tr>');
 	while ($db->nextRecord()) {
 		//we need an array so we dont have 8 td rows
@@ -69,7 +195,7 @@ try {
 				$restriction = '<font color="#06F">Newbie</font>';
 			break;
 			default:
-		    	$restriction = '&nbsp;';
+		    	$restriction = '-';
 		}
 	    $stat[] = $restriction;
 	
@@ -80,9 +206,50 @@ try {
 	    echo ('</tr>');
 	
 	}
-	echo ('</table>');
+	echo ('</table></div></div>');
+	
+
+
 }
 catch(Exception $e) {
 	handleException($e);
+}
+
+function buildSelector($db,  $id, $name, $table) {
+	$selector = '<br><form class="selector" action="" method="get">';
+	$selector .= '<select id="'.$id.'" name="'.$name.'" onchange="'.$id.'f()"><option value=null>All</option>';
+		$db->query("select distinct ".$name." from ".$table." order by ".$name);
+	while ($db->nextRecord()) {
+		$selector .= '<option value="'.$db->getField($name).'">'
+		.$db->getField($name).'</option>';
+	}
+	$selector .= '</select></form>';
+	return $selector;
+}
+
+function buildRestriction() {
+	$restrict = '<br><form class="selector" action="" method="get">'
+	.'<select id="restrictPick" name="restrict" onchange="restrictPickf()">'
+	.'<option value=null>All</option>'
+	.'<option value="-">None</option>'
+	."<option value='<font color=\"green\">Good</font>'>Good</option>"
+	."<option value='<font color=\"red\">Evil</font>' style=\"color: red;\">Evil</option>"
+	."<option value='<font color=\"#06F\">Newbie</font>' style=\"color: #06F;\">Newbie</option>"
+	."</select></form>";
+	
+	return $restrict;
+
+}
+
+function buildRaceBox($db) {
+	$racebox;
+	$racebox = '<form id="raceform" name="raceform" align="center" style="text-align:center;">';
+	$db->query("select * from race order by race_id");
+	while ($db->nextRecord()) {
+		$race = $db->getField("race_name");
+		$racebox .= '<input type="checkbox" name="races" value="'.$race.'" onClick="raceToggle()">'.$race;
+	}
+	$racebox .= '</form>';
+	return $racebox;
 }
 ?>
