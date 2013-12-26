c06e48bf3beca5fd48220ff35a2e67bc2490ccad
Page <pjgazzard@googlemail.com>
2013-12-26 16:54:46 +0000

    Separate alliance roster into a template.

--- engine/Default/alliance_roster.php
+++ engine/Default/alliance_roster.php
@@ -14,6 +14,7 @@ if(!isset($var['SortDesc'])) {
 }
 
 $alliance =& SmrAlliance::getAlliance($var['alliance_id'],$player->getGameID());
+$template->assignByRef('Alliance', $alliance);
 
 $template->assign('PageTopic',$alliance->getAllianceName() . ' (' . $alliance->getAllianceID() . ')');
 require_once(get_file_loc('menu.inc'));
@@ -21,7 +22,9 @@ create_alliance_menu($alliance->getAllianceID(),$alliance->getLeaderID());
 
 $varAction = isset($var['action']) ? $var['action'] : '';
 // Does anyone actually use these?
-if ($varAction == 'Show Alliance Roles') {
+$showRoles = $varAction == 'Show Alliance Roles';
+$template->assign('ShowRoles', $showRoles);
+if ($showRoles) {
 	// initialize with text
 	$roles = array();
 
@@ -34,19 +37,19 @@ if ($varAction == 'Show Alliance Roles') {
 	while ($db->nextRecord()) {
 		$roles[$db->getInt('role_id')] = $db->getField('role');
 	}
+	$template->assignByRef('Roles', $roles);
 
 	$container=create_container('alliance_roles_save_processing.php');
 	$container['alliance_id'] = $alliance->getAllianceID();
-	$form = create_form($container, 'Save Alliance Roles');
-	$PHP_OUTPUT.= $form['form'];
+	$template->assign('SaveAllianceRolesHREF', SmrSession::getNewHREF($container));
 }
 
 
 // If the alliance is the player's alliance they get live information
 // Otherwise it comes from the cache.
 $db->query('SELECT
-	sum(experience) as alliance_xp,
-	floor(avg(experience)) as alliance_avg
+	SUM(experience) AS alliance_xp,
+	FLOOR(AVG(experience)) AS alliance_avg
 	FROM player
 	WHERE alliance_id=' . $db->escapeNumber($alliance->getAllianceID()) . '
 	AND game_id = ' . $db->escapeNumber($alliance->getGameID()) . '
@@ -54,222 +57,49 @@ $db->query('SELECT
 );
 
 $db->nextRecord();
+$template->assign('AllianceExp', $db->getInt('alliance_xp'));
+$template->assign('AllianceAverageExp', $db->getInt('alliance_avg'));
 
-$PHP_OUTPUT.= '<div align="center">';
-$PHP_OUTPUT.= bbifyMessage($alliance->getDescription());
-if($account->hasPermission(PERMISSION_EDIT_ALLIANCE_DESCRIPTION)) {
-	$PHP_OUTPUT.= '<br /><br />';
-	$container=array();
-	$container['url'] = 'skeleton.php';
-	$container['body'] = 'alliance_stat.php';
+if($account->getAccountID() == $alliance->getLeaderID() || $account->hasPermission(PERMISSION_EDIT_ALLIANCE_DESCRIPTION)) {
+	$container = create_container('skeleton.php', 'alliance_stat.php');
 	$container['alliance_id'] = $alliance->getAllianceID();
-	$PHP_OUTPUT.=create_button($container,'Edit');
-}
-$PHP_OUTPUT.= '<br /><br />';
-
-$PHP_OUTPUT.= '
-<table class="standard inset">
-	<tr>
-		<th>Alliance Name</th>
-		<th>Total Experience</th>
-		<th>Average Experience</th>
-		<th>Members</th>
-	</tr>
-	<tr class="bold">
-		<td>';
-$PHP_OUTPUT.= $alliance->getAllianceName();
-$PHP_OUTPUT.= '
-		</td>
-		<td class="center shrink">';
-$PHP_OUTPUT.= $db->getField('alliance_xp');
-$PHP_OUTPUT.= '
-		</td>
-		<td class="center shrink">';
-$PHP_OUTPUT.= $db->getField('alliance_avg');
-$PHP_OUTPUT.= '
-		</td>
-		<td class="center shrink">';
-$PHP_OUTPUT.= $alliance->getNumMembers();
-$PHP_OUTPUT.= '
-		</td>';
-$PHP_OUTPUT.= '
-	</tr>
-</table>';
-
-$PHP_OUTPUT.= '</div><br />';
-
-$PHP_OUTPUT.= '<h2>Current Members</h2><br />';
-
-$PHP_OUTPUT.= '<div align="center">';
-
-$PHP_OUTPUT.= '
-<table class="standard fullwidth">
-	<tr>
-	<th>&nbsp;</th>
-	<th><a href="'.Globals::getAllianceRosterHREF($alliance->getAllianceID(),'getPlayerName',$var['SortKey']=='getPlayerName'?!$var['SortDesc']:false).'">Trader Name</a></th>
-	<th><a href="'.Globals::getAllianceRosterHREF($alliance->getAllianceID(),'getRaceName',$var['SortKey']=='getRaceName'?!$var['SortDesc']:false).'">Race</a></th>
-	<th><a href="'.Globals::getAllianceRosterHREF($alliance->getAllianceID(),'getExperience',$var['SortKey']=='getExperience'?!$var['SortDesc']:true).'">Experience</a></th>
-';
-
-if($varAction == 'Show Alliance Roles') {
-	$PHP_OUTPUT.= '<th>Role</th>';
+	$template->assign('EditAllianceDescriptionHREF', SmrSession::getNewHREF($container));
 }
 
-// Alliance members get to see active/inactive status of members
-if($player->getAllianceID() == $alliance->getAllianceID()) {
-	$PHP_OUTPUT.='<th class="shrink">Status</th>';
-	$ActiveIDs = $alliance->getActiveIDs();
-}
+$template->assign('SortPlayerNameHREF', Globals::getAllianceRosterHREF($alliance->getAllianceID(),'getPlayerName',$var['SortKey']=='getPlayerName'?!$var['SortDesc']:false));
+$template->assign('SortRaceNameHREF', Globals::getAllianceRosterHREF($alliance->getAllianceID(),'getRaceName',$var['SortKey']=='getRaceName'?!$var['SortDesc']:false));
+$template->assign('SortExperienceHREF', Globals::getAllianceRosterHREF($alliance->getAllianceID(),'getExperience',$var['SortKey']=='getExperience'?!$var['SortDesc']:true));
 
-$PHP_OUTPUT.= '</tr>';
-$count = 1;
-
-$db->query('SELECT * FROM player_has_alliance_role WHERE account_id = ' . $db->escapeNumber($player->getAccountID()) . ' AND game_id = ' . $db->escapeNumber($alliance->getGameID()) . ' AND alliance_id=' . $db->escapeNumber($alliance->getAllianceID()));
-if ($db->nextRecord()) {
-	$my_role_id = $db->getInt('role_id');
-}
-else {
-	$my_role_id = 0;
-}
 $db->query('SELECT 1 FROM alliance_has_roles WHERE alliance_id = ' . $db->escapeNumber($alliance->getAllianceID()) . ' AND game_id = ' . $db->escapeNumber($alliance->getGameID()) . '
-			AND role_id = ' . $db->escapeNumber($my_role_id) . ' AND change_roles = \'TRUE\'');
+			AND role_id = ' . $db->escapeNumber($player->getAllianceRole()) . ' AND change_roles = \'TRUE\'');
 $allowed = $db->nextRecord();
+$template->assign('CanChangeRoles', $allowed);
 
 $alliancePlayers =& SmrPlayer::getAlliancePlayers($player->getGameID(),$alliance->getAllianceID());
-
 if($var['SortKey']!='getExperience' || $var['SortDesc']!==true) {
 	Sorter::sortByNumMethod($alliancePlayers, $var['SortKey'], $var['SortDesc']);
 }
-
-foreach($alliancePlayers as &$alliancePlayer) {
-	$class='';
-	// check if this guy is the current guy
-	if ($player->equals($alliancePlayer)) {
-		$class .= 'bold';
-	}
-	if($alliancePlayer->getAccount()->isNewbie()) {
-		$class.= ' newbie';
-	}
-	if($class!='') {
-		$class = ' class="'.trim($class).'"';
-	}
-	$PHP_OUTPUT.= '<tr'.$class.'>';
-
-	$PHP_OUTPUT.= '<td class="center shrink">';
-
-	// counter
-	if ($alliancePlayer->getAccountID() == $alliance->getLeaderID()) {
-		$PHP_OUTPUT.= '*';
-	}
-	$PHP_OUTPUT.= ($count++);
-	$PHP_OUTPUT.= '</td><td>';
-
-	// player name
-	$PHP_OUTPUT.= $alliancePlayer->getLevelName();
-	$PHP_OUTPUT.= '&nbsp;';
-	$PHP_OUTPUT.=$alliancePlayer->getLinkedDisplayName(false);
-	$PHP_OUTPUT.= '</td><td class="center shrink">';
-
-	// race name (colored)
-	$container=create_container('skeleton.php', 'council_list.php');
-	$container['race_id'] = $alliancePlayer->getRaceID();
-
-	$PHP_OUTPUT.=create_link($container, $player->getColouredRaceName($alliancePlayer->getRaceID()));
-
-	// xp
-	$PHP_OUTPUT.= '</td><td class="shrink center">';
-	$PHP_OUTPUT.= $alliancePlayer->getExperience();
-	$PHP_OUTPUT.= '</td>';
-
-	// Roles
-	if ($varAction == 'Show Alliance Roles') {
-		$PHP_OUTPUT.= '<td class="shrink right">';
-
-		$db->query('SELECT role_id
-					FROM player_has_alliance_role
-					WHERE account_id=' . $db->escapeNumber($alliancePlayer->getAccountID()) . '
-					AND game_id=' . $db->escapeNumber($player->getGameID()) . '
-					AND alliance_id=' . $db->escapeNumber($alliancePlayer->getAllianceID()) . '
-					LIMIT 1');
-
-		if ($db->nextRecord()) {
-			$role_id = $db->getInt('role_id');
-		}
-		else {
-			$role_id = 0;
-		}
-
-		if ($allowed && $alliancePlayer->getAccountID() != $alliance->getLeaderID()) {
-			$PHP_OUTPUT.= '<select name="role[' . $alliancePlayer->getAccountID() . ']" id="InputFields">';
-			foreach ($roles as $curr_role_id => $role) {
-				$PHP_OUTPUT.= '<option value="' . $curr_role_id .'"';
-				if ($curr_role_id == $role_id) {
-					$PHP_OUTPUT.= ' selected="selected"';
-				}
-				$PHP_OUTPUT.= '>';
-				$PHP_OUTPUT.= stripslashes($role);
-				$PHP_OUTPUT.= '</option>';
-			}
-			$PHP_OUTPUT.= '</select>';
-		}
-		else {
-			$PHP_OUTPUT.= $roles[$role_id];
-		}
-		$PHP_OUTPUT.= '</td>';
-	}
-
-	if($player->getAllianceID() == $alliance->getAllianceID()) {
-		if(in_array($alliancePlayer->getAccountID(), $ActiveIDs)) {
-			$PHP_OUTPUT.= '<td class="center">Online</td>';
-		}
-		else if($player->getAccountID() == $alliance->getLeaderID() && $disabled = SmrAccount::getAccount($alliancePlayer->getAccountID())->isDisabled()) {
-			// Display ban status to account leader.
-				$PHP_OUTPUT .= '<td><p>Banned Until:<br/>' .  date(DATE_FULL_SHORT_SPLIT,$disabled['Time']) . '</p></td>';
-		}
-		else {
-			$PHP_OUTPUT.= '<td class="center">Offline</td>';
-		}
-	}
-	
-	$PHP_OUTPUT.= '</tr>';
-} unset($alliancePlayer);
-
-$PHP_OUTPUT.= '</table>';
-$PHP_OUTPUT.= '</div>';
+$template->assignByRef('AlliancePlayers', $alliancePlayers);
 
 if ($alliance->getAllianceID() == $player->getAllianceID()) {
-	$PHP_OUTPUT.= '<br /><h2>Options</h2><br />';
-	$container=array();
-	$container['url'] = 'skeleton.php';
-	$container['body'] = 'alliance_roster.php';
-	if($varAction == '' || $varAction == 'Hide Alliance Roles') {
-		$container['action'] = 'Show Alliance Roles';
-		$PHP_OUTPUT.=create_button($container,'Show Alliance Roles');
+	// Alliance members get to see active/inactive status of members
+	$template->assign('ActiveIDs', $alliance->getActiveIDs());
+	$container = create_container('skeleton.php', 'alliance_roster.php');
+	if($showRoles) {
+		$container['action'] = 'Hide Alliance Roles';
 	}
 	else {
-		$PHP_OUTPUT.= $form['submit'];
-		$PHP_OUTPUT.= '&nbsp;&nbsp;';
-		$container['action'] = 'Hide Alliance Roles';
-		$PHP_OUTPUT.=create_button($container,'Hide Alliance Roles');
-		$PHP_OUTPUT.= '</form>';
+		$container['action'] = 'Show Alliance Roles';
 	}
+	$template->assign('ToggleRolesHREF', SmrSession::getNewHREF($container));
 }
 
-if (($canJoin = $alliance->canJoinAlliance($player)) === true) {
-	$PHP_OUTPUT.= '<br />';
-	$container = array();
-	$container['url'] = 'alliance_join_processing.php';
+$canJoin = $alliance->canJoinAlliance($player);
+$template->assign('CanJoin', $canJoin);
+if ($canJoin === true) {
+	$container = create_container('alliance_join_processing.php');
 	$container['alliance_id'] = $alliance->getAllianceID();
-	$form = create_form($container, 'Join');
-	$PHP_OUTPUT.= $form['form'];
-	$PHP_OUTPUT.= 'Enter password to join alliance<br /><br />';
-	$PHP_OUTPUT.= '<input type="password" name="password" size="30">&nbsp;';
-	$PHP_OUTPUT.= $form['submit'];
-	$PHP_OUTPUT.= '</form>';
-}
-else if($canJoin !== false) {
-	$PHP_OUTPUT.= '<br />';
-	$PHP_OUTPUT .= $canJoin;
+	$template->assign('JoinHREF', SmrSession::getNewHREF($container));
 }
 
 ?>
---
+++ templates/Default/engine/Default/alliance_roster.php
@@ -0,0 +1,136 @@
+<?php
+
+$varAction = isset($var['action']) ? $var['action'] : '';
+// Does anyone actually use these?
+if ($ShowRoles) { ?>
+	<form class="standard" method="POST')" action="<?php echo $SaveAllianceRolesHREF; ?>"><?php
+} ?>
+
+
+<div align="center"><?php
+	echo bbifyMessage($Alliance->getDescription());
+	if(isset($EditAllianceDescriptionHREF)) { ?>
+		<br /><br />
+		<div class="buttonA"><a class="buttonA" href="<?php echo $EditAllianceDescriptionHREF; ?>">&nbsp;Edit&nbsp;</a></div><?php
+	} ?>
+
+	<br /><br />
+
+	<table class="standard inset">
+		<tr>
+			<th>Alliance Name</th>
+			<th>Total Experience</th>
+			<th>Average Experience</th>
+			<th>Members</th>
+		</tr>
+		<tr class="bold">
+			<td><?php echo $Alliance->getAllianceName(); ?></td>
+			<td class="center shrink"><?php echo $AllianceExp; ?></td>
+			<td class="center shrink"><?php echo $AllianceAverageExp; ?></td>
+			<td class="center shrink"><?php echo $Alliance->getNumMembers(); ?></td>
+		</tr>
+	</table>
+</div>
+<br />
+
+<h2>Current Members</h2><br />
+
+<div align="center">
+	<table class="standard fullwidth">
+		<tr>
+			<th>&nbsp;</th>
+			<th><a href="<?php echo $SortPlayerNameHREF; ?>">Trader Name</a></th>
+			<th><a href="<?php echo $SortRaceNameHREF; ?>">Race</a></th>
+			<th><a href="<?php echo $SortExperienceHREF; ?>">Experience</a></th><?php
+			if($ShowRoles) { ?>
+				<th>Role</th><?php
+			}
+			if(isset($ActiveIDs)) { ?>
+				<th class="shrink">Status</th><?php
+			} ?>
+		</tr><?php
+
+		foreach($AlliancePlayers as &$AlliancePlayer) {
+			$Count = 1;
+			$Class = '';
+			// check if this guy is the current guy
+			if ($ThisPlayer->equals($AlliancePlayer)) {
+				$Class .= 'bold';
+			}
+			if($AlliancePlayer->getAccount()->isNewbie()) {
+				$Class.= ' newbie';
+			}
+			if($Class!='') {
+				$Class = ' class="'.trim($Class).'"';
+			} ?>
+			<tr<?php echo $class; ?>>
+
+				<td class="center shrink"><?php
+					if ($AlliancePlayer->getAccountID() == $Alliance->getLeaderID()) { ?>*<?php }
+					echo $Count++; ?>
+				</td>
+				<td><?php
+					echo $AlliancePlayer->getLevelName(); ?>&nbsp;<?php echo $AlliancePlayer->getLinkedDisplayName(false); ?>
+				</td>
+				<td class="center shrink"><?php
+					echo $ThisPlayer->getColouredRaceName($AlliancePlayer->getRaceID()); ?>
+				</td>
+				<td class="shrink center"><?php
+					echo $AlliancePlayer->getExperience(); ?>
+				</td><?php
+				if ($ShowRoles) { ?>
+					<td class="shrink right"><?php
+						if ($CanChangeRoles && $AlliancePlayer->getAccountID() != $Alliance->getLeaderID()) {
+							$PlayerRole = $AlliancePlayer->getRoleID(); ?>
+							<select name="role[<?php echo $AlliancePlayer->getAccountID(); ?>]" id="InputFields"><?php
+								foreach ($Roles as $RoleID => $Role) { ?>
+									<option value="<?php echo $RoleID; ?>"<?php
+									if ($RoleID == $PlayerRole) { ?>
+										selected="selected"<?php
+									} ?>><?php
+										echo $Role; ?>
+									</option><?php
+								} ?>
+							</select><?php
+						} else {
+							echo $Roles[$PlayerRole];
+						} ?>
+					</td><?php
+				}
+
+				if($ThisPlayer->getAllianceID() == $Alliance->getAllianceID()) {
+					if(in_array($AlliancePlayer->getAccountID(), $ActiveIDs)) { ?>
+						<td class="center">Online</td><?php
+					}
+					else if($ThisPlayer->getAccountID() == $Alliance->getLeaderID() && $Disabled = SmrAccount::getAccount($AlliancePlayer->getAccountID())->isDisabled()) { ?>
+						<td><p>Banned Until:<br/><?php echo date(DATE_FULL_SHORT_SPLIT,$Disabled['Time']); ?></p></td><?php
+					}
+					else { ?>
+						<td class="center">Offline</td><?php
+					}
+				} ?>
+			</tr><?php
+		} ?>
+	</table>
+</div><?php
+
+if ($Alliance->getAllianceID() == $ThisPlayer->getAllianceID()) { ?>
+	<br /><h2>Options</h2><br /><?php
+	if ($ShowRoles) { ?>
+		<input class="submit" type="submit" name="action" value="Save Alliance Roles"></form>&nbsp;&nbsp;<?php
+	} ?>
+	<div class="buttonA"><a class="buttonA" href="<?php echo $ToggleRolesHREF; ?>">&nbsp;<?php if ($ShowRoles) { ?>Hide Alliance Roles<?php } else { ?>Hide Alliance Roles<?php } ?>&nbsp;</a></div><?php
+}
+
+if ($CanJoin === true) { ?>
+	<br />
+	<form class="standard" method="POST" action="<?php echo $JoinHREF; ?>">
+		Enter password to join alliance<br /><br />
+		<input type="password" name="password" size="30">&nbsp;<input class="submit" type="submit" name="action" value="Join">
+	</form><?php
+}
+else if($CanJoin !== false) { ?>
+	<br /><?php
+	echo $canJoin;
+}
+?>
