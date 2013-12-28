a79215095b25906f234f2c23c21aa9902a410db4
Page <pjgazzard@googlemail.com>
2013-12-28 22:42:48 +0000

    Update to jquery v1.10.2

--- templates/Default/engine/Default/skeleton.php
+++ templates/Default/engine/Default/skeleton.php
@@ -20,7 +20,7 @@
 			$this->includeTemplate($HeaderTemplateInclude);
 		} ?>
 		<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 		<script type="text/javascript" src="js/smr15.js"></script>
 	</head>
