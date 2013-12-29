257b8ba91a196f40a84aaf5d5e8dc44a55fad0e3
Page <pjgazzard@googlemail.com>
2013-12-29 17:20:40 +0000

    Some styling changes for the messageboard.

--- templates/Default/engine/Default/alliance_message.php
+++ templates/Default/engine/Default/alliance_message.php
@@ -1,40 +1,38 @@
 <?php
 if (count($Threads) > 0) { ?>
-	<div align="center">
-		<table id="topic-list" class="standard inset">
-			<thead>
-				<tr>
-					<th class="sort" data-sort="topic">Topic</th>
-					<th class="sort shrink" data-sort="author">Author</th>
-					<th class="sort shrink" data-sort="replies">Replies</th>
-					<th class="sort shrink" data-sort="lastReply">Last Reply</th>
-				</tr>
-			</thead>
-			<tbody class="list"><?php
-				foreach($Threads as $Thread) { ?>
-					<tr id="topic-<?php echo $Thread['ThreadID']; ?>" class="ajax">
-						<td class="topic"><?php
-							if ($Thread['Unread']) {
-								?><b><?php
-							}
-							?><a href="<?php echo $Thread['ViewHref']; ?>"><?php echo $Thread['Topic']; ?></a><?php
-							if ($Thread['Unread']) {
-								?></b><?php
-							} ?>
-						</td>
-						<td class="author noWrap"><?php
-							echo $Thread['Sender'];
-							if($Thread['CanDelete']) {
-								?><br /><small><a href="<?php echo $Thread['DeleteHref']; ?>">Delete Thread!</a></small><?php
-							} ?>
-						</td>
-						<td class="replies center"><?php echo $Thread['Replies']; ?></td>
-						<td class="lastReply noWrap"><?php echo date(DATE_FULL_SHORT, $Thread['SendTime']); ?></td>
-					</tr><?php
-				} ?>
-			</tbody>
-		</table>
-	</div><br /><?php
+	<table id="topic-list" class="centered standard inset">
+		<thead>
+			<tr>
+				<th class="sort" data-sort="topic">Topic</th>
+				<th class="sort shrink" data-sort="author">Author</th>
+				<th class="sort shrink" data-sort="replies">Replies</th>
+				<th class="sort shrink" data-sort="lastReply">Last Reply</th>
+			</tr>
+		</thead>
+		<tbody class="list"><?php
+			foreach($Threads as $Thread) { ?>
+				<tr id="topic-<?php echo $Thread['ThreadID']; ?>" class="ajax">
+					<td class="topic"><?php
+						if ($Thread['Unread']) {
+							?><b><?php
+						}
+						?><a href="<?php echo $Thread['ViewHref']; ?>"><?php echo $Thread['Topic']; ?></a><?php
+						if ($Thread['Unread']) {
+							?></b><?php
+						} ?>
+					</td>
+					<td class="author noWrap"><?php
+						echo $Thread['Sender'];
+						if($Thread['CanDelete']) {
+							?><br /><small><a href="<?php echo $Thread['DeleteHref']; ?>">Delete Thread!</a></small><?php
+						} ?>
+					</td>
+					<td class="replies center"><?php echo $Thread['Replies']; ?></td>
+					<td class="lastReply noWrap"><?php echo date(DATE_FULL_SHORT, $Thread['SendTime']); ?></td>
+				</tr><?php
+			} ?>
+		</tbody>
+	</table><br /><?php
 }
 
 if (isset($CreateNewThreadFormHref)) { ?>
@@ -45,7 +43,7 @@ if (isset($CreateNewThreadFormHref)) { ?>
 		<tr>
 			<td class="top">Topic:&nbsp;</td>
 			<td class="mb"><input type="text" name="topic" size="30" value="<?php if(isset($Topic)) { echo htmlspecialchars($Topic); } ?>"></td>
-			<td style="text-align:left;">For Alliance Eyes Only:<input id="InputFields" name="allEyesOnly" type="checkbox"<?php if(isset($AllianceEyesOnly) && $AllianceEyesOnly) { ?>checked="checked" <?php } ?>></td>
+			<td>For Alliance Eyes Only:<input id="InputFields" name="allEyesOnly" type="checkbox"<?php if(isset($AllianceEyesOnly) && $AllianceEyesOnly) { ?>checked="checked" <?php } ?>></td>
 		</tr>
 		<tr>
 			<td class="top">Body:&nbsp;</td>
