3a228a55bfc336110824f816b757272f1681f660
Page <pjgazzard@googlemail.com>
2013-12-29 02:36:24 +0000

    Make planet list auto-refresh play nicely with list.js

--- templates/Default/engine/Default/alliance_planets.php
+++ templates/Default/engine/Default/alliance_planets.php
@@ -1,8 +1,8 @@
 <div align="center">
-<a href="http://wiki.smrealms.de/index.php?title=Planets" target="_blank"><img align="right" src="images/silk/help.png" width="16" height="16" alt="Wiki Link" title="Goto SMR Wiki: Planets"/></a>
+	<a href="http://wiki.smrealms.de/index.php?title=Planets" target="_blank"><img align="right" src="images/silk/help.png" width="16" height="16" alt="Wiki Link" title="Goto SMR Wiki: Planets"/></a>
 	<?php
 	if (count($AlliancePlanets) > 0) { ?>
-		Your alliance currently has <?php echo count($AlliancePlanets); ?> planets in the universe!<br /><br /><?php
+	Your alliance currently has <span id="numplanets"><?php echo count($AlliancePlanets); ?></span> planets in the universe!<br /><br /><?php
 		$this->includeTemplate('includes/PlanetList.inc',array('Planets'=>&$AlliancePlanets));
 	}
 	else { ?>
