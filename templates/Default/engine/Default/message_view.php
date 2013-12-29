d0308a1fb58c42665381b12d55e629f81263957c
Page <pjgazzard@googlemail.com>
2013-12-29 19:26:53 +0000

    Allow sorting of message folders.

--- templates/Default/engine/Default/message_view.php
+++ templates/Default/engine/Default/message_view.php
@@ -2,23 +2,36 @@
 if (isset($MessageBoxes)) { ?>
 	<p>Please choose your Message folder!</p>
 
-	<table class="standard">
-		<tr>
-			<th>Folder</th>
-			<th>Messages</th>
-			<th>&nbsp;</th>
-		</tr><?php
-		foreach ($MessageBoxes as $MessageBox) { ?>
-			<tr<?php if($MessageBox['HasUnread']) { ?>  class="bold"<?php } ?>>
-				<td>
-					<a href="<?php echo $MessageBox['ViewHref']; ?>"><?php echo $MessageBox['Name']; ?></a>
-				</td>
-				<td align="center" class="yellow"><?php echo $MessageBox['MessageCount']; ?></td>
-				<td><a href="<?php echo $MessageBox['DeleteHref']; ?>">Empty Read Messages</a></td>
-			</tr><?php
-		} ?>
+	<table id="folders" class="standard">
+		<thead>
+			<tr>
+				<th class="sort" data-sort="name">Folder</th>
+				<th class="sort" data-sort="messages">Messages</th>
+				<th>&nbsp;</th>
+			</tr>
+		</thead>
+		<tbody class="list"><?php
+			foreach ($MessageBoxes as $MessageBox) { ?>
+				<tr id="<?php echo str_replace(' ','-',$MessageBox['Name']);?>" class="ajax<?php if($MessageBox['HasUnread']) { ?> bold<?php } ?>">
+					<td class="name">
+						<a href="<?php echo $MessageBox['ViewHref']; ?>"><?php echo $MessageBox['Name']; ?></a>
+					</td>
+					<td class="messages center yellow"><?php echo $MessageBox['MessageCount']; ?></td>
+					<td><a href="<?php echo $MessageBox['DeleteHref']; ?>">Empty Read Messages</a></td>
+				</tr><?php
+			} ?>
+		</tbody>
 	</table>
-	<p><a href="<?php echo $ManageBlacklistLink; ?>">Manage Player Blacklist</a></p><?php
+	<p><a href="<?php echo $ManageBlacklistLink; ?>">Manage Player Blacklist</a></p>
+	<script type="text/javascript" src="js/list.1.0.0.custom.min.js"></script>
+	<script>
+	var list = new List('folders', {
+		valueNames: ['name', 'messages'],
+		sortFunction: function(a, b) {
+			return list.sort.naturalSort(a.values()[this.valueName].replace(/<.*?>|,/g,''), b.values()[this.valueName].replace(/<.*?>|,/g,''), this);
+		}
+	});
+	</script><?php
 }
 else {
 	if ($MessageBox['Type'] == MSG_GLOBAL) { ?>
