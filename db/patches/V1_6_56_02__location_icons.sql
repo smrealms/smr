3935282cdb90ddf7e6d45d3d0faca41872c5db1a
wcyrek <github@witoldcyrek.com>
2014-01-30 22:34:42 -0500

    Adding more changes
    Added ability to add planet by type (default adds type 1)
    Added new colors (temporary) along with new icons
    Add ability to limit attackers on planet by planet type (Currently uses
    min of planet type and universal limiter)

--- db/patches/V1_6_56_02__location_icons.sql
+++ db/patches/V1_6_56_02__location_icons.sql
@@ -2,14 +2,25 @@
 
 #Fed Beacon
 UPDATE location_type lt SET lt.location_image = 'images/beacon.png' WHERE lt.location_type_id = 201;
-UPDATE location_type lt SET lt.location_image = 'images/beacon.png' WHERE lt.location_type_id = 203;
-UPDATE location_type lt SET lt.location_image = 'images/beacon.png' WHERE lt.location_type_id = 204;
-UPDATE location_type lt SET lt.location_image = 'images/beacon.png' WHERE lt.location_type_id = 205;
-UPDATE location_type lt SET lt.location_image = 'images/beacon.png' WHERE lt.location_type_id = 206;
-UPDATE location_type lt SET lt.location_image = 'images/beacon.png' WHERE lt.location_type_id = 207;
-UPDATE location_type lt SET lt.location_image = 'images/beacon.png' WHERE lt.location_type_id = 208;
-UPDATE location_type lt SET lt.location_image = 'images/beacon.png' WHERE lt.location_type_id = 209;
-UPDATE location_type lt SET lt.location_image = 'images/beacon.png' WHERE lt.location_type_id = 210;
+UPDATE location_type lt SET lt.location_image = 'images/beacon_als.png' WHERE lt.location_type_id = 203;
+UPDATE location_type lt SET lt.location_image = 'images/beacon_cre.png' WHERE lt.location_type_id = 204;
+UPDATE location_type lt SET lt.location_image = 'images/beacon_hum.png' WHERE lt.location_type_id = 205;
+UPDATE location_type lt SET lt.location_image = 'images/beacon_ikt.png' WHERE lt.location_type_id = 206;
+UPDATE location_type lt SET lt.location_image = 'images/beacon_sal.png' WHERE lt.location_type_id = 207;
+UPDATE location_type lt SET lt.location_image = 'images/beacon_the.png' WHERE lt.location_type_id = 208;
+UPDATE location_type lt SET lt.location_image = 'images/beacon_wq.png' WHERE lt.location_type_id = 209;
+UPDATE location_type lt SET lt.location_image = 'images/beacon_nij.png' WHERE lt.location_type_id = 210;
+
+#HQs
+UPDATE location_type lt SET lt.location_image = 'images/government.png' WHERE lt.location_type_id = 101;
+UPDATE location_type lt SET lt.location_image = 'images/government_als.png' WHERE lt.location_type_id = 103;
+UPDATE location_type lt SET lt.location_image = 'images/government_cre.png' WHERE lt.location_type_id = 104;
+UPDATE location_type lt SET lt.location_image = 'images/government_hum.png' WHERE lt.location_type_id = 105;
+UPDATE location_type lt SET lt.location_image = 'images/government_ikt.png' WHERE lt.location_type_id = 106;
+UPDATE location_type lt SET lt.location_image = 'images/government_sal.png' WHERE lt.location_type_id = 107;
+UPDATE location_type lt SET lt.location_image = 'images/government_the.png' WHERE lt.location_type_id = 108;
+UPDATE location_type lt SET lt.location_image = 'images/government_wq.png' WHERE lt.location_type_id = 109;
+UPDATE location_type lt SET lt.location_image = 'images/government_nij.png' WHERE lt.location_type_id = 110;
 
 #Weapon Shops
 UPDATE location_type lt SET lt.location_image = 'images/weapon_shop.png' WHERE lt.location_image = 'images/weapon_shop.gif';
