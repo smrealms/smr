4f8a2a55763eb07df7e0fcd36dbecca2af9bbc90
Page <pjgazzard@googlemail.com>
2014-02-09 23:38:19 +0000

    Remove some redundant code.

--- engine/Default/planet_defense.php
+++ engine/Default/planet_defense.php
@@ -10,18 +10,13 @@ $template->assign('ThisPlanet',$planet);
 require_once(get_file_loc('menu.inc'));
 create_planet_menu($planet);
 
-$container = array();
-$container['url'] = 'planet_defense_processing.php';
+$container = create_container('planet_defense_processing.php');
 $container['type_id'] = 1;
 $template->assign('TransferShieldsHref',SmrSession::getNewHREF($container));
 
-$container = array();
-$container['url'] = 'planet_defense_processing.php';
 $container['type_id'] = 4;
 $template->assign('TransferCDsHref',SmrSession::getNewHREF($container));
 
-$container = array();
-$container['url'] = 'planet_defense_processing.php';
 $container['type_id'] = 2;
 $template->assign('TransferArmourHref',SmrSession::getNewHREF($container));
 
