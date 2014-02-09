90a7278433dd9a7f962fe547f7dbc70d67167413
Page <pjgazzard@googlemail.com>
2014-02-09 16:00:42 +0000

    Formatting of SQL patches.

--- db/patches/V1_6_56_01__adding_planet_type_support.sql
+++ db/patches/V1_6_56_01__adding_planet_type_support.sql
@@ -1,24 +1,23 @@
-CREATE TABLE IF NOT EXISTS `planet_type` (
-	`planet_type_id` int(10) unsigned AUTO_INCREMENT PRIMARY KEY,
-	`planet_type_name` varchar( 100 ),
-	`planet_type_description` varchar( 100 ),
-	`planet_image_link` varchar( 100 ),
-	`planet_max_attackers` int(10) unsigned NOT NULL DEFAULT '10',
-	`planet_max_landed` int(10) unsigned NOT NULL DEFAULT '0';
-	
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS planet_type (
+	planet_type_id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	planet_type_name VARCHAR( 100 ),
+	planet_type_description VARCHAR( 100 ),
+	planet_image_link VARCHAR( 100 ),
+	planet_max_attackers INT(10) UNSIGNED NOT NULL,
+	planet_max_landed INT(10) UNSIGNED NOT NULL
+);
 
-INSERT INTO `planet_type` (`planet_type_id`, `planet_type_name`, `planet_type_description`, `planet_image_link`, `planet_max_attackers`, `planet_max_landed`) VALUES
+INSERT INTO planet_type (planet_type_id, planet_type_name, planet_type_description, planet_image_link, planet_max_attackers, planet_max_landed) VALUES
 (NULL, 'Terran Planet', 'A lush world, with forests, seas, sweeping meadows, and indigenous lifeforms', 'images/planet1.png', '10', '0'),
 (NULL, 'Arid Planet', 'A world mostly devoid of surface water, but capable of supporting life', 'images/planet2.png', '5', '5');
 
-CREATE TABLE IF NOT EXISTS `planet_type_has_section` (
-	planet_type_id int(10) unsigned NOT NULL,
+CREATE TABLE IF NOT EXISTS planet_type_has_section (
+	planet_type_id INT(10) UNSIGNED NOT NULL,
 	planet_section ENUM('CONSTRUCTION', 'DEFENSE', 'FINANCE', 'STOCKPILE', 'OWNERSHIP') NOT NULL,
 	PRIMARY KEY (planet_type_id, planet_section)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+);
 
-INSERT INTO `planet_type_has_section` (`planet_type_id`, `planet_section`) VALUES
+INSERT INTO planet_type_has_section (planet_type_id, planet_section) VALUES
 ('1', 'CONSTRUCTION'),
 ('1', 'DEFENSE'),
 ('1', 'FINANCE'),
@@ -29,43 +28,43 @@ INSERT INTO `planet_type_has_section` (`planet_type_id`, `planet_section`) VALUE
 ('2', 'STOCKPILE'),
 ('2', 'OWNERSHIP');
 
-CREATE TABLE IF NOT EXISTS `planet_can_build` (
-	`planet_type_id` int(10) unsigned NOT NULL DEFAULT '0',
-	`construction_id` int(10) unsigned NOT NULL DEFAULT '0',
-	`max_amount` int(10) unsigned NOT NULL DEFAULT '0',
-	`cost_time` int(10) unsigned NOT NULL DEFAULT '0',
-	`cost_credit` int(10) unsigned NOT NULL DEFAULT '0',
-	`exp_gain` int(10) unsigned NOT NULL DEFAULT '0'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS planet_can_build (
+	planet_type_id INT(10) UNSIGNED NOT NULL,
+	construction_id INT(10) UNSIGNED NOT NULL,
+	max_amount INT(10) UNSIGNED NOT NULL,
+	cost_time INT(10) UNSIGNED NOT NULL,
+	cost_credit INT(10) UNSIGNED NOT NULL,
+	exp_gain INT(10) UNSIGNED NOT NULL
+);
 
-ALTER TABLE `planet`
-ADD `planet_type_id` int(10) unsigned NOT NULL DEFAULT '1';
+ALTER TABLE planet
+ADD planet_type_id INT(10) UNSIGNED NOT NULL DEFAULT 1;
 
-ALTER TABLE `planet_construction`
-MODIFY `construction_description` VARCHAR( 100 );
-UPDATE `planet_construction`
-SET `construction_description` = 'Increases planet&rsquo;s maximum shield capacity by 100 shields' WHERE `construction_id` = 1;
-UPDATE `planet_construction`
-SET `construction_description` = 'Increases planet&rsquo;s maximum drone capacity by 20 drones' WHERE `construction_id` = 2;
-UPDATE `planet_construction`
-SET `construction_description` = 'Builds a turret capable of dealing 250 damage to enemy ships when fired on' WHERE `construction_id` = 3;
+ALTER TABLE planet_construction
+MODIFY construction_description VARCHAR( 100 );
+UPDATE planet_construction
+SET construction_description = 'Increases planet&rsquo;s maximum shield capacity by 100 shields' WHERE construction_id = 1;
+UPDATE planet_construction
+SET construction_description = 'Increases planet&rsquo;s maximum drone capacity by 20 drones' WHERE construction_id = 2;
+UPDATE planet_construction
+SET construction_description = 'Builds a turret capable of dealing 250 damage to enemy ships when fired on' WHERE construction_id = 3;
 
-ALTER TABLE `planet_construction`
-ADD `construction_image` varchar( 100 ) AFTER construction_description;
-UPDATE `planet_construction`
-SET `construction_image` = 'images/generator.png' WHERE `construction_id` = 1;
-UPDATE `planet_construction`
-SET `construction_image` = 'images/hangar.png' WHERE `construction_id` = 2;
-UPDATE `planet_construction`
-SET `construction_image` = 'images/turret.png' WHERE `construction_id` = 3;
+ALTER TABLE planet_construction
+ADD construction_image VARCHAR( 100 ) AFTER construction_description;
+UPDATE planet_construction
+SET construction_image = 'images/generator.png' WHERE construction_id = 1;
+UPDATE planet_construction
+SET construction_image = 'images/hangar.png' WHERE construction_id = 2;
+UPDATE planet_construction
+SET construction_image = 'images/turret.png' WHERE construction_id = 3;
 
-ALTER TABLE `planet`
-ADD `armour` int(10) unsigned NOT NULL DEFAULT '0' AFTER shields;
+ALTER TABLE planet
+ADD armour INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER shields;
 
-INSERT INTO `planet_construction` (`construction_id`, `construction_name`, `construction_description`, `construction_image`, `max_construction`, `exp_gain`) VALUES 
-(4, 'Bunker', 'Increases planet&rsquo;s maximum armour capacity by 100 armour', 'images/bunker.png', 0, 90);
+INSERT INTO planet_construction (construction_id, construction_name, construction_description, construction_image, max_construction, exp_gain) VALUES 
+(4, 'Bunker', 'Increases planet\'s maximum armour capacity by 100 armour', 'images/bunker.png', 0, 90);
 
-INSERT INTO `planet_can_build` (`planet_type_id`, `construction_id`, `max_amount`, `cost_time`, `cost_credit`, `exp_gain`) VALUES 
+INSERT INTO planet_can_build (planet_type_id, construction_id, max_amount, cost_time, cost_credit, exp_gain) VALUES 
 ('1', '1', '25', '10800', '100000', '90'),
 ('1', '2', '100', '21600', '100000', '180'),
 ('1', '3', '10', '64800', '1000000', '540'),
@@ -75,12 +74,12 @@ INSERT INTO `planet_can_build` (`planet_type_id`, `construction_id`, `max_amount
 ('2', '3', '15', '21600', '750000', '180'),
 ('2', '4', '25', '10800', '50000', '90');
 
-INSERT INTO `planet_cost_good` (`construction_id`, `good_id`, `amount`) VALUES 
+INSERT INTO planet_cost_good (construction_id, good_id, amount) VALUES 
 ('4', '3', '35'),
 ('4', '1', '20'),
 ('4', '8', '15');
 
 
 
-DROP TABLE `planet_cost_time`;
-DROP TABLE `planet_cost_credits`;
+DROP TABLE planet_cost_time;
+DROP TABLE planet_cost_credits;
--- db/patches/V1_6_56_02__location_icons.sql
+++ db/patches/V1_6_56_02__location_icons.sql
@@ -1,112 +1,112 @@
 /* adding some icons */
 
 #Fed Beacon
-UPDATE location_type lt SET lt.location_image = 'images/beacon.png' WHERE lt.location_type_id = 201;
-UPDATE location_type lt SET lt.location_image = 'images/beacon_als.png' WHERE lt.location_type_id = 203;
-UPDATE location_type lt SET lt.location_image = 'images/beacon_cre.png' WHERE lt.location_type_id = 204;
-UPDATE location_type lt SET lt.location_image = 'images/beacon_hum.png' WHERE lt.location_type_id = 205;
-UPDATE location_type lt SET lt.location_image = 'images/beacon_ikt.png' WHERE lt.location_type_id = 206;
-UPDATE location_type lt SET lt.location_image = 'images/beacon_sal.png' WHERE lt.location_type_id = 207;
-UPDATE location_type lt SET lt.location_image = 'images/beacon_the.png' WHERE lt.location_type_id = 208;
-UPDATE location_type lt SET lt.location_image = 'images/beacon_wq.png' WHERE lt.location_type_id = 209;
-UPDATE location_type lt SET lt.location_image = 'images/beacon_nij.png' WHERE lt.location_type_id = 210;
+UPDATE location_type SET location_image = 'images/beacon.png' WHERE location_type_id = 201;
+UPDATE location_type SET location_image = 'images/beacon_als.png' WHERE location_type_id = 203;
+UPDATE location_type SET location_image = 'images/beacon_cre.png' WHERE location_type_id = 204;
+UPDATE location_type SET location_image = 'images/beacon_hum.png' WHERE location_type_id = 205;
+UPDATE location_type SET location_image = 'images/beacon_ikt.png' WHERE location_type_id = 206;
+UPDATE location_type SET location_image = 'images/beacon_sal.png' WHERE location_type_id = 207;
+UPDATE location_type SET location_image = 'images/beacon_the.png' WHERE location_type_id = 208;
+UPDATE location_type SET location_image = 'images/beacon_wq.png' WHERE location_type_id = 209;
+UPDATE location_type SET location_image = 'images/beacon_nij.png' WHERE location_type_id = 210;
 
 #HQs
-UPDATE location_type lt SET lt.location_image = 'images/government.png' WHERE lt.location_type_id = 101;
-UPDATE location_type lt SET lt.location_image = 'images/government_als.png' WHERE lt.location_type_id = 103;
-UPDATE location_type lt SET lt.location_image = 'images/government_cre.png' WHERE lt.location_type_id = 104;
-UPDATE location_type lt SET lt.location_image = 'images/government_hum.png' WHERE lt.location_type_id = 105;
-UPDATE location_type lt SET lt.location_image = 'images/government_ikt.png' WHERE lt.location_type_id = 106;
-UPDATE location_type lt SET lt.location_image = 'images/government_sal.png' WHERE lt.location_type_id = 107;
-UPDATE location_type lt SET lt.location_image = 'images/government_the.png' WHERE lt.location_type_id = 108;
-UPDATE location_type lt SET lt.location_image = 'images/government_wq.png' WHERE lt.location_type_id = 109;
-UPDATE location_type lt SET lt.location_image = 'images/government_nij.png' WHERE lt.location_type_id = 110;
+UPDATE location_type SET location_image = 'images/government.png' WHERE location_type_id = 101;
+UPDATE location_type SET location_image = 'images/government_als.png' WHERE location_type_id = 103;
+UPDATE location_type SET location_image = 'images/government_cre.png' WHERE location_type_id = 104;
+UPDATE location_type SET location_image = 'images/government_hum.png' WHERE location_type_id = 105;
+UPDATE location_type SET location_image = 'images/government_ikt.png' WHERE location_type_id = 106;
+UPDATE location_type SET location_image = 'images/government_sal.png' WHERE location_type_id = 107;
+UPDATE location_type SET location_image = 'images/government_the.png' WHERE location_type_id = 108;
+UPDATE location_type SET location_image = 'images/government_wq.png' WHERE location_type_id = 109;
+UPDATE location_type SET location_image = 'images/government_nij.png' WHERE location_type_id = 110;
 
 #Weapon Shops
-UPDATE location_type lt SET lt.location_image = 'images/weapon_shop.png' WHERE lt.location_image = 'images/weapon_shop.gif';
+UPDATE location_type SET location_image = 'images/weapon_shop.png' WHERE location_image = 'images/weapon_shop.gif';
 
 #Allrenches
-UPDATE location_type lt SET lt.location_image = 'images/hardware2.png' WHERE lt.location_image = 'images/hardware.png';
+UPDATE location_type SET location_image = 'images/hardware2.png' WHERE location_image = 'images/hardware.png';
 
 #Allships
-UPDATE location_type lt SET lt.location_image = 'images/shipdealer.png' WHERE lt.location_image = 'images/shipdealer.gif';
+UPDATE location_type SET location_image = 'images/shipdealer.png' WHERE location_image = 'images/shipdealer.gif';
 
 #Add colors for races
-ALTER TABLE `race` 
-ADD `race_color` VARCHAR(6) AFTER `race_description`;
+ALTER TABLE race 
+ADD race_color VARCHAR(6) AFTER race_description;
 
-UPDATE `race` 
-SET `race_color` = 'FF8080' WHERE `race_id` = 9;
-UPDATE `race` 
-SET `race_color` = '804040' WHERE `race_id` = 8; 
-UPDATE `race` 
-SET `race_color` = '800000' WHERE `race_id` = 7; 
-UPDATE `race` 
-SET `race_color` = '00AA00' WHERE `race_id` = 6; 
-UPDATE `race` 
-SET `race_color` = 'BFBFFF' WHERE `race_id` = 5; 
-UPDATE `race` 
-SET `race_color` = '0000FF' WHERE `race_id` = 4; 
-UPDATE `race` 
-SET `race_color` = 'FF8000' WHERE `race_id` = 3; 
-UPDATE `race` 
-SET `race_color` = 'FF00FF' WHERE `race_id` = 2;
-UPDATE `race` 
-SET `race_color` = 'FFD800' WHERE `race_id` = 1;
+UPDATE race 
+SET race_color = 'FF8080' WHERE race_id = 9;
+UPDATE race 
+SET race_color = '804040' WHERE race_id = 8; 
+UPDATE race 
+SET race_color = '800000' WHERE race_id = 7; 
+UPDATE race 
+SET race_color = '00AA00' WHERE race_id = 6; 
+UPDATE race 
+SET race_color = 'BFBFFF' WHERE race_id = 5; 
+UPDATE race 
+SET race_color = '0000FF' WHERE race_id = 4; 
+UPDATE race 
+SET race_color = 'FF8000' WHERE race_id = 3; 
+UPDATE race 
+SET race_color = 'FF00FF' WHERE race_id = 2;
+UPDATE race 
+SET race_color = 'FFD800' WHERE race_id = 1;
 
 
 #Add new bank icons
-UPDATE `location_type`
-SET `location_image` = 'images/bank5.png' WHERE `location_type_id` = 701;
-UPDATE `location_type`
-SET `location_image` = 'images/bank3.png' WHERE `location_type_id` = 702;
-UPDATE `location_type`
-SET `location_image` = 'images/bank2.png' WHERE `location_type_id` = 703;
-UPDATE `location_type`
-SET `location_image` = 'images/bank4.png' WHERE `location_type_id` = 704;
+UPDATE location_type
+SET location_image = 'images/bank5.png' WHERE location_type_id = 701;
+UPDATE location_type
+SET location_image = 'images/bank3.png' WHERE location_type_id = 702;
+UPDATE location_type
+SET location_image = 'images/bank2.png' WHERE location_type_id = 703;
+UPDATE location_type
+SET location_image = 'images/bank4.png' WHERE location_type_id = 704;
 
 #Add new shipdealer icons
-UPDATE `location_type`
-SET `location_image` = 'images/shipdealer_als.png' WHERE `location_type_id` = 401;
-UPDATE `location_type`
-SET `location_image` = 'images/shipdealer_cre.png' WHERE `location_type_id` = 402;
-UPDATE `location_type`
-SET `location_image` = 'images/shipdealer_hum.png' WHERE `location_type_id` = 403;
-UPDATE `location_type`
-SET `location_image` = 'images/shipdealer_ikt.png' WHERE `location_type_id` = 404;
-UPDATE `location_type`
-SET `location_image` = 'images/shipdealer_sal.png' WHERE `location_type_id` = 405;
-UPDATE `location_type`
-SET `location_image` = 'images/shipdealer_the.png' WHERE `location_type_id` = 406;
-UPDATE `location_type`
-SET `location_image` = 'images/shipdealer_wq.png' WHERE `location_type_id` = 407;
-UPDATE `location_type`
-SET `location_image` = 'images/shipdealer_nij.png' WHERE `location_type_id` = 408;
+UPDATE location_type
+SET location_image = 'images/shipdealer_als.png' WHERE location_type_id = 401;
+UPDATE location_type
+SET location_image = 'images/shipdealer_cre.png' WHERE location_type_id = 402;
+UPDATE location_type
+SET location_image = 'images/shipdealer_hum.png' WHERE location_type_id = 403;
+UPDATE location_type
+SET location_image = 'images/shipdealer_ikt.png' WHERE location_type_id = 404;
+UPDATE location_type
+SET location_image = 'images/shipdealer_sal.png' WHERE location_type_id = 405;
+UPDATE location_type
+SET location_image = 'images/shipdealer_the.png' WHERE location_type_id = 406;
+UPDATE location_type
+SET location_image = 'images/shipdealer_wq.png' WHERE location_type_id = 407;
+UPDATE location_type
+SET location_image = 'images/shipdealer_nij.png' WHERE location_type_id = 408;
 
 #Add new racial uno icons
-UPDATE `location_type`
-SET `location_image` = 'images/hardware_als.png' WHERE `location_type_id` = 901;
-UPDATE `location_type`
-SET `location_image` = 'images/hardware_cre.png' WHERE `location_type_id` = 902;
-UPDATE `location_type`
-SET `location_image` = 'images/hardware_hum.png' WHERE `location_type_id` = 903;
-UPDATE `location_type`
-SET `location_image` = 'images/hardware_ikt.png' WHERE `location_type_id` = 904;
-UPDATE `location_type`
-SET `location_image` = 'images/hardware_sal.png' WHERE `location_type_id` = 905;
-UPDATE `location_type`
-SET `location_image` = 'images/hardware_the.png' WHERE `location_type_id` = 906;
-UPDATE `location_type`
-SET `location_image` = 'images/hardware_wq.png' WHERE `location_type_id` = 907;
-UPDATE `location_type`
-SET `location_image` = 'images/hardware_nij.png' WHERE `location_type_id` = 908;
+UPDATE location_type
+SET location_image = 'images/hardware_als.png' WHERE location_type_id = 901;
+UPDATE location_type
+SET location_image = 'images/hardware_cre.png' WHERE location_type_id = 902;
+UPDATE location_type
+SET location_image = 'images/hardware_hum.png' WHERE location_type_id = 903;
+UPDATE location_type
+SET location_image = 'images/hardware_ikt.png' WHERE location_type_id = 904;
+UPDATE location_type
+SET location_image = 'images/hardware_sal.png' WHERE location_type_id = 905;
+UPDATE location_type
+SET location_image = 'images/hardware_the.png' WHERE location_type_id = 906;
+UPDATE location_type
+SET location_image = 'images/hardware_wq.png' WHERE location_type_id = 907;
+UPDATE location_type
+SET location_image = 'images/hardware_nij.png' WHERE location_type_id = 908;
 
 #Add new bar icons
-UPDATE `location_type`
-SET `location_image` = 'images/bar2.png' WHERE `location_type_id` = 801;
-UPDATE `location_type`
-SET `location_image` = 'images/bar3.png' WHERE `location_type_id` = 802;
-UPDATE `location_type`
-SET `location_image` = 'images/bar4.png' WHERE `location_type_id` = 803;
-UPDATE `location_type`
-SET `location_image` = 'images/bar5.png' WHERE `location_type_id` = 804;
+UPDATE location_type
+SET location_image = 'images/bar2.png' WHERE location_type_id = 801;
+UPDATE location_type
+SET location_image = 'images/bar3.png' WHERE location_type_id = 802;
+UPDATE location_type
+SET location_image = 'images/bar4.png' WHERE location_type_id = 803;
+UPDATE location_type
+SET location_image = 'images/bar5.png' WHERE location_type_id = 804;
