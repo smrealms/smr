6d0f64359ef360f3db2d74219b5c0d74bd5f9486
Page <pjgazzard@googlemail.com>
2014-02-11 16:09:07 +0000

    Format money taken message.

--- engine/Default/port_payout_processing.php
+++ engine/Default/port_payout_processing.php
@@ -13,7 +13,7 @@ switch($var['PayoutType']) {
 $account->log(LOG_TYPE_TRADING, 'Player Triggers Payout: ' . $var['PayoutType'], $player->getSectorID());
 $port->update();
 $container = create_container('skeleton.php', 'current_sector.php');
-$container['msg'] = 'You have taken <span class="creds">' . $credits . '</span> from the port.';
+$container['msg'] = 'You have taken <span class="creds">' . number_format($credits) . '</span> from the port.';
 forward($container);
 
 ?>
