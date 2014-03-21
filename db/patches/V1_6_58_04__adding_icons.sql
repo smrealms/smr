49ca4f69b4987845df3376653a9d89df6ed82278
wcyrek <github@witoldcyrek.com>
2014-03-21 03:24:23 -0400

    Add more icons

--- db/patches/V1_6_58_04__adding_icons.sql
+++ db/patches/V1_6_58_04__adding_icons.sql
@@ -2,8 +2,16 @@
 
 #Locations
 #uno
-UPDATE location_type SET location_image = 'images/shields.png' WHERE location_type_id = 601;
+UPDATE location_type SET location_image = 'images/uno.png' WHERE location_type_id = 601;
 #drone farm
 UPDATE location_type SET location_image = 'images/cd.png' WHERE location_type_id = 609;
 #CA
-UPDATE location_type SET location_image = 'images/forces.png' WHERE location_type_id = 602;
+UPDATE location_type SET location_image = 'images/ca.png' WHERE location_type_id = 602;
+#UG
+UPDATE location_type SET location_image = 'images/ug.png' WHERE location_type_id = 102;
+#Accelerated systems
+UPDATE location_type SET location_image = 'images/jump.png' WHERE location_type_id = 603;
+#Hidden Technology
+UPDATE location_type SET location_image = 'images/cloak.png' WHERE location_type_id = 605;
+#Drone Confusion
+UPDATE location_type SET location_image = 'images/scrambler.png' WHERE location_type_id = 608;
