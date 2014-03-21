d616c34fa982ead85e23dafbc0746cb351853b32
wcyrek <github@witoldcyrek.com>
2014-03-21 01:48:49 -0400

    Adding few more icons

---
+++ db/patches/V1_6_58_04__adding_icons.sql
@@ -0,0 +1,9 @@
+/* adding more icons */
+
+#Locations
+#uno
+UPDATE location_type SET location_image = 'images/shields.png' WHERE location_type_id = 601;
+#drone farm
+UPDATE location_type SET location_image = 'images/cd.png' WHERE location_type_id = 609;
+#CA
+UPDATE location_type SET location_image = 'images/forces.png' WHERE location_type_id = 602;
