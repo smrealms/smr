smrealms/smr

commit 84a75b15f6d4283f4b069f138173dd941914e28a

Formatting and query escaping.

engine/Default/preferences_processing.php
@@ -15,14 +15,12 @@ $HoF_name = trim($_REQUEST['HoF_name']);
 $ircNick = trim($_REQUEST['irc_nick']);
 $cellPhone = trim($_REQUEST['cell_phone']);
 
-if (USE_COMPATIBILITY && $action == 'Link Account')
-{
+if (USE_COMPATIBILITY && $action == 'Link Account') {
 	if(!$account->linkAccount($_REQUEST['oldAccountLogin'],$_REQUEST['oldAccountPassword']))
 		create_error('There is no old account with that username/password.');
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have linked your old account.';
 }
-else if ($action == 'Save and resend validation code')
-{
+else if ($action == 'Save and resend validation code') {
 	// get user and host for the provided address
 	list($user, $host) = explode('@', $email);
 
@@ -62,8 +60,7 @@ else if ($action == 'Save and resend validation code')
 	$container['body'] = 'validate.php';
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have changed your email address, you will now need to revalidate with the code sent to the new email address.';
 }
-elseif ($action == 'Change Password')
-{
+elseif ($action == 'Change Password') {
 	if (empty($new_password))
 		create_error('You must enter a non empty password!');
 
@@ -79,8 +76,7 @@ elseif ($action == 'Change Password')
 	$account->setPassword($new_password);
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have changed your password.';
 }
-elseif ($action == 'Change Name')
-{
+elseif ($action == 'Change Name') {
 	$limited_char = 0;
 	for ($i = 0; $i < strlen($HoF_name); $i++) {
 		// disallow certain ascii chars
@@ -92,8 +88,7 @@ elseif ($action == 'Change Name')
 		// letters 97..122
 		if (!((ord($HoF_name[$i]) >= 48 && ord($HoF_name[$i]) <= 57) ||
 			(ord($HoF_name[$i]) >= 65 && ord($HoF_name[$i]) <= 90) ||
-			(ord($HoF_name[$i]) >= 97 && ord($HoF_name[$i]) <= 122)))
-		{
+			(ord($HoF_name[$i]) >= 97 && ord($HoF_name[$i]) <= 122))) {
 			$limited_char += 1;
 		}
 	}
@@ -113,8 +108,7 @@ elseif ($action == 'Change Name')
 	$account->setHofName($HoF_name);
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have changed your hall of fame name.';
 }
-elseif ($action == 'Change Nick')
-{
+elseif ($action == 'Change Nick') {
 	for ($i = 0; $i < strlen($ircNick); $i++) {
 		// disallow certain ascii chars (and whitespace!)
 		if (ord($ircNick[$i]) < 33 || ord($ircNick[$i]) > 127)
@@ -138,8 +132,7 @@ elseif ($action == 'Change Nick')
 	}
 
 }
-elseif ($action == 'Change cell phone')
-{
+elseif ($action == 'Change cell phone') {
 
 	if (empty($cellPhone) || $cellPhone == '') {
 		// delete cell phone
@@ -148,7 +141,7 @@ elseif ($action == 'Change cell phone')
 	} else {
 
 		// validate number
-		if (preg_match('/^\+[0-9]{3,24}$/', $cellPhone) == 0)
+		if (preg_match('/^\+[0-9] {3,24}$/', $cellPhone) == 0)
 			create_error('Cell phone numbers must be given in the international format, eg: +15551234567 (For details see this link: http://www.ehow.com/how_5547899_write-phone-number-international-format.html)');
 
 		// and save cell phone
@@ -157,8 +150,7 @@ elseif ($action == 'Change cell phone')
 	}
 
 }
-elseif ($action == 'Yes')
-{
+elseif ($action == 'Yes') {
 	$account_id = $var['account_id'];
 	$amount = $var['amount'];
 
@@ -171,8 +163,7 @@ elseif ($action == 'Yes')
 	$his_account->increaseSmrCredits($amount);
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have sent SMR credits.';
 }
-elseif ($action == 'Change Timezone')
-{
+elseif ($action == 'Change Timezone') {
 	$timez = $_REQUEST['timez'];
 	if (!is_numeric($timez))
 		create_error('Numbers only please');
@@ -180,29 +171,24 @@ elseif ($action == 'Change Timezone')
 	$db->query('UPDATE account SET offset = '.$timez.' WHERE account_id = '.$account->getAccountID());
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have changed your time offset.';
 }
-elseif ($action == 'Change Date Formats')
-{
+elseif ($action == 'Change Date Formats') {
 	$account->setShortDateFormat($_REQUEST['dateformat']);
 	$account->setShortTimeFormat($_REQUEST['timeformat']);
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have changed your date formats.';
 }
-elseif ($action == 'Change Images')
-{
+elseif ($action == 'Change Images') {
 	$account->setDisplayShipImages($_REQUEST['images']);
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have changed your ship images preferences.';
 }
-elseif ($action == 'Change Centering')
-{
+elseif ($action == 'Change Centering') {
 	$account->setCenterGalaxyMapOnPlayer($_REQUEST['centergalmap']=='Yes');
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have changed your centering galaxy map preferences.';
 }
-else if ($action == 'Change Size' && is_numeric($_REQUEST['fontsize']) && $_REQUEST['fontsize'] >= 50)
-{
+else if ($action == 'Change Size' && is_numeric($_REQUEST['fontsize']) && $_REQUEST['fontsize'] >= 50) {
 	$account->setFontSize($_REQUEST['fontsize']);
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have changed your font size.';
 }
-else if ($action == 'Change CSS Options')
-{
+else if ($action == 'Change CSS Options') {
 	$account->setCssLink($_REQUEST['csslink']);
 	$account->setDefaultCSSEnabled($_REQUEST['defaultcss']!='No');
 	if(isset($_REQUEST['template']))
@@ -211,18 +197,15 @@ else if ($action == 'Change CSS Options')
 		$account->setColourScheme($_REQUEST['colour_scheme']);
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have changed your CSS options.';
 }
-else if ($action == 'Change Kamikaze Setting')
-{
+else if ($action == 'Change Kamikaze Setting') {
 	$player->setCombatDronesKamikazeOnMines($_REQUEST['kamikaze']=='Yes');
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have changed your combat drones options.';
 }
-else if ($action == 'Change Message Setting')
-{
+else if ($action == 'Change Message Setting') {
 	$player->setForceDropMessages($_REQUEST['forceDropMessages']=='Yes');
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have changed your message options.';
 }
-else if ($action == 'Save Hotkeys')
-{
+else if ($action == 'Save Hotkeys') {
 	foreach(AbstractSmrAccount::getDefaultHotkeys() as $hotkey => $binding) {
 		if(isset($_REQUEST[$hotkey])) {
 			$account->setHotkey($hotkey, explode(' ', $_REQUEST[$hotkey]));
@@ -230,15 +213,13 @@ else if ($action == 'Save Hotkeys')
 	}
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have saved your hotkeys.';
 }
-else if (strpos(trim($action),'Alter Player')===0)
-{
+else if (strpos(trim($action),'Alter Player')===0) {
 	// trim input now
 	$player_name = trim($_POST['PlayerName']);
 	
 	$old_name = $player->getName();
 	
-	if($old_name == $player_name)
-	{
+	if($old_name == $player_name) {
 		create_error('Your player already has that name!');
 	}
 	
@@ -253,8 +234,7 @@ else if (strpos(trim($action),'Alter Player')===0)
 		// letters 97..122
 		if (!((ord($player_name[$i]) >= 48 && ord($player_name[$i]) <= 57) ||
 			(ord($player_name[$i]) >= 65 && ord($player_name[$i]) <= 90) ||
-			(ord($player_name[$i]) >= 97 && ord($player_name[$i]) <= 122)))
-		{
+			(ord($player_name[$i]) >= 97 && ord($player_name[$i]) <= 122))) {
 			$limited_char += 1;
 		}
 	}
@@ -266,16 +246,13 @@ else if (strpos(trim($action),'Alter Player')===0)
 		create_error('You must enter a player name!');
 	
 	// Check if name is in use.
-	$db->query('SELECT 1 FROM player WHERE game_id=' .$player->getGameID() . ' AND player_name=' . $db->escape_string($player_name) . ' LIMIT 1' );
-	if($db->getNumRows())
-	{
+	$db->query('SELECT 1 FROM player WHERE game_id=' . $db->escapeNumber($player->getGameID()) . ' AND player_name=' . $db->escape_string($player_name) . ' LIMIT 1' );
+	if($db->getNumRows()) {
 		create_error('Name is already being used in this game!');
 	}
 	
-	if($player->isNameChanged())
-	{
-		if($account->getTotalSmrCredits()<CREDITS_PER_NAME_CHANGE)
-		{
+	if($player->isNameChanged()) {
+		if($account->getTotalSmrCredits()<CREDITS_PER_NAME_CHANGE) {
 			create_error('You do not have enough credits to change your name.');
 		}
 		$account->decreaseTotalSmrCredits(CREDITS_PER_NAME_CHANGE);
@@ -284,7 +261,7 @@ else if (strpos(trim($action),'Alter Player')===0)
 	$player->setPlayerName($player_name);
 
 	$news = '<span class="blue">ADMIN</span> Please be advised that ' . $old_name . ' has changed their name to ' . $player->getBBLink() . '</span>';
-	$db->query('INSERT INTO news (time, news_message, game_id, dead_id,dead_alliance) VALUES (' . TIME . ',' . $db->escape_string($news, FALSE) . ',' . $player->getGameID() . ','.$player->getAccountID().','.$player->getAllianceID().')');
+	$db->query('INSERT INTO news (time, news_message, game_id, dead_id,dead_alliance) VALUES (' . TIME . ',' . $db->escape_string($news, FALSE) . ',' . $db->escapeNumber($player->getGameID()) . ',' . $db->escapeNumber($player->getAccountID()) . ',' . $db->escapeNumber($player->getAllianceID()) . ')');
 	$container['msg'] = '<span class="green">SUCCESS: </span>You have changed your player name.';
 }
 

commit 2210bf62973b1f8cfea553c440a57c9c5bf3c958

preferences_processing.php: fix Cell Phone regex
Fix an error introduced in 84a75b15f6d4283f4b069f138173dd941914e28a.
An extra space caused the Cell Phone regex to match 3-24 spaces
instead of 3-24 numbers. This commit removes the erroneous space.

engine/Default/preferences_processing.php
@@ -149,7 +149,7 @@ elseif ($action == 'Change cell phone') {
 	} else {
 
 		// validate number
-		if (preg_match('/^\+[0-9] {3,24}$/', $cellPhone) == 0)
+		if (preg_match('/^\+[0-9]{3,24}$/', $cellPhone) == 0)
 			create_error('Cell phone numbers must be given in the international format, eg: +15551234567 (For details see this link: http://www.ehow.com/how_5547899_write-phone-number-international-format.html)');
 
 		// and save cell phone