0ed2a38c3e91759857f6622d60eed39704a66226
Pagan Gazzard <pjgazzard@googlemail.com>
2015-04-05 23:54:15 +0100

    Share the header code with galaxy map and the rest of the game, to enable custom friend/foe colours for galaxy map. Fixes #68

--- htdocs/map_galaxy.php
+++ htdocs/map_galaxy.php
@@ -112,6 +112,7 @@ try {
 	
 	if($account->getCssLink()!=null)
 		$template->assign('ExtraCSSLink',$account->getCssLink());
+	$template->assign('Title', 'Galaxy Map');
 	$template->assign('CSSLink',URL.'/css/'.$account->getTemplate().'.css');
 	$template->assign('CSSColourLink',URL.'/css/'.$account->getTemplate().'/'.$account->getColourScheme().'.css');
 	$template->assignByRef('ThisGalaxy',$galaxy);
