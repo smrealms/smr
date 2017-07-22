b9e58b7b7310cbeb67e09d3818db9ed4aa0ede00
Dan Hemberger <daniel.hemberger@gmail.com>
2017-07-21 21:44:12 -0700

    Send messages to racial councils when a vote is started
    This fixes issue #52.
    
    A political message is sent to the racial councils when a vote
    is started. In the case of a peace vote, it is sent to both
    involved councils; in the case of a war vote, only the council
    starting the vote.

--- engine/Default/council_embassy_processing.php
+++ engine/Default/council_embassy_processing.php
@@ -23,14 +23,44 @@ if ($type == 'PEACE') {
 	}
 }
 
+// Create the vote for the player's race
 $db->query('REPLACE INTO race_has_voting
 			(game_id, race_id_1, race_id_2, type, end_time)
 			VALUES(' . $db->escapeNumber($player->getGameID()) . ', ' . $db->escapeNumber($player->getRaceID()) . ', '.$db->escapeNumber($race_id).', '.$db->escapeString($type).', '.$db->escapeNumber($time).')');
 
+// If voting for peace, the other race also has to vote
 if ($type == 'PEACE') {
 	$db->query('REPLACE INTO race_has_voting
 				(game_id, race_id_1, race_id_2, type, end_time)
 				VALUES(' . $db->escapeNumber($player->getGameID()) . ', '.$race_id.', ' . $db->escapeNumber($player->getRaceID()) . ', '.$db->escapeString($type).', '.$time.')');
+}
+
+
+// Send vote announcement to members of the player's council (war votes)
+// or both races' councils (peace votes).
+require_once(get_file_loc('council.inc'));
+$councilMembers = Council::getRaceCouncil($player->getGameID(),
+                                          $player->getRaceID());
+if ($type == 'PEACE') {
+	$otherCouncil = Council::getRaceCouncil($player->getGameID(), $race_id);
+	$councilMembers = array_merge($councilMembers, $otherCouncil);
+}
+
+// Construct the message to be sent to the council members.
+$color = ($type == 'PEACE' ? 'dgreen' : 'red');
+$type_fancy = "<span class=\"$color\">$type</span>";
+$message = $player->getLevelName() . " " . $player->getBBLink()
+           . " has initiated a vote for $type_fancy with the "
+           . Globals::getRaceName($race_id)
+           . "! You have " . format_time(TIME_FOR_COUNCIL_VOTE)
+           . " to cast your vote.";
+
+foreach($councilMembers as $accountID) {
+	// don't send to the player who started the vote
+	if ($player->getAccountID() != $accountID) {
+		SmrPlayer::sendMessageFromRace($player->getRaceID(), $player->getGameID(),
+		                               $accountID, $message, $time);
+  }
 }
 
 forward(create_container('skeleton.php', 'council_embassy.php'));
