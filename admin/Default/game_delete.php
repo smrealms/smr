339e6ee927d4e637f3950079e0bb37d931bed785
Dan Hemberger <daniel.hemberger@gmail.com>
2017-07-26 05:19:36 -0700

    game_delete.php: minor display cleanup
    * Prepend game numbers to game names in the dropdown to unambiguously
      identify games.
    * u -> you

--- admin/Default/game_delete.php
+++ admin/Default/game_delete.php
@@ -2,7 +2,7 @@
 
 $template->assign('PageTopic','Deleting A Game');
 
-$PHP_OUTPUT.=('What game do u want to delete');
+$PHP_OUTPUT.=('<p>What game do you want to delete?</p>');
 
 $container = array();
 $container['url'] = 'skeleton.php';
@@ -18,8 +18,9 @@ while($db->nextRecord()) {
 	//check to see if it needs to be deleted
 	$id_game = $db->getField('game_id');
 	$name = $db->getField('game_name');
+	$display = "($id_game) $name";
 
-	$PHP_OUTPUT.=('<option value="'.$id_game.'">'.$name.'</option>');
+	$PHP_OUTPUT.=('<option value="'.$id_game.'">'.$display.'</option>');
 
 }
 $PHP_OUTPUT.=('</select>');
