5d6f422b58b7057cb9cd29cd2035d134c7571299
Dan Hemberger <daniel.hemberger@gmail.com>
2017-08-23 03:38:45 -0700

    AbstractSmrPlayer.class.inc: add isDraftLeader method

--- engine/Draft/alliance_option.php
+++ engine/Draft/alliance_option.php
@@ -1,7 +1,7 @@
 <?php
 require(ENGINE.'Default/alliance_option.php');
 
-if($player->getAccountID()==$alliance->getLeaderID()) {
+if($player->isDraftLeader()) {
 	$PHP_OUTPUT.= '<br /><br /><b><big>';
 	$PHP_OUTPUT.=create_link(create_container('skeleton.php', 'alliance_pick.php',array('alliance_id'=>$alliance->getAllianceID())),'Pick Members');
 	$PHP_OUTPUT.= '</big></b><br />Pick alliance members.';
