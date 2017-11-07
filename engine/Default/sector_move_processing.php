smrealms/smr

commit 30a7213e8b16694bf2e6a5c1698a2ce8bd735dbd

Starting support for missions, and starting to do logging via mission system.

engine/Default/sector_move_processing.php
@@ -111,7 +111,7 @@ if ($player->hasPlottedCourse())
 }
 
 // log action
-$account->log(5, 'Moves to sector: ' . $var['target_sector'], $player->getSectorID());
+$player->actionTaken('WalkSector',array('Sector',&$sector));
 
 // send scout msg
 $sector->leavingSector($player,MOVEMENT_WALK);

lib/Default/AbstractSmrPlayer.class.inc
@@ -3,6 +3,7 @@ require_once('SmrShip.class.inc');
 require_once('SmrForce.class.inc');
 require_once('SmrGame.class.inc');
 require_once('SmrAlliance.class.inc');
+require_once('missions.inc');
 
 abstract class AbstractSmrPlayer
 {
@@ -44,6 +45,8 @@ abstract class AbstractSmrPlayer
 	protected $bounties;
 	protected $turns;
 	protected $lastCPLAction;
+	protected $completedMissions;
+	protected $missions;
 	
 	protected $visitedSectors;
 	
@@ -130,7 +133,9 @@ abstract class AbstractSmrPlayer
 	public function setSectorID($sectorID)
 	{
 		$this->lastSectorID = $this->getSectorID();
+		$this->actionTaken('LeaveSector',array('Sector'=>&$this->getSector)));
 		$this->sectorID = $sectorID;
+		$this->actionTaken('EnterSector',array('Sector'=>&$this->getSector()));
 		$this->hasChanged=true;
 	}
 	
@@ -1124,6 +1129,221 @@ abstract class AbstractSmrPlayer
 	public function updateLastCPLAction()
 	{
 		$this->setLastCPLAction(TIME);
+	}
+	
+	
+	function getCompletedMissions()
+	{
+		if(!isset($this->completedMissions))
+		{
+			//Get Player missions
+			$this->db->query('SELECT mission_id FROM player_completed_mission WHERE ' . $this->SQL);
+			$this->completedMissions = array();
+			while ($this->db->nextRecord())
+				$this->completedMissions[$this->db->getField('mission_id')] = $this->db->getField('mission_id');
+		}
+		return $this->completedMissions;
+	}
+	
+	function getMissions()
+	{
+		if(!isset($this->missions))
+		{
+			//Get Player missions
+			global $MISSIONS;
+			$this->db->query('SELECT * FROM player_has_mission WHERE ' . $this->SQL);
+			$this->missions = array();
+			while ($this->db->nextRecord())
+			{
+				$this->missions[$this->db->getField('mission_id')] = array(
+								'On Step' => $this->db->getField('on_step'),
+								'Total' => $this->db->getField('total_steps'),
+								'Progress' => $this->db->getField('progress'),
+								'Next Step' => $this->db->getField('next_step'),
+								'Expires' => $this->db->getField('step_fails'),
+								'Mission ID' => $this->db->getField('mission_id'),
+								'Sector' => $this->db->getField('mission_sector'),
+								'Starting Sector' => $this->db->getField('starting_sector')
+								);
+				$this->rebuildMission($this->db->getField('mission_id'));
+			}
+		}
+		return $this->missions;
+	}
+	
+	function getMission($missionID)
+	{
+		$missions = $this->getMissions();
+		if(isset($missions[$missionID]))
+			return $missions[$missionID];
+		return false;
+	}
+	
+	function updateMission($missionID,$keyOrUpdateArray,$newInfo=false)
+	{
+		$this->getMissions();
+		if(isset($this->missions[$missionID]))
+		{
+			if(is_array($keyOrUpdateArray))
+			{
+				$set = '';
+				foreach($keyOrUpdateArray as $bit => $newInfo)
+				{
+					$field = $this->missionKeyToField($bit);
+					if($field!==false)
+					{
+						$this->missions[$missionID][$bit]=$newInfo;
+						$set .= $field.' = '.$this->db->escapeString($newInfo).',';
+					}
+				}
+				if($set!=='')
+				{
+					$set=substr($set,0,-1);
+					$this->db->query('UPDATE player_has_mission SET '.$set.' WHERE '.$this->SQL.' AND mission_id = '.$missionID.' LIMIT 1');
+					$this->rebuildMission($missionID);
+					return true;
+				}
+			}
+			else
+			{
+				$field = $this->missionKeyToField($keyOrUpdateArray);
+				if($field!==false)
+				{
+					$this->missions[$missionID][$keyOrUpdateArray]=$newInfo;
+					$this->db->query('UPDATE player_has_mission SET '.$field.' = '.$newInfo.' WHERE '.$this->SQL.' AND mission_id = '.$missionID.' LIMIT 1');
+					$this->rebuildMission($missionID);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	function addMission($missionID,$randomSector)
+	{
+		global $MISSIONS;
+		$this->getMissions();
+
+		if(isset($this->missions[$missionID]))
+			return;
+			
+		$mission = $MISSIONS[$missionID];
+		$playerMission = array(
+						'On Step' => 1,
+						'Total' => sizeof($mission['Steps']),
+						'Progress' => 0,
+						'Next Step' => 'TRUE',
+						'Expires' => (TIME + 24 * 3600),
+						'Mission ID' => $missionID,
+						'Sector' => $randomSector,
+						'Starting Sector' => $this->getSectorID()
+						);
+						
+		$this->db->query('REPLACE INTO player_has_mission (game_id,account_id,mission_id,on_step,progress,next_step,total_steps,starting_sector,mission_sector,step_fails) ' .
+				'VALUES ('.$this->gameID.','.$this->accountID.','.$missionID.','.$playerMission['On Step'].','.$playerMission['Progress'].','.$this->db->escapeString($playerMission['Next Step']).','.$playerMission['Total'].','.$playerMission['Starting Sector'].','.$playerMission['Sector'].','.$playerMission['Expires'].')');
+		$this->missions[$missionID] = $playerMission;
+		$this->rebuildMission($missionID);
+	}
+	
+	function rebuildMission($missionID)
+	{
+		global $MISSIONS;
+		$RACES = Globals::getRaces();
+		$missionInfo = $this->missions[$missionID];
+		$currentStep = $MISSIONS[$missionID]['Steps'][$missionInfo['On Step']];
+		$detail = str_replace(array('<Race>','<Sector>','<Starting Sector>','<trader>'),array($RACES[$this->getRaceID()],$missionInfo['Sector'],$missionInfo['Starting Sector'],$this->playerName),$currentStep['Detail']);
+		$text = str_replace(array('<Race>','<Sector>','<Starting Sector>','<trader>'),array($RACES[$this->getRaceID()],$missionInfo['Sector'],$missionInfo['Starting Sector'],$this->playerName),$currentStep['Text']);
+		$task = str_replace(array('<Race>','<Sector>','<Starting Sector>','<trader>'),array($RACES[$this->getRaceID()],$missionInfo['Sector'],$missionInfo['Starting Sector'],$this->playerName),$currentStep['Task']);
+		if (isset($currentStep['Level']))
+			$step_level = str_replace('<Player Level>',$this->levelID,$currentStep['Level']);
+		else $step_level = 0;
+		$this->missions[$missionID]['Name'] = $MISSIONS[$missionID]['Name'];
+		$this->missions[$missionID]['Location'] = $MISSIONS[$missionID]['Location'];
+		$this->missions[$missionID]['Task']['Step'] = $currentStep['Step'];
+		$this->missions[$missionID]['Task']['Level'] = $step_level;
+		$this->missions[$missionID]['Task']['Detail'] = $detail;
+		$this->missions[$missionID]['Task']['Text'] = $text;
+		$this->missions[$missionID]['Task']['Task'] = $task;
+	}
+	
+	function deleteMission($missionID)
+	{
+		$this->getMissions();
+		if(isset($this->missions[$missionID]))
+		{
+			unset($this->missions[$missionID]);
+			$this->db->query('DELETE FROM player_has_mission WHERE '.$this->SQL.' AND mission_id = '.$missionID.' LIMIT 1');
+			return true;
+		}
+		return false;
+	}
+	
+	protected function missionKeyToField($bit)
+	{
+		$field = false;
+		switch($bit)
+		{
+			case 'On Step':
+				$field='on_step';
+			break;
+			case 'Total':
+				$field='total_steps';
+			break;
+			case 'Progress':
+				$field='progress';
+			break;
+			case 'Next Step':
+				$field='next_step';
+			break;
+			case 'Expires':
+				$field='step_fails';
+			break;
+			case 'Mission ID':
+				$field='mission_id';
+			break;
+		}
+		return $field;
+	}
+	
+	protected function getAvailableMissions($bit)
+	{
+		$availableMissions = array();
+		foreach($MISSIONS as $mission)
+		{
+			if($this->getSector()->hasX($mission['Location']))
+				$availableMissions[] = $mission;
+		}
+		return $availableMissions;
+	}
+	
+	public function actionTaken($actionID, array &$values)
+	{
+		if(!in_array($value,$MISSION_ACTIONS))
+			throw Exception('Unknown action: ' . $actionID);
+		if($this->getAccount()->isLoggingEnabled())
+			switch($actionID)
+			{
+				case 'WalkSector':
+					$this->getAccount()->log(5, 'Walks to sector: ' . $values['Sector']->getSectorID(), $this->getSectorID());
+				break;
+				case 'JoinAlliance':
+					$this->getAccount()->log(3, 'joined alliance: '.$values['Alliance']->getAllianceName(), $this->getSectorID());
+				break;
+				case 'LeaveAlliance':
+					$this->getAccount()->log(3, 'left alliance: '.$values['Alliance']->getAllianceName(), $this->getSectorID());
+				break;
+				case 'DisbandAlliance':
+					$this->getAccount()->log(3, 'disbanded alliance '.$values['Alliance']->getAllianceName(), $this->getSectorID());
+				break;
+				case 'KickPlayer':
+					$this->getAccount()->log(3, 'kicked '.$values['Player']->getAccount()->getLogin().' ('.$values['Player']->getPlayerName().') from alliance '.$values['Alliance']->getAllianceName(), 0);
+				break;
+				case 'PlayerKicked':
+					$this->getAccount()->log(3, 'was kicked from alliance '.$values['Alliance']->getAllianceName().' by '.$values['Player']->getAccount()->getLogin().' ('.$values['Player']->getPlayerName().')', 0);
+				break;
+				
+			}
+		
 	}
 	
 	public function canSeeAny(array &$otherPlayerArray)

lib/Default/SmrPlayer.class.inc
@@ -38,8 +38,6 @@ class SmrPlayer extends AbstractSmrPlayer
 	protected $useAJAX;
 	protected $ignoreGlobals;
 //	protected $changedStats;
-	protected $completedMissions;
-	protected $missions;
 	protected $mining;
 	protected $plottedCourse;
 	protected $nameChanged;
@@ -468,16 +466,16 @@ class SmrPlayer extends AbstractSmrPlayer
 		if($kickedBy != null)
 		{
 			$kickedBy->sendMessage($this->getAccountID(), 2, 'You were kicked out of the alliance!', false);
-			$this->getAccount()->log(3, 'was kicked from alliance '.$alliance->getAllianceName().' by '.$kickedBy->getAccount()->getLogin().' ('.$kickedBy->getPlayerName().')', 0);
-			$kickedBy->getAccount()->log(3, 'kicked '.$kickedBy->getAccount()->getLogin().' ('.$kickedBy->getPlayerName().') from alliance '.$alliance->getAllianceName(), 0);
+			$this->actionTaken('PlayerKicked', array('Alliance' => &$alliance, 'Player' => &$kickedBy));
+			$kickedBy->actionTaken('KickPlayer', array('Alliance' => &$alliance, 'Player' => &$this));
 		}
 		else if($this->isAllianceLeader())
 		{
-			$this->getAccount()->log(3, 'disbanded alliance '.$alliance->getAllianceName(), $this->getSectorID());
+			$this->actionTaken('DisbandAlliance', array('Alliance' => &$alliance));
 		}
 		else
 		{
-			$this->getAccount()->log(3, 'left alliance '.$alliance->getAllianceName(), $this->getSectorID());
+			$this->actionTaken('LeaveAlliance', array('Alliance' => &$alliance));
 			if ($alliance->getLeaderID() != 0 && $alliance->getLeaderID() != ACCOUNT_ID_NHL)
 			{
 				$this->sendMessage($alliance->getLeaderID(), 2, 'I left your alliance!', false);
@@ -504,7 +502,7 @@ class SmrPlayer extends AbstractSmrPlayer
 		if (!$this->isAllianceLeader())
 			$this->sendMessage($alliance->getLeaderID(), 2, 'I joined your alliance!', false);
 		$this->db->query('INSERT INTO player_has_alliance_role (game_id, account_id, role_id, alliance_id) VALUES ('.$this->getGameID().', '.$this->getAccountID().', 2,'.$alliance->getAllianceID().')');
-		$this->getAccount()->log(3, 'joined alliance: '.$alliance->getAllianceName(), $this->getSectorID());
+		$this->actionTaken('JoinAlliance', array('Alliance' => &$alliance));
 	}
 	
 	function getAllianceJoinable()
@@ -1117,180 +1115,6 @@ class SmrPlayer extends AbstractSmrPlayer
 			default:
 				throw new Exception('Invalid gadget attribute: '.$key);
 		}
-	}
-	
-	
-	function getCompletedMissions()
-	{
-		if(!isset($this->completedMissions))
-		{
-			//Get Player missions
-			$this->db->query('SELECT mission_id FROM player_completed_mission WHERE ' . $this->SQL);
-			$this->completedMissions = array();
-			while ($this->db->nextRecord())
-				$this->completedMissions[$this->db->getField('mission_id')] = $this->db->getField('mission_id');
-		}
-		return $this->completedMissions;
-	}
-	
-	function getMissions()
-	{
-		if(!isset($this->missions))
-		{
-			//Get Player missions
-			global $MISSIONS;
-			$this->db->query('SELECT * FROM player_has_mission WHERE ' . $this->SQL);
-			$this->missions = array();
-			while ($this->db->nextRecord())
-			{
-				$this->missions[$this->db->getField('mission_id')] = array(
-								'On Step' => $this->db->getField('on_step'),
-								'Total' => $this->db->getField('total_steps'),
-								'Progress' => $this->db->getField('progress'),
-								'Next Step' => $this->db->getField('next_step'),
-								'Expires' => $this->db->getField('step_fails'),
-								'Mission ID' => $this->db->getField('mission_id'),
-								'Sector' => $this->db->getField('mission_sector'),
-								'Starting Sector' => $this->db->getField('starting_sector')
-								);
-				$this->rebuildMission($this->db->getField('mission_id'));
-			}
-		}
-		return $this->missions;
-	}
-	
-	function getMission($missionID)
-	{
-		$missions = $this->getMissions();
-		if(isset($missions[$missionID]))
-			return $missions[$missionID];
-		return false;
-	}
-	
-	function updateMission($missionID,$keyOrUpdateArray,$newInfo=false)
-	{
-		$this->getMissions();
-		if(isset($this->missions[$missionID]))
-		{
-			if(is_array($keyOrUpdateArray))
-			{
-				$set = '';
-				foreach($keyOrUpdateArray as $bit => $newInfo)
-				{
-					$field = $this->missionKeyToField($bit);
-					if($field!==false)
-					{
-						$this->missions[$missionID][$bit]=$newInfo;
-						$set .= $field.' = '.$this->db->escapeString($newInfo).',';
-					}
-				}
-				if($set!=='')
-				{
-					$set=substr($set,0,-1);
-					$this->db->query('UPDATE player_has_mission SET '.$set.' WHERE '.$this->SQL.' AND mission_id = '.$missionID.' LIMIT 1');
-					$this->rebuildMission($missionID);
-					return true;
-				}
-			}
-			else
-			{
-				$field = $this->missionKeyToField($keyOrUpdateArray);
-				if($field!==false)
-				{
-					$this->missions[$missionID][$keyOrUpdateArray]=$newInfo;
-					$this->db->query('UPDATE player_has_mission SET '.$field.' = '.$newInfo.' WHERE '.$this->SQL.' AND mission_id = '.$missionID.' LIMIT 1');
-					$this->rebuildMission($missionID);
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
-	function addMission($missionID,$randomSector)
-	{
-		global $MISSIONS;
-		$this->getMissions();
-
-		if(isset($this->missions[$missionID]))
-			return;
-			
-		$mission = $MISSIONS[$missionID];
-		$playerMission = array(
-						'On Step' => 1,
-						'Total' => sizeof($mission['Steps']),
-						'Progress' => 0,
-						'Next Step' => 'TRUE',
-						'Expires' => (TIME + 24 * 3600),
-						'Mission ID' => $missionID,
-						'Sector' => $randomSector,
-						'Starting Sector' => $this->getSectorID()
-						);
-						
-		$this->db->query('REPLACE INTO player_has_mission (game_id,account_id,mission_id,on_step,progress,next_step,total_steps,starting_sector,mission_sector,step_fails) ' .
-				'VALUES ('.$this->gameID.','.$this->accountID.','.$missionID.','.$playerMission['On Step'].','.$playerMission['Progress'].','.$this->db->escapeString($playerMission['Next Step']).','.$playerMission['Total'].','.$playerMission['Starting Sector'].','.$playerMission['Sector'].','.$playerMission['Expires'].')');
-		$this->missions[$missionID] = $playerMission;
-		$this->rebuildMission($missionID);
-	}
-	
-	function rebuildMission($missionID)
-	{
-		global $MISSIONS;
-		$RACES = Globals::getRaces();
-		$missionInfo = $this->missions[$missionID];
-		$currentStep = $MISSIONS[$missionID]['Steps'][$missionInfo['On Step']];
-		$detail = str_replace(array('<Race>','<Sector>','<Starting Sector>','<trader>'),array($RACES[$this->getRaceID()],$missionInfo['Sector'],$missionInfo['Starting Sector'],$this->playerName),$currentStep['Detail']);
-		$text = str_replace(array('<Race>','<Sector>','<Starting Sector>','<trader>'),array($RACES[$this->getRaceID()],$missionInfo['Sector'],$missionInfo['Starting Sector'],$this->playerName),$currentStep['Text']);
-		$task = str_replace(array('<Race>','<Sector>','<Starting Sector>','<trader>'),array($RACES[$this->getRaceID()],$missionInfo['Sector'],$missionInfo['Starting Sector'],$this->playerName),$currentStep['Task']);
-		if (isset($currentStep['Level']))
-			$step_level = str_replace('<Player Level>',$this->levelID,$currentStep['Level']);
-		else $step_level = 0;
-		$this->missions[$missionID]['Name'] = $MISSIONS[$missionID]['Name'];
-		$this->missions[$missionID]['Location'] = $MISSIONS[$missionID]['Location'];
-		$this->missions[$missionID]['Task']['Step'] = $currentStep['Step'];
-		$this->missions[$missionID]['Task']['Level'] = $step_level;
-		$this->missions[$missionID]['Task']['Detail'] = $detail;
-		$this->missions[$missionID]['Task']['Text'] = $text;
-		$this->missions[$missionID]['Task']['Task'] = $task;
-	}
-	
-	function deleteMission($missionID)
-	{
-		$this->getMissions();
-		if(isset($this->missions[$missionID]))
-		{
-			unset($this->missions[$missionID]);
-			$this->db->query('DELETE FROM player_has_mission WHERE '.$this->SQL.' AND mission_id = '.$missionID.' LIMIT 1');
-			return true;
-		}
-		return false;
-	}
-	
-	protected function missionKeyToField($bit)
-	{
-		$field = false;
-		switch($bit)
-		{
-			case 'On Step':
-				$field='on_step';
-			break;
-			case 'Total':
-				$field='total_steps';
-			break;
-			case 'Progress':
-				$field='progress';
-			break;
-			case 'Next Step':
-				$field='next_step';
-			break;
-			case 'Expires':
-				$field='step_fails';
-			break;
-			case 'Mission ID':
-				$field='mission_id';
-			break;
-		}
-		return $field;
 	}
 	
 	function getMining($mineID=false)

lib/Default/missions.inc
@@ -1,0 +1,305 @@
+<?php
+
+$MISSION_ACTIONS = array('LeaveSector','EnterSector','WalkSector','JoinAlliance','LeaveAlliance','DisbandAlliance','KickPlayer','PlayerKicked');
+
+//REQUIREMENTS
+//if you use an array as a requirement and the requirement name represents an array, it will check every value and all must pass
+//if you use an array as a requirement and the requirement name is not an array, only one of the checks must pass
+//ie 'Completed Missions' => array(2,3) means the player must have completed BOTH missions
+//ie 'Ship ID' => array(1,2) means the player must be in EITHER ship id 1 or 2
+//STEPS
+/*key types:
+*'KillName' - kill 'Detail' Player/NPC
+*'KillNPCs' - kill 'Detail' NPCs
+*'KillPlayers' - kill 'Detail' Players
+*'KillSpawn' - Spawn 'Detail' Type NPC and kill it, DB field Progress with then be NPC_ID, also requires a 'Level' element, use -1 for normal
+'Trade' -
+^'Visit' - Examine 'Detail' location
+*'DrinkAmount' - Buy 'Detail' drinks at a bar
+*'Drink' - Buy 'Detail' drink name at a bar
+*'Move' - Move 'Detail' sectors anywhere
+*'MoveSector' - Move to 'Detail' sector
+*'MoveRacial' - Move to galaxy containing 'Detail' race HQ (use racial id)
+*'MoveGal' - Move to 'Detail' galaxy
+'ClearNPC' - Clear 'Detail' stacks of NPC forces in sector (use MoveSector) command to tell them which sector, also stored as mission_sector in DB
+*'StartPortRaid' - start raiding 'detail' ports
+*'RaidPort' - raid 'detail' ports
+'Bring' -  bring 'detail' to starting sector
+
+Replacements:
+<Race> - Current race name
+<Starting Sector> - Sector where mission was accepted
+<Sector> - Random sector for mission.
+
+* = implemented
+^ = partial implementaion
+	Visit - done for 'Bar'
+*/
+$MISSIONS = array(
+	array(
+		'Name' => 'Drunk Guy',
+		'Offerer' => 'Drunk',
+		'Time Limit' => 0,
+		'Location' => 'Bar',
+		'Rewards' => array(
+			'Credits' => 500000,
+			'Experience' => 1000,
+			'Text' => '*Hiccup* For your...service *Hiccup* to me, take these *Hiccup* 500,000 credits and 1000 experience *Hiccup*!'
+		),
+		'Steps' => array(
+			array(
+				'Step' => 'MoveRacial',
+				'Detail' => 60, //Salvene
+				'Text' => '*Hiccup* Hey! I need you to...*Hiccup* do me a favor.  All the Salvene Swamp Water in this bar is awful!  Go to the Sal...*Hiccup*...the Salvene galaxy, visit a bar and get me some real Salvene Swamp Water.',
+				'Task' => 'Go to the Salvene Galaxy'
+			),
+			array(
+				'Step' => 'Drink',
+				'Detail' => 'Salvene Swamp Water',
+				'Text' => 'Here we are! The Salvene Galaxy! Now lets find a bar.',
+				'Task' => 'Go to a bar and buy a Salvene Swamp Water from the bartender.  This may take many tries.'
+			),
+			array(
+				'Step' => 'MoveSector',
+				'Detail' => '<Starting Sector>',
+				'Text' => 'Finally! A true Salvene Swamp Water, let return to that drunk!',
+				'Task' => 'Return to <Stating Sector> to claim your reward.'
+			),
+			array(
+				'Step' => 'Claim',
+				'Detail' => 'Bar',
+				'Text' => 'Finally! A true Salvene Swamp Water, let return to that drunk!',
+				'Task' => 'Enter the bar to claim your reward.'
+			)
+		)
+	)
+);
+
+/*
+function getMissionText($mission)
+{
+	global $RACES,$GAME_ID,$TIME,$id;
+	$text = get_subtitle($mission['Name']);
+	if ($mission['On Step'] > $mission['Total'])
+	{
+		$text .= get_status_message('green','Mission','Mission '.$mission['Name'].' Completed.');
+	}
+	else
+	{
+		$progress = $mission['Progress'];
+		$need = $mission['Task']['Detail'];
+		$remain = $need - $progress;
+		$plural = ($remain==1?'':'s');
+		$text .= get_status_message('green','Task',$mission['Task']['Text']);
+		if ($mission['Task']['Step'] == 'KillSpawn')
+		{
+			//get the npc
+			$sql = query('SELECT * FROM npcs WHERE game_id = '.$GAME_ID.' AND npc_id = '.$mission['Progress'].' AND dead = \'FALSE\' LIMIT 1');
+			if ($result = next_record($sql))
+			{
+				$name = stripslashes($result['name']);
+				$text .= get_status_message('green','Status','Kill the NPC '.$name.'.');
+			}
+			elseif ($mission['Expires'] < $TIME)
+			{
+				$link = array();
+				$link['body'] = 'trader_missions.php';
+				$link['respawn'] = TRUE;
+				$link['mission_id'] = $mission['Mission ID'];
+				$link['text'] = 'Respawn NPC';
+				$text .= create_button($link,$id,'',TRUE);
+				$text .= '<br /><br />';
+			} else
+				$text .= get_status_message('green','Status','You must wait for a respawn.');
+			
+		}
+		elseif ($mission['Task']['Step'] == 'KillNPCs')
+		{
+			$text .= get_status_message('green','Status','Kill '.$remain.' more NPCs.');
+		}
+		elseif ($mission['Task']['Step'] == 'KillPlayers')
+		{
+			$text .= get_status_message('green','Status','Kill '.$remain.' more players.');
+		}
+		elseif ($mission['Task']['Step'] == 'KillName')
+		{
+			$text .= get_status_message('green','Status','Kill '.$need.'.');
+		}
+		elseif ($mission['Task']['Step'] == 'Visit')
+		{
+			$text .= get_status_message('green','Status','Visit a '.$need.'.');
+		}
+		elseif ($mission['Task']['Step'] == 'DrinkAmount')
+		{
+			$text .= get_status_message('green','Status','Buy '.$remain.' more drink'.$plural.'.');
+		}
+		elseif ($mission['Task']['Step'] == 'Drink')
+		{
+			$text .= get_status_message('green','Status','Buy a '.$need.' drink.');
+		}
+		elseif ($mission['Task']['Step'] == 'Move')
+		{
+			$text .= get_status_message('green','Status','Move '.$remain.' more sector'.$plural.'.');
+		}
+		elseif ($mission['Task']['Step'] == 'MoveSector')
+		{
+			$text .= get_status_message('green','Status','Move to sector '.$need.'.');
+		}
+		elseif ($mission['Task']['Step'] == 'MoveRacial')
+		{
+			$text .= get_status_message('green','Status','Move to the racial galaxy of the '.$RACES[$need].'.');
+		}
+		elseif ($mission['Task']['Step'] == 'MoveGal')
+		{
+			$text .= get_status_message('green','Status','Move to the '.$need.' galaxy.');
+		}
+		elseif ($mission['Task']['Step'] == 'ClearNPC')
+		{
+			$text .= get_status_message('green','Status','Clear '.$remain.' more NPC force'.$plural.'.');
+		}
+		elseif ($mission['Task']['Step'] == 'StartPortRaid')
+		{
+			$text .= get_status_message('green','Status','Start '.$remain.' more port raid'.$plural.'.');
+		}
+		elseif ($mission['Task']['Step'] == 'Claim')
+		{
+			$text .= get_status_message('green','Status','Visit a '.$need.' to claim your reward.');
+		}
+		else
+		{
+			$text .= get_status_message('green','Status','Unknown Mission Type: '.$mission['Task']['Step'].'.');
+		}
+		$text .= '<br />';
+		return $text;
+	}
+}
+
+function checkMissionRequirements($requirements,$THIS_SECTOR,$THIS_PLAYER,$THIS_SHIP)
+{
+	$must_meet = sizeof($requirements);
+	$met = 0;
+	foreach ($requirements as $requirement => $value)
+	{
+		$require_name = $requirement;
+		$require_attrib = $value;
+	
+		if (strpos($require_name,'.'))
+		{
+			list($player_object_name,$real_require_name) = explode('.',$require_name);
+			if (strtolower($player_object_name) == 'ship') $player_object = $THIS_SHIP;
+			elseif (strtolower($player_object_name) == 'sector') $player_object = $THIS_SECTOR;
+			elseif (strtolower($player_object_name) == 'player') $player_object = $THIS_PLAYER;
+		}
+		else
+		{
+			$player_object = $THIS_PLAYER;
+			$real_require_name = $require_name;
+		}
+		
+		if ($player_object == $THIS_PLAYER)
+			$requireObjectActual = $player_object->get($real_require_name);
+		else
+			$requireObjectActual = $player_object[$real_require_name];
+		if (is_array($requireObjectActual))
+		{
+			if (is_array($require_attrib))
+			{
+				//add that they must meet all (-1 because we already accounted for 1 in the array)
+				$must_meet += sizeof($require_attrib) - 1;
+				foreach ($require_attrib as $check_val)
+				{
+					if (in_array($check_val,$requireObjectActual)) $met++;
+				}
+			}
+		}
+		elseif (is_array($require_attrib))
+		{
+			//they only need to meet one of these
+			foreach ($require_attrib as $check_val)
+			{
+				if ($check_val == $requireObjectActual)
+				{
+					$met++;
+					break;
+				}
+			}
+		}
+		elseif (is_numeric($require_attrib))
+		{
+			//we just do a straight == compare
+			if ($requireObjectActual == $require_attrib)
+				$met++;
+		}
+		else
+		{
+			//we need to determine what to do with this number
+			if (strpos($require_attrib,'<') !== FALSE)
+			{
+				list($crap,$require_attrib_real) = explode('<',$require_attrib);
+				if ($requireObjectActual < $require_attrib_real)
+					$met++;
+			}
+			elseif (strstr($require_attrib,'>') !== FALSE)
+			{
+				list($crap,$require_attrib_real) = explode('>',$require_attrib);
+				if ($requireObjectActual > $require_attrib_real)
+					$met++;
+			}
+			elseif (strstr($require_attrib,'!') !== FALSE)
+			{
+				list($crap,$require_attrib_real) = explode('!',$require_attrib);
+				if ($requireObjectActual != $require_attrib_real)
+					$met++;
+			}
+			elseif (strstr($require_attrib,'|') !== FALSE)
+			{
+				list($lower,$upper) = explode('|',$require_attrib);
+				if ($requireObjectActual > $lower && $requireObjectActual < $upper)
+					$met++;
+			}
+			else
+			{
+				if ($requireObjectActual == $require_attrib)
+				$met++;
+			}
+		}
+	}
+	//echo 'Mission ' . $mission_id . ': Must meet ' . $must_meet . ' and met ' . $met . '<br />';
+	if ($must_meet == $met)
+		return true;
+	return false;
+}
+
+function check_missions($player_object = '')
+{
+	global $error, $THIS_PLAYER, $GAME_ID,$weapons,$upgrades,$gadgets,$ship_id;
+	if ($player_object == '') $player_object = $THIS_PLAYER;
+	$playerMissions = $player_object->getMissions();
+	foreach ($playerMissions as $mission_id => $mission_stuff)
+	{
+		//see if we need to do anything for this step
+		if ($mission_stuff['Next Step'] == 'TRUE')
+		{
+			//display the appropriate text
+			if ($mission_stuff['On Step'] == 1)
+				$error .= get_status_message('green','Mission Accepted',$mission_stuff['Task']['Text']);
+			else
+				$error .= get_status_message('green','Mission Accomplished',$mission_stuff['Task']['Text']);
+			$player_object->updateMission($mission_id,'Next Step','FALSE');
+		}
+		if ($mission_stuff['Task']['Step'] == 'KillSpawn' && $mission_stuff['Progress'] == 0)
+		{
+			//spawn the NPC
+			get_ship_setup($mission_stuff['Task']['Detail'],$weapons,$upgrades,$gadgets,$ship_id);
+			$npc_id = create_npc($GAME_ID,'',$player_object->getSectorID(),$ship_id,$mission_stuff['Task']['Level'],0,'',$weapons,$upgrades,$gadgets,0,'Member','None','Attacker',0);
+			//update the db for this npc id
+			$player_object->updateMission($mission_id,'Progress',$npc_id);
+		}
+		elseif ($mission_stuff['Task']['Step'] == 'ClearNPC' && $mission_stuff['Progress'] == 0)
+		{
+			//spawn forces
+			//update db so that more forces are not spawned everytime
+		}
+	}
+} */
+?>

templates/Default/engine/Default/current_sector.php
@@ -5,7 +5,8 @@
 		</td>
 		<td style="padding:0px;vertical-align:top;width:32em;"><?php
 			$this->includeTemplate('includes/PlottedCourse.inc');
-			$this->includeTemplate('includes/Ticker.inc'); ?>
+			$this->includeTemplate('includes/Ticker.inc');
+			$this->includeTemplate('includes/Missions.inc'); ?>
 			<span id="secmess"><?php
 				if(isset($ErrorMessage))
 				{

templates/Default/engine/Default/includes/Missions.inc
@@ -1,0 +1,13 @@
+<?php
+
+$AvailableMissions =& $ThisPlayer->getAvailableMissions();
+if(count($AvailableMissions) > 0)
+{
+	foreach($AvailableMissions as &$AvailableMission)
+	{
+		echo $AvailableMission['Steps'][0]['Text']; ?>
+		<div class="buttonA"><a href="<?php /*TODO*/ ?>" class="buttonA"> Accept </a></div><?php
+	}
+}
+
+?>

commit ea96cc9c2aa949094f64f74cb04f335e6c7e67cb

Fix logging of sector movement
Sector movement was incorrectly logged because the target sector
and the current sector were set to the same value. This bug was
introduced in 30a7213e8b16, when logging for sector movement was
wrapped inside the mission actions.

engine/Default/sector_move_processing.php
@@ -88,7 +88,8 @@ if ($player->hasPlottedCourse()) {
 }
 
 // log action
-$player->actionTaken('WalkSector',array('Sector'=>&$sector));
+$targetSector =& SmrSector::getSector($player->getGameID(), $var['target_sector']);
+$player->actionTaken('WalkSector', array('Sector' => &$targetSector));
 
 // send scout msg
 $sector->leavingSector($player,MOVEMENT_WALK);