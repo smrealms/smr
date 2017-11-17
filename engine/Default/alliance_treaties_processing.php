cefdb97109b7fee8a9d7b1fff018d7079f467f90
Dan Hemberger <daniel.hemberger@gmail.com>
2017-11-17 05:04:06 -0800

    alliance_treaties_processing.php: fix database error
    Cannot use `escapeNumber` to escape a boolean.

--- engine/Default/alliance_treaties_processing.php
+++ engine/Default/alliance_treaties_processing.php
@@ -114,7 +114,7 @@ if (isset($_REQUEST['proposedAlliance'])) {
 	$alliance_id_2 = $var['proposedAlliance'];
 	$db->query('INSERT INTO alliance_treaties (alliance_id_1,alliance_id_2,game_id,trader_assist,trader_defend,trader_nap,raid_assist,planet_land,planet_nap,forces_nap,aa_access,mb_read,mb_write,mod_read,official)
 				VALUES (' . $db->escapeNumber($alliance_id_1) . ', ' . $db->escapeNumber($alliance_id_2) . ', ' . $db->escapeNumber($player->getGameID()) . ', ' . $db->escapeBoolean($var['traderAssist']) . ', ' .
-				$db->escapeBoolean($var['traderDefend']) . ', ' . $db->escapeNumber($var['traderNAP']) . ', ' . $db->escapeBoolean($var['raidAssist']) . ', ' . $db->escapeBoolean($var['planetLand']) . ', ' . $db->escapeBoolean($var['planetNAP']) . ', ' .
+				$db->escapeBoolean($var['traderDefend']) . ', ' . $db->escapeBoolean($var['traderNAP']) . ', ' . $db->escapeBoolean($var['raidAssist']) . ', ' . $db->escapeBoolean($var['planetLand']) . ', ' . $db->escapeBoolean($var['planetNAP']) . ', ' .
 				$db->escapeBoolean($var['forcesNAP']) . ', ' . $db->escapeBoolean($var['aaAccess']) . ', ' . $db->escapeBoolean($var['mbRead']) . ', ' . $db->escapeBoolean($var['mbWrite']) . ', ' . $db->escapeBoolean($var['modRead']) . ', \'FALSE\')');
 	//send a message to the leader letting them know the offer is waiting.
 	$db->query('SELECT leader_id FROM alliance WHERE game_id=' . $db->escapeNumber($player->getGameID()) . ' AND alliance_id=' . $db->escapeNumber($alliance_id_2) . ' LIMIT 1');
