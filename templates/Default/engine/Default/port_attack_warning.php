ba2acef789996ee91e74ca28b332cc3b9fef4e63
Dan Hemberger <daniel.hemberger@gmail.com>
2017-11-20 19:14:59 -0800

    port_attack_warning.php: revamp page
    * Replace textual scan results ("Your scanners detect...") with a
      table that looks like the sector scan results. It gives the page
      a little more substance and also makes it more consistent with
      how other pages look.
    
    * Add a short bit of roleplaying text to help newer players realize
      that raiding a port might not be a good idea. A link to the wiki
      port combat page is provided. This was tip for newbies was
      frequently requested by players.

--- templates/Default/engine/Default/port_attack_warning.php
+++ templates/Default/engine/Default/port_attack_warning.php
@@ -1,12 +1,38 @@
 <span class="red">WARNING WARNING</span> port assault about to commence!!<br />
-Are you sure you want to attack this port?<br /><br />
+
+<br />
+Suddenly, sirens sound and warning lights flash as your onboard sensors detect
+that the port has enough defensive firepower to reduce your ship to space debris.
+Without an armada behind you, the outcome may not be pleasant.
+<a href="<?php echo WIKI_URL; ?>/game-guide/combat#ports" target="_blank"><img src="images/silk/help.png" width="16" height="16" alt="Wiki Link" title="Goto SMR Wiki: Port Combat"/></a>
+<br /><br />
+
 <?php
 if ($ThisShip->hasScanner()) {
 	$Port = $ThisSector->getPort(); ?>
-	Your scanners detect that there <?php echo $this->pluralise('is', $Port->getShields()); ?> <span id="portShields"><?php echo $Port->getShields(); ?></span> <?php echo $this->pluralise('shield', $Port->getShields()); ?>,
-	and <span id="portCDs"><?php echo $Port->getCDs(); ?></span> <?php echo $this->pluralise('combat drone', $Port->getCDs()); ?>,
-	and <span id="portArmour"><?php echo $Port->getArmour(); ?></span> <?php echo $this->pluralise('plate', $Port->getArmour()); ?> of armour.<br /><br /><?php
+<p>
+	<table class="standard">
+		<tr>
+			<th>Port</th>
+			<th>Scan Results</th>
+		<tr>
+		<tr>
+			<td>Shields</td>
+			<td align="center"><?php echo $Port->getShields(); ?></td>
+		</tr>
+		<tr>
+			<td>Combat Drones</td>
+			<td align="center"><?php echo $Port->getCDs(); ?></td>
+		</tr>
+		<tr>
+			<td>Armour</td>
+			<td align="center"><?php echo $Port->getArmour(); ?></td>
+		</tr>
+	</table>
+</p><?php
 } ?>
+
+Are you sure you want to attack this port?<br /><br />
 
 <div class="buttonA">
 	<a class="buttonA" href="<?php echo $PortAttackHREF; ?>">&nbsp;Yes&nbsp;</a>
