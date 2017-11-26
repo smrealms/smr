d4f8a68e6eebc114de9afa5a7399d291c44b3ac7
Dan Hemberger <daniel.hemberger@gmail.com>
2017-11-25 22:45:09 -0800

    Clean up Galactic Post editor options
    * Fix whitespace problems.
    * Show number of articles in red/green depending on if they meet
      the criteria for a paper.
    * Replace "Hit the press!" with a bold green "Published!" when a
      paper has been published.

--- engine/Default/galactic_post.php
+++ engine/Default/galactic_post.php
@@ -5,8 +5,8 @@ if ($player->isGPEditor()) {
 	$db2 = new SmrMySqlDatabase();
 	require_once(get_file_loc('menu.inc'));
 	create_galactic_post_menu();
+	$PHP_OUTPUT.=('Welcome '.$player->getPlayerName().', your position is <i>Editor</i><br /><br />');
 	$PHP_OUTPUT.=('<b>EDITOR OPTIONS<br /></b>');
-	$PHP_OUTPUT.=('Welcome '.$player->getPlayerName().' your position is <i>Editor</i><br />');
 	$PHP_OUTPUT.=create_link(create_container('skeleton.php', 'galactic_post_view_applications.php'), 'View the applications');
 	$PHP_OUTPUT.=('<br />');
 	$PHP_OUTPUT.=create_link(create_container('skeleton.php', 'galactic_post_view_article.php'), 'View the articles');
@@ -14,24 +14,31 @@ if ($player->isGPEditor()) {
 	$PHP_OUTPUT.=create_link(create_container('skeleton.php', 'galactic_post_make_paper.php'), 'Make a paper');
 	$PHP_OUTPUT.=('<br />');
 	$PHP_OUTPUT.=create_link(create_container('skeleton.php', 'galactic_post_view_members.php'), 'View Members');
-	$PHP_OUTPUT.=('<br />');
+	$PHP_OUTPUT.=('<br /><br />');
 	$db->query('SELECT * FROM galactic_post_paper WHERE game_id = ' . $db->escapeNumber($player->getGameID()));
 	if ($db->getNumRows()) {
-		$PHP_OUTPUT.=('The following papers are already made (note papers must have 3-8 articles to go to the press)');
+		$PHP_OUTPUT.=('The following papers are already made (papers must have 3-8 articles to go to the press):<br /><br />');
 	}
 	while($db->nextRecord()) {
 		$paper_name = $db->getField('title');
 		$paper_id = $db->getField('paper_id');
+		$published = $db->getInt('online_since');
+
 		$PHP_OUTPUT.=('<span class="red">***</span><i>'.$paper_name.'</i>');
 		$db2->query('SELECT * FROM galactic_post_paper_content WHERE paper_id = ' . $db2->escapeNumber($paper_id) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()));
-		$PHP_OUTPUT.=(' which contains <span class="red"> ' . $db2->getNumRows() . ' </span>articles. ');
-		if ($db2->getNumRows() > 2 && $db2->getNumRows() < 9) {
+		$hasEnoughArticles = $db2->getNumRows() > 2 && $db2->getNumRows() < 9;
+
+		// Use red/green to show if a paper has enough articles
+		$PHP_OUTPUT.=(' which contains <span class="' . ($hasEnoughArticles ? "green" : "red") . '"> ' . $db2->getNumRows() . ' </span>articles. ');
+		if (!empty($published)) {
+			$PHP_OUTPUT.=('<span class="bold green">PUBLISHED!</span>');
+		} else if ($hasEnoughArticles) {
 			$container = array();
 			$container['url'] = 'galactic_post_make_current.php';
 			$container['id'] = $paper_id;
 			$PHP_OUTPUT.=create_link($container, '<b>HIT THE PRESS!</b>');
-
 		}
+
 		$PHP_OUTPUT.=('<br />');
 		$container = array();
 		$container['url'] = 'skeleton.php';
