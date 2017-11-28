smrealms/smr

commit d4d738114b88ecd6afc47b83286a6555bbcf6324

Move path defines to config.php
There is no benefit to specifying the repository path defines
in config.specific.php. These paths cannot be changed, so we
hard-code them into config.php instead of relying on the user
to correctly define them.

Furthermore, having to manually specify `ROOT` can only ever
cause problems. The correct value for `ROOT` can be determined
programmatically using `__DIR__`.

htdocs/config.php
@@ -1,11 +1,22 @@
 <?php
 @date_default_timezone_set(@date_default_timezone_get());
-require_once(dirname(__FILE__) . '/config.specific.php');
 
 if (ENABLE_DEBUG) {
 	// Warn about everything when in debug mode
 	error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 }
+
+// Repository paths
+define('ROOT', __DIR__ . '/../');
+define('LIB', ROOT.'lib/');
+define('ENGINE', ROOT.'engine/');
+define('WWW', ROOT.'htdocs/');
+define('UPLOAD', WWW.'upload/');
+define('ADMIN', ROOT.'admin/');
+define('TOOLS', ROOT.'tools/');
+
+// Define server-specific constants
+require_once(WWW . '/config.specific.php');
 
 define('ACCOUNT_ID_PORT',65535);
 define('ACCOUNT_ID_ADMIN',65534);

htdocs/config.specific.sample.php
@@ -2,13 +2,6 @@
 define('USE_COMPATIBILITY',false);
 
 define('URL','http://localhost');
-define('ROOT','/smr/');
-define('LIB',ROOT.'lib/');
-define('ENGINE',ROOT.'engine/');
-define('WWW',ROOT.'htdocs/');
-define('UPLOAD',WWW.'upload/');
-define('ADMIN',ROOT.'admin/');
-define('TOOLS',ROOT.'tools/');
 
 define('ENABLE_DEBUG', true); // This is useful for debugging on dev machines.
 define('ENABLE_BETA', false);

commit ea7f3385f38e63f7e1a1ae29959daa3fac11302e

config.php: fix define ordering error
Make sure `ENABLE_DEBUG` is defined before it is used.
This bug was introduced in PR #174.

htdocs/config.php
@@ -1,11 +1,6 @@
 <?php
 @date_default_timezone_set(@date_default_timezone_get());
 
-if (ENABLE_DEBUG) {
-	// Warn about everything when in debug mode
-	error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-}
-
 // Repository paths
 define('ROOT', __DIR__ . '/../');
 define('LIB', ROOT.'lib/');
@@ -17,6 +12,11 @@ define('TOOLS', ROOT.'tools/');
 
 // Define server-specific constants
 require_once(WWW . '/config.specific.php');
+
+if (ENABLE_DEBUG) {
+	// Warn about everything when in debug mode
+	error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+}
 
 define('ACCOUNT_ID_PORT',65535);
 define('ACCOUNT_ID_ADMIN',65534);