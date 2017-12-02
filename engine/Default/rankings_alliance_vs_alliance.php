72e0b25944121f5eba567f4838dc5d1c93dd6438
Dan Hemberger <daniel.hemberger@gmail.com>
2017-12-01 22:40:06 -0800

    rankings_alliance_vs_alliance.php: fix header highlight
    Highlight the "Versus" subheading of "Alliance Rankings" when it is
    clicked on. It was previously highlighting the "Death" subheading.

--- engine/Default/rankings_alliance_vs_alliance.php
+++ engine/Default/rankings_alliance_vs_alliance.php
@@ -3,7 +3,7 @@ require_once(get_file_loc('SmrAlliance.class.inc'));
 $template->assign('PageTopic','Alliance VS Alliance Rankings');
 
 require_once(get_file_loc('menu.inc'));
-create_ranking_menu(1, 3);
+create_ranking_menu(1, 4);
 $db2 = new SmrMySqlDatabase();
 $container = array();
 $container['url'] = 'skeleton.php';
