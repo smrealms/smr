f72a09b4c2f9abca943c8e37f1814dfe34785bd0
Dan Hemberger <daniel.hemberger@gmail.com>
2017-12-04 19:54:27 -0800

    Add chat_sharing.php
    Provides a new database table `account_shares_info` and an interface
    to manage this table.
    
    This will be used to allow users in chat to display information
    about more than just the account associated with their Discord ID
    (or IRC Nick). It is generic, but the primary motivation is to
    allow players to display their main and multi.

---
+++ db/patches/V1_6_61_09__account_shares_info.sql
@@ -0,0 +1,7 @@
+-- Allow sharing info between accounts in chat
+CREATE TABLE IF NOT EXISTS `account_shares_info` (
+  `from_account_id` int(10) unsigned NOT NULL,
+  `to_account_id` int(10) unsigned NOT NULL,
+  `game_id` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (from_account_id, to_account_id, game_id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
