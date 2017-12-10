4215eb04c3b668101279388feb55dfc19e713515
Dan Hemberger <daniel.hemberger@gmail.com>
2017-12-10 13:00:23 -0800

    alliance_mod.php: add op response
    When an op is scheduled, a new block of text will be displayed on
    the alliance MOD page with an option to respond to the op.

--- engine/Default/alliance_mod.php
+++ engine/Default/alliance_mod.php
@@ -13,6 +13,36 @@ create_alliance_menu($alliance->getAllianceID(),$alliance->getLeaderID());
 
 $PHP_OUTPUT.= '<div align="center">';
 
+// Check to see if an alliance op is scheduled
+// Display it for 1 hour past start time (late arrivals, etc.)
+$db->query('SELECT time FROM alliance_has_op WHERE alliance_id=' . $db->escapeNumber($player->getAllianceID()) . ' AND game_id=' . $db->escapeNumber($player->getGameID()) . ' AND time > ' . $db->escapeNumber(TIME - 3600) . ' LIMIT 1');
+if ($db->nextRecord()) {
+	$time = $db->getInt('time');
+	$opDate = date(DATE_FULL_SHORT, $time);
+	$opCountdown = format_time($time - TIME);
+
+	// Has player responded yet?
+	$db2 = new SmrMySqlDatabase();
+	$db2->query('SELECT response FROM alliance_has_op_response WHERE alliance_id=' . $db2->escapeNumber($player->getAllianceID()) . ' AND game_id=' . $db2->escapeNumber($player->getGameID()) . ' AND account_id=' . $db2->escapeNumber($player->getAccountID()) . ' LIMIT 1');
+
+	$response = $db2->nextRecord() ? $db2->getField('response') : null;
+	$responseHREF = SmrSession::getNewHREF(create_container('alliance_op_response_processing.php'));
+
+	$PHP_OUTPUT .= '<table class="center nobord opResponse">';
+	$PHP_OUTPUT .= '<tr><th>ENCRYPTED ALLIANCE TELEGRAM</th></tr>';
+	$PHP_OUTPUT .= '<tr><td>Your leader has scheduled an important alliance operation for ' . $opDate . '</td></tr>';
+	$PHP_OUTPUT .= '<tr><td><span id="countdown">' . $opCountdown . '</span></td></tr>';
+	$PHP_OUTPUT .= '<tr><td><b>Will you join the operation?</b></td></tr>';
+	$PHP_OUTPUT .= '<tr><td><form method="POST" action="' . $responseHREF . '">';
+	$responseInputs = array();
+	foreach (array('Yes', 'No', 'Maybe') as $option) {
+		$style = strtoupper($option) == $response ? 'style="background: green"' : '';
+		$responseInputs[] = '<input type="submit" name="op_response" ' . $style . ' value="' . $option . '" />';
+	}
+	$PHP_OUTPUT .= join('&nbsp;', $responseInputs);
+	$PHP_OUTPUT .= '</td></tr></table><br />';
+}
+
 if ($alliance->hasImageURL()) {
 	$PHP_OUTPUT.= '<img class="alliance" src="' . $alliance->getImageURL() . '" alt="' . htmlspecialchars($alliance->getAllianceName()) . ' Banner"><br /><br />';
 }
---
+++ engine/Default/alliance_op_response_processing.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!isset($_POST['op_response'])) {
+	create_error('No op response specified!');
+}
+
+$response = strtoupper($_POST['op_response']);
+
+$db->query('REPLACE INTO alliance_has_op_response (alliance_id, game_id, account_id, response) VALUES (' . $db->escapeNumber($player->getAllianceID()) . ',' . $db->escapeNumber($player->getGameID()) . ',' . $db->escapeNumber($player->getAccountID()) . ', ' . $db->escapeString($response) . ')');
+
+forward(create_container('skeleton.php', 'alliance_mod.php'));
+
+?>
