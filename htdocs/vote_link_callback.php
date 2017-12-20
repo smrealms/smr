1198e3bf8afa68968a8a4c43dd720f224ee11276
Dan Hemberger <daniel.hemberger@gmail.com>
2017-12-20 02:01:33 -0800

    vote_link_callback.php: prevent possible exploits
    Two things we want to prevent:
    1. Spamming the callback so that all requests pass the sanity
       check before the first one updates `turns_claimed=true`.
    2. Locking the player's sector before we check `turns_claimed=false`,
       which could be spammed to freeze a player in place.
    
    To accomplish both of these we:
    * Leave the initial check for `turns_claimed=false` in place as a
      barrier against the sector lock.
    * Perform another `turns_claimed=false` check once we have the
      sector lock to make sure another request hasn't already
      processed the vote.
    * Set `turns_claimed=true` while we still have the sector lock
      to make sure no additional requests slip in between the release
      and the database update.

--- htdocs/vote_link_callback.php
+++ htdocs/vote_link_callback.php
@@ -33,12 +33,21 @@ if ($db->nextRecord()) {
 	// Refresh player after lock is acquired in case any values are stale
 	acquire_lock($player->getSectorID());
 	SmrPlayer::refreshCache();
-	$player->setLastTurnUpdate($player->getLastTurnUpdate()-VOTE_BONUS_TURNS_TIME); //Give turns via added time, no rounding errors.
-	$player->save();
-	release_lock();
+
+	// Now that we are locked, check the database again to make sure turns
+	// weren't claimed while we were waiting for the lock.
+	// This prevents players from manually spamming the callback for lots of free turns.
+	$db->query('SELECT timeout FROM vote_links WHERE account_id=' . $db->escapeNumber($accountId) . ' AND link_id=' . $db->escapeNumber($linkId) . ' AND turns_claimed=' . $db->escapeBoolean(false) . ' LIMIT 1');
+	if (!$db->nextRecord()) {
+		exit;
+	}
 
 	// Prevent getting additional turns until a valid free turns link is clicked again
 	$db->query('REPLACE INTO vote_links (account_id, link_id, timeout, turns_claimed) VALUES(' . $db->escapeNumber($accountId) . ',' . $db->escapeNumber($linkId) . ',' . $db->escapeNumber(TIME) . ',' . $db->escapeBoolean(true) . ')');
+
+	$player->setLastTurnUpdate($player->getLastTurnUpdate()-VOTE_BONUS_TURNS_TIME); //Give turns via added time, no rounding errors.
+	$player->save();
+	release_lock();
 }
 
 ?>
