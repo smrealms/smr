aa70298f911f95e86286d8903f29f45bbfc99238
Dan Hemberger <daniel.hemberger@gmail.com>
2017-12-26 20:18:24 -0800

    mysql_cleanup.php: add error handling
    Catch exceptions in `mysql_cleanup` for two reasons:
    
    1. I encountered another scenario where DiscordPHP (or more
       likely ReactPHP) was swallowing exceptions that occurred in
       the command closures. I thought that the new infrastructure
       prevented this, but it does not seem to work anymore (perhaps
       some PHP module was updated improperly). Therefore it is safer
       to catch the exception ourself and print the error message.
    
    2. Catching the exception allows us to provide some feedback to
       users when their command fails, rather than silently dying and
       then restarting. This will indicate to users that the error
       should be reported to an admin.
    
    NOTE: in PHP7, we should catch both Error and Exception using
    by catching the Throwable class.

--- tools/discord/mysql_cleanup.php
+++ tools/discord/mysql_cleanup.php
@@ -7,7 +7,13 @@ function mysql_cleanup(callable $func) {
 	// Create a new closure that wraps the original closure
 	$func_wrapper = function ($message, $params) use ($func) {
 		// First, call the original closure
-		$func($message, $params);
+		try {
+			$func($message, $params);
+		} catch (Exception $e) {  // NOTE: in PHP7, switch to Throwable
+			print('Error in '.$e->getFile().' line '.$e->getLine().':'.EOL);
+			print($e->getMessage() . EOL);
+			$message->reply('I encountered an error. Please report this to an admin!');
+		}
 
 		// Then, close the mysql connection to prevent timeouts
 		$db = new SmrMySqlDatabase();
