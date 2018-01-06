7d4f3e14bacddb9c1a896b9e31a389a93f5b1878
Dan Hemberger <daniel.hemberger@gmail.com>
2018-01-06 12:37:42 -0800

    alliance_set_op_processing.php: send alliance message
    To make it more obvious when an op is scheduled, send a message
    to the alliance.
    
    The messages will be deleted when the op is canceled.

--- engine/Default/alliance_set_op_processing.php
+++ engine/Default/alliance_set_op_processing.php
@@ -9,6 +9,12 @@ if (!empty($var['cancel'])) {
 	// just get rid of op
 	$db->query('DELETE FROM alliance_has_op WHERE alliance_id=' . $db->escapeNumber($player->getAllianceID()) . ' AND game_id=' . $db->escapeNumber($player->getGameID()));
 	$db->query('DELETE FROM alliance_has_op_response WHERE alliance_id=' . $db->escapeNumber($player->getAllianceID()) . ' AND game_id=' . $db->escapeNumber($player->getGameID()));
+
+	// Delete the announcement from alliance members message boxes
+	$db->query('DELETE FROM message WHERE game_id=' . $db->escapeNumber($player->getGameID()) . ' AND sender_id=' . $db->escapeNumber(ACCOUNT_ID_OP_ANNOUNCE) . ' AND account_id IN (' . $db->escapeArray($player->getAlliance()->getMemberIDs()) . ')');
+
+	// NOTE: for simplicity we don't touch `player_has_unread_messages` here,
+	// so they may get an errant alliance message icon if logged in.
 }
 else {
 	// schedule an op
@@ -23,6 +29,13 @@ else {
 
 	// add op to db
 	$db->query('INSERT INTO alliance_has_op (alliance_id, game_id, time) VALUES (' . $db->escapeNumber($player->getAllianceID()) . ', ' . $db->escapeNumber($player->getGameID()) . ', ' . $db->escapeNumber($time) . ')');
+
+	// Send an alliance message that expires at the time of the op.
+	// Since the message is procedural, don't exclude this player.
+	$message = $player->getBBLink() . ' has scheduled an operation for ' . date(DATE_FULL_SHORT, $time) . '. Navigate to your Alliance console to respond!';
+	foreach ($player->getAlliance()->getMemberIDs() as $memberAccountID) {
+		$player->sendMessageFromOpAnnounce($memberAccountID, $message, $time);
+	}
 }
 
 forward(create_container('skeleton.php', 'alliance_set_op.php'));
