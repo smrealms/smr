280f8c9f05910d41117c115015ed44a3cec49370
Dan Hemberger <daniel.hemberger@gmail.com>
2018-01-07 00:46:24 -0800

    planet_ownership.php: add message about losing ownership
    When you go to claim a planet, it is often annoying to check to
    see if you already own a planet (especially now that we have multis).
    
    With this change, a message will be displayed letting you know that
    if you take ownership when you already have a planet, you will lose
    ownership of that other planet. This implicitly declares the
    "one planet per person" rule, which was otherwise not stated anywhere.
    
    Also minor rewording to make the new text fit better in context.

--- engine/Default/planet_ownership.php
+++ engine/Default/planet_ownership.php
@@ -15,4 +15,10 @@ $template->assign('ProcessingHREF', SmrSession::getNewHREF($container));
 $template->assign('Planet', $planet);
 $template->assign('Player', $player);
 
+// Check if this player already owns a planet
+$db->query('SELECT sector_id FROM planet WHERE game_id=' . $db->escapeNumber($player->getGameID()) . ' AND owner_id=' . $db->escapeNumber($player->getAccountID()));
+if ($db->nextRecord()) {
+	$template->assign('PlayerPlanet', $db->getInt('sector_id'));
+}
+
 ?>
--- templates/Default/engine/Default/planet_ownership.php
+++ templates/Default/engine/Default/planet_ownership.php
@@ -1,28 +1,38 @@
 <?php
 
 if (!$Planet->hasOwner()) { ?>
-	<p>The planet is unclaimed.</p>
+	<p>
+		This planet is unclaimed.<?php
+		if (isset($PlayerPlanet)) { ?>
+			<br />If you claim it, you will lose ownership of the planet in Sector #<?php echo $PlayerPlanet; ?>!<?php
+		} ?>
+	</p>
 	<form method="POST" action="<?php echo $ProcessingHREF; ?>">
 		<input type="submit" name="action" value="Take Ownership" id="InputFields" />
 	</form><?php
 }
 else {
 	if ($Planet->getOwnerID() != $Player->getAccountID()) { ?>
-		<p>You can claim the planet when you enter the correct password.</p>
+		<p><?php echo SmrPlayer::getPlayer($Planet->getOwnerID(), $Planet->getGameID())->getLinkedDisplayName(false); ?> owns this planet.</p>
+		<p>
+			You can claim the planet when you enter the correct password.<?php
+			if (isset($PlayerPlanet)) { ?>
+				<br />If you do, you will lose ownership of the planet in Sector #<?php echo $PlayerPlanet; ?>!<?php
+			} ?>
+		</p>
 		<form method="POST" action="<?php echo $ProcessingHREF; ?>">
 			<input type="text" name="password" id="InputFields">&nbsp;&nbsp;&nbsp;
 			<input type="submit" name="action" value="Take Ownership" id="InputFields" />
 		</form><?php
 	}
 	else { ?>
-		<p>You can set a password for the planet.</p>
+		<p>You own this planet!</p>
 		<form method="POST" action="<?php echo $ProcessingHREF; ?>">
 			<input type="text" name="password" value="<?php echo htmlspecialchars($Planet->getPassword()); ?>" id="InputFields" />&nbsp;&nbsp;&nbsp;
 			<input type="submit" name="action" value="Set Password" id="InputFields" />
 		</form>
 		<br />
 
-		<p>You can rename the planet.</p>
 		<form method="POST" action="<?php echo $ProcessingHREF; ?>">
 			<input type="text" name="name" value="<?php echo htmlspecialchars($Planet->getName()); ?>" id="InputFields" />&nbsp;&nbsp;&nbsp;
 			<input type="submit" name="action" value="Rename" id="InputFields" />
