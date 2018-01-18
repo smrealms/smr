ed6add28ad6994644a659d3eedae3600bf510468
Dan Hemberger <daniel.hemberger@gmail.com>
2018-01-17 18:58:39 -0800

    config.php: define RELATIONS_{WAR,PEACE} constants

--- engine/Default/shop_weapon_processing.php
+++ engine/Default/shop_weapon_processing.php
@@ -11,7 +11,7 @@ if (!isset($var['OrderID'])) {
 		create_error('We do not sell that weapon here!');
 	}
 	
-	if ($weapon->getRaceID() != RACE_NEUTRAL && $player->getRelation($weapon->getRaceID()) < 300) {
+	if ($weapon->getRaceID() != RACE_NEUTRAL && $player->getRelation($weapon->getRaceID()) < RELATIONS_PEACE) {
 		create_error('We are at WAR!!! Do you really think I\'m gonna sell you that weapon?');
 	}
 
