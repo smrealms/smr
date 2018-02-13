smrealms/smr

commit 954b89bd77fcba8a791e45e55aa8084e133cacd7

newsletter_send_processing.php: swap From and Reply-To
When sending newsletters in bulk, we want the From address to be
consistent (newsletter@smrealms.de) and the Reply-To address to
be unique (bounce_<account_id>@smrealms.de) for the purpose of
unsubscribing from e-mails.

This is recommended by Google's bulk sender guidelines.

admin/Default/newsletter_send_processing.php
@@ -11,7 +11,7 @@ $mail->SMTPKeepAlive	= true;
 
 //$mail->ConfirmReadingTo       = 'newsletter-read@smrealms.de';
 
-$mail->AddReplyTo('newsletter@smrealms.de', 'SMR Support');
+$mail->AddReplyTo('support@smrealms.de', 'SMR Support');
 $mail->Encoding = 'base64';
 $mail->WordWrap = 72;
 
@@ -67,7 +67,7 @@ if($_REQUEST['to_email']=='*') {
 		echo $account_id.'. Preparing mail for '.$to_name.' <'.$to_email.'>... ';
 
 		// set a bounce address we can process later
-		$mail->From = 'bounce_' . $account_id . '@smrealms.de';
+		$mail->AddReplyTo('bounce_' . $account_id . '@smrealms.de', 'SMR Support');
 		$mail->AddAddress($to_email, $to_name);
 
 		if(!$mail->Send()) {

commit 6c3285dd90b0e8de1cf46acfd95daa8bfcb59586

newsletter_send_processing.php: fix reply-tos
Fix a bug introduced in 954b89bd77f where the reply-to addresses
were not cleared at the end of each iteration for the mass mailings.
This resulted in the Nth account having N reply-to addresses.

We now clear the reply-to addresses in addition to the to-addresses.

admin/Default/newsletter_send_processing.php
@@ -89,8 +89,8 @@ if($_REQUEST['to_email']=='*') {
 		}
 
 		// Clear all addresses for next loop
+		$mail->ClearReplyTos();
 		$mail->ClearAddresses();
-
 	}
 
 	echo '<br />Done! Total '.$sent.' mails sent.';