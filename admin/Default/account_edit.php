smrealms/smr

commit cd34fdd0625f041e5d6a35c402b8f62bed361c16

account_edit.php: minor cleanup
* Use `SmrSession::getRequestVar` to vastly simplify the boilerplate.

* Fix undefined index PHP warnings for `$var['msg']` and
  `$var['errorMsg']`.

admin/Default/account_edit.php
@@ -2,42 +2,12 @@
 
 $template->assign('PageTopic','Edit Account');
 
-if(isset($_REQUEST['account_id']) && is_numeric($_REQUEST['account_id'])) {
-	SmrSession::updateVar('account_id',$_REQUEST['account_id']);
-}
-elseif(!isset($var['account_id'])) {
-	SmrSession::updateVar('account_id', 0);
-}
-if(isset($_REQUEST['login'])) {
-	SmrSession::updateVar('login',$_REQUEST['login']);
-}
-elseif(!isset($var['login'])) {
-	SmrSession::updateVar('login', '');
-}
-if(isset($_REQUEST['val_code'])) {
-	SmrSession::updateVar('val_code',$_REQUEST['val_code']);
-}
-elseif(!isset($var['val_code'])) {
-	SmrSession::updateVar('val_code', '');
-}
-if(isset($_REQUEST['email'])) {
-	SmrSession::updateVar('email',$_REQUEST['email']);
-}
-elseif(!isset($var['email'])) {
-	SmrSession::updateVar('email', '');
-}
-if(isset($_REQUEST['hofname'])) {
-	SmrSession::updateVar('hofname',$_REQUEST['hofname']);
-}
-elseif(!isset($var['hofname'])) {
-	SmrSession::updateVar('hofname', '');
-}
-if(isset($_REQUEST['player_name'])) {
-	SmrSession::updateVar('player_name',$_REQUEST['player_name']);
-}
-elseif(!isset($var['player_name'])) {
-	SmrSession::updateVar('player_name', '');
-}
+$account_id = SmrSession::getRequestVar('account_id', 0);
+$player_name = SmrSession::getRequestVar('player_name', '');
+SmrSession::getRequestVar('login', '');
+SmrSession::getRequestVar('val_code', '');
+SmrSession::getRequestVar('email', '');
+SmrSession::getRequestVar('hofname', '');
 if(isset($_REQUEST['game_id'])) {
 	SmrSession::updateVar('SearchGameID',$_REQUEST['game_id']);
 }
@@ -45,12 +15,9 @@ elseif(!isset($var['SearchGameID'])) {
 	SmrSession::updateVar('SearchGameID', 0);
 }
 
-if(!empty($var['account_id']) && !is_numeric($var['account_id'])) {
+if(!empty($account_id) && !is_numeric($account_id)) {
 	create_error('Account ID must be a number.');
 }
-
-$account_id = $var['account_id'];
-$player_name = $var['player_name'];
 
 // create account object
 $curr_account = false;
@@ -144,7 +111,11 @@ if ($curr_account!==false) {
 }
 
 
-$template->assign('ErrorMessage', $var['errorMsg']);
-$template->assign('Message', $var['msg']);
+if (isset($var['errorMsg'])) {
+	$template->assign('ErrorMessage', $var['errorMsg']);
+}
+if (isset($var['msg'])) {
+	$template->assign('Message', $var['msg']);
+}
 
 ?>

templates/Default/admin/Default/account_edit.php
@@ -312,6 +312,6 @@
 if(isset($ErrorMessage)) { ?>
 	<div align="center"><?php echo $ErrorMessage; ?></div><?php
 }
-
-?>
-<div align="center"><?php echo $Message; ?></div>
+if(isset($Message)) { ?>
+	<div align="center"><?php echo $Message; ?></div><?php
+} ?>

commit 6e3da3c147892923ca8b3f09a2aa89ab9353faf4

account_edit.php: fix searching with no account ID
Fix a bug introduced in cd34fdd0625, which resulted because
`$_REQUEST['account_id']` is _always_ specified due to the way
that the form is set up. I thought the default value would be 0,
but it will always be the empty string if not specified. Then
this empty string caused a `$db->escapeNumber` to fail.

We now explciitly set `$account_id = 0` if it is not specified.

admin/Default/account_edit.php
@@ -2,7 +2,7 @@
 
 $template->assign('PageTopic','Edit Account');
 
-$account_id = SmrSession::getRequestVar('account_id', 0);
+$account_id = SmrSession::getRequestVar('account_id', '');
 $player_name = SmrSession::getRequestVar('player_name', '');
 SmrSession::getRequestVar('login', '');
 SmrSession::getRequestVar('val_code', '');
@@ -15,7 +15,9 @@ elseif(!isset($var['SearchGameID'])) {
 	SmrSession::updateVar('SearchGameID', 0);
 }
 
-if(!empty($account_id) && !is_numeric($account_id)) {
+if (empty($account_id)) {
+	$account_id = 0;
+} elseif (!is_numeric($account_id)) {
 	create_error('Account ID must be a number.');
 }
 