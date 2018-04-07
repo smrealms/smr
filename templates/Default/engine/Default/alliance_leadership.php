892d1d1aaf0285a410da0af51ec33f2a63557b36
Dan Hemberger <daniel.hemberger@gmail.com>
2018-04-06 23:02:02 -0700

    alliance_leadership.php: convert to template
    Also add `SmrAlliance::getMembers` as a convenience wrapper around
    `SmrPlayer::getAlliancePlayers`.

---
+++ templates/Default/engine/Default/alliance_leadership.php
@@ -0,0 +1,16 @@
+Please select the new Leader:
+
+<form method="POST" action="<?php echo $HandoverHREF; ?>">
+	<select name="leader_id" id="InputFields" size="1">
+		<?php
+		foreach ($AlliancePlayers as $alliancePlayer) {
+			$selected = $alliancePlayer->equals($ThisPlayer) ? 'selected="selected"' : '';
+			?>
+			<option value="<?php echo $alliancePlayer->getAccountID(); ?>" <?php echo $selected; ?>>
+				<?php echo $alliancePlayer->getDisplayName(); ?>
+			</option><?php
+		} ?>
+	</select>
+	<br /><br />
+	<input type="submit" name="action" id="InputFields" value="Handover Leadership" />
+</form>
