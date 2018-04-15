fcc860c63eba40f1fff713ee9bb464d6559e57b4
Dan Hemberger <daniel.hemberger@gmail.com>
2018-04-15 01:29:17 -0700

    Remove restriction on admin announcement message length
    Messages can now be unlimited in size, instead of limited to
    255 characters.

--- admin/Default/announcement_create_processing.php
+++ admin/Default/announcement_create_processing.php
@@ -6,10 +6,6 @@ if($_REQUEST['action'] == 'Preview announcement') {
 	forward($container);
 }
 
-if (strlen($message) > 255) {
-	create_error('No more than 255 characters per announcement!');
-}
-
 // put the msg into the database
 $db->query('INSERT INTO announcement (time, admin_id, msg) VALUES('.$db->escapeNumber(TIME).', '.$db->escapeNumber(SmrSession::$account_id).', '.$db->escapeString($message).')');
 
