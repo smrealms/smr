c255b82d6ba4064a6547f581b97578b0dc7240f1
Dan Hemberger <daniel.hemberger@gmail.com>
2018-04-23 17:46:00 -0700

    SmrAlliance: Move validation check into canJoinAlliance
    The validation check was previously part of
    `alliance_join_processing.php`, but it makes more sense to perform
    this check any time we are determining if a player can join an
    alliance.

--- engine/Default/alliance_join_processing.php
+++ engine/Default/alliance_join_processing.php
@@ -2,11 +2,6 @@
 
 require_once(get_file_loc('SmrAlliance.class.inc'));
 
-// is account validated?
-if (!$account->isValidated()) {
-	create_error('You are not validated. You can\'t join an alliance yet.');
-}
-
 $alliance = SmrAlliance::getAlliance($var['alliance_id'], $player->getGameID());
 
 $canJoin = $alliance->canJoinAlliance($player);
