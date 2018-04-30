68a75285580d906afebd0ab1a2c4d6c4cdb17805
Dan Hemberger <daniel.hemberger@gmail.com>
2018-04-30 15:48:50 -0700

    Rename "Game Open/Close" admin tool
    Since we usually use the term 'Game' to refer to an individual
    `SmrGame`, we change this tool to refer to 'Server' instead,
    which should clarify for admins what this tool actually does.

--- admin/Default/game_status.php
+++ admin/Default/game_status.php
@@ -3,19 +3,19 @@
 $db->query('SELECT * FROM game_disable');
 if (!$db->getNumRows()) {
 
-	$template->assign('PageTopic','Close Game');
+	$template->assign('PageTopic','Close Server');
 
 	$container = array();
 	$container['url'] = 'game_status_processing.php';
 	$PHP_OUTPUT.=create_echo_form($container);
-	$PHP_OUTPUT.=('If you wish to close the game please enter a reason for the closure.<br /><br />');
+	$PHP_OUTPUT.=('If you wish to close Space Merchant Realms, please enter a reason for the closure.<br /><br />');
 	$PHP_OUTPUT.=('<input spellcheck="true" type="text" name="close_reason" maxlength="255" size="100" id="InputFields"><br /><br />');
 	$PHP_OUTPUT.=create_submit('Close');
 	$PHP_OUTPUT.=('</form>');
 
 } else {
 
-	$template->assign('PageTopic','Open Game');
+	$template->assign('PageTopic','Open Server');
 
 	$container = array();
 	$container['url'] = 'game_status_processing.php';
