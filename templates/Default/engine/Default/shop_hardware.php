f16c781c1ae6cef0c7102a7ed6475eb68fa11a50
Dan Hemberger <daniel.hemberger@gmail.com>
2018-05-27 09:29:30 -0700

    Allow reselling combat drones
    A feature often requested by players is a way to sell combat
    drones. Here we add the ability to resell combat drones at
    any location that sells combat drones.
    
    We only allow selling combat drones because they are the only
    hardware that affects your combat rating (i.e. most similar to
    weapons).
    
    The only reason one may even want to consider selling any other
    type of hardware is during a ship trade. Rather than allowing
    any hardware to be sold (which could be dangerous for e.g. new
    players who accidentally sell all their armour and shields),
    it would make more sense to simply provide a refund for surplus
    hardware when buying a new ship. We may consider this in a
    future patch.
    
    Issues in shop_hardware_processing.php regarding attack
    warnings:
    
    * Removed `isUnderAttack`, which was never needed when buying
      hardware anyway. Removing it should prevent suppressing some
      attack warnings in certain scenarios (i.e. someone fires on
      you right before you buy hardware -- you'd want to know this,
      but the previous code would not warn you).
    
    * When selling CDs, there may be cases where attack warnings are
      suppressed. This is not ideal, but necessary in the current
      code infrastructure. It will only be suppressed if you are
      selling CDs and the attacker only does CD damage to you.

--- templates/Default/engine/Default/shop_hardware.php
+++ templates/Default/engine/Default/shop_hardware.php
@@ -1,39 +1,62 @@
 <?php
 if (isset($HardwareSold)) { ?>
 	<script type="text/javascript">
-		function recalcOnKeyUp(hardwareTypeID, cost) {
-			var form = document.getElementById('hardwareForm' + hardwareTypeID);
+		function recalcOnKeyUp(transaction, hardwareTypeID, cost) {
+			var form = document.getElementById(transaction + hardwareTypeID);
 			form.total.value = form.amount.value * cost;
 		}
 	</script>
-	<table class="center standard">
+	<h2>Buy Hardware</h2>
+	<table class="standard">
 		<tr>
 			<th>Name</th>
 			<th>Purchase Amount</th>
-			<th>&nbsp;</th>
-			<th>Unit Cost</th>
-			<th>&nbsp;</th>
-			<th width="75">Totals</th>
+			<th width="80">Unit Cost</th>
+			<th>Total Cost</th>
 			<th>Action</th>
 		</tr><?php
 		foreach ($HardwareSold as $HardwareTypeID => $Hardware) {
 			$AmountToBuy = $ThisShip->getMaxHardware($HardwareTypeID) - $ThisShip->getHardware($HardwareTypeID); ?>
 
-			<form method="POST" id="hardwareForm<?php echo $HardwareTypeID; ?>" action="<?php echo $Hardware['HREF']; ?>">
+			<form method="POST" id="buy<?php echo $HardwareTypeID; ?>" action="<?php echo $Hardware['HREF']; ?>">
 				<tr>
 					<td><?php echo $Hardware['Name']; ?></td>
-					<td><input type="number" name="amount" value="<?php echo $AmountToBuy; ?>" size="5" onKeyUp="recalcOnKeyUp(<?php echo $HardwareTypeID; ?>,<?php echo $Hardware['Cost']; ?>)" id="InputFields" class="center"></td>
-					<td>*</td>
-					<td><?php echo number_format($Hardware['Cost']); ?></td>
-					<td>=</td>
+					<td><input type="number" name="amount" value="<?php echo $AmountToBuy; ?>" size="5" onKeyUp="recalcOnKeyUp('buy',<?php echo $HardwareTypeID; ?>,<?php echo $Hardware['Cost']; ?>)" id="InputFields" class="center"></td>
+					<td class="center"><?php echo number_format($Hardware['Cost']); ?></td>
 					<td><input type="number" name="total" disabled="disabled" value="<?php echo $AmountToBuy * $Hardware['Cost']; ?>" size="7" id="InputFields" class="center"></td>
-					<td>
+					<td class="center">
 						<input type="submit" name="action" value="Buy" id="InputFields" />
 					</td>
 				</tr>
 			</form><?php
 		} ?>
 	</table><?php
+
+	if (isset($HardwareSold[HARDWARE_COMBAT])) { ?>
+		<br />
+		<h2>Sell Hardware</h2>
+		<table class="standard">
+			<tr>
+				<th>Name</th>
+				<th>Sell Amount</th>
+				<th width="80">Unit Value</th>
+				<th>Total Refund</th>
+				<th>Action</th>
+			</tr><?php
+			$Hardware = $HardwareSold[HARDWARE_COMBAT];
+			$UnitRefund = round($Hardware['Cost'] * CDS_REFUND_PERCENT);
+			$MaxAmountToSell = $ThisShip->getHardware(HARDWARE_COMBAT); ?>
+			<form method="POST" id="sell<?php echo HARDWARE_COMBAT; ?>" action="<?php echo $Hardware['HREF']; ?>">
+				<tr>
+					<td><?php echo $Hardware['Name']; ?></td>
+					<td><input type="number" name="amount" value="<?php echo $MaxAmountToSell; ?>" size="5" onKeyUp="recalcOnKeyUp('sell',<?php echo HARDWARE_COMBAT; ?>,<?php echo $UnitRefund; ?>)" class="center"></td>
+					<td class="center"><?php echo number_format($UnitRefund); ?></td>
+					<td><input type="number" name="total" disabled="disabled" value="<?php echo $MaxAmountToSell * $UnitRefund; ?>" size="7" class="center"></td>
+					<td class="center"><input type="submit" name="action" value="Sell" /></td>
+				</tr>
+			</form>
+		</table><?php
+	}
 }
 else {
 	?>I have nothing to sell to you. Get out of here!<?php
