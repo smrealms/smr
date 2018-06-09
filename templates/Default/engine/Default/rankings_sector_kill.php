d794ebb24eb4b7832d342c943281c6d54a9bda7d
Dan Hemberger <daniel.hemberger@gmail.com>
2018-06-08 18:46:56 -0700

    rankings_sector_kill.php: convert to template
    Also make the size of the two tables the same.

---
+++ templates/Default/engine/Default/rankings_sector_kill.php
@@ -0,0 +1,14 @@
+<div align="center">
+	<p>Here are the most deadly Sectors!</p>
+	<?php $this->includeTemplate('includes/SectorKillList.inc', array('Rankings' => $TopTen)); ?>
+
+	<form method="POST" action="<?php echo $SubmitHREF; ?>">
+		<p>
+			<input type="number" name="min_rank" value="<?php echo $MinRank; ?>" size="3" id="InputFields" class="center">&nbsp;-&nbsp;
+			<input type="number" name="max_rank" value="<?php echo $MaxRank; ?>" size="3" id="InputFields" class="center">&nbsp;
+			<input type="submit" name="action" value="Show" />
+		</p>
+	</form>
+
+	<?php $this->includeTemplate('includes/SectorKillList.inc', array('Rankings' => $TopCustom)); ?>
+</div>
