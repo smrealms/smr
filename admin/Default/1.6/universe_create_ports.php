844dab95be1561598a1ad2a9967f09c3842dc4ec
Dan Hemberger <daniel.hemberger@gmail.com>
2018-06-19 22:26:43 -0700

    Remove "mining"
    This feature was supposed to be a new type of location (i.e. an
    asteroid) that you could mine for precious metals.
    
    We remove it for 3 reasons:
    
    1. This feature of the game was not used.
    
    2. The database tables associated with this feature do not exist,
       so we could not enable it without significant work.
    
    3. It uses a poorly chosen name ("mining" and "mines", which
       conflict with mines as forces).
    
    Also clean up "Create Ports"
    * Remove "...and Mines"
    * Make totals disabled inputs because they shouldn't be modified
      manually.

--- admin/Default/1.6/universe_create_ports.php
+++ admin/Default/1.6/universe_create_ports.php
@@ -8,23 +8,14 @@ $galaxy = SmrGalaxy::getGalaxy($var['game_id'],$var['gal_on']);
 //get totals
 $total = array();
 $totalPorts = array();
-$totalMines = array();
 $total['Ports'] = 0;
-$total['Mines'] = 0;
 for ($i=1;$i<=9;$i++) {
 	$totalPorts[$i] = 0;
-}
-for ($i=1;$i<=20;$i++) {
-	$totalMines[$i] = 0;
 }
 foreach ($galaxy->getSectors() as $galSector) {
 	if($galSector->hasPort()) {
 		$totalPorts[$galSector->getPort()->getLevel()]++;
 		$total['Ports']++;
-	}
-	if($galSector->hasMine()) {
-		$totalMines[$galSector->getMine()->getLevel()]++;
-		$total['Mines']++;
 	}
 }
 
@@ -36,44 +27,35 @@ $container['body'] = '1.6/universe_create_sectors.php';
 
 $PHP_OUTPUT.= create_echo_form($container);
 $PHP_OUTPUT.= 'Working on Galaxy : ' . $galaxy->getName() . ' (' . $galaxy->getGalaxyID() . ')<br />';
-$PHP_OUTPUT.= '<table width="100%"><tr><th>Ports</th><th>Port Races</th><th>Starting Mines</th></tr><tr><td class="center">';
-$PHP_OUTPUT.= '<table class="standard">';
+$PHP_OUTPUT.= '<table width="100%"><tr><td class="center">';
+$PHP_OUTPUT.= '<table class="standard"><tr><th>Port Level</th><th>Number of Ports</th></tr>';
 for ($i=1;$i<=9;$i++) {
-	$PHP_OUTPUT.= '<tr><td class="right">Level ' . $i . ' Ports</td><td>';
+	$PHP_OUTPUT.= '<tr><td class="right">Level ' . $i . '</td><td>';
 	$PHP_OUTPUT.= '<input type="number" value="';
 	$PHP_OUTPUT.= $totalPorts[$i];
 	$PHP_OUTPUT.= '" size="5" name="port' . $i . '" onFocus="startCalc();" onBlur="stopCalc();"></td></tr>';
 }
-$PHP_OUTPUT.= '<tr><td type="number" class="right">Total Ports</td><td><input type="number" size="5" name="total" value="';
+$PHP_OUTPUT.= '<tr><th class="right">Total</th><td><input type="number" disabled="disabled" size="5" name="total" value="';
 $PHP_OUTPUT.= $total['Ports'];
 $PHP_OUTPUT.= '"></td></tr>';
 $PHP_OUTPUT.= '</table>';
 $PHP_OUTPUT.= '</td><td class="center">';
-$PHP_OUTPUT.= '<table class="standard"><tr><th colspan="2">Port Race % Distribution</th></tr>';
+$PHP_OUTPUT.= '<table class="standard"><tr><th>Port Race</th><th>% Distribution</th></tr>';
 
 foreach (Globals::getRaces() as $race) {
 	$PHP_OUTPUT.= '<tr><td class="right">' . $race['Race Name'] . '</td><td><input type="number" size="5" name="race' . $race['Race ID'] . '" value="0" onFocus="startRaceCalc();" onBlur="stopRaceCalc();"></td></tr>';
 }
-$PHP_OUTPUT.= '<tr><td class="right">Total</td><td><input type="number" size="5" name="racedist" value="0"></td></tr>';
+$PHP_OUTPUT.= '<tr><th class="right">Total</th><td><input type="number" disabled="disabled" size="5" name="racedist" value="0"></td></tr>';
 $PHP_OUTPUT.= '<tr><td class="center" colspan="2">';
 $PHP_OUTPUT.= '<div class="buttonA"><a class="buttonA" onClick="setEven();">&nbsp;Set All Equal&nbsp;</a></div></td></tr>';
 $PHP_OUTPUT.= '</table>';
-$PHP_OUTPUT.= '</td><td class="center"><table class="standard">';
-for ($i=1;$i<=20;$i++) {
-	$PHP_OUTPUT.= '<tr><td class="right">Level ' . $i . ' Mines</td><td>';
-	$PHP_OUTPUT.= '<input type="number" value="';
-	$PHP_OUTPUT.= $totalMines[$i];
-	$PHP_OUTPUT.= '" size="5" name="mine' . $i . '" onFocus="startCalcM();" onBlur="stopCalcM();"></td></tr>';
-}
-$PHP_OUTPUT.= '<tr><td class="right">Total Mines</td><td><input type="number" size="5" name="totalM" value="';
-$PHP_OUTPUT.= $total['Mines'];
-$PHP_OUTPUT.= '"></td></tr>';
-$PHP_OUTPUT.= '</table></td></tr>';
+$PHP_OUTPUT.= '</td>';
+$PHP_OUTPUT.= '</tr>';
 
-$PHP_OUTPUT.= '<tr><td colspan="3" class="center"><input type="submit" name="submit" value="Create Ports and Mines">';
+$PHP_OUTPUT.= '<tr><td colspan="3" class="center"><input type="submit" name="submit" value="Create Ports">';
 $PHP_OUTPUT.= '<br /><br /><input type="submit" name="submit" value="Cancel"></td></tr></table>';
 
 $PHP_OUTPUT.= '</form>';
 
-$PHP_OUTPUT.= '<span class="small">Note: When you press "Create Ports and Mines" this will rearrange all current ports and mines.<br />';
-$PHP_OUTPUT.= 'To add new ports and mines without rearranging everything use the edit sector feature.</span>';
+$PHP_OUTPUT.= '<span class="small">Note: When you press "Create Ports" this will rearrange all current ports.<br />';
+$PHP_OUTPUT.= 'To add new ports without rearranging everything use the edit sector feature.</span>';
