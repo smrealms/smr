bc432a401ce19356ded0b853691f6cbc5874286f
Dan Hemberger <daniel.hemberger@gmail.com>
2018-07-03 16:53:19 -0700

    announcements.php: convert to template

--- engine/Default/announcements.php
+++ engine/Default/announcements.php
@@ -2,36 +2,27 @@
 
 $template->assign('PageTopic','Announcements');
 
-$PHP_OUTPUT.= '<table class="standard fullwidth">';
-$PHP_OUTPUT.= '<tr><th>Time</th><th>Message</th></tr>';
-
 if (!isset($var['view_all'])) {
-	$db->query('SELECT time, login, msg
+	$db->query('SELECT time, msg
 				FROM announcement
-				JOIN account ON announcement.admin_id=account.account_id
-				AND time > ' . $db->escapeNumber($account->getLastLogin()) . '
+				WHERE time > ' . $db->escapeNumber($account->getLastLogin()) . '
 				ORDER BY time DESC');
 }
 else {
-	$db->query('SELECT time, login, msg
+	$db->query('SELECT time, msg
 				FROM announcement
-				JOIN account ON announcement.admin_id=account.account_id
 				ORDER BY time DESC');
 }
 
+$announcements = [];
 while ($db->nextRecord()) {
-	$PHP_OUTPUT.= '<tr>';
-	$PHP_OUTPUT.= '<td class="shrink top noWrap">';
-	//$PHP_OUTPUT.= $db->getField('login');
-	//$PHP_OUTPUT.= '<br />';
-	$PHP_OUTPUT.= date(DATE_FULL_SHORT_SPLIT, $db->getField('time'));
-	$PHP_OUTPUT.= '</td><td class="top">';
-	$PHP_OUTPUT.= bbifyMessage($db->getField('msg'));
-	$PHP_OUTPUT.= '</td></tr>';
+	$announcements[] = ['Time' => $db->getInt('time'),
+	                    'Msg' => $db->getField('msg')];
 }
+$template->assign('Announcements', $announcements);
 
 $PHP_OUTPUT.= '</table><br />';
 
 $container = create_container('login_check_processing.php');
 $container['CheckType'] = 'Updates';
-$PHP_OUTPUT .= create_button($container, 'Continue');
+$template->assign('ContinueHREF', SmrSession::getNewHREF($container));
