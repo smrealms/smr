8ac39416c5c7e9664cc51a6126c89b4dbe0730c6
Dan Hemberger <daniel.hemberger@gmail.com>
2018-07-07 13:40:41 -0700

    universe_create_ports.php: convert to template

--- admin/Default/1.6/universe_create_ports.php
+++ admin/Default/1.6/universe_create_ports.php
@@ -1,15 +1,18 @@
 <?php
 require_once(get_file_loc('SmrGalaxy.class.inc'));
 
-if (isset($var['gal_on'])) $gal_on = $var['gal_on'];
-else $PHP_OUTPUT.= 'Gal_on not found!!';
+if (!isset($var['gal_on'])) {
+	throw Exception('Gal_on not found!');
+}
 
 $galaxy = SmrGalaxy::getGalaxy($var['game_id'],$var['gal_on']);
+$template->assign('Galaxy', $galaxy);
+
 //get totals
 $total = array();
 $totalPorts = array();
 $total['Ports'] = 0;
-for ($i=1;$i<=9;$i++) {
+for ($i=1; $i<=SmrPort::MAX_LEVEL; $i++) {
 	$totalPorts[$i] = 0;
 }
 foreach ($galaxy->getSectors() as $galSector) {
@@ -19,43 +22,10 @@ foreach ($galaxy->getSectors() as $galSector) {
 	}
 }
 
-//universe_create_ports.php
-//get totals
 $container = $var;
 $container['url'] = '1.6/universe_create_save_processing.php';
 $container['body'] = '1.6/universe_create_sectors.php';
+$template->assign('CreateHREF', SmrSession::getNewHREF($container));
 
-$PHP_OUTPUT.= create_echo_form($container);
-$PHP_OUTPUT.= 'Working on Galaxy : ' . $galaxy->getName() . ' (' . $galaxy->getGalaxyID() . ')<br />';
-$PHP_OUTPUT.= '<table width="100%"><tr><td class="center">';
-$PHP_OUTPUT.= '<table class="standard"><tr><th>Port Level</th><th>Number of Ports</th></tr>';
-for ($i=1;$i<=9;$i++) {
-	$PHP_OUTPUT.= '<tr><td class="right">Level ' . $i . '</td><td>';
-	$PHP_OUTPUT.= '<input type="number" value="';
-	$PHP_OUTPUT.= $totalPorts[$i];
-	$PHP_OUTPUT.= '" size="5" name="port' . $i . '" onFocus="startCalc();" onBlur="stopCalc();"></td></tr>';
-}
-$PHP_OUTPUT.= '<tr><th class="right">Total</th><td><input type="number" disabled="disabled" size="5" name="total" value="';
-$PHP_OUTPUT.= $total['Ports'];
-$PHP_OUTPUT.= '"></td></tr>';
-$PHP_OUTPUT.= '</table>';
-$PHP_OUTPUT.= '</td><td class="center">';
-$PHP_OUTPUT.= '<table class="standard"><tr><th>Port Race</th><th>% Distribution</th></tr>';
-
-foreach (Globals::getRaces() as $race) {
-	$PHP_OUTPUT.= '<tr><td class="right">' . $race['Race Name'] . '</td><td><input type="number" size="5" name="race' . $race['Race ID'] . '" value="0" onFocus="startRaceCalc();" onBlur="stopRaceCalc();"></td></tr>';
-}
-$PHP_OUTPUT.= '<tr><th class="right">Total</th><td><input type="number" disabled="disabled" size="5" name="racedist" value="0"></td></tr>';
-$PHP_OUTPUT.= '<tr><td class="center" colspan="2">';
-$PHP_OUTPUT.= '<div class="buttonA"><a class="buttonA" onClick="setEven();">&nbsp;Set All Equal&nbsp;</a></div></td></tr>';
-$PHP_OUTPUT.= '</table>';
-$PHP_OUTPUT.= '</td>';
-$PHP_OUTPUT.= '</tr>';
-
-$PHP_OUTPUT.= '<tr><td colspan="3" class="center"><input type="submit" name="submit" value="Create Ports">';
-$PHP_OUTPUT.= '<br /><br /><input type="submit" name="submit" value="Cancel"></td></tr></table>';
-
-$PHP_OUTPUT.= '</form>';
-
-$PHP_OUTPUT.= '<span class="small">Note: When you press "Create Ports" this will rearrange all current ports.<br />';
-$PHP_OUTPUT.= 'To add new ports without rearranging everything use the edit sector feature.</span>';
+$template->assign('Total', $total);
+$template->assign('TotalPorts', $totalPorts);
---
+++ templates/Default/admin/Default/1.6/universe_create_ports.php
@@ -0,0 +1,64 @@
+<form name="FORM" method="POST" action="<?php echo $CreateHREF; ?>">
+	Working on Galaxy : <?php echo $Galaxy->getName(); ?> (<?php echo $Galaxy->getGalaxyID(); ?>)<br />
+	<table>
+		<tr>
+			<td class="center">
+
+				<table class="standard">
+					<tr>
+						<th>Port Level</th>
+						<th>Number of Ports</th>
+					</tr><?php
+					foreach ($TotalPorts as $Level => $Count) { ?>
+						<tr>
+							<td class="right">Level <?php echo $Level; ?></td>
+							<td><input type="number" value="<?php echo $Count; ?>" size="5" name="port<?php echo $Level; ?>" onFocus="startCalc();" onBlur="stopCalc();"></td>
+						</tr><?php
+					} ?>
+					<tr>
+						<th class="right">Total</th>
+						<td><input type="number" disabled="disabled" size="5" name="total" value="<?php echo $Total['Ports']; ?>"></td>
+					</tr>
+				</table>
+			</td>
+
+			<td class="center">
+				<table class="standard">
+					<tr>
+						<th>Port Race</th>
+						<th>% Distribution</th>
+					</tr><?php
+					foreach (Globals::getRaces() as $race) { ?>
+						<tr>
+							<td class="right"><?php echo $race['Race Name']; ?></td>
+							<td><input type="number" size="5" name="race<?php echo $race['Race ID']; ?>" value="0" onFocus="startRaceCalc();" onBlur="stopRaceCalc();"></td>
+						</tr><?php
+					} ?>
+					<tr>
+						<th class="right">Total</th>
+						<td><input type="number" disabled="disabled" size="5" name="racedist" value="0"></td>
+					</tr>
+					<tr>
+						<td class="center" colspan="2">
+							<div class="buttonA">
+								<a class="buttonA" onClick="setEven();">&nbsp;Set All Equal&nbsp;</a>
+							</div>
+						</td>
+					</tr>
+				</table>
+
+			</td>
+		</tr>
+
+		<tr>
+			<td colspan="3" class="center">
+				<input type="submit" name="submit" value="Create Ports">
+				<br /><br />
+				<input type="submit" name="submit" value="Cancel">
+			</td>
+		</tr>
+	</table>
+</form>
+
+<span class="small">Note: When you press "Create Ports" this will rearrange all current ports.<br />
+To add new ports without rearranging everything use the Edit Sector feature.</span>
