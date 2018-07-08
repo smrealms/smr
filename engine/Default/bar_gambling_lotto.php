bc02988dd91ab40c02f2797cf06577ec693e2284
Dan Hemberger <daniel.hemberger@gmail.com>
2018-07-07 20:32:34 -0700

    bar_gambling_lotto.php: convert to template

--- engine/Default/bar_gambling_lotto.php
+++ engine/Default/bar_gambling_lotto.php
@@ -2,20 +2,9 @@
 require_once(get_file_loc('bar.functions.inc'));
 checkForLottoWinner($player->getGameID());
 $lottoInfo = getLottoInfo($player->getGameID());
+$template->assign('LottoInfo', $lottoInfo);
 
-//find the time remaining in this jackpot. (which is 2 days from the first purchased ticket)
-$PHP_OUTPUT.=('<br /><div align="center">Currently <b>'.format_time($lottoInfo['TimeRemaining']).'</b> remain until the winning ticket');
-$PHP_OUTPUT.=(' is drawn, and the prize is $' . number_format($lottoInfo['Prize']) . '.<br /><br />');
-$PHP_OUTPUT.=('Ahhhh so your interested in the lotto huh?  ');
-$PHP_OUTPUT.=('Well here is how it works.  First you will need to buy a ticket, ');
-$PHP_OUTPUT.=('they cost $1,000,000 a piece.  Next you need to watch the news.  Once the winning ');
-$PHP_OUTPUT.=('lotto ticket is drawn there will be a section in the news with the winner.');
-$PHP_OUTPUT.=('  If you win you can come to any bar and claim your prize!');
-$PHP_OUTPUT.=('<br /><br />');
-$container = array();
-$container['url'] = 'skeleton.php';
-$container['body'] = 'bar_main.php';
+$container = create_container('skeleton.php', 'bar_main.php');
 $container['script'] = 'bar_gambling_processing.php';
 $container['action'] = 'process';
-$PHP_OUTPUT.=create_button($container,'Buy a Ticket ($1,000,000)');
-$PHP_OUTPUT.=('</div>');
+$template->assign('BuyTicketHREF', SmrSession::getNewHREF($container));
---
+++ templates/Default/engine/Default/bar_gambling_lotto.php
@@ -0,0 +1,16 @@
+<br />
+<div class="center">
+	Currently <b><?php echo format_time($LottoInfo['TimeRemaining']); ?></b> remain until the winning ticket
+	is drawn.<br />
+	The prize is $<?php echo number_format($LottoInfo['Prize']); ?>.
+	<br /><br />
+	Ahhhh so your interested in the lotto huh?
+	Well here is how it works. First you need to buy a ticket,
+	they cost $1,000,000 each. Next you need to watch the news. Once the winning
+	ticket is drawn there will be a section in the news announcing the winner.
+	If you win you can claim your prize at any bar!
+	<br /><br />
+	<div class="buttonA">
+		<a class="buttonA" href="<?php echo $BuyTicketHREF; ?>">&nbsp;Buy a Ticket ($1,000,000)&nbsp;</a>
+	</div>
+</div>
