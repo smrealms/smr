94e3e644fbbe99697be3ede64f5578becc86b29d
Dan Hemberger <daniel.hemberger@gmail.com>
2018-08-05 22:29:13 -0700

    shop_ship.php: replace explicit query with methods
    Use the `SmrLocation::getShipsSold` method instead of explicitly
    duplicating the query here.

--- engine/Default/shop_ship.php
+++ engine/Default/shop_ship.php
@@ -1,21 +1,13 @@
 <?php
 $template->assign('PageTopic','Ship Dealer');
 
-$db->query('SELECT ship_type_id FROM location
-			JOIN location_sells_ships USING (location_type_id)
-			WHERE sector_id = ' . $db->escapeNumber($player->getSectorID()) . '
-				AND game_id = ' . $db->escapeNumber($player->getGameID()) . '
-				AND location_type_id = '.$db->escapeNumber($var['LocationID']));
-$shipsSold = array();
-if ($db->getNumRows() > 0 ) {
+$shipsSold = $location->getShipsSold();
+if ($shipsSold) {
 	$container = create_container('skeleton.php','shop_ship.php');
 	transfer('LocationID');
 
-	while ($db->nextRecord()) {
-		$shipTypeID = $db->getField('ship_type_id');
-		$shipsSold[$shipTypeID] =& AbstractSmrShip::getBaseShip(Globals::getGameType($player->getGameID()),$shipTypeID);
+	foreach (array_keys($shipsSold) as $shipTypeID) {
 		$container['ship_id'] = $shipTypeID;
-		$container['level_needed'] = $shipsSold[$shipTypeID]['Level'];
 		$shipsSoldHREF[$shipTypeID] = SmrSession::getNewHREF($container);
 	}
 }
@@ -23,7 +15,7 @@ $template->assign('ShipsSold',$shipsSold);
 $template->assign('ShipsSoldHREF',$shipsSoldHREF);
 
 if (isset($var['ship_id'])) {
-	$compareShip = AbstractSmrShip::getBaseShip(Globals::getGameType($player->getGameID()),$var['ship_id']);
+	$compareShip = $shipsSold[$var['ship_id']];
 	$compareShip['RealSpeed'] = $compareShip['Speed'] * Globals::getGameSpeed($player->getGameID());
 	$compareShip['Turns'] = round($player->getTurns()*$compareShip['Speed']/$ship->getSpeed());
 
