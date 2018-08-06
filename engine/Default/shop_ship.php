8b1ce1c6dede5ce73f09b6125e3b4be7d398acdd
Dan Hemberger <daniel.hemberger@gmail.com>
2018-08-05 22:00:54 -0700

    Revert "Refactor shop_ship.php"
    This reverts commit b4cfab7de8d46a8e8a19a6ea9b7b7ffa1f7a8dbd.
    
    Page identified that the better way to do this is by abstracting
    SmrLocation and overriding `getShipsSold`.

--- engine/Default/shop_ship.php
+++ engine/Default/shop_ship.php
@@ -1,9 +1,36 @@
 <?php
+$template->assign('PageTopic','Ship Dealer');
 
 $db->query('SELECT ship_type_id FROM location
 			JOIN location_sells_ships USING (location_type_id)
 			WHERE sector_id = ' . $db->escapeNumber($player->getSectorID()) . '
 				AND game_id = ' . $db->escapeNumber($player->getGameID()) . '
 				AND location_type_id = '.$db->escapeNumber($var['LocationID']));
+$shipsSold = array();
+if ($db->getNumRows() > 0 ) {
+	$container = create_container('skeleton.php','shop_ship.php');
+	transfer('LocationID');
 
-include('shop_ship.inc');
+	while ($db->nextRecord()) {
+		$shipTypeID = $db->getField('ship_type_id');
+		$shipsSold[$shipTypeID] =& AbstractSmrShip::getBaseShip(Globals::getGameType($player->getGameID()),$shipTypeID);
+		$container['ship_id'] = $shipTypeID;
+		$container['level_needed'] = $shipsSold[$shipTypeID]['Level'];
+		$shipsSoldHREF[$shipTypeID] = SmrSession::getNewHREF($container);
+	}
+}
+$template->assign('ShipsSold',$shipsSold);
+$template->assign('ShipsSoldHREF',$shipsSoldHREF);
+
+if (isset($var['ship_id'])) {
+	$compareShip = AbstractSmrShip::getBaseShip(Globals::getGameType($player->getGameID()),$var['ship_id']);
+	$compareShip['RealSpeed'] = $compareShip['Speed'] * Globals::getGameSpeed($player->getGameID());
+	$compareShip['Turns'] = round($player->getTurns()*$compareShip['Speed']/$ship->getSpeed());
+
+	$container = create_container('shop_ship_processing.php');
+	transfer('LocationID');
+	transfer('ship_id');
+	$compareShip['BuyHREF'] = SmrSession::getNewHREF($container);
+
+	$template->assign('CompareShip',$compareShip);
+}
