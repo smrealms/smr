697bd58f9255f4e1731e96d5d94d3d05d8bca705
Dan Hemberger <daniel.hemberger@gmail.com>
2018-08-11 14:14:04 -0700

    cleanup_old_data.sql: add tables to clean up
    * player_read_thread
    * weighted_random
    
    These are the largest remaining tables that have no usefulness
    after a game ends.

--- db/maintenance/cleanup_old_data.sql
+++ db/maintenance/cleanup_old_data.sql
@@ -4,5 +4,7 @@ DELETE FROM player_visited_port WHERE game_id IN ( SELECT game_id FROM game WHER
 DELETE FROM player_visited_sector WHERE game_id IN ( SELECT game_id FROM game WHERE end_date < UNIX_TIMESTAMP() );
 DELETE FROM port_info_cache WHERE game_id IN ( SELECT game_id FROM game WHERE end_date < UNIX_TIMESTAMP() );
 DELETE FROM player_has_unread_messages WHERE game_id IN ( SELECT game_id FROM game WHERE end_date < UNIX_TIMESTAMP() );
+DELETE FROM player_read_thread WHERE game_id IN ( SELECT game_id FROM game WHERE end_date < UNIX_TIMESTAMP() );
+DELETE FROM weighted_random WHERE game_id IN ( SELECT game_id FROM game WHERE end_date < UNIX_TIMESTAMP() );
 
 DELETE FROM npc_logs;
