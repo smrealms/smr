8e115ec19308ee5451c983ef91db1afb4d5eb680
Dan Hemberger <daniel.hemberger@gmail.com>
2018-09-16 19:29:27 -0700

    Remove Password Check admin tool
    Now that password hashes are salted, they will no longer be the
    same hash even if the plain-text passwords are the same. Therefore,
    there is no longer a benefit to looking for duplicate password
    hashes. We remove the admin tool.

--- admin/Default/password_check_processing.php
+++
@@ -1,7 +0,0 @@
-<?php
-$disable_account = $_REQUEST['disable_account'];
-foreach ($disable_account as $currAccountID) {
-	//never expire
-	SmrAccount::getAccount($currAccountID)->banAccount(0,$account,2,'Double password');
-}
-forward(create_container('skeleton.php', 'admin_tools.php'));
