8781965828abb722fe9275b6d280db2085126be7
Dan Hemberger <daniel.hemberger@gmail.com>
2018-12-31 10:23:16 -0800

    trader_savings.php: convert to template
    Also fix a couple issues:
    
    * Use `format_time` instead of explicitly converting the time to
      a string.
    
    * Fix having multiple PageTopics specified that were overwriting
      each other.

--- engine/Default/trader_savings.php
+++ engine/Default/trader_savings.php
@@ -1,51 +1,41 @@
 <?php
 
-$template->assign('PageTopic','Anonymous accounts for '.$player->getPlayerName());
+$template->assign('PageTopic', 'Savings');
 
 Menu::trader();
 
-$PHP_OUTPUT.=('<br /><br />');
+$anonAccounts = [];
 $db->query('SELECT * FROM anon_bank WHERE owner_id = ' . $db->escapeNumber($player->getAccountID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()));
-if ($db->getNumRows()) {
-	$PHP_OUTPUT.=('You own the following accounts<br /><br />');
-	while ($db->nextRecord()) {
-		$acc_id = $db->getInt('anon_id');
-		$pass = $db->getField('password');
-		$PHP_OUTPUT.=('Account <span class="yellow">'.$acc_id.'</span> with password <span class="yellow">'.$pass.'</span><br />');
-	}
+while ($db->nextRecord()) {
+	$anonAccounts[] = [
+		'ID' => $db->getInt('anon_id'),
+		'Password' => $db->getField('password'),
+	];
 }
-else
-	$PHP_OUTPUT.=('You own no anonymous accounts<br />');
+$template->assign('AnonAccounts', $anonAccounts);
 
 require_once(get_file_loc('bar.functions.inc'));
 checkForLottoWinner($player->getGameID());
-$lottoInfo = getLottoInfo($player->getGameID());
+$template->assign('LottoInfo', getLottoInfo($player->getGameID()));
 
-$template->assign('PageTopic','Lotto Tickets for '.$player->getPlayerName());
-$days = floor($lottoInfo['TimeRemaining'] / 60 / 60 / 24);
-$lottoInfo['TimeRemaining'] -= $days * 60 * 60 * 24;
-$hours = floor($lottoInfo['TimeRemaining'] / 60 / 60);
-$lottoInfo['TimeRemaining'] -= $hours * 60 * 60;
-$mins = floor($lottoInfo['TimeRemaining'] / 60);
-$lottoInfo['TimeRemaining'] -= $mins * 60;
-$secs = $lottoInfo['TimeRemaining'];
-$lottoInfo['TimeRemaining'] = '<b>'.$days.' Days, '.$hours.' Hours, '.$mins.' Minutes, and '.$secs.' Seconds</b>';
-	
+// Number of active lotto tickets this player has
 $db->query('SELECT count(*) FROM player_has_ticket WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND account_id = ' . $db->escapeNumber($player->getAccountID()) . ' AND time > 0');
 $db->nextRecord();
 $tickets = $db->getInt('count(*)');
-$PHP_OUTPUT.=('<br />You own <span class="yellow">'.$tickets.'</span> Lotto Tickets.<br />There are '.$lottoInfo['TimeRemaining'].' remaining until the drawing.');
+$template->assign('LottoTickets', $tickets);
+
+// Number of active lotto tickets all players have
 $db->query('SELECT count(*) FROM player_has_ticket WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND time > 0');
 $db->nextRecord();
 $tickets_tot = $db->getInt('count(*)');
-if ($tickets_tot > 0) {
-	$chance = round(($tickets / $tickets_tot) * 100,2);
-	$PHP_OUTPUT.=('<br />Currently you have a '.$chance.' % chance to win.');
-	
+if ($tickets == 0) {
+	$win_chance = 0;
+} else {
+	$win_chance = round(100 * $tickets / $tickets_tot, 2);
 }
-$db->query('SELECT count(*) FROM player_has_ticket WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND account_id = ' .
-			$player->getAccountID().' AND time = 0');
+$template->assign('LottoWinChance', $win_chance);
+
+// Number of winning lotto tickets this player has to claim
+$db->query('SELECT count(*) FROM player_has_ticket WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND account_id = ' . $db->escapeNumber($player->getAccountID()) . ' AND time = 0');
 $db->nextRecord();
-$winningTickets = $db->getInt('count(*)');
-if ($winningTickets > 0)
-	$PHP_OUTPUT.=('<br /><br />You currently own '.$winningTickets.' winning tickets.  You should go to the bar to claim your prize.');
+$template->assign('WinningTickets', $db->getInt('count(*)'));
