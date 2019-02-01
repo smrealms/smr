802e4784876b3c000d810341ce99757693fad04f
Dan Hemberger <daniel.hemberger@gmail.com>
2019-02-01 15:34:20 -0800

    alliance_mod.php: convert to template

--- engine/Default/alliance_mod.php
+++ engine/Default/alliance_mod.php
@@ -4,21 +4,18 @@ if (!isset($var['alliance_id'])) {
 }
 
 $alliance = SmrAlliance::getAlliance($var['alliance_id'], $player->getGameID());
+$template->assign('Alliance', $alliance);
 
 Globals::canAccessPage('AllianceMOTD', $player, array('AllianceID' => $alliance->getAllianceID()));
 
 $template->assign('PageTopic', $alliance->getAllianceName(false, true));
 Menu::alliance($alliance->getAllianceID(), $alliance->getLeaderID());
 
-$PHP_OUTPUT.= '<div align="center">';
-
 // Check to see if an alliance op is scheduled
 // Display it for 1 hour past start time (late arrivals, etc.)
 $db->query('SELECT time FROM alliance_has_op WHERE alliance_id=' . $db->escapeNumber($player->getAllianceID()) . ' AND game_id=' . $db->escapeNumber($player->getGameID()) . ' AND time > ' . $db->escapeNumber(TIME - 3600) . ' LIMIT 1');
 if ($db->nextRecord()) {
-	$time = $db->getInt('time');
-	$opDate = date(DATE_FULL_SHORT, $time);
-	$opCountdown = format_time($time - TIME);
+	$template->assign('OpTime', $db->getInt('time'));
 
 	// Has player responded yet?
 	$db2 = new SmrMySqlDatabase();
@@ -26,43 +23,24 @@ if ($db->nextRecord()) {
 
 	$response = $db2->nextRecord() ? $db2->getField('response') : null;
 	$responseHREF = SmrSession::getNewHREF(create_container('alliance_op_response_processing.php'));
+	$template->assign('OpResponseHREF', $responseHREF);
 
-	$PHP_OUTPUT .= '<table class="center nobord opResponse">';
-	$PHP_OUTPUT .= '<tr><th>ENCRYPTED ALLIANCE TELEGRAM</th></tr>';
-	$PHP_OUTPUT .= '<tr><td>Your leader has scheduled an important alliance operation for ' . $opDate . '</td></tr>';
-	$PHP_OUTPUT .= '<tr><td><span id="countdown">' . $opCountdown . '</span></td></tr>';
-	$PHP_OUTPUT .= '<tr><td><b>Will you join the operation?</b></td></tr>';
-	$PHP_OUTPUT .= '<tr><td><form method="POST" action="' . $responseHREF . '">';
 	$responseInputs = array();
 	foreach (array('Yes', 'No', 'Maybe') as $option) {
 		$style = strtoupper($option) == $response ? 'style="background: green"' : '';
-		$responseInputs[] = '<input type="submit" name="op_response" ' . $style . ' value="' . $option . '" />';
+		$responseInputs[$option] = $style;
 	}
-	$PHP_OUTPUT .= join('&nbsp;', $responseInputs);
-	$PHP_OUTPUT .= '</td></tr></table><br />';
+	$template->assign('ResponseInputs', $responseInputs);
 }
 
-if ($alliance->hasImageURL()) {
-	$PHP_OUTPUT.= '<img class="alliance" src="' . $alliance->getImageURL() . '" alt="' . htmlspecialchars($alliance->getAllianceName()) . ' Banner"><br /><br />';
-}
-
-$PHP_OUTPUT.= '<span class="yellow">Message from your leader</span><br /><br />';
-$PHP_OUTPUT.= bbifyMessage($alliance->getMotD());
-
+// Does the player have edit permission?
 $role_id = $player->getAllianceRole($alliance->getAllianceID());
-
 $db->query('SELECT * FROM alliance_has_roles WHERE alliance_id = ' . $db->escapeNumber($player->getAllianceID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND role_id = ' . $db->escapeNumber($role_id));
 $db->nextRecord();
 if ($db->getBoolean('change_mod') || $db->getBoolean('change_pass')) {
-	$PHP_OUTPUT.= '<br /><br />';
 	$container=create_container('skeleton.php','alliance_stat.php');
 	$container['alliance_id'] = $alliance->getAllianceID();
-	$PHP_OUTPUT.=create_button($container,'Edit');
+	$template->assign('EditHREF', SmrSession::getNewHREF($container));
 }
 
-$discordServer = $alliance->getDiscordServer();
-if (!empty($discordServer)) {
-	$PHP_OUTPUT .= '<br /><br /><br /><iframe src="https://discordapp.com/widget?id=' . $discordServer . '&theme=dark" width="350" height="375" allowtransparency="true" frameborder="0"></iframe>';
-}
-
-$PHP_OUTPUT.= '</div>';
+$template->assign('DiscordServer', $alliance->getDiscordServer());
---
+++ templates/Default/engine/Default/alliance_mod.php
@@ -0,0 +1,43 @@
+<div class="center">
+
+<?php
+if (isset($OpTime)) { ?>
+	<table class="center nobord opResponse">
+		<tr><th>ENCRYPTED ALLIANCE TELEGRAM</th></tr>
+		<tr><td>Your leader has scheduled an important alliance operation for <?php echo date(DATE_FULL_SHORT, $OpTime); ?></td></tr>
+		<tr><td><span id="countdown"><?php echo format_time($OpTime - TIME); ?></span></td></tr>
+		<tr><td><b>Will you join the operation?</b></td></tr>
+		<tr><td>
+			<form method="POST" action="<?php echo $OpResponseHREF; ?>"><?php
+				foreach ($ResponseInputs as $option => $style) { ?>
+					<span style="padding: 0 4px 0 4px">
+						<input type="submit" name="op_response" <?php echo $style; ?> value="<?php echo $option; ?>" />
+					</span><?php
+				} ?>
+			</form>
+		</td></tr>
+	</table><br /><?php
+}
+
+if ($Alliance->hasImageURL()) { ?>
+	<img class="alliance" src="<?php echo $Alliance->getImageURL(); ?>" alt="">
+	<br /><br /><?php
+} ?>
+
+<span class="yellow">Message from your leader</span>
+<p><?php echo bbifyMessage($Alliance->getMotD()); ?></p>
+
+<?php
+if (isset($EditHREF)) { ?>
+	<div class="buttonA">
+		<a class="buttonA" href="<?php echo $EditHREF; ?>">&nbsp;Edit&nbsp;</a>
+	</div><?php
+}
+
+if (!empty($DiscordServer)) { ?>
+	<br /><br />
+	<iframe src="https://discordapp.com/widget?id=<?php echo $DiscordServer; ?>&theme=dark" width="350" height="375" allowtransparency="true" frameborder="0"></iframe>
+	<?php
+} ?>
+
+</div>
