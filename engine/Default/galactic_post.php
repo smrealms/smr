82e9247c7612c6da9280e1abd1e252b96351cdf6
Dan Hemberger <daniel.hemberger@gmail.com>
2019-02-21 12:40:15 -0800

    galactic_post.php: convert to template
    Also raise exception if page is accessed by non-Editor.

--- engine/Default/galactic_post.php
+++ engine/Default/galactic_post.php
@@ -1,60 +1,58 @@
 <?php
 
-$template->assign('PageTopic','Galactic Post');
-if ($player->isGPEditor()) {
-	$db2 = new SmrMySqlDatabase();
-	Menu::galactic_post();
-
-	$PHP_OUTPUT.=('Welcome '.$player->getPlayerName().', your position is <i>Editor</i><br /><br />');
-	$PHP_OUTPUT.=('<b>EDITOR OPTIONS<br /></b>');
-	$PHP_OUTPUT.=create_link(create_container('skeleton.php', 'galactic_post_view_applications.php'), 'View the applications');
-	$PHP_OUTPUT.=('<br />');
-	$PHP_OUTPUT.=create_link(create_container('skeleton.php', 'galactic_post_view_article.php'), 'View the articles');
-	$PHP_OUTPUT.=('<br />');
-	$PHP_OUTPUT.=create_link(create_container('skeleton.php', 'galactic_post_make_paper.php'), 'Make a paper');
-	$PHP_OUTPUT.=('<br />');
-	$PHP_OUTPUT.=create_link(create_container('skeleton.php', 'galactic_post_view_members.php'), 'View Members');
-	$PHP_OUTPUT.=('<br /><br />');
-	$db->query('SELECT * FROM galactic_post_paper WHERE game_id = ' . $db->escapeNumber($player->getGameID()));
-	if ($db->getNumRows()) {
-		$PHP_OUTPUT.=('The following papers are already made (papers must have 3-8 articles to go to the press):<br /><br />');
-	}
-	while($db->nextRecord()) {
-		$paper_name = $db->getField('title');
-		$paper_id = $db->getField('paper_id');
-		$published = $db->getInt('online_since');
-
-		$PHP_OUTPUT.=('<span class="red">***</span><i>'.$paper_name.'</i>');
-		$db2->query('SELECT * FROM galactic_post_paper_content WHERE paper_id = ' . $db2->escapeNumber($paper_id) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()));
-		$hasEnoughArticles = $db2->getNumRows() > 2 && $db2->getNumRows() < 9;
-
-		// Use red/green to show if a paper has enough articles
-		$PHP_OUTPUT.=(' which contains <span class="' . ($hasEnoughArticles ? "green" : "red") . '"> ' . $db2->getNumRows() . ' </span>articles. ');
-		if (!empty($published)) {
-			$PHP_OUTPUT.=('<span class="bold green">PUBLISHED!</span>');
-		} else if ($hasEnoughArticles) {
-			$container = array();
-			$container['url'] = 'galactic_post_make_current.php';
-			$container['id'] = $paper_id;
-			$PHP_OUTPUT.=create_link($container, '<b>HIT THE PRESS!</b>');
-		}
-
-		$PHP_OUTPUT.=('<br />');
-		$container = array();
-		$container['url'] = 'skeleton.php';
-		$container['body'] = 'galactic_post_delete_confirm.php';
-		$container['paper'] = 'yes';
-		$container['id'] = $paper_id;
-		$PHP_OUTPUT.=create_link($container, 'Delete '.$paper_name);
-		$PHP_OUTPUT.=('<br />');
-		$container = array();
-		$container['url'] = 'skeleton.php';
-		$container['body'] = 'galactic_post_paper_edit.php';
-		$container['id'] = $paper_id;
-		$PHP_OUTPUT.=create_link($container, 'Edit '.$paper_name);
-		$PHP_OUTPUT.=('<br /><br />');
-	}
-	$PHP_OUTPUT.=('<br />');
-	$PHP_OUTPUT.=('<span class="blue">If you wish to edit an article you must first view it.</span>');
-	$PHP_OUTPUT.=('<br /><br />');
+if (!$player->isGPEditor()) {
+	throw new Exception('Only the GP Editor is allowed to view this page!');
 }
+
+$template->assign('PageTopic','Galactic Post');
+Menu::galactic_post();
+
+	$db2 = new SmrMySqlDatabase();
+
+$container = create_container('skeleton.php', 'galactic_post_view_applications.php');
+$template->assign('ViewApplicationsHREF', SmrSession::getNewHREF($container));
+$container['body'] = 'galactic_post_view_article.php';
+$template->assign('ViewArticlesHREF', SmrSession::getNewHREF($container));
+$container['body'] = 'galactic_post_make_paper.php';
+$template->assign('MakePaperHREF', SmrSession::getNewHREF($container));
+$container['body'] = 'galactic_post_view_members.php';
+$template->assign('ViewWritersHREF', SmrSession::getNewHREF($container));
+
+$db->query('SELECT * FROM galactic_post_paper WHERE game_id = ' . $db->escapeNumber($player->getGameID()));
+$papers = [];
+while ($db->nextRecord()) {
+	$paper_name = $db->getField('title');
+	$paper_id = $db->getInt('paper_id');
+	$published = $db->getInt('online_since');
+
+	$db2->query('SELECT count(*) FROM galactic_post_paper_content WHERE paper_id = ' . $db2->escapeNumber($paper_id) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()));
+	$db2->nextRecord();
+	$numArticles = $db2->getField('count(*)');
+	$hasEnoughArticles = $numArticles > 2 && $numArticles < 9;
+
+	$paper = [
+		'title' => $db->getField('title'),
+		'num_articles' => $numArticles,
+		'color' => $hasEnoughArticles ? 'green' : 'red',
+		'published' => !empty($published) && $published > 0,
+	];
+
+	if (!empty($published)) {
+	} else if ($hasEnoughArticles) {
+		$container = create_container('galactic_post_make_current.php');
+		$container['id'] = $paper_id;
+		$paper['PublishHREF'] = SmrSession::getNewHREF($container);
+	}
+
+	$container = create_container('skeleton.php', 'galactic_post_delete_confirm.php');
+	$container['paper'] = 'yes';
+	$container['id'] = $paper_id;
+	$paper['DeleteHREF'] = SmrSession::getNewHREF($container);
+
+	$container = create_container('skeleton.php', 'galactic_post_paper_edit.php');
+	$container['id'] = $paper_id;
+	$paper['EditHREF'] = SmrSession::getNewHREF($container);
+
+	$papers[] = $paper;
+}
+$template->assign('Papers', $papers);
