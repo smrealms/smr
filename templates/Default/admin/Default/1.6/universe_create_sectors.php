9375b080937238e80a245b7616b4123ba771d1b6
Dan Hemberger <daniel.hemberger@gmail.com>
2019-02-27 20:58:41 -0800

    universe_create_sectors.php: move options to top of page
    Consolidate options at the top of the page, because for large
    galaxies it can be laborious to continually scroll to the bottom
    of the page.

--- templates/Default/admin/Default/1.6/universe_create_sectors.php
+++ templates/Default/admin/Default/1.6/universe_create_sectors.php
@@ -1,57 +1,56 @@
-<div class="center"><?php
-	if(isset($Message)) {
-		echo $Message; ?><br /><br /><?php
-	} ?>
+<table class="center standard">
+	<tr>
+		<th>ID</th>
+		<th>Name</th>
+		<th>Type</th>
+		<th>Size</th>
+		<th>Max Force Time</th>
+		<th>Connectivity</th>
+	</tr>
+	<tr>
+		<td><?php echo $Galaxy->getGalaxyID(); ?> / <?php echo count($Galaxies); ?></td>
+		<td><?php echo $Galaxy->getName(); ?></td>
+		<td><?php echo $Galaxy->getGalaxyType(); ?></td>
+		<td><?php echo $Galaxy->getWidth(); ?> x <?php echo $Galaxy->getHeight(); ?></td>
+		<td><?php echo $Galaxy->getMaxForceTime() / 3600; ?> hours</td>
+		<td><?php echo $ActualConnectivity; ?>%</td>
+	</tr>
+</table>
+<br />
 
-	<table align="center" class="standard">
-		<tr>
-			<th>ID</th>
-			<th>Name</th>
-			<th>Type</th>
-			<th>Size</th>
-			<th>Max Force Time</th>
-			<th>Connectivity</th>
-		</tr>
-		<tr>
-			<td><?php echo $Galaxy->getGalaxyID(); ?> / <?php echo count($Galaxies); ?></td>
-			<td><?php echo $Galaxy->getName(); ?></td>
-			<td><?php echo $Galaxy->getGalaxyType(); ?></td>
-			<td><?php echo $Galaxy->getWidth(); ?> x <?php echo $Galaxy->getHeight(); ?></td>
-			<td><?php echo $Galaxy->getMaxForceTime() / 3600; ?> hours</td>
-			<td><?php echo $ActualConnectivity; ?>%</td>
-		</tr>
-	</table>
-	<br />
-
-	<form method="POST" action="<?php echo $JumpGalaxyHREF; ?>">
-		<select name="gal_on" onchange="this.form.submit()"><?php
-			foreach($Galaxies as &$CurrentGalaxy) { ?>
-				<option value="<?php echo $CurrentGalaxy->getGalaxyID(); ?>"<?php if($CurrentGalaxy->equals($Galaxy)) { ?> selected="SELECTED"<?php } ?>><?php
-					echo $CurrentGalaxy->getName(); ?>
-				</option><?php
-			} ?>
-		</select>
-		<input type="submit" value="Jump To Galaxy">
-	</form>
-</div><br />
-<?php $this->includeTemplate('includes/SectorMap.inc'); ?>
 <table class="center">
 	<tr>
 		<td>
+			<form method="POST" action="<?php echo $JumpGalaxyHREF; ?>">
+				<select name="gal_on" onchange="this.form.submit()"><?php
+					foreach($Galaxies as &$CurrentGalaxy) { ?>
+						<option value="<?php echo $CurrentGalaxy->getGalaxyID(); ?>"<?php if($CurrentGalaxy->equals($Galaxy)) { ?> selected="SELECTED"<?php } ?>><?php
+							echo $CurrentGalaxy->getName(); ?>
+						</option><?php
+					} ?>
+				</select>
+				<input type="submit" value="Select Galaxy">
+			</form>
+			<p><a href="<?php echo $SMRFileHREF; ?>" class="submitStyle" target="_self">Create SMR file</a></p>
+		</td>
+
+		<td>
+			<table class="center standard">
+				<tr><th>Modify Galaxy</th></tr>
+				<tr><td><a href="<?php echo $ModifyLocationsHREF; ?>">Locations</a></td></tr>
+				<tr><td><a href="<?php echo $ModifyPlanetsHREF; ?>">Planets</a></td></tr>
+				<tr><td><a href="<?php echo $ModifyPortsHREF; ?>">Ports</a></td></tr>
+				<tr><td><a href="<?php echo $ModifyWarpsHREF; ?>">Warps</a></td></tr>
+			</table>
+		</td>
+
+		<td>
 			<form method="POST" action="<?php echo $SubmitChangesHREF; ?>">
-				Connection Percent<br />
+				Connection %<br />
 				<input type="number" name="connect" value="<?php echo $RequestedConnectivity; ?>" size="3"><br />
 				<input type="submit" name="submit" value="Redo Connections">
 			</form>
-		</td>
-		<td>
-			<a href="<?php echo $ModifyLocationsHREF; ?>" class="submitStyle">Modify Locations</a><br /><br />
-			<a href="<?php echo $ModifyPlanetsHREF; ?>" class="submitStyle">Modify Planets</a><br /><br />
-			<a href="<?php echo $ModifyPortsHREF; ?>" class="submitStyle">Modify Ports</a><br /><br />
-			<a href="<?php echo $ModifyWarpsHREF; ?>" class="submitStyle">Modify Warps</a><br /><br />
-			<a href="<?php echo $SMRFileHREF; ?>" class="submitStyle" target="_self">Create SMR file</a>
-		</td>
-		<td>
+			<br />
 			<form method="POST" action="<?php echo $SubmitChangesHREF; ?>">
 				Sector ID<br />
 				<input type="number" size="5" name="sector_edit"><br />
@@ -60,3 +59,10 @@
 		</td>
 	</tr>
 </table>
+
+<?php
+if (isset($Message)) { ?>
+	<p class="center"><?php echo $Message; ?></p><?php
+} ?>
+
+<?php $this->includeTemplate('includes/SectorMap.inc'); ?>
