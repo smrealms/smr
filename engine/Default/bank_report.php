94734bd708f00cfd5111c11476aa99e367d16147
Dan Hemberger <daniel.hemberger@gmail.com>
2019-03-27 13:36:10 -0700

    bank_report.php: add separate processing page
    The separate processing page is run only once, which avoids the issue
    of the bank_report.php page re-updating the alliance message board
    every time the page ajax updates.
    
    We also remove some checks on the existence of array indices, which
    we ensure exist earlier in the script.

--- engine/Default/bank_report.php
+++ engine/Default/bank_report.php
@@ -40,12 +40,6 @@ $text .= '<tr><th>Player</th><th>Deposits</th><th>Withdrawals</th><th>Total</th>
 $balance = 0;
 foreach ($totals as $accId => $total) {
 	$balance += $total;
-	if (empty($trans[$accId][DEPOSIT])) {
-		$trans[$accId][DEPOSIT] = 0;
-	}
-	if (empty($trans[$accId][WITHDRAW])) {
-		$trans[$accId][WITHDRAW] = 0;
-	}
 	$text .= '<tr>';
 	$text .= '<td><span class="yellow">' . $players[$accId] . '</span></td>';
 	$text .= '<td class="right">' . number_format($trans[$accId][DEPOSIT]) . '</td>';
@@ -63,36 +57,13 @@ foreach ($totals as $accId => $total) {
 $text .= '</table>';
 $text = '<div class="center"><br />Ending Balance: ' . number_format($balance) . '</div><br />' . $text;
 
-$container=create_container('skeleton.php', 'bank_report.php');
-$container['alliance_id'] = $alliance_id;
-$container['text'] = $text;
-if (isset($var['text'])) {
-	$thread_id = 0;
-	$db->query('SELECT thread_id FROM alliance_thread_topic WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND alliance_id = ' . $db->escapeNumber($alliance_id) . ' AND topic = \'Bank Statement\' LIMIT 1');
-	if ($db->nextRecord()) {
-		$thread_id = $db->getInt('thread_id');
-	}
-	if ($thread_id == 0) {
-		$db->query('SELECT thread_id FROM alliance_thread_topic WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND alliance_id = ' . $db->escapeNumber($alliance_id) . ' ORDER BY thread_id DESC LIMIT 1');
-		if ($db->nextRecord()) {
-			$thread_id = $db->getField('thread_id') + 1;
-		}
-		else {
-			$thread_id = 1;
-		}
-		$db->query('INSERT INTO alliance_thread_topic (game_id, alliance_id, thread_id, topic)
-					VALUES (' . $db->escapeNumber($player->getGameID()) . ', ' . $db->escapeNumber($alliance_id) . ', ' . $db->escapeNumber($thread_id) . ', \'Bank Statement\')');
-		$db->query('INSERT INTO alliance_thread (game_id, alliance_id, thread_id, reply_id, text, sender_id, time)
-					VALUES (' . $db->escapeNumber($player->getGameID()) . ', ' . $db->escapeNumber($alliance_id) . ', ' . $db->escapeNumber($thread_id) . ', 1, ' . $db->escapeString($text) . ', ' . $db->escapeNumber(ACCOUNT_ID_BANK_REPORTER) . ', ' . $db->escapeNumber(TIME) . ')');
-	}
-	else {
-		$db->query('UPDATE alliance_thread SET time = ' . $db->escapeNumber(TIME) . ', text = ' . $db->escapeString($text) . ' WHERE thread_id = ' . $db->escapeNumber($thread_id) . ' AND alliance_id = ' . $db->escapeNumber($alliance_id) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND reply_id = 1');
-		$db->query('DELETE FROM player_read_thread WHERE thread_id = ' . $db->escapeNumber($thread_id) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND alliance_id = ' . $db->escapeNumber($alliance_id));
-	}
-
+if (isset($var['sent_report'])) {
 	$PHP_OUTPUT.=('<div class="center">A statement has been sent to the alliance.</div><br />');
 }
 else {
+	$container=create_container('bank_report_processing.php');
+	$container['alliance_id'] = $alliance_id;
+	$container['text'] = $text;
 	$PHP_OUTPUT.=('<div class="center">');
 	$PHP_OUTPUT.=create_button($container,'Send Report to Alliance');
 	$PHP_OUTPUT.=('</div>');
