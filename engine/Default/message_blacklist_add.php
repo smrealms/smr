4b160b3461a0c93429fc38c6bf91726a60894448
Dan Hemberger <daniel.hemberger@gmail.com>
2019-04-08 13:32:17 -0700

    message_blacklist.php: convert to template
    Also, in the event of an error, set the error text directly instead
    of referring to them by a numeric ID. In the case of internal errors,
    just raise an exception.

--- engine/Default/message_blacklist_add.php
+++ engine/Default/message_blacklist_add.php
@@ -4,8 +4,8 @@ $container = array();
 $container['url'] = 'skeleton.php';
 $container['body'] = 'message_blacklist.php';
 
-if(!isset($_REQUEST['PlayerName']) && !isset($var['account_id'])) {
-	$container['error'] = 1;
+if (empty($_REQUEST['PlayerName']) && !isset($var['account_id'])) {
+	$container['msg'] = '<span class="red bold">ERROR: </span>You must enter a player name.';
 	forward($container);
 }
 
@@ -18,7 +18,7 @@ else {
 	$db->query('SELECT account_id FROM player WHERE player_name=' . $db->escapeString($_REQUEST['PlayerName']) . ' AND game_id=' . $db->escapeNumber($player->getGameID()) . ' LIMIT 1');
 
 	if(!$db->nextRecord()) {
-		$container['error'] = 1;
+		$container['msg'] = '<span class="red bold">ERROR: </span>Player does not exist.';
 		forward($container);
 	}
 	$blacklisted_id = $db->getField('account_id');
@@ -27,11 +27,11 @@ else {
 $db->query('SELECT account_id FROM message_blacklist WHERE account_id=' . $db->escapeNumber($player->getAccountID()) . ' AND blacklisted_id=' . $db->escapeNumber($blacklisted_id) . ' AND game_id=' . $db->escapeNumber($player->getGameID()) . ' LIMIT 1');
 
 if($db->nextRecord()) {
-	$container['error'] = 2;
+	$container['msg'] = '<span class="red bold">ERROR: </span>Player is already blacklisted.';
 	forward($container);
 }
 
 $db->query('INSERT INTO message_blacklist (game_id,account_id,blacklisted_id) VALUES (' . $db->escapeNumber($player->getGameID()) . ',' . $db->escapeNumber($player->getAccountID()) . ',' . $db->escapeNumber($blacklisted_id) . ')');
 
-$container['error'] = 3;
+$container['msg'] = '<span class="yellow">' . $_REQUEST['PlayerName'] . '</span> has been added to your blacklist.';
 forward($container);
---
+++ templates/Default/engine/Default/message_blacklist.php
@@ -0,0 +1,44 @@
+<?php
+if (isset($Message)) {
+	echo $Message; ?><br /><br /><?php
+} ?>
+
+<h2>Blacklisted Players</h2>
+
+<?php
+if ($Blacklist) { ?>
+	<br />
+	<form method="POST" action="<?php echo $BlacklistDeleteHREF; ?>">
+		<table class="standard" width="50%">
+			<tr>
+				<th>Option</th>
+				<th>Game&nbsp;ID</th>
+				<th>Name</th>
+			</tr><?php
+			foreach ($Blacklist as $Entry) { ?>
+				<tr>
+					<td class="center shrink">
+						<input type="checkbox" name="entry_ids[]" value="<?php echo $Entry['entry_id']; ?>">
+					</td>
+					<td class="center shrink"><?php echo $Entry['game_id']; ?></td>
+					<td><?php echo $Entry['player_name']; ?></td>
+				</tr><?php
+			} ?>
+		</table><br />
+		<input type="submit" name="action" value="Remove Selected" />
+	</form>
+	<br /><?php
+} else { ?>
+	<p>You are currently accepting all communications.</p><?php
+} ?>
+
+<br /><h2>Blacklist Player</h2><br />
+<form method="POST" action="<?php echo $BlacklistAddHREF; ?>">
+	<table cellspacing="0" cellpadding="0" class="nobord nohpad">
+		<tr>
+			<td class="top">Name:&nbsp;</td>
+			<td class="mb"><input type="text" name="PlayerName" size="30"></td>
+		</tr>
+	</table><br />
+	<input type="submit" name="action" value="Blacklist" />
+</form>
