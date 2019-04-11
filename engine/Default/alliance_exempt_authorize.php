7d927d1204a24bdc5410a7f8cb0b3b2320ec60a5
Dan Hemberger <daniel.hemberger@gmail.com>
2019-04-10 17:09:34 -0700

    alliance_exempt_authorize.php: convert to template

--- engine/Default/alliance_exempt_authorize.php
+++ engine/Default/alliance_exempt_authorize.php
@@ -3,33 +3,26 @@ $alliance = $player->getAlliance();
 $template->assign('PageTopic', $alliance->getAllianceName(false, true));
 Menu::alliance($alliance->getAllianceID(), $alliance->getLeaderID());
 
-$PHP_OUTPUT.= '<h2>Exemption Requests</h2><br />';
 //get rid of already approved entries
 $db->query('UPDATE alliance_bank_transactions SET request_exempt = 0 WHERE exempt = 1');
-//build player array
-$db->query('SELECT * FROM player WHERE alliance_id = ' . $db->escapeNumber($alliance->getAllianceID()) . ' AND game_id = ' . $db->escapeNumber($alliance->getGameID()));
-while ($db->nextRecord()) {
-	$players[$db->getInt('account_id')] = $db->getField('player_name');
-}
+
+
 $db->query('SELECT * FROM alliance_bank_transactions WHERE request_exempt = 1 ' . 
 			'AND alliance_id = ' . $db->escapeNumber($alliance->getAllianceID()) . ' AND game_id = ' . $db->escapeNumber($alliance->getGameID()) . ' AND exempt = 0');
+$transactions = [];
 if ($db->getNumRows()) {
-	$container=create_container('bank_alliance_exempt_processing.php');
-	$form = create_form($container,'Make Exempt');
-	$PHP_OUTPUT.=('Selecting a box will authorize it, leaving a box unselected will make it unauthorized after you submit.<br />');
-	$PHP_OUTPUT.= $form['form'];
-	$PHP_OUTPUT.=create_table();
-	$PHP_OUTPUT.=('<tr><th>Player Name</th><th>Type</th><th>Reason</th><th>Amount</th><th>Approve</th></tr>');
+	$container = create_container('bank_alliance_exempt_processing.php');
+	$template->assign('ExemptHREF', SmrSession::getNewHREF($container));
+
+	$players = $alliance->getMembers();
 	while ($db->nextRecord()) {
-		$trans = $db->getField('transaction') == 'Payment' ? 'Withdraw' : 'Deposit';
-		$PHP_OUTPUT.=('<tr><td>' . $players[$db->getInt('payee_id')] . '</td><td>' . $trans . '</td><td>' . $db->getField('reason') . '</td><td>' . number_format($db->getInt('amount')) . '</td>');
-		$PHP_OUTPUT.=('<td><input type="checkbox" name="exempt[' . $db->getField('transaction_id') . ']"></td>');
-		$PHP_OUTPUT.=('</tr>');
+		$transactions[] = [
+			'type' => $db->getField('transaction') == 'Payment' ? 'Withdraw' : 'Deposit',
+			'player' => $players[$db->getInt('payee_id')]->getPlayerName(),
+			'reason' => $db->getField('reason'),
+			'amount' => number_format($db->getInt('amount')),
+			'transactionID' => $db->getInt('transaction_id'),
+		];
 	}
-	$PHP_OUTPUT.=('</table><br />');
-	$PHP_OUTPUT.= $form['submit'];
-	$PHP_OUTPUT.=('</form>');
 }
-else {
-	$PHP_OUTPUT.=('Nothing to authorize.');
-}
+$template->assign('Transactions', $transactions);
---
+++ templates/Default/engine/Default/alliance_exempt_authorize.php
@@ -0,0 +1,29 @@
+<h2>Exemption Requests</h2>
+<br /><?php
+if (!empty($Transactions)) { ?>
+	Alliance members have requested exemptions for the following transactions.<br /><br />
+	<form method="POST" action="<?php echo $ExemptHREF; ?>">
+		<table class="standard">
+			<tr>
+				<th>Player Name</th>
+				<th>Type</th>
+				<th>Reason</th>
+				<th>Amount</th>
+				<th>Approve</th>
+			</tr><?php
+			foreach ($Transactions as $Trans) { ?>
+				<tr>
+					<td><?php echo $Trans['player']; ?></td>
+					<td><?php echo $Trans['type']; ?></td>
+					<td><?php echo $Trans['reason']; ?></td>
+					<td><?php echo $Trans['amount']; ?></td>
+					<td><input type="checkbox" name="exempt[<?php echo $Trans['transactionID']; ?>]"></td>
+				</tr><?php
+			} ?>
+		</table>
+		<br />
+		<input type="submit" name="action" value="Make Exempt" />
+	</form><?php
+} else { ?>
+	Nothing to authorize.<?php
+} ?>
