d7f7afc02a5457a9b4404250bce2959b20f9e9f8
Dan Hemberger <daniel.hemberger@gmail.com>
2019-04-14 14:33:17 -0700

    galactic_post_delete_confirm.php: convert to template
    Add an option when deleting a paper to also delete all the articles
    the paper contains (previously it deleted the articles without asking).

--- engine/Default/galactic_post_delete_confirm.php
+++ engine/Default/galactic_post_delete_confirm.php
@@ -1,54 +1,31 @@
 <?php
 
-$db2 = new SmrMySqlDatabase();
-$db3 = new SmrMySqlDatabase();
 if (isset($var['article'])) {
+	$template->assign('PageTopic', 'Delete Article - Confirm');
 	$db->query('SELECT * FROM galactic_post_article WHERE article_id = '.$db->escapeNumber($var['id']).' AND game_id = ' . $db->escapeNumber($player->getGameID()));
 	$db->nextRecord();
-	$title = $db->getField('title');
-	$PHP_OUTPUT.=('Are you sure you want to delete the article named '.$title.'?');
-	$container = array();
-	$container['url'] = 'galactic_post_delete.php';
+	$template->assign('ArticleTitle', $db->getField('title'));
+	$container = create_container('galactic_post_delete.php');
 	transfer('article');
 	transfer('id');
-	$PHP_OUTPUT.=create_echo_form($container);
-	$PHP_OUTPUT.=create_submit('Yes');
-	$PHP_OUTPUT.=('</form>');
-	$container = array();
-	$container['url'] = 'skeleton.php';
-	$container['body'] = 'galactic_post_view_article.php';
-	transfer('id');
-	$PHP_OUTPUT.=create_echo_form($container);
-	$PHP_OUTPUT.=create_submit('No');
-	$PHP_OUTPUT.=('</form>');
+	$template->assign('SubmitHREF', SmrSession::getNewHREF($container));
 }
 else {
+	// Delete paper
+	$template->assign('PageTopic', 'Delete Paper - Confirm');
 	$db->query('SELECT * FROM galactic_post_paper WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND paper_id = '.$db->escapeNumber($var['id']));
 	$db->nextRecord();
-	$title = $db->getField('title');
-	$PHP_OUTPUT.=('Are you sure you want to delete the paper titled '.$title.' and the following articles with it<br /><br />');
-	$db2->query('SELECT * FROM galactic_post_paper_content WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND paper_id = '.$db->escapeNumber($var['id']));
-	while($db2->nextRecord()) {
-		$article_id = $db2->getField('article_id');
-		$db3->query('SELECT * FROM galactic_post_article WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND article_id = '.$db->escapeNumber($article_id));
-		$db3->nextRecord();
-		$article_title = bbifyMessage($db3->getField('title'));
-		$PHP_OUTPUT.=($article_title.'<br />');
-	}
-	$PHP_OUTPUT.=('<br />');
+	$template->assign('PaperTitle', $db->getField('title'));
 
-	$container = array();
-	$container['url'] = 'galactic_post_delete.php';
+	$articles = [];
+	$db->query('SELECT title FROM galactic_post_paper_content JOIN galactic_post_article USING (game_id, article_id) WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND paper_id = '.$db->escapeNumber($var['id']));
+	while ($db->nextRecord()) {
+		$articles[] = bbifyMessage($db->getField('title'));
+	}
+	$template->assign('Articles', $articles);
+
+	$container = create_container('galactic_post_delete.php');
 	transfer('paper');
 	transfer('id');
-	$PHP_OUTPUT.=create_echo_form($container);
-	$PHP_OUTPUT.=create_submit('Yes');
-	$PHP_OUTPUT.=('</form>');
-	$container = array();
-	$container['url'] = 'skeleton.php';
-	$container['body'] = 'galactic_post_view_article.php';
-	transfer('id');
-	$PHP_OUTPUT.=create_echo_form($container);
-	$PHP_OUTPUT.=create_submit('No');
-	$PHP_OUTPUT.=('</form>');
+	$template->assign('SubmitHREF', SmrSession::getNewHREF($container));
 }
