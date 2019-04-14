132119e4e1bf642c8b280f994b44960dc0af6a33
Dan Hemberger <daniel.hemberger@gmail.com>
2019-04-14 15:38:17 -0700

    error.php: convert to template

--- engine/Default/error.php
+++ engine/Default/error.php
@@ -9,10 +9,6 @@ if (SmrSession::hasGame() && is_object($player) && $lock) {
 	forward($container);
 }
 else {
-	$PHP_OUTPUT.=('<h1>ERROR</h1>');
-	$PHP_OUTPUT.=('<p class="big bold">' . $var['message'] . '</p>');
-	$PHP_OUTPUT.=('<br /><br /><br />');
-	$PHP_OUTPUT.=('<p><small>If the error was caused by something you entered, press back and try again.</small></p>');
-	$PHP_OUTPUT.=('<p><small>If it was a DB Error, press back and try again, or logoff and log back on.</small></p>');
-	$PHP_OUTPUT.=('<p><small>If the error was unrecognizable, please notify the administrators.</small></p>');
+	$template->assign('PageTopic', 'Error');
+	$template->assign('Message', $var['message']);
 }
