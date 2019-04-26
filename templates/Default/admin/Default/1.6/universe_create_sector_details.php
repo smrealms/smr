9ecdf9c3ee7b402e754b705981635271f3e9f9db
Dan Hemberger <daniel.hemberger@gmail.com>
2019-04-26 11:52:48 -0700

    Add functionality to the "Edit Sector" uni gen page
    Reorganize the page into Planet, Port, and Locations sections.
    
    Planet: Now displays the time when the planet will become habitable.
    
    Port: Allow explicitly modifying the port goods. Note that this
    feature needs to be used carefully, since we currently don't do any
    error checking to make sure the port has the correct number of goods
    for its level.
    
    Change the following library functions:
    
    * Add SmrPort::setPortGoods to manually set port goods in a safe
      and non-destructive way.
    
    * SmrPort::{add,remove}PortGood return early if the requested change
      would have no effect (i.e. the good already does/doesn't exist).
    
    * Add SmrPlanet::getInhabitableTime so that it can be displayed in
      the planet info.

--- templates/Default/admin/Default/1.6/universe_create_sector_details.php
+++ templates/Default/admin/Default/1.6/universe_create_sector_details.php
@@ -1,35 +1,73 @@
+<a href="<?php echo $CancelHREF; ?>">&lt;&lt; Back</a><br /><br />
 <form method="POST" action="<?php echo $EditHREF; ?>">
+	<h2>Planet</h2><br />
+	<b>Type: </b>
+	<select name="plan_type" class="InputFields">
+		<option value="0">No Planet</option><?php
+		foreach (array_keys(SmrPlanetType::PLANET_TYPES) as $type) { ?>
+			<option value="<?php echo $type; ?>" <?php echo ($type == $SelectedPlanetType ? 'selected' : ''); ?>><?php echo SmrPlanetType::getTypeInfo($type)->name(); ?></option><?php
+		} ?>
+	</select><br /><?php
+	if ($SelectedPlanetType) { ?>
+		<b>Habitable: </b><?php echo date(DATE_FULL_SHORT, $Planet->getInhabitableTime()); ?><br /><?php
+	} ?>
+	<br />
+
+	<h2>Port</h2>
+	<select name="port_level" class="InputFields">
+		<option value="0">No Port</option><?php
+		for ($i=1; $i<=SmrPort::MAX_LEVEL; $i++) { ?>
+			<option value="<?php echo $i; ?>" <?php echo ($i == $SelectedPortLevel ? 'selected' : ''); ?>>Level <?php echo $i; ?></option><?php
+		} ?>
+	</select>&nbsp;
+
+	<select name="port_race" class="InputFields"><?php
+		foreach (Globals::getRaces() as $race) { ?>
+		<option value="<?php echo $race['Race ID']; ?>" <?php echo ($race['Race ID'] == $SelectedPortRaceID ? 'selected' : ''); ?>><?php echo $race['Race Name']; ?></option><?php
+	} ?>
+	</select>
+	<br />
+
+  <?php
+	if ($SelectedPortLevel) { ?>
+		<br /><span class="bold red">WARNING: </span>Ports should only have (Level + 2) number of goods.
+		These options will be ignored if you are changing the port level.
+		<input type="hidden" name="select_goods" value="1" />
+		<table class="nobord">
+			<tr><?php
+				foreach ([1, 2, 3] as $class) { ?>
+					<td class="top">
+					<table class="nobord"><?php
+						foreach (Globals::getGoods() as $good) {
+							if ($good['Class'] == $class) { ?>
+								<tr>
+								<td>
+									<img class="bottom" src="<?php echo $good['ImageLink']; ?>">&nbsp;
+									<select name="good<?php echo $good['ID']; ?>" class="InputFields">
+										<option value="None">--</option><?php
+										foreach (['Buy', 'Sell'] as $trans) { ?>
+											<option <?php if ($Port->hasGood($good['ID'], $trans)) { ?> selected <?php } ?> value="<?php echo $trans; ?>"><?php echo $trans; ?></option><?php
+										} ?>
+									</select>
+								</td>
+								</tr><?php
+							}
+						} ?>
+					</table></td><?php
+				} ?>
+			</tr>
+		</table><?php
+	} ?>
+	<br />
+
+	<h2>Locations</h2>
 	<table class="shrink">
 		<tr>
 			<td class="center noWrap">
-				Planet Type:
-				<select name="plan_type">
-					<option value="0">No Planet</option><?php
-					foreach (array_keys(SmrPlanetType::PLANET_TYPES) as $type) { ?>
-						<option value="<?php echo $type; ?>" <?php echo ($type == $SelectedPlanetType ? 'selected' : ''); ?>><?php echo SmrPlanetType::getTypeInfo($type)->name(); ?></option><?php
-					} ?>
-				</select>
-				<br /><br />
-
-				Port:
-				<select name="port_level">
-					<option value="0">No Port</option><?php
-					for ($i=1; $i<=SmrPort::MAX_LEVEL; $i++) { ?>
-						<option value="<?php echo $i; ?>" <?php echo ($i == $SelectedPortLevel ? 'selected' : ''); ?>>Level <?php echo $i; ?></option><?php
-					} ?>
-				</select>
-
-				<select name="port_race"><?php
-					foreach (Globals::getRaces() as $race) { ?>
-						<option value="<?php echo $race['Race ID']; ?>" <?php echo ($race['Race ID'] == $SelectedPortRaceID ? 'selected' : ''); ?>><?php echo $race['Race Name']; ?></option><?php
-					} ?>
-				</select>
-				<br /><br />
-
 				<?php
 				for ($i=0; $i<UNI_GEN_LOCATION_SLOTS; $i++) { ?>
-					Location <?php echo ($i + 1); ?>:
-					<select name="loc_type<?php echo $i; ?>">
+					<b><?php echo ($i + 1); ?>. </b>
+					<select name="loc_type<?php echo $i; ?>" class="InputFields">
 						<option value="0">No Location</option><?php
 						foreach (SmrLocation::getAllLocations() as $id => $location) { ?>
 							<option value="<?php echo $id ?>" <?php echo ($id == $SectorLocationIDs[$i] ? 'selected' : ''); ?>><?php echo $location->getName(); ?></option><?php
@@ -50,5 +88,9 @@
 
 	<input type="submit" name="submit" value="Edit Sector">
 	<br /><br />
-	<a href="<?php echo $CancelHREF; ?>" class="submitStyle">Cancel</a>
 </form>
+
+<?php
+if (isset($Message)) {
+	echo $Message;
+} ?>
