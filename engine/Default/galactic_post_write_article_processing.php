11d454ff686e4ffad349497faf387a822d599980
Dan Hemberger <daniel.hemberger@gmail.com>
2019-05-24 15:16:07 -0700

    Send message to GP editors when article submitted
    GP Editors are likely not checking for submitted articles frequently
    these days, but we don't want to miss any submissions, so we now send
    notifications to all GP Editors when an article is submitted.
    
    Add a new method `Globals::getGalacticPostEditorIDs` to reuse the
    same query between manage_post_editors.php and
    galactic_post_write_article_processing.php.
    
    Also make the article submission fields `required` to avoid the
    need to use `create_error` if they aren't.

--- engine/Default/galactic_post_write_article_processing.php
+++ engine/Default/galactic_post_write_article_processing.php
@@ -17,19 +17,20 @@ if($_REQUEST['action'] == 'Preview article') {
 	forward($container);
 }
 
-if (empty($title)) {
-	create_error('You must enter a title.');
-}
-if(empty($message)) {
-	create_error('You must enter some text.');
-}
-
 if(isset($var['id'])) {
+	// Editing an article
 	$db->query('UPDATE galactic_post_article SET last_modified = ' . $db->escapeNumber(TIME) . ', text = '.$db->escapeString($message).', title = '.$db->escapeString($title).' WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND article_id = '.$db->escapeNumber($var['id']));
-	//its been changed send back now
 	forward(create_container('skeleton.php','galactic_post_view_article.php'));
 }
 else {
+	// Adding a new article
+	$message = 'Dear Galactic Post editors,<br /><br />[player='.$player->getPlayerID().'] has just submitted an article to the Galactic Post!';
+	foreach (Globals::getGalacticPostEditorIDs($player->getGameID()) as $editorID) {
+		if ($editorID != $player->getAccountID()) {
+			SmrPlayer::sendMessageFromAdmin($player->getGameID(), $editorID, $message);
+		}
+	}
+
 	$db->query('SELECT MAX(article_id) article_id FROM galactic_post_article WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' LIMIT 1');
 	$db->nextRecord();
 	$num = $db->getField('article_id') + 1;
--- templates/Default/engine/Default/galactic_post_write_article.php
+++ templates/Default/engine/Default/galactic_post_write_article.php
@@ -13,8 +13,8 @@ if(isset($Preview)) { ?>
 } ?>
 What is the title?<br />
 <form name="GPArticleForm" method="POST" action="<?php echo $SubmitArticleHref; ?>">
-	<input type="text" name="title" class="InputFields center" style="width:525;" value="<?php if(isset($PreviewTitle)) { echo htmlspecialchars($PreviewTitle); } ?>"><br /><br />
+	<input type="text" name="title" class="InputFields center" style="width:525;" value="<?php if(isset($PreviewTitle)) { echo htmlspecialchars($PreviewTitle); } ?>" required><br /><br />
 	<br />Write what you want to write here!<br />
-	<textarea spellcheck="true" name="message" class="InputFields"><?php if(isset($Preview)) { echo $Preview; } ?></textarea><br /><br />
+	<textarea spellcheck="true" name="message" class="InputFields" required><?php if(isset($Preview)) { echo $Preview; } ?></textarea><br /><br />
 	<input type="submit" name="action" value="Submit article" class="InputFields" />&nbsp;<input type="submit" name="action" value="Preview article" class="InputFields" />
 </form>
