6b5634f109ae7d5e67599feac242d826c23a6c3b
Dan Hemberger <daniel.hemberger@gmail.com>
2019-05-25 11:13:52 -0700

    Add "Database Cleanup" admin tool
    This replaces the functionality of the `cleanup_old_data.sql`
    script in `db/maintenance`.
    
    We also provide info about the size of the database, the number
    of rows deleted from each table, and the change in database size.
    
    Add `route_cache` (for NPC's) to the list of tables to clean up.

---
+++ admin/Default/db_cleanup.php
@@ -0,0 +1,26 @@
+<?php
+
+$template->assign('PageTopic', 'Database Cleanup');
+
+function bytesToMB($bytes) {
+	return round($bytes / (1024 * 1024), 1) . ' MB';
+}
+
+$template->assign('DbSizeMB', bytesToMB($db->getDbBytes()));
+
+if (isset($var['results'])) {
+	// Display the results
+	$template->assign('Results', $var['results']);
+	$template->assign('DiffMB', bytesToMB($var['diffBytes']));
+	$template->assign('Action', $var['action']);
+	$template->assign('EndedGames', $var['endedGames']);
+	$container = create_container('skeleton.php', 'db_cleanup.php');
+	$template->assign('BackHREF', SmrSession::getNewHREF($container));
+} else {
+	// Create processing links
+	$container = create_container('db_cleanup_processing.php');
+	$container['action'] = 'delete';
+	$template->assign('DeleteHREF', SmrSession::getNewHREF($container));
+	$container['action'] = 'preview';
+	$template->assign('PreviewHREF', SmrSession::getNewHREF($container));
+}
