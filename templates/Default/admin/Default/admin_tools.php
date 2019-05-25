30b430cf407596c003f08a45557b409e06dc4c73
Dan Hemberger <daniel.hemberger@gmail.com>
2019-05-24 20:18:21 -0700

    permission_manage.php: group by category
    Group the permissions by category, just like we do in "Admin Tools".
    This should make it easier to manage permissions.
    
    Slight modifications to the `AdminPermissions` class interface, and
    how we structure the data that we pass to the template for both
    "Admin Tools" and "Manage Admin Permissions".

--- templates/Default/admin/Default/admin_tools.php
+++ templates/Default/admin/Default/admin_tools.php
@@ -8,13 +8,13 @@ if(isset($Message)) {
 if(isset($AdminPermissions)) { ?>
 	<h1>Admin Tools</h1>
 	<br /><?php
-	foreach ($AdminPermissions as $Category => $Permissions) { ?>
-		<h2><?php echo $Category; ?></h2>
+	foreach ($AdminPermissions as $CategoryID => $Permissions) { ?>
+		<h2><?php echo AdminPermissions::getCategoryName($CategoryID); ?></h2>
 		<ul><?php
 		foreach ($Permissions as $Permission) { ?>
 			<li><?php
-				if ($Permission['PermissionLink'] !== false) { ?>
-					<a href="<?php echo $Permission['PermissionLink']; ?>"><?php echo $Permission['Name']; ?></a><?php
+				if ($Permission['Link'] !== false) { ?>
+					<a href="<?php echo $Permission['Link']; ?>"><?php echo $Permission['Name']; ?></a><?php
 				} else { ?>
 					<i><?php echo $Permission['Name']; ?></i><?php
 				} ?>
