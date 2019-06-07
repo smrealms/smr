2a035b52f4d2907b5f43bbb64d000223a510eb52
Pagan Gazzard <pjgazzard@googlemail.com>
2019-06-07 22:39:53 +0100

    Avoid unnecessarily referencing `window`

--- htdocs/js/filter_list.js
+++ htdocs/js/filter_list.js
@@ -1,8 +1,8 @@
 // For use with {ship,weapon}_list.php
 
-// Use window variable to store filter values. Since this is used for both
+// Use variable to store filter values. Since this is used for both
 // the ship and weapon tables, it needs to have enough elements for either.
-window.filter = ["All", "All", "All", "All", "All", "All", "All", "All", "All", "All",
+var filter = ["All", "All", "All", "All", "All", "All", "All", "All", "All", "All",
                  "All", "All", "All", "All", "All", "All", "All", "All", "All"];
 
 //reset all check boxes
@@ -17,16 +17,16 @@ function filterSelect(element) {
 	var selected = element.options[element.selectedIndex].value;
 	var columnId = element.parentElement.cellIndex;
 
-	window.filter[columnId] = selected;
+	filter[columnId] = selected;
 	applyFilter('data-list');
 }
 
 function raceToggle() {
 	var toggle = document.getElementById("raceform");
-	window.filter[1] = [];
+	filter[1] = [];
 	for (var i = 0; i < toggle.races.length; i++) {
 		if (toggle.races[i].checked) {
-			window.filter[1].push(toggle.races[i].value);
+			filter[1].push(toggle.races[i].value);
 		}
 	}
 	applyFilter('data-list');
@@ -37,16 +37,16 @@ function applyFilter(tableId) {
 	for (var i=1; i < table.rows.length; i++) {
 		var show = true;
 		for (var j=0; j < table.rows[i].cells.length; j++) {
-			if (window.filter[j] == "All") {
+			if (filter[j] == "All") {
 				continue;
 			}
-			if (Array.isArray(window.filter[j])) {
-				if (window.filter[j].indexOf(table.rows[i].cells[j].textContent) === -1) {
+			if (Array.isArray(filter[j])) {
+				if (filter[j].indexOf(table.rows[i].cells[j].textContent) === -1) {
 					show = false;
 					break;
 				}
 			} else {
-				if (table.rows[i].cells[j].textContent != window.filter[j]) {
+				if (table.rows[i].cells[j].textContent != filter[j]) {
 					show = false;
 					break;
 				}
