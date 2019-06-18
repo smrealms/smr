90ab2aab86fd0dfd88e3e81e0cc9ac32c0023afe
Dan Hemberger <daniel.hemberger@gmail.com>
2019-06-17 23:21:51 -0700

    SmrAlliance: add setLeaderID method

--- engine/Default/alliance_leadership_processing.php
+++ engine/Default/alliance_leadership_processing.php
@@ -3,9 +3,11 @@ $leader_id = $_REQUEST['leader_id'];
 if (!is_numeric($leader_id)) {
 	create_error('Leader ID must be a number.');
 }
-$db->query('UPDATE alliance SET leader_id = ' . $db->escapeNumber($leader_id) . '
-			WHERE alliance_id = ' . $db->escapeNumber($player->getAllianceID()) . '
-			AND game_id = ' . $db->escapeNumber($player->getGameID()));
+
+$alliance = $player->getAlliance();
+$alliance->setLeaderID($leader_id);
+$alliance->update();
+
 $db->query('UPDATE player_has_alliance_role SET role_id = 2 WHERE account_id = ' . $db->escapeNumber($player->getAccountID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND alliance_id=' . $db->escapeNumber($player->getAllianceID()));
 $db->query('UPDATE player_has_alliance_role SET role_id = 1 WHERE account_id = ' . $db->escapeNumber($leader_id) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND alliance_id=' . $db->escapeNumber($player->getAllianceID()));
 
