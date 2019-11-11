d63958a0866a7312c76a581aa6be1023acc32818
stupidnewbie <55667763+stupidnewbie@users.noreply.github.com>
2019-11-11 14:33:51 -0500

    Create V_Unknown_ship_starter_changes.sql

---
+++ db/patches/V_Unknown_ship_starter_changes.sql
@@ -0,0 +1,33 @@
+--Galactic Semi cargo holds from 60 to 95.  Trade throughput increase from 480 (8tph * 60 holds) to 760 (8tph * 95 cargo holds).
+UPDATE ship_type_support_hardware SET max_amount = 95 WHERE ship_type_id = 1 AND hardware_type_id = 3;
+
+--Armored Semi cargo holds from 60 to 140.  TTP increase from 420 to 980.
+UPDATE ship_type_support_hardware SET max_amount = 140 WHERE ship_type_id = 2 AND hardware_type_id = 3;
+
+--Small Escort cargo holds from 30 to 110.  TTP increase from 300 to 1100.
+UPDATE ship_type_support_hardware SET max_amount = 110 WHERE ship_type_id = 14 AND hardware_type_id = 3;
+
+--Medium Cargo Hulk cargo holds from 60 to 85.  TTP increase from 480 to 680.
+UPDATE ship_type_support_hardware SET max_amount = 85 WHERE ship_type_id = 34 AND hardware_type_id = 3;
+
+--Light Freighter cargo holds from 60 to 100.  TTP increase from 480 to 800.
+UPDATE ship_type_support_hardware SET max_amount = 100 WHERE ship_type_id = 39 AND hardware_type_id = 3;
+
+--Tiny Delight cargo holds from 60 to 100.  TTP increase from 480 to 800.
+UPDATE ship_type_support_hardware SET max_amount = 100 WHERE ship_type_id = 44 AND hardware_type_id = 3;
+
+--Slip Freighter cargo holds from 60 to 90.  TTP increase from 540 to 810.
+UPDATE ship_type_support_hardware SET max_amount = 90 WHERE ship_type_id = 62 AND hardware_type_id = 3;
+
+--A Hatchling's Due cargo holds from 60 to 100.  TTP increase from 420 to 700.
+UPDATE ship_type_support_hardware SET max_amount = 100 WHERE ship_type_id = 50 AND hardware_type_id = 3;
+
+--Swift Venture cargo holds from 60 to 80.  TTP increase from 540 to 720.
+UPDATE ship_type_support_hardware SET max_amount = 80 WHERE ship_type_id = 56 AND hardware_type_id = 3;
+
+--Redeemer cargo holds from 45 to 75.  TTP increase from 405 to 720.
+UPDATE ship_type_support_hardware SET max_amount = 80 WHERE ship_type_id = 70 AND hardware_type_id = 3;
+
+
+
+
