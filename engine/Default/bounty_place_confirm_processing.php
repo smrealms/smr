6dc9c1e5526b3ce38f70f27946337abec56eb199
Dan Hemberger <846186+hemberger@users.noreply.github.com>
2020-02-24 15:18:36 -0800

    bounty_place.php: restructure forwarding (#831)
    Move input validation out of bounty_place_confirm.php and into
    bounty_place_processing.php.
    
    So the flow now looks like this:
    
    bp.php -> Display the input form
    bp_processing.php -> Validate the input data and forward
    bp_confirm.php -> Display confirmation of input data
    bp_confirm_processing.php -> Validate confirmation and process
    
    This flow avoids any need for SmrSession::getRequestVar, and instead
    we can just use the Request class in the processing pages. (When the
    form validation was in bp_confirm.php, we were reprocessing the form
    validation on each ajax refresh as well!)

---
+++ engine/Default/bounty_place_confirm_processing.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+if (!$player->getSector()->hasLocation($var['LocationID'])) {
+	create_error('That location does not exist in this sector');
+}
+
+$location = SmrLocation::getLocation($var['LocationID']);
+$container = create_container('skeleton.php');
+transfer('LocationID');
+if ($location->isHQ()) {
+	$container['body'] = 'government.php';
+	$type = 'HQ';
+} elseif ($location->isUG()) {
+	$container['body'] = 'underground.php';
+	$type = 'UG';
+} else {
+	create_error('The location is not a UG or HQ, how did you get here?');
+}
+// if we don't have a yes we leave immediatly
+if (Request::get('action') != 'Yes') {
+	forward($container);
+}
+
+// get values from container (validated in bounty_place_processing.php)
+$amount = $var['amount'];
+$smrCredits = $var['SmrCredits'];
+$account_id = $var['account_id'];
+
+// take the bounty from the cash
+$player->decreaseCredits($amount);
+$account->decreaseSmrCredits($smrCredits);
+
+$player->increaseHOF($smrCredits, array('Bounties', 'Placed', 'SMR Credits'), HOF_PUBLIC);
+$player->increaseHOF($amount, array('Bounties', 'Placed', 'Money'), HOF_PUBLIC);
+$player->increaseHOF(1, array('Bounties', 'Placed', 'Number'), HOF_PUBLIC);
+
+$placed = SmrPlayer::getPlayer($account_id, $player->getGameID());
+$placed->increaseCurrentBountyAmount($type, $amount);
+$placed->increaseCurrentBountySmrCredits($type, $smrCredits);
+$placed->increaseHOF($smrCredits, array('Bounties', 'Received', 'SMR Credits'), HOF_PUBLIC);
+$placed->increaseHOF($amount, array('Bounties', 'Received', 'Money'), HOF_PUBLIC);
+$placed->increaseHOF(1, array('Bounties', 'Received', 'Number'), HOF_PUBLIC);
+
+//Update for top bounties list
+$player->update();
+$account->update();
+$placed->update();
+forward($container);
--- engine/Default/bounty_place_processing.php
+++ engine/Default/bounty_place_processing.php
@@ -1,56 +1,24 @@
 <?php declare(strict_types=1);
 
-if (!$player->getSector()->hasLocation($var['LocationID'])) {
-	create_error('That location does not exist in this sector');
+$amount = Request::getInt('amount');
+$smrCredits = Request::getInt('smrcredits');
+
+if ($player->getCredits() < $amount) {
+	create_error('You dont have that much money.');
 }
 
-$location = SmrLocation::getLocation($var['LocationID']);
-$container = create_container('skeleton.php');
+if ($account->getSmrCredits() < $smrCredits) {
+	create_error('You dont have that many SMR credits.');
+}
+
+if ($amount <= 0 && $smrCredits <= 0) {
+	create_error('You must enter an amount greater than 0!');
+}
+
+$container = create_container('skeleton.php', 'bounty_place_confirm.php');
+$container['amount'] = $amount;
+$container['SmrCredits'] = $smrCredits;
+$container['player_id'] = Request::getInt('player_id');
 transfer('LocationID');
-if ($location->isHQ()) {
-	$container['body'] = 'government.php';
-	$type = 'HQ';
-} elseif ($location->isUG()) {
-	$container['body'] = 'underground.php';
-	$type = 'UG';
-} else {
-	create_error('The location is not a UG or HQ, how did you get here?');
-}
-// if we don't have a yes we leave immediatly
-if (Request::get('action') != 'Yes') {
-	forward($container);
-}
 
-// get values from container
-$amount = $var['amount'];
-$smrCredits = $var['SmrCredits'];
-$account_id = $var['account_id'];
-if ((!is_numeric($amount) && !is_numeric($smrCredits)) || ($amount == 0 && $smrCredits == 0)) {
-	create_error('You must enter an amount!');
-}
-if ($amount < 0) {
-	create_error('You must enter a positive amount!');
-}
-if ($smrCredits < 0) {
-	create_error('You must enter a positive SMR credits amount!');
-}
-// take the bounty from the cash
-$player->decreaseCredits($amount);
-$account->decreaseSmrCredits($smrCredits);
-
-$player->increaseHOF($smrCredits, array('Bounties', 'Placed', 'SMR Credits'), HOF_PUBLIC);
-$player->increaseHOF($amount, array('Bounties', 'Placed', 'Money'), HOF_PUBLIC);
-$player->increaseHOF(1, array('Bounties', 'Placed', 'Number'), HOF_PUBLIC);
-
-$placed = SmrPlayer::getPlayer($account_id, $player->getGameID());
-$placed->increaseCurrentBountyAmount($type, $amount);
-$placed->increaseCurrentBountySmrCredits($type, $smrCredits);
-$placed->increaseHOF($smrCredits, array('Bounties', 'Received', 'SMR Credits'), HOF_PUBLIC);
-$placed->increaseHOF($amount, array('Bounties', 'Received', 'Money'), HOF_PUBLIC);
-$placed->increaseHOF(1, array('Bounties', 'Received', 'Number'), HOF_PUBLIC);
-
-//Update for top bounties list
-$player->update();
-$account->update();
-$placed->update();
 forward($container);
