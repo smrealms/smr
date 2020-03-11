smrealms/smr

commit 553b87f5458b281a77c7aede69071b568683af6d

Add Request class (#824)
Data in the `$_REQUEST` superglobal is stored as strings and is not
sanitized in any way. By demanding a specific type when we query the
data, we a) facilitate the use of strict typing throughout the code
and b) can skip a lot of sanity checks that are automatically taken
care of by the type conversion.

This synergizes with HTML5 form validation, which makes the implicit
type conversion in the Request class less destructive (because we
should only get unexpected data types in the cases where players are
circumventing the HTML5 validation to falsify form data).

The Request class is designed for use in processing pages. Most of
the time, display pages should be using `SmrSession::getRequestVar`.

We switch from POST -> REQUEST in many places because they are both
user submitted, so there is no reason to demand one or the other.

htdocs/resend_password_processing.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 try {
 
-	if (empty($_REQUEST['email'])) {
+	if (empty(Request::get('email'))) {
 		header('Location: /error.php?msg=' . rawurlencode('You must specify an e-mail address!'));
 		exit;
 	}
@@ -10,7 +10,7 @@ try {
 	require_once(LIB . 'Default/smr.inc');
 
 	// get this user from db
-	$account = SmrAccount::getAccountByEmail($_REQUEST['email']);
+	$account = SmrAccount::getAccountByEmail(Request::get('email'));
 	if ($account == null) {
 		// unknown user
 		header('Location: /error.php?msg=' . rawurlencode('The specified e-mail address is not registered!'));

commit 003065fc98ee42f85e485a29904b860aa6979b51

resend_password_processing.php: fix include error
When the Request class was introduced here in 553b87f54, the include
for config.inc needed to be moved up so that the Request class could
be autoloaded. This fixes that omission.

htdocs/resend_password_processing.php
@@ -1,13 +1,11 @@
 <?php declare(strict_types=1);
 try {
+	require_once('config.inc');
 
 	if (empty(Request::get('email'))) {
 		header('Location: /error.php?msg=' . rawurlencode('You must specify an e-mail address!'));
 		exit;
 	}
-
-	require_once('config.inc');
-	require_once(LIB . 'Default/smr.inc');
 
 	// get this user from db
 	$account = SmrAccount::getAccountByEmail(Request::get('email'));