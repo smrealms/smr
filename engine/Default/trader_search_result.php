smrealms/smr

commit 6b0a05da68a57b7350961f6e838302926e59c9bd

SmrSession: improve getRequestVar for strict types
* getRequestVar returns a string
* getRequestVarInt returns an int
* getRequestVarIntArray returns an array of ints

These are now thin wrappers around the `Request::getVar*`, which has
a few important differences than the previous implementation:

The index must exist in either $var or $_REQUEST. If it is found in
both, then an exception will be raised (previously, it would return
the value in $_REQUEST). If it is found in neither, then an exception
will also be raised (previously, it would return null).

This ensures that we are precise about our var data sources.

engine/Default/trader_search_result.php
@@ -1,10 +1,8 @@
 <?php declare(strict_types=1);
 
-$player_id = SmrSession::getRequestVar('player_id');
+$player_id = SmrSession::getRequestVarInt('player_id');
 $player_name = SmrSession::getRequestVar('player_name');
-if (!is_numeric($player_id) && !empty($player_id)) {
-	create_error('Please enter only numbers!');
-}
+
 if (empty($player_name) && empty($player_id)) {
 	create_error('You must specify either a player name or ID!');
 }

admin/Default/1.6/universe_create_galaxies.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-$numGals = SmrSession::getRequestVar('num_gals', 12);
+$numGals = SmrSession::getRequestVarInt('num_gals', 12);
 
 $game = SmrGame::getGame($var['game_id']);
 $template->assign('PageTopic', 'Create Galaxies : ' . $game->getDisplayName());

admin/Default/1.6/universe_create_sector_details.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-$editSectorID = SmrSession::getRequestVar('sector_edit');
+$editSectorID = SmrSession::getRequestVarInt('sector_edit');
 $editSector = SmrSector::getSector($var['game_id'], $editSectorID);
 $template->assign('PageTopic', 'Edit Sector #' . $editSector->getSectorID() . ' (' . $editSector->getGalaxyName() . ')');
 $template->assign('EditSector', $editSector);

admin/Default/1.6/universe_create_sectors.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 
-SmrSession::getRequestVar('game_id');
-SmrSession::getRequestVar('gal_on', 1);
+SmrSession::getRequestVarInt('game_id');
+SmrSession::getRequestVarInt('gal_on', 1);
 
 $galaxies = SmrGalaxy::getGameGalaxies($var['game_id']);
 if (empty($galaxies)) {
@@ -40,7 +40,6 @@ if (isset($var['message'])) {
 
 $container = create_container('skeleton.php', '1.6/universe_create_sectors.php');
 transfer('game_id');
-transfer('gal_on');
 $template->assign('JumpGalaxyHREF', SmrSession::getNewHref($container));
 
 $container['url'] = '1.6/universe_create_save_processing.php';

admin/Default/admin_message_send.php
@@ -2,7 +2,7 @@
 
 $template->assign('PageTopic', 'Send Admin Message');
 
-$gameID = SmrSession::getRequestVar('SendGameID');
+$gameID = SmrSession::getRequestVarInt('SendGameID');
 $container = create_container('admin_message_send_processing.php');
 $container['SendGameID'] = $gameID;
 $template->assign('AdminMessageSendFormHref', SmrSession::getNewHREF($container));

admin/Default/album_moderate.php
@@ -4,7 +4,7 @@ $template->assign('PageTopic', 'Moderate Photo Album');
 
 require_once(LIB . 'Album/album_functions.php');
 
-$account_id = SmrSession::getRequestVar('account_id');
+$account_id = SmrSession::getRequestVarInt('account_id');
 
 // check if the given account really has an entry
 $db->query('SELECT * FROM album WHERE account_id = ' . $db->escapeNumber($account_id) . ' AND Approved = \'YES\'');

admin/Default/anon_acc_view.php
@@ -6,8 +6,8 @@ $template->assign('PageTopic', 'View Anonymous Account Info');
 $container = create_container('skeleton.php', 'anon_acc_view_select.php');
 $template->assign('BackHREF', SmrSession::getNewHREF($container));
 
-$anonID = SmrSession::getRequestVar('anon_account');
-$gameID = SmrSession::getRequestVar('view_game_id');
+$anonID = SmrSession::getRequestVarInt('anon_account');
+$gameID = SmrSession::getRequestVarInt('view_game_id');
 
 $db->query('SELECT *
 			FROM anon_bank_transactions

admin/Default/game_delete_confirm.php
@@ -2,7 +2,7 @@
 
 $template->assign('PageTopic', 'Delete Game - Confirmation');
 
-SmrSession::getRequestVar('delete_game_id');
+SmrSession::getRequestVarInt('delete_game_id');
 $template->assign('Game', SmrGame::getGame($var['delete_game_id']));
 
 $container = create_container('game_delete_processing.php');

commit d908584cf7eece5e1e00393a90ffc129a76dacb5

trader_search_result.php: make player_name optional
This page is also used as the action for clicking on a player name,
and in this pathway we only provide the `player_id`, so we need to
supply a default value (the empty string) for `player_name`.

Fixes a bug introduced in 6b0a05da68, when we made `getRequestVar`
raise an exception when the index does not exist.

engine/Default/trader_search_result.php
@@ -1,7 +1,8 @@
 <?php declare(strict_types=1);
 
 $player_id = SmrSession::getRequestVarInt('player_id');
-$player_name = SmrSession::getRequestVar('player_name');
+// When clicking on a player name, only the 'player_id' is supplied
+$player_name = SmrSession::getRequestVar('player_name', '');
 
 if (empty($player_name) && empty($player_id)) {
 	create_error('You must specify either a player name or ID!');