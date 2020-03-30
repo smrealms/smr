a2e803b756620598f632ff736c083e5d6f6559da
Dan Hemberger <846186+hemberger@users.noreply.github.com>
2020-03-29 23:53:59 -0700

    Throw user error on invalid social login type (#837)
    Because the social login type should be internally consistent between
    the URL on the login page and the types accepted by the SocialLogin
    class, we were raising an exception if the type passed in the URL to
    the login_social_processing.php page was unknown. This helped ensure
    that we would be notified (by bugs emails) if we accidentally broke
    social logins.
    
    Unfortunately, because the login_social_processing.php page is linked
    on the public-facing login page, it has been added to a bunch of bot
    databases. Most of the search engine spiders respect our robots.txt
    file, but some do not, and SQL injection attacks obviously do not.
    
    As a result, we get a lot of unwanted bugs emails where a bot uses an
    outdated or malicious social login type. This is just too much noise,
    and so we now redirect to error.php if the social login type is not
    valid.

--- lib/Default/SocialLogin.class.php
+++ lib/Default/SocialLogin.class.php
@@ -1,6 +1,11 @@
 <?php declare(strict_types=1);
 
 /**
+ * Exception thrown when a SocialLogin type cannot be found
+ */
+class SocialLoginNotFound extends Exception {}
+
+/**
  * Defines the methods to be implemented by each social login platform.
  */
 abstract class SocialLogin {
@@ -23,7 +28,7 @@ abstract class SocialLogin {
 		} elseif ($loginType === SocialLogins\Twitter::getLoginType()) {
 			return new SocialLogins\Twitter();
 		} else {
-			throw new Exception('Unknown social login type: ' . $loginType);
+			throw new SocialLoginNotFound('Unknown social login type: ' . $loginType);
 		}
 	}
 
