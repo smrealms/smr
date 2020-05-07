6b35d20405fb38b0d99076eb1e03aab7532b8a18
Dan Hemberger <daniel.hemberger@gmail.com>
2020-05-07 12:12:22 -0700

    planet_main.php: fix structure tooltips
    The use of the `<label>` element here was intended to extend the
    `<img>` tooltip to the structure name as well. However, this does
    not actually work (and we were missing the `</label>` as well).
    
    Put the title on the enclosing `<td>` element instead, which will
    give the entire table cell the tooltip.

--- templates/Default/engine/Default/planet_main.php
+++ templates/Default/engine/Default/planet_main.php
@@ -40,8 +40,9 @@ if (isset($Msg)) {
 				</tr><?php
 				foreach ($ThisPlanet->getStructureTypes() as $StructureID => $Structure) { ?>
 				<tr>
-					<td><img id="tip<?php echo $StructureID; ?>" src="images/<?php echo $Structure->image(); ?>" width="16" height="16" alt="" title="<?php echo $Structure->tooltip(); ?>" />
-						<label for="tip<?php echo $StructureID; ?>">&nbsp;<?php echo $Structure->name(); ?></td>
+					<td title="<?php echo $Structure->tooltip(); ?>">
+						<img src="images/<?php echo $Structure->image(); ?>" width="16" height="16" alt="" />&nbsp;<?php echo $Structure->name(); ?>
+					</td>
 					<td class="center"><span id="planetStructure<?php echo $StructureID; ?>"><?php echo $ThisPlanet->getBuilding($StructureID); ?></span></td>
 					<td class="center"><?php echo $ThisPlanet->getMaxBuildings($StructureID); ?></td>
 				</tr><?php
