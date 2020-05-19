44d38b29e9460f4f48561e0b876abe70ede20518
Dan Hemberger <daniel.hemberger@gmail.com>
2020-05-18 20:51:43 -0700

    alliance_invite_player.php: ajax update list
    The "Pending Invitations" do not have a form, so they can be safely
    ajax-updated.

--- templates/Default/engine/Default/alliance_invite_player.php
+++ templates/Default/engine/Default/alliance_invite_player.php
@@ -48,25 +48,27 @@ if (count($InvitePlayers) == 0) { ?>
 <br /><br />
 <h2>Pending Invitations</h2>
 
+<div class="ajax" id="pending">
 <?php
 if (count($PendingInvites) == 0) { ?>
 	<p>Your alliance has no pending invitations.</p><?php
 } else { ?>
 	<br />
-	<table class="standard">
+	<table class="standard inset">
 		<tr>
 			<th>Invited Player</th>
 			<th>Invited By</th>
 			<th>Expires</th>
-			<th>Cancel</th>
+			<th class="shrink">Cancel</th>
 		</tr><?php
 		foreach ($PendingInvites as $invite) { ?>
 			<tr>
 				<td><?php echo $invite['invited']; ?></td>
 				<td><?php echo $invite['invited_by']; ?></td>
-				<td><?php echo $invite['expires']; ?></td>
+				<td class="center"><?php echo $invite['expires']; ?></td>
 				<td class="center"><a href="<?php echo $invite['cancelHREF']; ?>"><img class="bottom" src="images/silk/cross.png" width="16" height="16" alt="Cancel" title="Cancel Invitation" /></a></td>
 			</tr><?php
 		} ?>
 	</table><?php
 } ?>
+</div>
