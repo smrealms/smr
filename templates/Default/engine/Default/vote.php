a6689886d0765ccc6144e79470c5e00c51325821
Dan Hemberger <846186+hemberger@users.noreply.github.com>
2020-05-19 13:45:29 -0700

    vote.php: display end date of ended votes (#851)
    The vote history is more useful when you can see the end dates for
    old votes.

--- templates/Default/engine/Default/vote.php
+++ templates/Default/engine/Default/vote.php
@@ -4,7 +4,7 @@ if (isset($Voting)) {
 	foreach ($Voting as $Vote) {
 		?><br /><br />
 		<form name="FORM" method="POST" action="<?php echo $Vote['HREF'] ?>">
-			<span class="bold"><?php echo bbifyMessage($Vote['Question']); ?></span> <?php if (isset($Vote['TimeRemaining'])) { ?>(<?php echo $Vote['TimeRemaining']; ?> Remaining)<?php } else { ?>(Ended)<?php } ?><br /><?php
+			<span class="bold"><?php echo bbifyMessage($Vote['Question']); ?></span> <?php if (isset($Vote['TimeRemaining'])) { ?>(<?php echo $Vote['TimeRemaining']; ?> Remaining)<?php } else { ?>(Ended <?php echo $Vote['EndDate']; ?>)<?php } ?><br /><?php
 			foreach ($Vote['Options'] as $VoteOption) { ?>
 				<input type="radio" name="vote" <?php if (!isset($Vote['TimeRemaining'])) { ?>disabled="disabled" <?php } ?>value="<?php echo $VoteOption['ID']; ?>"<?php if ($VoteOption['Chosen']) { ?> checked<?php } ?>><?php echo bbifyMessage($VoteOption['Text']); ?> (<?php echo $VoteOption['Votes']; ?> votes)<br /><?php
 			} ?>
