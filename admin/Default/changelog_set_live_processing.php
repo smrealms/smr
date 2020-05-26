ac039188e2e7cfe7b4e7b372baeff0f3b4cd9677
Dan Hemberger <daniel.hemberger@gmail.com>
2020-05-25 17:55:18 -0700

    changelog_set_live_processing.php: clarify insert
    Make the code a little clearer and explain a bit what is going on,
    since it is not obvious.

--- admin/Default/changelog_set_live_processing.php
+++ admin/Default/changelog_set_live_processing.php
@@ -5,9 +5,15 @@ $db->query('UPDATE version
 			WHERE version_id = ' . $db->escapeNumber($var['version_id'])
 		   );
 
+// Initialize the next version (since the version set live is not always the
+// last one, we INSERT IGNORE to skip this step in this case).
 $db->query('SELECT * FROM version WHERE version_id = ' . $db->escapeNumber($var['version_id']));
 $db->nextRecord();
+$versionID = $db->getInt('version_id') + 1;
+$major = $db->getInt('major_version');
+$minor = $db->getInt('minor_version');
+$patch = $db->getInt('patch_level') + 1;
 $db->query('INSERT IGNORE INTO version (version_id, major_version, minor_version, patch_level, went_live) VALUES
-			('.$db->escapeNumber($db->getInt('version_id') + 1) . ',' . $db->escapeNumber($db->getInt('major_version')) . ',' . $db->escapeNumber($db->getInt('minor_version')) . ',' . $db->escapeNumber($db->getInt('patch_level') + 1) . ',0);');
+			('.$db->escapeNumber($versionID) . ',' . $db->escapeNumber($major) . ',' . $db->escapeNumber($minor) . ',' . $db->escapeNumber($patch) . ',0);');
 
 forward(create_container('skeleton.php', 'changelog.php'));
