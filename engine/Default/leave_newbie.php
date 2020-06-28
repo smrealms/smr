59248775b55f55b9566d1e1892154b485ad161ae
Dan Hemberger <daniel.hemberger@gmail.com>
2020-06-28 09:01:17 -0700

    leave_newbie.php: only drop newbies in started games
    In normal games, dropping newbie protection before the start time
    is fine because you can't move, and are therefore stuck in Federal
    protection.
    
    However, in special games where players might not be starting in Fed,
    they could drop newbies and be podded before the game starts. Better
    to just cover our bases and disable it before the game has started.

--- engine/Default/leave_newbie.php
+++ engine/Default/leave_newbie.php
@@ -1,2 +1,7 @@
 <?php declare(strict_types=1);
+
+if (!$player->getGame()->hasStarted()) {
+	create_error('You cannot leave newbie protection before the game begins!');
+}
+
 $template->assign('PageTopic', 'Leave Newbie Protection');
