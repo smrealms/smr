8d924289433bf93936a3c47083a8ed053605265f
Dan Hemberger <daniel.hemberger@gmail.com>
2020-07-16 12:13:26 -0700

    bank_anon_detail_processing.php: remove code dup
    Code was (nearly) duplicated for both "Deposit" and "Payment" actions.
    We can pull out the sections that get the transaction ID and do the
    logging, since they can be easily extended to both actions.
    
    Note that I needed to change the POST action to "Payment" since that
    is the appropriate enum type (even though "Withdrawal" is a much more
    accurate description of the action).

--- engine/Default/bank_anon_detail_processing.php
+++ engine/Default/bank_anon_detail_processing.php
@@ -1,5 +1,9 @@
 <?php declare(strict_types=1);
 $action = Request::get('action');
+if (!in_array($action, ['Deposit', 'Payment'])) {
+	throw new Exception('Invalid action submitted: ' . $action);
+}
+
 $amount = Request::getInt('amount');
 $account_num = $var['account_num'];
 // no negative amounts are allowed
@@ -7,46 +11,41 @@ if ($amount <= 0) {
 	create_error('You must actually enter an amount > 0!');
 }
 
+// Get the next transaction ID for this anon bank
+$db->query('SELECT transaction_id FROM anon_bank_transactions WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND anon_id = ' . $db->escapeNumber($account_num) . ' ORDER BY transaction_id DESC LIMIT 1');
+if ($db->nextRecord()) {
+	$trans_id = $db->getInt('transaction_id') + 1;
+} else {
+	$trans_id = 1;
+}
+
+// Update the credit amounts for the player and the bank
 if ($action == 'Deposit') {
 	if ($player->getCredits() < $amount) {
 		create_error('You don\'t own that much money!');
 	}
 
 	$player->decreaseCredits($amount);
-	$db->query('SELECT transaction_id FROM anon_bank_transactions WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND anon_id = ' . $db->escapeNumber($account_num) . ' ORDER BY transaction_id DESC LIMIT 1');
-	if ($db->nextRecord()) {
-		$trans_id = $db->getInt('transaction_id') + 1;
-	} else {
-		$trans_id = 1;
-	}
-	$db->query('INSERT INTO anon_bank_transactions (account_id, game_id, anon_id, transaction_id, transaction, amount, time) ' .
-							'VALUES (' . $db->escapeNumber($player->getAccountID()) . ', ' . $db->escapeNumber($player->getGameID()) . ', ' . $db->escapeNumber($account_num) . ', ' . $db->escapeNumber($trans_id) . ', \'Deposit\', ' . $db->escapeNumber($amount) . ', ' . $db->escapeNumber(TIME) . ')');
 	$db->query('UPDATE anon_bank SET amount = amount + ' . $db->escapeNumber($amount) . ' WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND anon_id = ' . $db->escapeNumber($account_num));
 	$player->update();
-
-	// log action
-	$account->log(LOG_TYPE_BANK, 'Deposits ' . $amount . ' credits in anonymous account #' . $account_num, $player->getSectorID());
 } else {
 	$db->query('SELECT * FROM anon_bank WHERE anon_id = ' . $db->escapeNumber($account_num) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()));
 	$db->requireRecord();
 	if ($db->getInt('amount') < $amount) {
 		create_error('You don\'t have that much money on your account!');
 	}
-	$db->query('SELECT transaction_id FROM anon_bank_transactions WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND anon_id = ' . $db->escapeNumber($account_num) . ' ORDER BY transaction_id DESC LIMIT 1');
-	if ($db->nextRecord()) {
-		$trans_id = $db->getInt('transaction_id') + 1;
-	} else {
-		$trans_id = 1;
-	}
-	$db->query('INSERT INTO anon_bank_transactions (account_id, game_id, anon_id, transaction_id, transaction, amount, time)
-				VALUES (' . $db->escapeNumber($player->getAccountID()) . ', ' . $db->escapeNumber($player->getGameID()) . ', ' . $db->escapeNumber($account_num) . ', ' . $db->escapeNumber($trans_id) . ', \'Payment\', ' . $db->escapeNumber($amount) . ', ' . $db->escapeNumber(TIME) . ')');
+
 	$db->query('UPDATE anon_bank SET amount = amount - ' . $db->escapeNumber($amount) . ' WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND anon_id = ' . $db->escapeNumber($account_num));
 	$player->increaseCredits($amount);
 	$player->update();
-
-	// log action
-	$account->log(LOG_TYPE_BANK, 'Takes ' . $amount . ' credits from anonymous account #' . $account_num, $player->getSectorID());
 }
+
+// Log the bank transaction
+$db->query('INSERT INTO anon_bank_transactions (account_id, game_id, anon_id, transaction_id, transaction, amount, time)
+			VALUES (' . $db->escapeNumber($player->getAccountID()) . ', ' . $db->escapeNumber($player->getGameID()) . ', ' . $db->escapeNumber($account_num) . ', ' . $db->escapeNumber($trans_id) . ', ' . $db->escapeString($action) . ', ' . $db->escapeNumber($amount) . ', ' . $db->escapeNumber(TIME) . ')');
+
+// Log the player action
+$account->log(LOG_TYPE_BANK, $action . ' of ' . $amount . ' credits in anonymous account #' . $account_num, $player->getSectorID());
 
 $container = create_container('skeleton.php', 'bank_anon_detail.php');
 $container['account_num'] = $account_num;
