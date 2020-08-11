3c1f52232b17766caad2c0b904d3df5bd8664724
Dan Hemberger <daniel.hemberger@gmail.com>
2020-08-11 11:22:10 -0700

    PlayingCard.class.php: minor clean up
    * Rename `rankID` attribute to just `rank` for clarity, since it is
      the actual rank.
    
    * Add method type hints and typed properties.
    
    * Fix some wording in comments about pip card values/names.

--- lib/Default/PlayingCard.class.php
+++ lib/Default/PlayingCard.class.php
@@ -8,13 +8,13 @@ class PlayingCard {
 	const NUM_DECKS = 1;
 	const MAX_CARDS = 52 * self::NUM_DECKS;
 
-	// Special card rank IDs
+	// Special card ranks
 	private const RANK_ACE = 1;
 	private const RANK_JACK = 11;
 	private const RANK_QUEEN = 12;
 	private const RANK_KING = 13;
 
-	// Mapping between rank IDs and display/suit name
+	// Mapping between ranks and display/suit name
 	private const RANK_NAMES = [
 		self::RANK_ACE => 'A',
 		self::RANK_JACK => 'J',
@@ -23,43 +23,43 @@ class PlayingCard {
 	];
 	private const SUITS = ['hearts', 'clubs', 'diamonds', 'spades'];
 
-	private $cardID; // unique ID in the deck (0-indexed)
-	private $rankID; // non-unique rank ID of the card (1-indexed)
+	private int $cardID; // unique ID in all the decks (0-indexed)
+	private int $rank; // non-unique rank of the card (1-indexed)
 
 	/**
 	 * Create a specific or random card in the deck.
 	 */
-	public function __construct($cardID = null) {
+	public function __construct(int $cardID = null) {
 		if (is_null($cardID)) {
 			$this->cardID = rand(0, self::MAX_CARDS - 1);
 		} else {
 			$this->cardID = $cardID;
 		}
 		// 52 cards per deck, 13 cards per suit
-		$this->rankID = ($this->cardID % 52) % 13 + 1;
+		$this->rank = ($this->cardID % 52) % 13 + 1;
 	}
 
 	/**
 	 * Return the card's blackjack value.
 	 */
-	public function getValue() {
-		if ($this->rankID == self::RANK_JACK ||
-		    $this->rankID == self::RANK_QUEEN ||
-		    $this->rankID == self::RANK_KING) {
+	public function getValue() : int {
+		if ($this->rank == self::RANK_JACK ||
+		    $this->rank == self::RANK_QUEEN ||
+		    $this->rank == self::RANK_KING) {
 			return 10;
 		} elseif ($this->isAce()) {
 			return 11;
 		} else {
-			// For normal pip (non-face) cards, value and name are the same.
-			return $this->rankID;
+			// For normal pip (non-face) cards, value and rank are the same.
+			return $this->rank;
 		}
 	}
 
-	public function isAce() {
-		return $this->rankID == self::RANK_ACE;
+	public function isAce() : bool {
+		return $this->rank == self::RANK_ACE;
 	}
 
-	public function getSuitName() {
+	public function getSuitName() : string {
 		$deckID = $this->cardID % 52; //which card is this in the deck?
 		$suitID = floor($deckID / 13);
 		return self::SUITS[$suitID];
@@ -68,12 +68,12 @@ class PlayingCard {
 	/**
 	 * Returns the rank name of this card (of the 13 ranks).
 	 */
-	public function getRankName() {
-		if (isset(self::RANK_NAMES[$this->rankID])) {
-			return self::RANK_NAMES[$this->rankID];
+	public function getRankName() : string {
+		if (isset(self::RANK_NAMES[$this->rank])) {
+			return self::RANK_NAMES[$this->rank];
 		} else {
-			// For normal pip (non-face) cards, value and name are the same.
-			return $this->rankID;
+			// For normal pip (non-face) cards, name and rank are the same.
+			return (string) $this->rank;
 		}
 	}
 }
