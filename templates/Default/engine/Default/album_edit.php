smrealms/smr

commit 5f2987ea592327b1d22465675b5eaaf4cc38e800

Use HTML5 features.
Use the HTML5 input types in a load of places to allow for more client
side validation and make text areas be spell checked for
@Red-Cross-Knight

templates/Default/engine/Default/album_edit.php
@@ -21,25 +21,25 @@ Your image will be posted under your <i>Hall Of Fame</i> nick!<br />
 		
 		<tr>
 			<td align="right" class="bold">Email Address:</td>
-			<td><input type="text" name="email" id="InputFields" value="<?php if(isset($AlbumEntry)){ echo htmlspecialchars($AlbumEntry['Email']); } else { ?>N/A<?php } ?>" style="width:303px;" onFocus="javascript:if (this.value == 'N/A') {this.value = '';}" onBlur="javascript:if (this.value == '') {this.value = 'N/A';}"></td>
+			<td><input type="email" name="email" id="InputFields" value="<?php if(isset($AlbumEntry)){ echo htmlspecialchars($AlbumEntry['Email']); } ?>" style="width:303px;"></td>
 		</tr>
 		
 		<tr>
 			<td align="right" class="bold">Website:</td>
-			<td><input type="text" name="website" id="InputFields" style="width:303px;" value="<?php if(isset($AlbumEntry) && $AlbumEntry['Website'] != ''){ echo htmlspecialchars($AlbumEntry['Website']); } else { ?>http://<?php } ?>" onBlur="javascript:if (this.value == '') {this.value = 'http://';}"></td>
+			<td><input type="url" name="website" id="InputFields" style="width:303px;" value="<?php if(isset($AlbumEntry) && $AlbumEntry['Website'] != ''){ echo htmlspecialchars($AlbumEntry['Website']); } ?>"></td>
 		</tr>
 		
 		<tr>
 			<td align="right" class="bold">Birthdate:</td>
-			<td>Month:&nbsp;<input type="text" name="day" id="InputFields" value="<?php if(isset($AlbumEntry)){ echo htmlspecialchars($AlbumEntry['Day']); } else { ?>N/A<?php } ?>" size="3" maxlength="2" class="center" onFocus="javascript:if (this.value == 'N/A') {this.value = '';}" onBlur="javascript:if (this.value == '') {this.value = 'N/A';}">&nbsp;&nbsp;&nbsp;
-				Day:&nbsp;<input type="text" name="month" id="InputFields" value="<?php if(isset($AlbumEntry)){ echo htmlspecialchars($AlbumEntry['Month']); } else { ?>N/A<?php } ?>" size="3" maxlength="2" class="center" onFocus="javascript:if (this.value == 'N/A') {this.value = '';}" onBlur="javascript:if (this.value == '') {this.value = 'N/A';}">&nbsp;&nbsp;&nbsp;
+			<td>Day:&nbsp;<input type="text" name="month" id="InputFields" value="<?php if(isset($AlbumEntry)){ echo htmlspecialchars($AlbumEntry['Month']); } else { ?>N/A<?php } ?>" size="3" maxlength="2" class="center" onFocus="javascript:if (this.value == 'N/A') {this.value = '';}" onBlur="javascript:if (this.value == '') {this.value = 'N/A';}">&nbsp;&nbsp;&nbsp;
+				Month:&nbsp;<input type="text" name="day" id="InputFields" value="<?php if(isset($AlbumEntry)){ echo htmlspecialchars($AlbumEntry['Day']); } else { ?>N/A<?php } ?>" size="3" maxlength="2" class="center" onFocus="javascript:if (this.value == 'N/A') {this.value = '';}" onBlur="javascript:if (this.value == '') {this.value = 'N/A';}">&nbsp;&nbsp;&nbsp;
 				Year:&nbsp;<input type="text" name="year" id="InputFields" value="<?php if(isset($AlbumEntry)){ echo htmlspecialchars($AlbumEntry['Year']); } else { ?>N/A<?php } ?>" size="3" maxlength="4" class="center" onFocus="javascript:if (this.value == 'N/A') {this.value = '';}" onBlur="javascript:if (this.value == '') {this.value = 'N/A';}">
 			</td>
 		</tr>
 		
 		<tr>
 			<td align="right" valign="top" class="bold">Other Info:<br /><small>(AIM/ICQ)</small></td>
-			<td><textarea name="other" id="InputFields" style="width:303px;height:100px;" onFocus="javascript:if (this.value == 'N/A') {this.value = '';}" onBlur="javascript:if (this.value == '') {this.value = 'N/A';}"><?php if(isset($AlbumEntry)){ echo $AlbumEntry['Other']; } else { ?>N/A<?php } ?></textarea></td>
+			<td><textarea spellcheck="true" name="other" id="InputFields" style="width:303px;height:100px;" onFocus="javascript:if (this.value == 'N/A') {this.value = '';}" onBlur="javascript:if (this.value == '') {this.value = 'N/A';}"><?php if(isset($AlbumEntry)){ echo $AlbumEntry['Other']; } else { ?>N/A<?php } ?></textarea></td>
 		</tr>
 		
 		<tr>

commit 7232bcf834da32fd85412d94fae3ab6706e39fb0

album_edit.php: fix day/month mixup
This error was introduced in 5f2987ea5923 (Aug 2012!).

templates/Default/engine/Default/album_edit.php
@@ -31,8 +31,8 @@ Your image will be posted under your <i>Hall Of Fame</i> nick!<br />
 		
 		<tr>
 			<td class="right bold">Birthdate:</td>
-			<td>Day:&nbsp;<input type="number" name="month" class="center" value="<?php if (isset($AlbumEntry)) { echo htmlspecialchars($AlbumEntry['Month']); } ?>" min="1" max="12">&nbsp;&nbsp;&nbsp;
-				Month:&nbsp;<input type="number" name="day" class="center" value="<?php if (isset($AlbumEntry)) { echo htmlspecialchars($AlbumEntry['Day']); } ?>" min="1" max="31">&nbsp;&nbsp;&nbsp;
+			<td>Day:&nbsp;<input type="number" name="day" class="center" value="<?php if (isset($AlbumEntry)) { echo htmlspecialchars($AlbumEntry['Day']); } ?>" min="1" max="31">&nbsp;&nbsp;&nbsp;
+				Month:&nbsp;<input type="number" name="month" class="center" value="<?php if (isset($AlbumEntry)) { echo htmlspecialchars($AlbumEntry['Month']); } ?>" min="1" max="12">&nbsp;&nbsp;&nbsp;
 				Year:&nbsp;<input type="number" name="year" class="center" value="<?php if (isset($AlbumEntry)) { echo htmlspecialchars($AlbumEntry['Year']); } ?>" min="1900" max="<?php echo date('Y'); ?>">
 			</td>
 		</tr>