fc82fcaba8ca8b9adddf64d081c06aa686a39cfa
Dan Hemberger <daniel.hemberger@gmail.com>
2020-09-22 00:24:11 -0700

    shop_weapon.php: minor display changes
    * Display Cost in number format.
    
    * Rename a few columns for clarity
      (Race -> Seller, Credits -> Resale Value)
    
    * Reorder columns in the "Buy Weapons" table to keep the combat
      fields closer together.
    
    * Add weapon damage/accuracy columns to the "Sell Weapons" table so
      that people can make better informed decisions.

--- templates/Default/engine/Default/shop_weapon.php
+++ templates/Default/engine/Default/shop_weapon.php
@@ -1,5 +1,6 @@
 <?php
 if ($ThisLocation->isWeaponSold()) { ?>
+	<h2>Buy weapons:</h2><br />
 	<table id="weapon-list" class="center standard">
 		<thead>
 			<tr>
@@ -7,8 +8,8 @@ if ($ThisLocation->isWeaponSold()) { ?>
 				<th class="sort shrink" data-sort="sort_shield">Shield Damage</th>
 				<th class="sort shrink" data-sort="sort_armor">Armour Damage</th>
 				<th class="sort" data-sort="sort_acc">Accuracy</th>
-				<th class="sort" data-sort="sort_race">Race</th>
 				<th class="sort shrink" data-sort="sort_power">Power Level</th>
+				<th class="sort" data-sort="sort_race">Seller</th>
 				<th class="sort" data-sort="sort_cost">Cost</th>
 				<th>Action</th>
 			</tr>
@@ -21,9 +22,9 @@ if ($ThisLocation->isWeaponSold()) { ?>
 					<td class="sort_shield"><?php echo $Weapon->getShieldDamage(); ?></td>
 					<td class="sort_armor"><?php echo $Weapon->getArmourDamage(); ?></td>
 					<td class="sort_acc"><?php echo $Weapon->getBaseAccuracy(); ?>%</td>
-					<td class="sort_race"><?php echo $ThisPlayer->getColouredRaceName($Weapon->getRaceID(), true); ?></td>
 					<td class="sort_power"><?php echo $Weapon->getPowerLevel(); ?></td>
-					<td class="sort_cost"><?php echo $Weapon->getCost(); ?></td>
+					<td class="sort_race"><?php echo $ThisPlayer->getColouredRaceName($Weapon->getRaceID(), true); ?></td>
+					<td class="sort_cost"><?php echo number_format($Weapon->getCost()); ?></td>
 					<td><a href="<?php echo $Weapon->getBuyHREF($ThisLocation); ?>" class="submitStyle">Buy</a></td>
 				</tr><?php
 			} ?>
@@ -35,18 +36,23 @@ if ($ThisLocation->isWeaponSold()) { ?>
 
 if ($ThisShip->hasWeapons()) { ?>
 	<br /><br />
-	<h1>Sell Weapons</h1><br />
-
+	<h2>Sell weapons:</h2><br />
 	<table class="standard">
 		<tr class="center">
 			<th>Name</th>
+			<th>Shield<br />Damage</th>
+			<th>Armour<br /> Damage</th>
+			<th>Accuracy</th>
 			<th>Power<br />Level</th>
-			<th>Credits</th>
+			<th>Resale<br />Value</th>
 			<th>Action</th>
 		</tr><?php
 		foreach ($ThisShip->getWeapons() as $OrderID => $Weapon) { ?>
 			<tr class="center">
 				<td><?php echo $Weapon->getName(); ?></td>
+				<td><?php echo $Weapon->getShieldDamage(); ?></td>
+				<td><?php echo $Weapon->getArmourDamage(); ?></td>
+				<td><?php echo $Weapon->getBaseAccuracy(); ?>%</td>
 				<td><?php echo $Weapon->getPowerLevel(); ?></td>
 				<td><?php echo number_format(floor($Weapon->getCost() * WEAPON_REFUND_PERCENT)); ?></td>
 				<td><a href="<?php echo $Weapon->getSellHREF($ThisLocation, $OrderID); ?>" class="submitStyle">Sell</a></td>
