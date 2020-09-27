2d850a2f2a78776208e4f2c4172d0cd712b53107
stupidnewbie <55667763+stupidnewbie@users.noreply.github.com>
2020-09-27 19:48:35 -0400

    add some convenience features to port attack warning screen
    add sector to port examine screen
    
    display attackers for port examine like a fleet battle would show
    
    another one from accepted feature requests

--- engine/Default/port_attack_warning.php
+++ engine/Default/port_attack_warning.php
@@ -8,7 +8,7 @@ if ($sector->getPort()->isDestroyed()) {
 	forward(create_container('skeleton.php', 'port_attack.php'));
 }
 
-$template->assign('PageTopic', 'Port Raid');
+$template->assign('PageTopic', 'Port Raid In Sector #' . $player->getSectorID());
 
 $template->assign('PortAttackHREF', SmrSession::getNewHREF(create_container('port_attack_processing.php')));
 $template->assign('Port', $sector->getPort());
