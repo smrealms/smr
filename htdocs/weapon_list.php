4c3ebb60a98ed3db3ec2d4d415d7cf59af166971
Dan Hemberger <daniel.hemberger@gmail.com>
2020-09-30 20:41:42 -0700

    weapon_list.php: add locations where weapons are sold
    The Weapon List page now displays a "Locations" column that lists
    all the shops where each weapon is sold.
    
    This was requested as a result of the Enhanced Weapons feature,
    since it will be very helpful to know what weapons are sold at
    each shop.
    
    Note that we sacrifice a bit of visual conformity (equal height
    rows) to support this feature.

--- htdocs/weapon_list.php
+++ htdocs/weapon_list.php
@@ -4,6 +4,20 @@ try {
 
 	$template = new Template();
 
+	// Get a list of all the shops that sell each weapon
+	$weaponLocs = [];
+	$db = new SmrMySqlDatabase();
+	$db->query('SELECT weapon_type_id, location_type.* FROM location_sells_weapons JOIN weapon_type USING (weapon_type_id) JOIN location_type USING (location_type_id) WHERE location_type_id != ' . $db->escapeNumber(RACE_WARS_WEAPONS));
+	while ($db->nextRecord()) {
+		$weaponLocs[$db->getInt('weapon_type_id')][] = SmrLocation::getLocation($db->getInt('location_type_id'), false, $db)->getName();
+	}
+
+	// Get a list of all locations that sell weapons
+	$allLocs = array_unique(array_merge(...$weaponLocs));
+	sort($allLocs);
+	$template->assign('AllLocs', $allLocs);
+
+	// Get all the properties to display for each weapon
 	$weapons = [];
 	foreach (SmrWeaponType::getAllWeaponTypes() as $weapon) {
 		switch ($weapon->getBuyerRestriction()) {
@@ -35,6 +49,7 @@ try {
 			'armour_damage' => $weapon->getArmourDamage(),
 			'accuracy' => $weapon->getAccuracy(),
 			'power_level' => $weapon->getPowerLevel(),
+			'locs' => $weaponLocs[$weapon->getWeaponTypeID()] ?? [],
 		];
 	}
 	$template->assign('Weapons', $weapons);
