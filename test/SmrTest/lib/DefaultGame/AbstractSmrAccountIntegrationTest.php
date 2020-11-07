cf02fcec63aa20391983aa94a0297d420757de93
Jerome Doby <xaerodegreaz@gmail.com>
2020-11-07 20:33:53 +0900

    rmove no longer needed model

--- test/SmrTest/lib/DefaultGame/AbstractSmrAccountIntegrationTest.php
+++ test/SmrTest/lib/DefaultGame/AbstractSmrAccountIntegrationTest.php
@@ -9,33 +9,6 @@ use SmrTest\BaseIntegrationSpec;
 use SocialLogin;
 use SocialLogins\Facebook;
 
-class Record {
-	public int $account_id = 1;
-	public string $login = "login";
-	public string $password = "password";
-	public string $email = "email";
-	public string $last_login = "last login";
-	public string $validation_code = "validation code";
-	public string $offset = "offset";
-	public string $images = "Yes";
-	public string $fontsize = "font size";
-	public string $password_reset = "password reset";
-	public int $mail_banned = 0;
-	public string $friendly_colour = "colour";
-	public string $neutral_colour = "colour";
-	public string $enemy_colour = "colour";
-	public string $css_link = "css link";
-	public string $referral_id = 'referral_id';
-	public string $max_rank_achieved = 'max_rank_achieved';
-	public string $hof_name = 'hof_name';
-	public string $discord_id = 'discord_id';
-	public string $irc_nick = 'irc_nick';
-	public string $date_short = 'date_short';
-	public string $time_short = 'time_short';
-	public string $template = 'template';
-	public string $colour_scheme = 'colour_scheme';
-}
-
 /**
  * Class AbstractSmrAccountTest
  * @covers AbstractSmrAccount
