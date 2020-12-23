2519c086fe2522397882645b67be95595438370c
Dan Hemberger <daniel.hemberger@gmail.com>
2020-12-22 23:56:29 -0800

    bar_talk_bartender.php: fix XSS vulnerability
    The user input messages were rendered without escaping, which made
    them vulnerable to broken HTML and JavaScript insertion. We now
    appropriately escape the content before displaying it.
    
    Also added a word filter to the message processing.
    
    Thanks to Target for reporting this issue!

--- engine/Default/bar_talk_bartender.php
+++ engine/Default/bar_talk_bartender.php
@@ -7,7 +7,7 @@ Menu::bar();
 if (!isset($var['Message'])) {
 	$db->query('SELECT * FROM bar_tender WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . ' ORDER BY rand() LIMIT 1');
 	if ($db->nextRecord()) {
-		$message = 'I heard... ' . $db->getField('message') . '<br /><br />Got anything else to tell me?';
+		$message = 'I heard... ' . htmlentities(word_filter($db->getField('message'))) . '<br /><br />Got anything else to tell me?';
 	} else {
 		$message = 'I havent heard anything recently... got anything to tell me?';
 	}
