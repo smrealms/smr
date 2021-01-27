240b261e15ce85954f5809eab27ef67e9c809f37
Pagan Gazzard <pjgazzard@googlemail.com>
2021-01-27 20:12:44 +0000

    Rework port searches/evil goods
    Increase base search chance from 15% to 20%
    Increase smuggling search reduction from 4% to 10%
    Increase narcotics price from 259 to 295
    Increase weapons price from 168 to 191
    Increase slaves price from 89 to 101

---
+++ db/patches/V1_6_65_12__evil_goods_adjustment.sql
@@ -0,0 +1,3 @@
+UPDATE `good` SET `base_price` = 295 WHERE `good_name` = 'Narcotics';
+UPDATE `good` SET `base_price` = 191 WHERE `good_name` = 'Weapons';
+UPDATE `good` SET `base_price` = 101 WHERE `good_name` = 'Slaves';
