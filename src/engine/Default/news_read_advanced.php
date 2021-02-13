smrealms/smr

commit 67658e8a5f690322bb0de28cf9440596f7cac5b2

news_read_advanced.php: use SmrSession::getRequestVar

src/engine/Default/news_read_advanced.php
@@ -39,21 +39,11 @@ $template->assign('NewsAlliances', $newsAlliances);
 
 $template->assign('AdvancedNewsFormHref', SmrSession::getNewHREF(create_container('skeleton.php', 'news_read_advanced.php', $basicContainer)));
 
-
-if (isset($_REQUEST['submit'])) {
-	$submit_value = $_REQUEST['submit'];
-} elseif (isset($var['submit'])) {
-	$submit_value = $var['submit'];
-} else {
-	$submit_value = 'Default';
-}
+// No submit value when first navigating to the page
+$submit_value = SmrSession::getRequestVar('submit', '');
 
 if ($submit_value == 'Search For Player') {
-	if (isset($_REQUEST['playerName'])) {
-		$p_name = $_REQUEST['playerName'];
-	} else {
-		$p_name = $var['playerName'];
-	}
+	$p_name = SmrSession::getRequestVar('playerName');
 	$template->assign('ResultsFor', $p_name);
 	$db->query('SELECT * FROM player WHERE player_name LIKE ' . $db->escapeString('%' . $p_name . '%') . ' AND game_id = ' . $db->escapeNumber($gameID));
 	$IDs = array(0);
@@ -62,23 +52,19 @@ if ($submit_value == 'Search For Player') {
 	}
 	$db->query('SELECT * FROM news WHERE game_id = ' . $db->escapeNumber($gameID) . ' AND (killer_id IN (' . $db->escapeArray($IDs) . ') OR dead_id IN (' . $db->escapeArray($IDs) . ')) ORDER BY news_id DESC');
 } elseif ($submit_value == 'Search For Alliance') {
-	if (isset($_REQUEST['allianceID'])) {
-		SmrSession::updateVar('AllianceID', $_REQUEST['allianceID']);
-	}
-	if (!isset($var['AllianceID'])) {
-		create_error('No alliance was specified!');
-	}
-	$allianceID = $var['AllianceID'];
+	$allianceID = SmrSession::getRequestVar('allianceID');
 	$template->assign('ResultsFor', $newsAlliances[$allianceID]['Name']);
 	$db->query('SELECT * FROM news WHERE game_id = ' . $db->escapeNumber($gameID) . ' AND ((killer_alliance = ' . $db->escapeNumber($allianceID) . ' AND killer_id != ' . $db->escapeNumber(ACCOUNT_ID_PORT) . ') OR (dead_alliance = ' . $db->escapeNumber($allianceID) . ' AND dead_id != ' . $db->escapeNumber(ACCOUNT_ID_PORT) . ')) ORDER BY news_id DESC');
 } elseif ($submit_value == 'Search For Players') {
-	$template->assign('ResultsFor', $_REQUEST['player1'] . ' vs. ' . $_REQUEST['player2']);
-	$db->query('SELECT * FROM player WHERE player_name LIKE ' . $db->escapeString('%' . $_REQUEST['player1'] . '%') . ' AND game_id = ' . $db->escapeNumber($gameID));
+	$player1 = SmrSession::getRequestVar('player1');
+	$player2 = SmrSession::getRequestVar('player2');
+	$template->assign('ResultsFor', $player1 . ' vs. ' . $player2);
+	$db->query('SELECT * FROM player WHERE player_name LIKE ' . $db->escapeString('%' . $player1 . '%') . ' AND game_id = ' . $db->escapeNumber($gameID));
 	$IDs = array(0);
 	while ($db->nextRecord()) {
 		$IDs[] = $db->getInt('account_id');
 	}
-	$db->query('SELECT * FROM player WHERE player_name LIKE ' . $db->escapeString('%' . $_REQUEST['player2'] . '%') . ' AND game_id = ' . $db->escapeNumber($gameID));
+	$db->query('SELECT * FROM player WHERE player_name LIKE ' . $db->escapeString('%' . $player2 . '%') . ' AND game_id = ' . $db->escapeNumber($gameID));
 	$IDs2 = array(0);
 	while ($db->nextRecord()) {
 		$IDs2[] = $db->getInt('account_id');
@@ -91,13 +77,15 @@ if ($submit_value == 'Search For Player') {
 						(killer_id IN (' . $db->escapeArray($IDs2) . ') AND dead_id IN (' . $db->escapeArray($IDs) . '))
 					) ORDER BY news_id DESC');
 } elseif ($submit_value == 'Search For Alliances') {
-	$template->assign('ResultsFor', $newsAlliances[$_REQUEST['alliance1']]['Name'] . ' vs. ' . $newsAlliances[$_REQUEST['alliance2']]['Name']);
+	$allianceID1 = SmrSession::getRequestVar('alliance1');
+	$allianceID2 = SmrSession::getRequestVar('alliance2');
+	$template->assign('ResultsFor', $newsAlliances[$allianceID1]['Name'] . ' vs. ' . $newsAlliances[$allianceID2]['Name']);
 	$db->query('SELECT * FROM news
 				WHERE game_id = ' . $db->escapeNumber($gameID) . '
 					AND (
-						(killer_alliance = ' . $db->escapeNumber($_REQUEST['alliance1']) . ' AND dead_alliance = ' . $db->escapeNumber($_REQUEST['alliance2']) . ')
+						(killer_alliance = ' . $db->escapeNumber($allianceID1) . ' AND dead_alliance = ' . $db->escapeNumber($allianceID2) . ')
 						OR
-						(killer_alliance = ' . $db->escapeNumber($_REQUEST['alliance2']) . ' AND dead_alliance = ' . $db->escapeNumber($_REQUEST['alliance1']) . ')
+						(killer_alliance = ' . $db->escapeNumber($allianceID2) . ' AND dead_alliance = ' . $db->escapeNumber($allianceID1) . ')
 					) ORDER BY news_id DESC');
 } else {
 	$db->query('SELECT * FROM news WHERE game_id = ' . $db->escapeNumber($gameID) . ' ORDER BY news_id DESC LIMIT 50');

commit b1138ba25d365718286a37a8b2f2c3ac5c047ff3

Fix Globals::getAllianceNewsHREF regression
In 67658e8a, the expected var name in news_read_advanced.php changed
from `AllianceID` to `allianceID`. Since it is passed through the
SmrSession as an int, we also need to fetch it with `getRequestVarInt`
instead of `getRequestVar` (which is for string types only).

This fixes the "View News" link in the alliance menu.

src/engine/Default/news_read_advanced.php
@@ -52,7 +52,7 @@ if ($submit_value == 'Search For Player') {
 	}
 	$db->query('SELECT * FROM news WHERE game_id = ' . $db->escapeNumber($gameID) . ' AND (killer_id IN (' . $db->escapeArray($IDs) . ') OR dead_id IN (' . $db->escapeArray($IDs) . ')) ORDER BY news_id DESC');
 } elseif ($submit_value == 'Search For Alliance') {
-	$allianceID = SmrSession::getRequestVar('allianceID');
+	$allianceID = SmrSession::getRequestVarInt('allianceID');
 	$template->assign('ResultsFor', $newsAlliances[$allianceID]['Name']);
 	$db->query('SELECT * FROM news WHERE game_id = ' . $db->escapeNumber($gameID) . ' AND ((killer_alliance = ' . $db->escapeNumber($allianceID) . ' AND killer_id != ' . $db->escapeNumber(ACCOUNT_ID_PORT) . ') OR (dead_alliance = ' . $db->escapeNumber($allianceID) . ' AND dead_id != ' . $db->escapeNumber(ACCOUNT_ID_PORT) . ')) ORDER BY news_id DESC');
 } elseif ($submit_value == 'Search For Players') {

src/lib/Default/Globals.class.php
@@ -398,7 +398,7 @@ class Globals {
 	}
 
 	public static function getAllianceNewsHREF($allianceID) {
-		return SmrSession::getNewHREF(create_container('skeleton.php', 'news_read_advanced.php', array('AllianceID'=>$allianceID, 'submit' => 'Search For Alliance')));
+		return SmrSession::getNewHREF(create_container('skeleton.php', 'news_read_advanced.php', array('allianceID'=>$allianceID, 'submit' => 'Search For Alliance')));
 	}
 
 	public static function getAllianceMotdHREF($allianceID) {