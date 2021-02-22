15b7781be75227f4730769fa3e2318796f9c5133
Dan Hemberger <daniel.hemberger@gmail.com>
2021-02-22 01:04:06 -0800

    ip_view_results.php: minor cleanup
    * Remove unused query for the last account ID.
    * Use `SmrAccount::isDisabled` instead of duplicating the query.
    * Convert strings to ints before passing them as IDs into functions
      that only take ints (e.g. `SmrAlliance::getAlliance`).

--- src/admin/Default/ip_view.php
+++ src/admin/Default/ip_view.php
@@ -1,8 +1,5 @@
 <?php declare(strict_types=1);
 
 $template->assign('PageTopic', 'IP Search');
-$db->query('SELECT max(account_id) max_account_id FROM account');
-$db->requireRecord();
-$template->assign('MaxAccountID', $db->getInt('max_account_id'));
 
 $template->assign('IpFormHref', SmrSession::getNewHREF(create_container('skeleton.php', 'ip_view_results.php')));
