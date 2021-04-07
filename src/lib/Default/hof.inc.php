1d74abad814ce0009f88e2f7f5cf212b79cd92b2
Dan Hemberger <daniel.hemberger@gmail.com>
2021-04-07 01:24:37 -0700

    hof.inc.php: remove global player/account
    Some of the global variables declared in the HoF functions were not
    used and could be removed (`$account` in `getHofRank` and `$player` in
    `displayHOFRow`). The others were replaced with the account and/or
    player from the Session.

--- src/lib/Default/hof.inc.php
+++ src/lib/Default/hof.inc.php
@@ -52,11 +52,12 @@ function getHofCategories($hofTypes, $game_id, $account_id) {
  * - private stats for players who are not the current player
  */
 function applyHofVisibilityMask($amount, $vis, $gameID, $accountID) {
-	global $account, $player;
+	$session = Smr\Session::getInstance();
+	$account = $session->getAccount();
 	if (($vis == HOF_PRIVATE && $account->getAccountID() != $accountID) ||
 	    ($vis == HOF_ALLIANCE && isset($gameID) &&
 	     !SmrGame::getGame($gameID)->hasEnded() &&
-	     !SmrPlayer::getPlayer($accountID, $gameID)->sameAlliance($player)))
+	     !SmrPlayer::getPlayer($accountID, $gameID)->sameAlliance($session->getPlayer())))
 	{
 		return '-';
 	} else {
@@ -65,7 +66,6 @@ function applyHofVisibilityMask($amount, $vis, $gameID, $accountID) {
 }
 
 function getHofRank($view, $viewType, $accountID, $gameID) {
-	global $account;
 	$db = MySqlDatabase::getInstance();
 	// If no game specified, show total amount from completed games only
 	$gameIDSql = ' AND game_id ' . (isset($gameID) ? '= ' . $db->escapeNumber($gameID) : 'IN (SELECT game_id FROM game WHERE end_time < ' . Smr\Epoch::time() . ' AND ignore_stats = ' . $db->escapeBoolean(false) . ')');
@@ -110,7 +110,9 @@ function getHofRank($view, $viewType, $accountID, $gameID) {
 }
 
 function displayHOFRow($rank, $accountID, $amount) {
-	global $account, $player, $var;
+	global $var;
+
+	$account = Smr\Session::getInstance()->getAccount();
 	if (isset($var['game_id']) && Globals::isValidGame($var['game_id'])) {
 		try {
 			$hofPlayer = SmrPlayer::getPlayer($accountID, $var['game_id']);
