c6dab960983b936df29a1191c8d8701a613f0d9b
Dan Hemberger <daniel.hemberger@gmail.com>
2021-04-08 22:57:12 -0700

    bootstrap.php: remove global account/player
    In `logException`, get the player and account from the Session instead
    of using globals. This is a bit less fault-tolerant, but I think we
    need to assume some basic level of functioning for this logging to
    work at all in the first place.
    
    Also add "Game ID" to the output, since somehow that was missing from
    the logging!

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,14 +3,18 @@
 use Smr\Container\DiContainer;
 
 function logException(Throwable $e) : void {
-	global $account, $var, $player;
+	global $var;
 	$message = '';
 	$delim = "\n\n-----------\n\n";
 
-	if (is_object($account)) {
+	$session = Smr\Session::getInstance();
+
+	if ($session->hasAccount()) {
+		$account = $session->getAccount();
 		$message .= 'Login: ' . $account->getLogin() . "\n" .
+			'E-Mail: ' . $account->getEmail() . "\n" .
 			'Account ID: ' . $account->getAccountID() . "\n" .
-			'E-Mail: ' . $account->getEmail() . $delim;
+			'Game ID: ' . $session->getGameID() . $delim;
 	}
 	$message .= 'Error Message: ' . $e . $delim;
 
@@ -56,11 +60,11 @@ function logException(Throwable $e) : void {
 	}
 
 	// Send error message to the in-game auto bugs mailbox
-	if (is_object($player) && method_exists($player, 'sendMessageToBox')) {
-		$player->sendMessageToBox(BOX_BUGS_AUTO, $message);
-	} elseif (is_object($account) && method_exists($account, 'sendMessageToBox')) {
+	if ($session->hasGame()) {
+		$session->getPlayer()->sendMessageToBox(BOX_BUGS_AUTO, $message);
+	} elseif ($session->hasAccount()) {
 		// Will be logged without a game_id
-		$account->sendMessageToBox(BOX_BUGS_AUTO, $message);
+		$session->getAccount()->sendMessageToBox(BOX_BUGS_AUTO, $message);
 	} else {
 		// Will be logged without a game_id or sender_id
 		SmrAccount::doMessageSendingToBox(0, BOX_BUGS_AUTO, $message, 0);
