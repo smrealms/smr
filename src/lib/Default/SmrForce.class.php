smrealms/smr

commit 772828c58a579be05f15629bd6def62ad798804b

Replace array containers with the Page class (#1031)
* Replace array containers with the Page class

The container mechanism for creating links and forwarding to new pages
originally used arrays. Now we use the Page class, which inherits from
ArrayObject. Since Objects (including ArrayObject) behave differently
for copying and references than arrays, this first stage of refactoring
only attempts to recover the old behavior without modifying the _logic_
of container usage everywhere in the code. Further stages may attempt
to treat containers as Objects from the ground up.

In particular, since we often reuse containers to make multiple HREFs,
we need to do a lot of (otherwise unnecessary) copying of the data in
the ArrayObject to prevent overwriting the data from prior usage.

The new interface maps fairly directly to the previous one:

* create_container(...) -> Page::create(...)
* $container = $var -> $container = Page::copy($var)
* SmrSession::getNewHREF($container) -> $container->href()
* forward($container) -> $container->go()
* resetContainer($container) -> $container->useAsGlobalVar()
* transfer(...) -> $container->addVar(...)

There are a number of subtle considerations in this refactoring. Here
is a mostly comprehensive list of them:

* Some of the container manipulation performed in SmrSession methods
  is now performed in the Page class (such as generating the CommonID
  and setting the ReaminingPageLoads.

* Functions that had container arguments can now be type-hinted as
  `Page` instead of the much more ambiguous `array`.

* We no longer use `$container` as a global variable. It's not clear
  why or if this was needed in the past, but it did not appear to be
  needed before or after this refactoring.

* Using `Page::copy($var)` as a static function instead of using a
  method (e.g. `$var->copy()`) will enable better static analysis,
  because the analyis will be able to infer the return type (whereas

src/lib/Default/SmrForce.class.php
@@ -387,22 +387,22 @@ class SmrForce {
 	}
 
 	public function getExamineDropForcesHREF() {
-		$container = create_container('skeleton.php', 'forces_drop.php');
+		$container = Page::create('skeleton.php', 'forces_drop.php');
 		$container['owner_id'] = $this->getOwnerID();
-		return SmrSession::getNewHREF($container);
+		return $container->href();
 	}
 
 	public function getAttackForcesHREF() {
-		$container = create_container('forces_attack_processing.php');
+		$container = Page::create('forces_attack_processing.php');
 		$container['action'] = 'attack';
 		$container['owner_id'] = $this->getOwnerID();
-		return SmrSession::getNewHREF($container);
+		return $container->href();
 	}
 
 	public function getRefreshHREF() {
-		$container = create_container('forces_refresh_processing.php');
+		$container = Page::create('forces_refresh_processing.php');
 		$container['owner_id'] = $this->getOwnerID();
-		return SmrSession::getNewHREF($container);
+		return $container->href();
 	}
 
 	protected function getDropContainer() {
@@ -412,42 +412,42 @@ class SmrForce {
 	public function getDropSDHREF() {
 		$container = $this->getDropContainer();
 		$container['drop_scout_drones'] = 1;
-		return SmrSession::getNewHREF($container);
+		return $container->href();
 	}
 
 	public function getTakeSDHREF() {
 		$container = $this->getDropContainer();
 		$container['take_scout_drones'] = 1;
-		return SmrSession::getNewHREF($container);
+		return $container->href();
 	}
 
 	public function getDropCDHREF() {
 		$container = $this->getDropContainer();
 		$container['drop_combat_drones'] = 1;
-		return SmrSession::getNewHREF($container);
+		return $container->href();
 	}
 
 	public function getTakeCDHREF() {
 		$container = $this->getDropContainer();
 		$container['take_combat_drones'] = 1;
-		return SmrSession::getNewHREF($container);
+		return $container->href();
 	}
 
 	public function getDropMineHREF() {
 		$container = $this->getDropContainer();
 		$container['drop_mines'] = 1;
-		return SmrSession::getNewHREF($container);
+		return $container->href();
 	}
 
 	public function getTakeMineHREF() {
 		$container = $this->getDropContainer();
 		$container['take_mines'] = 1;
-		return SmrSession::getNewHREF($container);
+		return $container->href();
 	}
 
 	public static function getRefreshAllHREF() {
-		$container = create_container('forces_mass_refresh.php');
-		return SmrSession::getNewHREF($container);
+		$container = Page::create('forces_mass_refresh.php');
+		return $container->href();
 	}
 
 	public function shootPlayers(array $targetPlayers, $minesAreAttacker) {

commit 4fc0033105b4a43390d0dcd8c1d30fdfed7d2a70

SmrForce: fix getDropContainer
The `SmrForce::getDropContainer` method was returning an array, but
then was being used in the `get{Take,Drop}{SD,CD,Mine}HREF` functions
as if it were a `Page` object.

This fixes a bug introduced in #1031.

src/lib/Default/SmrForce.class.php
@@ -406,7 +406,9 @@ class SmrForce {
 	}
 
 	protected function getDropContainer() {
-		return array('url' => 'forces_drop_processing.php', 'owner_id' => $this->getOwnerID());
+		$container = Page::create('forces_drop_processing.php');
+		$container['owner_id'] = $this->getOwnerID();
+		return $container;
 	}
 
 	public function getDropSDHREF() {