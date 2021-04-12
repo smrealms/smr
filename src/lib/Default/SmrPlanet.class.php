smrealms/smr

commit 77f874719637ad1107086c1b13cefbaddb49db75

SmrPlanet: rework canBuild method
Replace `canBuild` with `getBuildRestriction`, which essentially
flips the boolean return value from `true` to `false` in the case
where there is a build restriction.

This is helpful because `false` is a special type that is allowed
_only_ in unions. Previously, we needed to specify `bool` because
`true` is not a valid return type (alone or in unions).

Additional changes:

* Moved the turn cost logic into `getBuildRestriction` (to check if
  the player has enough turns) and `startBuilding` (to consume the
  turns). This consolidates the set of build restrictions to a
  single function.

* Since `startBuilding` already calls `getBuildRestriction`, we don't
  need to call it separately in planet_construction_processing.php.
  Instead, `startBuilding` will throw a `UserWarning` and the engine
  will catch it and forward the message to `create_error`. This way
  we only perform the restriction checks once.

src/lib/Default/SmrPlanet.class.php
@@ -918,7 +918,11 @@ class SmrPlanet {
 		return count($this->getCurrentlyBuilding()) > 0;
 	}
 
-	public function canBuild(AbstractSmrPlayer $constructor, int $constructionID) : string|bool {
+	/**
+	 * Returns the reason a build cannot be performed, or false if there is
+	 * no restriction.
+	 */
+	public function getBuildRestriction(AbstractSmrPlayer $constructor, int $constructionID) : string|false {
 		if ($this->hasCurrentlyBuilding()) {
 			return 'There is already a building in progress!';
 		}
@@ -928,6 +932,9 @@ class SmrPlanet {
 		$cost = $this->getStructureTypes($constructionID)->creditCost();
 		if ($constructor->getCredits() < $cost) {
 			return 'You do not have enough credits.';
+		}
+		if ($constructor->getTurns() < TURNS_TO_BUILD) {
+			return 'You do not have enough turns to build.';
 		}
 		foreach ($this->getStructureTypes($constructionID)->hardwareCost() as $hardwareID) {
 			if (!$constructor->getShip()->getHardware($hardwareID)) {
@@ -940,7 +947,7 @@ class SmrPlanet {
 				return 'There are not enough goods available.';
 			}
 		}
-		return true;
+		return false;
 	}
 
 	// Modifier for planet building based on the number of buildings.
@@ -964,17 +971,13 @@ class SmrPlanet {
 		return $constructionTime;
 	}
 
+	/**
+	 * @throws \Smr\UserException If the player cannot build the structure.
+	 */
 	public function startBuilding(AbstractSmrPlayer $constructor, int $constructionID) : void {
-		if (($message = $this->canBuild($constructor, $constructionID)) !== true) {
-			throw new Exception('Unable to start building: ' . $message);
-		}
-		$constructor->decreaseCredits($this->getStructureTypes($constructionID)->creditCost());
-		// take the goods that are needed
-		foreach ($this->getStructureTypes($constructionID)->goods() as $goodID => $amount) {
-			$this->decreaseStockpile($goodID, $amount);
-		}
-		foreach ($this->getStructureTypes($constructionID)->hardwareCost() as $hardwareID) {
-			$constructor->getShip()->setHardware($hardwareID, 0);
+		$restriction = $this->getBuildRestriction($constructor, $constructionID);
+		if ($restriction !== false) {
+			throw \Smr\UserException('Unable to start building: ' . $restriction);
 		}
 
 		// gets the time for the buildings
@@ -989,6 +992,16 @@ class SmrPlanet {
 			'Finishes' => $timeComplete,
 			'TimeRemaining' => $timeComplete - SmrSession::getTime()
 		);
+
+		// Consume the required resources
+		$constructor->decreaseCredits($this->getStructureTypes($constructionID)->creditCost());
+		$constructor->takeTurns(TURNS_TO_BUILD);
+		foreach ($this->getStructureTypes($constructionID)->goods() as $goodID => $amount) {
+			$this->decreaseStockpile($goodID, $amount);
+		}
+		foreach ($this->getStructureTypes($constructionID)->hardwareCost() as $hardwareID) {
+			$constructor->getShip()->setHardware($hardwareID, 0);
+		}
 	}
 
 	public function stopBuilding(int $constructionID) : bool {

src/engine/Default/planet_construction_processing.php
@@ -5,17 +5,12 @@ if (!$player->isLandedOnPlanet()) {
 $planet = $player->getSectorPlanet();
 $action = $var['action'];
 if ($action == 'Build') {
-	if (($message = $planet->canBuild($player, $var['construction_id'])) !== true) {
+	// now start the construction
+	try {
+		$planet->startBuilding($player, $var['construction_id']);
+	} catch (\Smr\UserException $message) {
 		create_error($message);
 	}
-
-	if ($player->getTurns() < TURNS_TO_BUILD) {
-		create_error('You don\'t have enough turns to build!');
-	}
-	$player->takeTurns(TURNS_TO_BUILD);
-
-	// now start the construction
-	$planet->startBuilding($player, $var['construction_id']);
 	$player->increaseHOF(1, array('Planet', 'Buildings', 'Started'), HOF_ALLIANCE);
 
 	$player->log(LOG_TYPE_PLANETS, 'Player starts a ' . $planet->getStructureTypes($var['construction_id'])->name() . ' on planet.');

src/templates/Default/engine/Default/planet_construction.php
@@ -43,7 +43,7 @@ You are currently building: <?php
 				<br /><?php echo format_time($ThisPlanet->getConstructionTime($StructureID), true); ?>
 		</td>
 			<td><?php
-				if ($ThisPlanet->canBuild($ThisPlayer, $StructureID) === true) { ?>
+				if ($ThisPlanet->getBuildRestriction($ThisPlayer, $StructureID) === false) { ?>
 					<div class="buttonA"><a class="buttonA" href="<?php echo $ThisPlanet->getBuildHREF($StructureID); ?>">Build (<?php echo TURNS_TO_BUILD; ?>)</a></div><?php
 				} ?>
 			</td>

commit 6a86459dfbeef0cd595e1d4567ab310250c49cde

SmrPlanet: fix bug in error message
The `startBuilding` method was throwing a UserException without the
`new` keyword. This bug was introduced in 77f874719637.

src/lib/Default/SmrPlanet.class.php
@@ -977,7 +977,7 @@ class SmrPlanet {
 	public function startBuilding(AbstractSmrPlayer $constructor, int $constructionID) : void {
 		$restriction = $this->getBuildRestriction($constructor, $constructionID);
 		if ($restriction !== false) {
-			throw \Smr\UserException('Unable to start building: ' . $restriction);
+			throw new \Smr\UserException('Unable to start building: ' . $restriction);
 		}
 
 		// gets the time for the buildings