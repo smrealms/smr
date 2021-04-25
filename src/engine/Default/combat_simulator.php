6e95f389a7c00ea1a723ecaefc79190814bdc0b1
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2021-04-25 03:28:11 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/engine/Default/combat_simulator.php
+++ src/engine/Default/combat_simulator.php
@@ -2,21 +2,21 @@
 
 $template = Smr\Template::getInstance();
 
-$template->assign('PageTopic','Combat Simulator');
+$template->assign('PageTopic', 'Combat Simulator');
 
-$template->assign('EditDummysLink', Page::create('skeleton.php','edit_dummys.php')->href());
+$template->assign('EditDummysLink', Page::create('skeleton.php', 'edit_dummys.php')->href());
 $template->assign('DummyNames', DummyPlayer::getDummyPlayerNames());
 
 $duplicates = false;
 $usedNames = array();
 $realAttackers = array();
 $attackers = array();
-$i=1;
-if(isset($_POST['attackers']))
-	foreach($_POST['attackers'] as $attackerName) {
-		if($attackerName=='none')
+$i = 1;
+if (isset($_POST['attackers']))
+	foreach ($_POST['attackers'] as $attackerName) {
+		if ($attackerName == 'none')
 			continue;
-		if(isset($usedNames[$attackerName])) {
+		if (isset($usedNames[$attackerName])) {
 			$duplicates = true;
 			continue;
 		}
@@ -27,18 +27,18 @@ if(isset($_POST['attackers']))
 		++$i;
 	}
 
-for(;$i<=10;++$i)
+for (;$i <= 10; ++$i)
 	$attackers[$i] = null;
-$template->assign('Attackers',$attackers);
+$template->assign('Attackers', $attackers);
 
-$i=1;
+$i = 1;
 $realDefenders = array();
 $defenders = array();
-if(isset($_POST['defenders']))
-	foreach($_POST['defenders'] as $defenderName) {
-		if($defenderName=='none')
+if (isset($_POST['defenders']))
+	foreach ($_POST['defenders'] as $defenderName) {
+		if ($defenderName == 'none')
 			continue;
-		if(isset($usedNames[$defenderName])) {
+		if (isset($usedNames[$defenderName])) {
 			$duplicates = true;
 			continue;
 		}
@@ -49,34 +49,34 @@ if(isset($_POST['defenders']))
 		++$i;
 	}
 
-for(;$i<=10;++$i)
+for (;$i <= 10; ++$i)
 	$defenders[$i] = null;
-$template->assign('Defenders',$defenders);
+$template->assign('Defenders', $defenders);
 
-$template->assign('Duplicates',$duplicates);
+$template->assign('Duplicates', $duplicates);
 
-$template->assign('CombatSimHREF', Page::create('skeleton.php','combat_simulator.php')->href());
+$template->assign('CombatSimHREF', Page::create('skeleton.php', 'combat_simulator.php')->href());
 
 if (!empty($realAttackers) && !empty($realDefenders)) {
-	if(isset($_REQUEST['run'])) {
-		runAnAttack($realAttackers,$realDefenders);
+	if (isset($_REQUEST['run'])) {
+		runAnAttack($realAttackers, $realDefenders);
 	}
-	if(isset($_REQUEST['death_run'])) {
-		while(count($realAttackers)>0 && count($realDefenders)>0) {
-			runAnAttack($realAttackers,$realDefenders);
-			foreach($realAttackers as $key => &$teamPlayer) {
-				if($teamPlayer->isDead())
+	if (isset($_REQUEST['death_run'])) {
+		while (count($realAttackers) > 0 && count($realDefenders) > 0) {
+			runAnAttack($realAttackers, $realDefenders);
+			foreach ($realAttackers as $key => &$teamPlayer) {
+				if ($teamPlayer->isDead())
 					unset($realAttackers[$key]);
 			} unset($teamPlayer);
-			foreach($realDefenders as $key => &$teamPlayer) {
-				if($teamPlayer->isDead())
+			foreach ($realDefenders as $key => &$teamPlayer) {
+				if ($teamPlayer->isDead())
 					unset($realDefenders[$key]);
 			} unset($teamPlayer);
 		}
 	}
 }
 
-function runAnAttack($realAttackers,$realDefenders) {
+function runAnAttack($realAttackers, $realDefenders) {
 	$results = array('Attackers' => array('Traders' => array(), 'TotalDamage' => 0),
 					'Defenders' => array('Traders' => array(), 'TotalDamage' => 0));
 	foreach ($realAttackers as $accountID => $teamPlayer) {
@@ -86,10 +86,10 @@ function runAnAttack($realAttackers,$realDefenders) {
 	}
 	foreach ($realDefenders as $accountID => $teamPlayer) {
 		$playerResults = $teamPlayer->shootPlayers($realAttackers);
-		$results['Defenders']['Traders'][]  = $playerResults;
+		$results['Defenders']['Traders'][] = $playerResults;
 		$results['Defenders']['TotalDamage'] += $playerResults['TotalDamage'];
 	}
 
 	$template = Smr\Template::getInstance();
-	$template->assign('TraderCombatResults',$results);
+	$template->assign('TraderCombatResults', $results);
 }
--- src/lib/Default/DummyPlayer.class.php
+++ src/lib/Default/DummyPlayer.class.php
@@ -1,32 +1,32 @@
 <?php declare(strict_types=1);
 
 class DummyPlayer extends AbstractSmrPlayer {
-	public function __construct($gameID=0,$playerName='Dummy',$raceID=1,$experience=1000,$alignment=100,$allianceID=0,$shipTypeID=60) {
-		$this->accountID				= 0;
-		$this->gameID					= (int) $gameID;
-		$this->playerName				= (string) $playerName;
-		$this->playerID					= 0;
-		$this->sectorID					= 0;
-		$this->lastSectorID				= 0;
-		$this->turns					= 1000;
-		$this->newbieTurns				= 0;
-		$this->lastNewsUpdate			= 0;
-		$this->dead						= false;
-		$this->landedOnPlanet			= false;
-		$this->lastActive				= 0;
-		$this->lastCPLAction			= 0;
-		$this->raceID					= (int) $raceID;
-		$this->credits					= 0;
-		$this->experience				= (int) $experience;
-		$this->alignment				= (int) $alignment;
-		$this->militaryPayment			= 0;
-		$this->allianceID				= (int) $allianceID;
-		$this->shipID					= (int) $shipTypeID;
-		$this->kills					= 0;
-		$this->deaths					= 0;
-		$this->lastPort					= 0;
-		$this->bank						= 0;
-		$this->zoom						= 0;
+	public function __construct($gameID = 0, $playerName = 'Dummy', $raceID = 1, $experience = 1000, $alignment = 100, $allianceID = 0, $shipTypeID = 60) {
+		$this->accountID = 0;
+		$this->gameID = (int)$gameID;
+		$this->playerName = (string)$playerName;
+		$this->playerID = 0;
+		$this->sectorID = 0;
+		$this->lastSectorID = 0;
+		$this->turns = 1000;
+		$this->newbieTurns = 0;
+		$this->lastNewsUpdate = 0;
+		$this->dead = false;
+		$this->landedOnPlanet = false;
+		$this->lastActive = 0;
+		$this->lastCPLAction = 0;
+		$this->raceID = (int)$raceID;
+		$this->credits = 0;
+		$this->experience = (int)$experience;
+		$this->alignment = (int)$alignment;
+		$this->militaryPayment = 0;
+		$this->allianceID = (int)$allianceID;
+		$this->shipID = (int)$shipTypeID;
+		$this->kills = 0;
+		$this->deaths = 0;
+		$this->lastPort = 0;
+		$this->bank = 0;
+		$this->zoom = 0;
 
 		$this->personalRelations = array();
 		$this->bounties = array();
@@ -48,9 +48,9 @@ class DummyPlayer extends AbstractSmrPlayer {
 	}
 
 	public function setAllianceID($ID) {
-		if($this->allianceID == $ID)
+		if ($this->allianceID == $ID)
 			return;
-		$this->allianceID=$ID;
+		$this->allianceID = $ID;
 	}
 
 	public function killPlayerByPlayer(AbstractSmrPlayer $killer) {
@@ -75,7 +75,7 @@ class DummyPlayer extends AbstractSmrPlayer {
 		$db = Smr\Database::getInstance();
 		$db->query('REPLACE INTO cached_dummys ' .
 					'(type, id, info) ' .
-					'VALUES (\'DummyPlayer\', '.$db->escapeString($this->getPlayerName()).', '.$db->escapeObject($this).')');
+					'VALUES (\'DummyPlayer\', ' . $db->escapeString($this->getPlayerName()) . ', ' . $db->escapeObject($this) . ')');
 	}
 
 	public static function getCachedDummyPlayer($name) {
@@ -83,7 +83,7 @@ class DummyPlayer extends AbstractSmrPlayer {
 		$db->query('SELECT info FROM cached_dummys
 					WHERE type = \'DummyPlayer\'
 						AND id = ' . $db->escapeString($name) . ' LIMIT 1');
-		if($db->nextRecord()) {
+		if ($db->nextRecord()) {
 			return $db->getObject('info');
 		}
 		else {
@@ -96,7 +96,7 @@ class DummyPlayer extends AbstractSmrPlayer {
 		$db->query('SELECT id FROM cached_dummys
 					WHERE type = \'DummyPlayer\'');
 		$dummyNames = array();
-		while($db->nextRecord()) {
+		while ($db->nextRecord()) {
 			$dummyNames[] = $db->getField('id');
 		}
 		return $dummyNames;
