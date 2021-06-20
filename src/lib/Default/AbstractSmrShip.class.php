smrealms/smr

commit c2cffc5294cfe6b25de8942c87691c0af716cf09

SmrShip: improvements to cargo functions
* Fix cargo initialization. We shouldn't call `checkForExcess` in the
  AbstractSmrShip constructor because it uses the `cargo` property
  before it is defined (nor does it even make sense to check for
  excess before any of the weapons/cargo/hardware have been loaded!).

* Remove `isset` check on `cargo` in `loadCargo` for consistency with
  `load{Hardware,Weapons}`. The "caching" isn't needed because we only
  call this function once in the constructor anyway.

* Simplify `getCargo` with the null coalescing operator.

* Simplify `hasCargo` by calling `getUsedHolds` internally.

src/lib/Default/AbstractSmrShip.class.php
@@ -148,7 +148,6 @@ abstract class AbstractSmrShip {
 
 	protected function regenerateBaseShip() {
 		$this->baseShip = AbstractSmrShip::getBaseShip($this->player->getShipTypeID());
-		$this->checkForExcess();
 	}
 
 	public function checkForExcess() {
@@ -796,24 +795,17 @@ abstract class AbstractSmrShip {
 	}
 
 	public function getCargo($goodID = false) {
-		if ($goodID !== false) {
-			if (isset($this->cargo[$goodID])) {
-				return $this->cargo[$goodID];
-			}
-			$cargo = 0;
-			return $cargo;
+		if ($goodID === false) {
+			return $this->cargo;
 		}
-		return $this->cargo;
+		return $this->cargo[$goodID] ?? 0;
 	}
 
 	public function hasCargo($goodID = false) {
-		if ($goodID !== false) {
-			return $this->getCargo($goodID) > 0;
+		if ($goodID === false) {
+			return $this->getUsedHolds() > 0;
 		}
-		if (is_array($cargo = $this->getCargo())) {
-			return array_sum($cargo) > 0;
-		}
-		return false;
+		return $this->getCargo($goodID) > 0;
 	}
 
 	public function setCargo($goodID, $amount) {

src/lib/Default/DummyShip.class.php
@@ -30,7 +30,7 @@ class DummyShip extends AbstractSmrShip {
 //			if(!isset($this->weapons[$i]))
 //				$this->weapons[$i] = SmrWeapon::getWeapon(1);
 //		}
-		$this->checkForExcessWeapons();
+		$this->checkForExcess();
 	}
 
 	public function decloak() {

src/lib/Default/SmrShip.class.php
@@ -69,16 +69,14 @@ class SmrShip extends AbstractSmrShip {
 	}
 
 	protected function loadCargo() {
-		if (!isset($this->cargo)) {
-			// initialize cargo array
-			$this->cargo = array();
+		// initialize cargo array
+		$this->cargo = array();
 
-			// get cargo from db
-			$this->db->query('SELECT * FROM ship_has_cargo WHERE ' . $this->SQL);
-			while ($this->db->nextRecord()) {
-				// adding cargo and amount to array
-				$this->cargo[$this->db->getInt('good_id')] = $this->db->getInt('amount');
-			}
+		// get cargo from db
+		$this->db->query('SELECT * FROM ship_has_cargo WHERE ' . $this->SQL);
+		while ($this->db->nextRecord()) {
+			// adding cargo and amount to array
+			$this->cargo[$this->db->getInt('good_id')] = $this->db->getInt('amount');
 		}
 		$this->checkForExcessCargo();
 	}

commit 8e3037e08bfa2185b71cc105db0a5538c408d79f

Fix missing check for excess when buying a new ship
We need to add a call to `checkForExcess` in `setShipTypeID`. Without
this, the excess hardware/weapons/cargo are not removed when buying a
new ship.

This was broken by c2cffc5294, where `checkForExcess` was removed from
`regenerateBaseShip`, which is called in both `setShipTypeID` and the
ctor. However, the excess check must only be done in `setShipTypeID`,
since hardware/weapons/cargo aren't even loaded in the ctor.

src/lib/Default/AbstractSmrShip.class.php
@@ -526,9 +526,10 @@ class AbstractSmrShip {
 		$oldSpeed = $this->getSpeed();
 		$this->getPlayer()->setShipTypeID($shipTypeID);
 		$this->regenerateBaseShip();
-		$newSpeed = $this->getSpeed();
+		$this->checkForExcess();
 
 		// Update the player's turns to account for the speed change
+		$newSpeed = $this->getSpeed();
 		$oldTurns = $this->getPlayer()->getTurns();
 		$this->getPlayer()->setTurns(IRound($oldTurns * $newSpeed / $oldSpeed));
 	}