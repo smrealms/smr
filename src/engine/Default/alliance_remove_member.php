f0c5528347bd399b19fea4432143db0311c6499c
Dan Hemberger <daniel.hemberger@gmail.com>
2021-06-24 00:30:34 -0700

    alliance_remove_member.php: replace db calls
    Use SmrAlliance and SmrPlayer methods to replace the db calls.

--- src/engine/Default/alliance_remove_member.php
+++ src/engine/Default/alliance_remove_member.php
@@ -9,33 +9,26 @@ $alliance = $player->getAlliance();
 $template->assign('PageTopic', $alliance->getAllianceDisplayName(false, true));
 Menu::alliance($alliance->getAllianceID());
 
-$db = Smr\Database::getInstance();
-$db->query('
-SELECT
-account_id,
-player_id,
-player_name,
-last_cpl_action
-FROM player
-WHERE game_id = ' . $db->escapeNumber($alliance->getGameID()) . '
-AND alliance_id = ' . $db->escapeNumber($alliance->getAllianceID()) . '
-AND account_id != ' . $db->escapeNumber($player->getAccountID()) . '
-ORDER BY last_cpl_action DESC
-');
-
 $container = Page::create('alliance_remove_member_processing.php');
 $template->assign('BanishHREF', $container->href());
 
 $members = [];
-while ($db->nextRecord()) {
+foreach ($alliance->getMembers() as $alliancePlayer) {
+	// You can't remove yourself from the alliance
+	if ($alliancePlayer->equals($player)) {
+		continue;
+	}
 	// get the amount of time since last_active
-	$diff = 864000 + max(-864000, $db->getInt('last_cpl_action') - Smr\Epoch::time());
-	$lastActive = get_colored_text_range($diff, 864000, date($account->getDateTimeFormat(), $db->getInt('last_cpl_action')));
+	$lastActive = $alliancePlayer->getLastCPLAction();
+	$diff = 864000 + max(-864000, $lastActive - Smr\Epoch::time());
+	$lastActiveDate = get_colored_text_range($diff, 864000, date($account->getDateTimeFormat(), $lastActive));
 
 	$members[] = [
-		'last_active' => $lastActive,
-		'display_name' => htmlentities($db->getField('player_name')) . ' (' . $db->getInt('player_id') . ')',
-		'account_id' => $db->getInt('account_id'),
+		'sort_order' => $lastActive,
+		'last_active' => $lastActiveDate,
+		'display_name' => $alliancePlayer->getDisplayName(),
+		'account_id' => $alliancePlayer->getAccountID(),
 	];
 }
+Sorter::sortByNumElement($members, 'sort_order', true);
 $template->assign('Members', $members);
