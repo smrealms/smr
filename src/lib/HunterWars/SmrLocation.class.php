76535aeaa4692bf12cc3c316ec5f634519606c68
Dan Hemberger <daniel.hemberger@gmail.com>
2021-06-26 20:57:26 -0700

    SmrLocation: avoid db calls in HunterWars child
    To avoid (mostly) redundant database calls, we call the parent
    implementation of this method first, and then remove ships from the
    list that don't belong in a Hunter Wars game.
    
    The removal of the database call if worth the trivial extra expense
    of constructing a few extra `SmrShipType` objects (since we don't
    incur any additional database queries).

--- src/lib/HunterWars/SmrLocation.class.php
+++ src/lib/HunterWars/SmrLocation.class.php
@@ -4,15 +4,14 @@ class SmrLocation extends AbstractSmrLocation {
 
 	public function getShipsSold() : array {
 		if (!isset($this->shipsSold)) {
-			$this->shipsSold = array();
-			$this->db->query('SELECT * FROM location_sells_ships
-			                  JOIN ship_type USING (ship_type_id)
-			                  WHERE ' . $this->SQL . '
-			                    AND ship_class_id != ' . $this->db->escapeNumber(Smr\ShipClass::RAIDER) . '
-			                    AND ship_type_id != ' . $this->db->escapeNumber(SHIP_TYPE_PLANETARY_SUPER_FREIGHTER));
-			while ($this->db->nextRecord()) {
-				$shipTypeID = $this->db->getInt('ship_type_id');
-				$this->shipsSold[$shipTypeID] = SmrShipType::get($shipTypeID, $this->db);
+			// Generate the full ship list from the base class
+			parent::getShipsSold();
+			// Remove ships that are not allowed in Hunter Wars
+			unset($this->shipsSold[SHIP_TYPE_PLANETARY_SUPER_FREIGHTER]);
+			foreach ($this->shipsSold as $shipID => $ship) {
+				if ($ship->getClassID() === Smr\ShipClass::RAIDER) {
+					unset($this->shipsSold[$shipID]);
+				}
 			}
 		}
 		return $this->shipsSold;
