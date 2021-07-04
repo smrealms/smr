smrealms/smr

commit 7054e3d9c18f4cf904d542b9cb68c19ce3b08840

Refactor Database
Create separate classes for storing the database result and each
record of the result (DatabaseResult and DatabaseRecord respectively).
This simplifies the Database class, which now only holds the `dbConn`
property.

* Split Database::query into `write` and `read` methods, to clearly
  delineate between the different return types of mysqli::query.
  The `write` method is for queries that return "true" on success.
  The `read` method is for queries that return a `mysqli_result`
  object, from which we get our records.

* Interface changes:
    - Database::nextRecord -> DatabaseResult::records
    - Database::requireRecord -> DatabaseResult::record
    - Database::getNumRows -> DatabaseResult::getNumRecords
    - Added DatabaseResult::hasRecord

* When looping over SELECT records, we return them with a generator
  (`DatabaseResult::records`), which allows us to use a foreach loop,
  instead of a while loop.

* In queries where we only check if a record exists, use `SELECT 1` to
  optimize the query (instead of selecting columns that we won't use).

* Note that because we use `getInstance` properly now (i.e. there
  really only is one `Database` that is ever retrieved from the DI
  Container), once we store the result from a query in a DatabaseResult,
  we can immediately reuse the Database for additional queries.
  (That is, we can remove `$db2`, `$db3`, etc.)

src/tools/irc/server.php
@@ -39,13 +39,11 @@ function server_msg_307($fp, $rdata)
 		echo_r('[SERVER_307] ' . $server . ' said that ' . $nick . ' is registered');
 
 		$db = Smr\Database::getInstance();
-		$db2 = Smr\Database::getInstance();
+		$dbResult = $db->read('SELECT * FROM irc_seen WHERE nick = ' . $db->escapeString($nick));
+		foreach ($dbResult->record() as $dbRecord) {
+			$seen_id = $dbRecord->getInt('seen_id');
 
-		$db->query('SELECT * FROM irc_seen WHERE nick = ' . $db->escapeString($nick));
-		while ($db->nextRecord()) {
-			$seen_id = $db->getInt('seen_id');
-
-			$db2->query('UPDATE irc_seen SET ' .
+			$db->write('UPDATE irc_seen SET ' .
 						'registered = 1 ' .
 						'WHERE seen_id = ' . $seen_id);
 		}
@@ -70,13 +68,12 @@ function server_msg_318($fp, $rdata)
 		echo_r('[SERVER_318] ' . $server . ' end of /WHOIS for ' . $nick);
 
 		$db = Smr\Database::getInstance();
-		$db2 = Smr\Database::getInstance();
 
-		$db->query('SELECT * FROM irc_seen WHERE nick = ' . $db->escapeString($nick) . ' AND registered IS NULL');
-		while ($db->nextRecord()) {
-			$seen_id = $db->getInt('seen_id');
+		$dbResult = $db->read('SELECT * FROM irc_seen WHERE nick = ' . $db->escapeString($nick) . ' AND registered IS NULL');
+		foreach ($dbResult->records() as $dbRecord) {
+			$seen_id = $dbRecord->getInt('seen_id');
 
-			$db2->query('UPDATE irc_seen SET ' .
+			$db->write('UPDATE irc_seen SET ' .
 						'registered = 0 ' .
 						'WHERE seen_id = ' . $seen_id);
 		}
@@ -93,8 +90,8 @@ function server_msg_318($fp, $rdata)
 				unset($actions[$key]);
 
 				// so we should do a callback but need to check first if the guy has registered
-				$db->query('SELECT * FROM irc_seen WHERE nick = ' . $db->escapeString($nick) . ' AND registered = 1 AND channel = ' . $db->escapeString($action[1]));
-				if ($db->nextRecord()) {
+				$dbResult = $db->read('SELECT 1 FROM irc_seen WHERE nick = ' . $db->escapeString($nick) . ' AND registered = 1 AND channel = ' . $db->escapeString($action[1]));
+				if ($dbResult->hasRecord()) {
 					//Forward to a NICKSERV INFO call.
 					$action[0] = 'NICKSERV_INFO';
 					$action[4] = time();
@@ -133,13 +130,13 @@ function server_msg_352($fp, $rdata)
 		$db = Smr\Database::getInstance();
 
 		// check if we have seen this user before
-		$db->query('SELECT * FROM irc_seen WHERE nick = ' . $db->escapeString($nick) . ' AND channel = ' . $db->escapeString($channel));
+		$dbResult = $db->read('SELECT * FROM irc_seen WHERE nick = ' . $db->escapeString($nick) . ' AND channel = ' . $db->escapeString($channel));
 
-		if ($db->nextRecord()) {
+		if ($dbResult->hasRecord()) {
 			// exiting nick?
-			$seen_id = $db->getInt('seen_id');
+			$seen_id = $dbResult->record()->getInt('seen_id');
 
-			$db->query('UPDATE irc_seen SET ' .
+			$db->write('UPDATE irc_seen SET ' .
 					   'signed_on = ' . time() . ', ' .
 					   'signed_off = 0, ' .
 					   'user = ' . $db->escapeString($user) . ', ' .
@@ -149,7 +146,7 @@ function server_msg_352($fp, $rdata)
 
 		} else {
 			// new nick?
-			$db->query('INSERT INTO irc_seen (nick, user, host, channel, signed_on) ' .
+			$db->write('INSERT INTO irc_seen (nick, user, host, channel, signed_on) ' .
 					   'VALUES(' . $db->escapeString($nick) . ', ' . $db->escapeString($user) . ', ' . $db->escapeString($host) . ', ' . $db->escapeString($channel) . ', ' . time() . ')');
 		}
 
@@ -176,12 +173,12 @@ function server_msg_401($fp, $rdata)
 		$db = Smr\Database::getInstance();
 
 		// get the user in question
-		$db->query('SELECT * FROM irc_seen WHERE nick = ' . $db->escapeString($nick) . ' AND signed_off = 0');
-		if ($db->nextRecord()) {
-			$seen_id = $db->getInt('seen_id');
+		$dbResult = $db->read('SELECT * FROM irc_seen WHERE nick = ' . $db->escapeString($nick) . ' AND signed_off = 0');
+		if ($dbResult->hasRecord()) {
+			$seen_id = $dbResult->record()->getInt('seen_id');
 
 			// maybe he left without us noticing, so we fix this now
-			$db->query('UPDATE irc_seen SET ' .
+			$db->write('UPDATE irc_seen SET ' .
 					   'signed_off = ' . time() . ', ' .
 					   'WHERE seen_id = ' . $seen_id);
 

commit 2c79fa6baf31c46d5c8c1f1eeedb0c39e4e1e0b4

Fix error with database refactor
Used the `DatabaseResult::record` method as an iterable in a foreach
loop instead of `DatabaseResult::records`.

This error was introduced in 7054e3d9c18.

src/tools/irc/server.php
@@ -40,7 +40,7 @@ function server_msg_307($fp, $rdata)
 
 		$db = Smr\Database::getInstance();
 		$dbResult = $db->read('SELECT * FROM irc_seen WHERE nick = ' . $db->escapeString($nick));
-		foreach ($dbResult->record() as $dbRecord) {
+		foreach ($dbResult->records() as $dbRecord) {
 			$seen_id = $dbRecord->getInt('seen_id');
 
 			$db->write('UPDATE irc_seen SET ' .