smrealms/smr

commit b15b117add0792ee94761c88c41de2972c2552ca

SmrPort: remove getGood method
This method was complicated by the possible `false` return if the good
was not traded at the port. In all cases, the use of `getGood` could
be more robustly replaced with `Globals::getGood`, which is guaranteed
to return a good array.

src/lib/Default/AbstractSmrPort.class.php
@@ -282,24 +282,10 @@ class AbstractSmrPort {
 		return $this->goodIDs[TRADER_BUYS];
 	}
 
-	public function getGood(int $goodID) : array|false {
-		if (!$this->hasGood($goodID)) {
-			return false;
-		}
-		return Globals::getGood($goodID);
-	}
-
 	public function getGoodDistance(int $goodID) : int {
 		if (!isset($this->goodDistances[$goodID])) {
-			$x = $this->getGood($goodID);
-			if ($x === false) {
-				throw new Exception('This port does not have this good!');
-			}
-			if ($this->hasGood($goodID, TRADER_BUYS)) {
-				$x['TransactionType'] = TRADER_SELLS;
-			} else {
-				$x['TransactionType'] = TRADER_BUYS;
-			}
+			$x = Globals::getGood($goodID);
+			$x['TransactionType'] = $this->getGoodTransaction($goodID);
 			$di = Plotter::findDistanceToX($x, $this->getSector(), true);
 			if (is_object($di)) {
 				$di = $di->getRelativeDistance();
@@ -334,7 +320,7 @@ class AbstractSmrPort {
 			throw new Exception('Cannot update a cached port!');
 		}
 		// The new amount must be between 0 and the max for this good
-		$amount = max(0, min($amount, $this->getGood($goodID)['Max']));
+		$amount = max(0, min($amount, Globals::getGood($goodID)['Max']));
 		if ($this->getGoodAmount($goodID) == $amount) {
 			return;
 		}

src/engine/Default/port_loot_processing.php
@@ -5,24 +5,21 @@ $var = $session->getCurrentVar();
 $player = $session->getPlayer();
 $ship = $player->getShip();
 
-// get good name, id, ...
 $good_id = $var['GoodID'];
-$good_name = Globals::getGoodName($good_id);
 $amount = Smr\Request::getInt('amount');
 if ($amount <= 0) {
 	create_error('You must enter an amount > 0!');
 }
 
 $port = $player->getSectorPort();
-$good = $port->getGood($good_id);
 // check if there are enough left at port
-if ($good['Amount'] < $amount) {
-   create_error('There isnt that much to loot.');
+if ($port->getGoodAmount($good_id) < $amount) {
+   create_error('There isn\'t that much to loot.');
 }
 
 // check if we have enough room for the thing we are going to buy
-if ($good['TransactionType'] === TRADER_BUYS && $amount > $ship->getEmptyHolds()) {
-   create_error('Scanning your ships indicates that you do not have enough free cargo bays!');
+if ($amount > $ship->getEmptyHolds()) {
+   create_error('Scanning your ship indicates that you do not have enough free cargo bays!');
 }
 
 // do we have enough turns?
@@ -30,7 +27,7 @@ if ($player->getTurns() == 0) {
    create_error('You don\'t have enough turns to loot.');
 }
 
-$player->log(LOG_TYPE_TRADING, 'Player Loots ' . $amount . ' ' . $good_name);
+$player->log(LOG_TYPE_TRADING, 'Player Loots ' . $amount . ' ' . Globals::getGoodName($good_id));
 $ship->increaseCargo($good_id, $amount);
 $port->decreaseGoodAmount($good_id, $amount);
 

src/engine/Default/shop_goods_processing.php
@@ -44,7 +44,6 @@ if (!$port->hasGood($good_id)) {
 	create_error('I don\'t trade in that good.');
 }
 
-$portGood = $port->getGood($good_id);
 // check if there are enough left at port
 if ($port->getGoodAmount($good_id) < $amount) {
 	create_error('I\'m short of ' . $good_name . '. So I\'m not going to sell you ' . $amount . ' pcs.');
@@ -129,6 +128,7 @@ if ($transaction === TRADER_STEALS ||
 	// if offered equals ideal we get a problem (division by zero)
 	$gained_exp = IRound($port->calculateExperiencePercent($ideal_price, $bargain_price, $transaction) * $base_xp);
 
+	$portGood = Globals::getGood($good_id);
 	if ($transaction === TRADER_BUYS) {
 		$msg_transaction = 'bought';
 		$ship->increaseCargo($good_id, $amount);

src/engine/Default/shop_goods_trade.php
@@ -11,7 +11,7 @@ require_once(LIB . 'Default/shop_goods.inc.php');
 $port = $player->getSectorPort();
 // get values from request
 $good_id = $var['good_id'];
-$portGood = $port->getGood($good_id);
+$portGood = Globals::getGood($good_id);
 $transaction = $port->getGoodTransaction($good_id);
 
 // Has the player failed a bargain?

commit e770fa04c4279063d1c297c4f75124ce5821a9fd

Fix incorrect trade good distances
A bug was introduced in b15b117a that caused trade good distances to
be computed with the wrong transaction type. This is now fixed.

src/lib/Default/AbstractSmrPort.class.php
@@ -285,7 +285,11 @@ class AbstractSmrPort {
 	public function getGoodDistance(int $goodID) : int {
 		if (!isset($this->goodDistances[$goodID])) {
 			$x = Globals::getGood($goodID);
-			$x['TransactionType'] = $this->getGoodTransaction($goodID);
+			// Calculate distance to the opposite of the offered transaction
+			$x['TransactionType'] = match($this->getGoodTransaction($goodID)) {
+				TRADER_BUYS => TRADER_SELLS,
+				TRADER_SELLS => TRADER_BUYS,
+			};
 			$di = Plotter::findDistanceToX($x, $this->getSector(), true);
 			if (is_object($di)) {
 				$di = $di->getRelativeDistance();