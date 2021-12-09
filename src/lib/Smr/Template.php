smrealms/smr

commit 2b4257472a80ebf1e0ef858e118d254714890e67

Template: don't make unnecessary DOMDocuments
* The DOMXPath::query method can take a second "context" argument,
  which allows us to directly specify which node of the DOMDocument
  to search.

* To get the inner HTML of some element, we can simply concatenate
  the HTML from each of the child nodes.

While the `formatOutput` bug is fixed in PHP 7.3.0, it seems to no
longer be relevant (though I'm not exactly sure why; maybe since we
are no longer trimming whitespace in the original HTML, or because
the `preserveWhitespace` attribute of DOMDocument defaults to true,
and the help text says `formatOutput` is ignored in this case).

src/lib/Smr/Template.php
@@ -219,16 +219,12 @@ class Template {
 
 		$session = Session::getInstance();
 
-		// To get inner html, we need to construct a separate DOMDocument.
-		// See PHP Bug #76285.
-		$getInnerHTML = function(DOMNode $node) {
-			$dom = new DOMDocument();
-			$dom->formatOutput = false;
+		$getInnerHTML = function(DOMNode $node) : string {
+			$innerHTML = '';
 			foreach ($node->childNodes as $child) {
-				$dom->appendChild($dom->importNode($child, true));
+				$innerHTML .= $child->ownerDocument->saveHTML($child);
 			}
-			// Trim to remove trailing newlines
-			return trim(@$dom->saveHTML());
+			return $innerHTML;
 		};
 
 		// Helper function to canonicalize making an XML element,
@@ -262,11 +258,8 @@ class Template {
 				$doAjaxMiddle = false;
 			} else {
 				// Skip if middle_panel has ajax-enabled children.
-				$domMid = new DOMDocument();
-				$domMid->appendChild($domMid->importNode($mid, true));
-				$xpathMid = new DOMXPath($domMid);
 				foreach ($ajaxSelectors as $selector) {
-					if (count($xpathMid->query($selector)) > 0) {
+					if (count($xpath->query($selector, $mid)) > 0) {
 						$doAjaxMiddle = false;
 						break;
 					}

commit f56238efdb4d0d2e149cf75f3fad77b350016be1

Fix middle panel ajax updates
The middle panel was not ajax updating due to a Template bug that was
introduced in 2b4257472. Basically, we were using the `contextNode`
argument of `DOMXPath::query` to check for ajax-enabled content in the
middle panel, but even though the `contextNode` was the middle panel
element, we were using an XPath expression starting with "//", which:

> Selects nodes in the document from the current node that match the
> selection no matter where they are

As a result, the query was escaping the context node and finding ajax-
enabled elements _outside_ of the middle panel. The context node is
_only_ relevant for _relative_ XPath queries (i.e. it is not the same
as creating a new DOMDocument out of the context node).

The fix is simple: we convert the queries to be relative by prepending
a ".". This works in both the case where we do not pass a context node
(when we search for ajax-enabled elements in the entire document) and
the case where we do (when we only want ajax-enabled elements in the
middle panel).

Added a test case for the failing example where an element outside the
middle panel ajax-updates, to make sure it does not disable the middle
panel from also ajax-updating.

src/lib/Smr/Template.php
@@ -237,7 +237,11 @@ class Template {
 		$dom = new DOMDocument();
 		$dom->loadHTML($str);
 		$xpath = new DOMXPath($dom);
-		$ajaxSelectors = array('//span[@id]', '//*[contains(@class,"ajax")]');
+
+		// Use relative xpath selectors so that they can be reused when we
+		// pass the middle panel as the xpath query's context node.
+		$ajaxSelectors = array('.//span[@id]', './/*[contains(@class,"ajax")]');
+
 		foreach ($ajaxSelectors as $selector) {
 			$matchNodes = $xpath->query($selector);
 			foreach ($matchNodes as $node) {

test/SmrTest/lib/DefaultGame/TemplateTest.php
@@ -94,6 +94,8 @@ class TemplateTest extends \PHPUnit\Framework\TestCase {
 			['<div id="middle_panel">Foo</div>', '', true],
 			// Empty string
 			['', ''],
+			// Ajax-enabled elements both outside and inside middle panel
+			['<span id="foo">Test</span><div id="middle_panel">Foo</div>', '<foo>Test</foo><middle_panel>Foo</middle_panel>']
 		];
 	}
 