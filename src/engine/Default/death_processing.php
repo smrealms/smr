6101e6b0588b414b095153f3549158d0e608c40e
Dan Hemberger <daniel.hemberger@gmail.com>
2022-01-24 13:12:48 -0800

    death_processing.php: remove deletePlottedCourse
    This is already done in SmrPlayer::killPlayer, so there is no need to
    do it again when the player processes their death screen.

--- src/engine/Default/death_processing.php
+++ src/engine/Default/death_processing.php
@@ -4,7 +4,6 @@ $session = Smr\Session::getInstance();
 $player = $session->getPlayer();
 
 $player->setDead(false);
-$player->deletePlottedCourse();
 
 $player->log(LOG_TYPE_TRADER_COMBAT, 'Player sees death screen');
 Page::create('skeleton.php', 'death.php')->go();
