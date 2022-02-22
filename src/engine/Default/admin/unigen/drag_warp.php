71dde4d3415d5c440ae4324af46393e0c1d00d3e
Dan Hemberger <846186+hemberger@users.noreply.github.com>
2022-02-21 21:20:01 -0800

    Uni Gen: add drag & drop for warps (#1247)

---
+++ src/engine/Default/admin/unigen/drag_warp.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+$session = Smr\Session::getInstance();
+$var = $session->getCurrentVar();
+
+// Move a warp from one sector to another
+$targetSectorID = Smr\Request::getInt('TargetSectorID');
+$origSectorID = Smr\Request::getInt('OrigSectorID');
+
+$origSector = SmrSector::getSector($var['game_id'], $origSectorID);
+$warpSector = $origSector->getWarpSector();
+$targetSector = SmrSector::getSector($var['game_id'], $targetSectorID);
+
+// Skip if target sector already has a warp
+if (!$targetSector->hasWarp()) {
+	$origSector->removeWarp();
+	$targetSector->setWarp($warpSector);
+	SmrSector::saveSectors();
+}
+
+$container = Page::create('skeleton.php', 'admin/unigen/universe_create_sectors.php');
+$container->addVar('game_id');
+$container->addVar('gal_on');
+$container->go();
