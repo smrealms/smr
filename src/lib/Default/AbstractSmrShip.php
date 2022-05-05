bc8172daa0661fd9bf69688af47a4da78d27be6c
stupidnewbie <55667763+stupidnewbie@users.noreply.github.com>
2022-05-04 21:57:58 -0400

    and fix another typo

--- src/lib/Default/AbstractSmrShip.php
+++ src/lib/Default/AbstractSmrShip.php
@@ -373,7 +373,7 @@ class AbstractSmrShip {
 	 *  returns whether or not an ambush bonus is active
 	 */
 	public function getAmbush() : bool {
-		return $this->$ambush;
+		return $this->ambush;
 	}
 	
 	/**
