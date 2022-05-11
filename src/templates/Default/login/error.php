4ef3c706f7f0fb3ab7f38a129186e7c29d0164f4
Dan Hemberger <daniel.hemberger@gmail.com>
2022-05-11 00:13:25 -0700

    Use login skeleton for external error page
    The external error page (which users only get when they're not logged
    into the game) was one of the last pages to use the old big green box
    skeletons. We switch to the login skeleton for consistency.
    
    Also added a picture of an escape pod because it seemed appropriate.

---
+++ src/templates/Default/login/error.php
@@ -0,0 +1,15 @@
+<div class="centered" style="width: 510px;">
+	<h1><span class="red">ERROR</span></h1>
+
+	<p class="big bold"><?php echo htmlentities($ErrorMessage, ENT_NOQUOTES, 'utf-8'); ?></p>
+	<br />
+	<p><small>If the error was caused by something you entered, press back and try again.</small></p>
+	<p><small>If it was a game error, press back and try again, or logoff and log back on.</small></p>
+	<p><small>If the error was unrecognizable, please notify the administrators.
+		<a href="<?php echo WIKI_URL; ?>/contact-us">
+			<img src="images/silk/help.png" width="16" height="16" alt="Wiki Link" title="Contact Us"/>
+		</a>
+	</small></p>
+
+	<p><img src="images/escape_pod.jpg"></p>
+</div>
