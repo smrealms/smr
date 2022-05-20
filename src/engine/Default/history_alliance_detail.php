126046a9567550b33f2294a44772f00797693fd2
Dan Hemberger <daniel.hemberger@gmail.com>
2022-05-20 11:25:08 -0700

    Identify leader in history games alliance lists
    Put a "*" next to the leader in the alliance lists for history games,
    just like we do in modern alliance lists.

--- src/engine/Default/history_alliance_detail.php
+++ src/engine/Default/history_alliance_detail.php
@@ -17,16 +17,20 @@ $id = $var['alliance_id'];
 
 $db = Smr\Database::getInstance();
 $db->switchDatabases($var['HistoryDatabase']);
-$dbResult = $db->read('SELECT alliance_name FROM alliance WHERE alliance_id = ' . $db->escapeNumber($id) . ' AND game_id = ' . $db->escapeNumber($game_id));
-$template->assign('PageTopic', 'Alliance Roster - ' . htmlentities($dbResult->record()->getString('alliance_name')));
+$dbResult = $db->read('SELECT alliance_name, leader_id FROM alliance WHERE alliance_id = ' . $db->escapeNumber($id) . ' AND game_id = ' . $db->escapeNumber($game_id));
+$dbRecord = $dbResult->record();
+$leaderID = $dbRecord->getInt('leader_id');
+$template->assign('PageTopic', 'Alliance Roster - ' . htmlentities($dbRecord->getString('alliance_name')));
 
 //get alliance members
 $oldAccountID = $account->getOldAccountID($var['HistoryDatabase']);
 $dbResult = $db->read('SELECT * FROM player WHERE alliance_id = ' . $db->escapeNumber($id) . ' AND game_id = ' . $db->escapeNumber($game_id) . ' ORDER BY experience DESC');
 $players = [];
 foreach ($dbResult->records() as $dbRecord) {
+	$memberAccountID = $dbRecord->getInt('account_id');
 	$players[] = [
-		'bold' => $dbRecord->getInt('account_id') == $oldAccountID ? 'class="bold"' : '',
+		'leader' => $memberAccountID == $leaderID ? '*' : '',
+		'bold' => $memberAccountID == $oldAccountID ? 'class="bold"' : '',
 		'player_name' => $dbRecord->getString('player_name'),
 		'experience' => number_format($dbRecord->getInt('experience')),
 		'alignment' => number_format($dbRecord->getInt('alignment')),
--- src/templates/Default/engine/Default/history_alliance_detail.php
+++ src/templates/Default/engine/Default/history_alliance_detail.php
@@ -3,6 +3,7 @@
 
 	<table class="center standard">
 		<tr>
+			<th></th>
 			<th>Player Name</th>
 			<th>Experience</th>
 			<th>Alignment</th>
@@ -11,8 +12,9 @@
 			<th>Deaths</th>
 			<th>Bounty</th>
 		</tr><?php
-		foreach ($Players as $player) { ?>
+		foreach ($Players as $i => $player) { ?>
 			<tr <?php echo $player['bold']; ?>>
+				<td><?php echo ($i + 1) . $player['leader']; ?></td>
 				<td><?php echo htmlentities($player['player_name']); ?></td>
 				<td><?php echo $player['experience']; ?></td>
 				<td><?php echo $player['alignment']; ?></td>
