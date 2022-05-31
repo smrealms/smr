a266ef793e54e8b9549714b0423122e164d50601
Dan Hemberger <daniel.hemberger@gmail.com>
2022-05-31 02:06:09 -0700

    alliance_roles_processing.php: fix phpstan warning
    > Variable $role_id might not be defined.
    
    The result will never be null because alliances are auto-bestowed some
    roles on creation.

--- src/engine/Default/alliance_roles_processing.php
+++ src/engine/Default/alliance_roles_processing.php
@@ -42,14 +42,12 @@ if (!isset($var['role_id'])) {
 
 	$db->lockTable('alliance_has_roles');
 
-	// get last id
+	// get last id (always has one, since some roles are auto-bestowed)
 	$dbResult = $db->read('SELECT MAX(role_id)
 				FROM alliance_has_roles
 				WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . '
 					AND alliance_id = ' . $db->escapeNumber($alliance_id));
-	if ($dbResult->hasRecord()) {
-		$role_id = $dbResult->record()->getInt('MAX(role_id)') + 1;
-	}
+	$role_id = $dbResult->record()->getInt('MAX(role_id)') + 1;
 
 	$db->insert('alliance_has_roles', [
 		'alliance_id' => $db->escapeNumber($alliance_id),
