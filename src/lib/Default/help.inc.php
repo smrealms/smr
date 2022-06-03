7b1ec68b31980ab0dbb712c23995336b888d1a66
Dan Hemberger <daniel.hemberger@gmail.com>
2022-06-03 16:25:30 -0700

    Fix manual navigation links
    The "Next" navigation links weren't being constructed properly.
    Now they properly go to the next parent section at the end of a
    subsection.
    
    Also fix a few phpstan warnings:
    
    > Variable $previous_topic might not be defined.
    > Variable $up_topic might not be defined.
    > Variable $next_topic might not be defined.

--- src/lib/Default/help.inc.php
+++ src/lib/Default/help.inc.php
@@ -6,105 +6,109 @@ function echo_nav($topic_id) {
 
 	// get current entry
 	$dbResult = $db->read('SELECT * FROM manual WHERE topic_id = ' . $db->escapeNumber($topic_id));
+	if (!$dbResult->hasRecord()) {
+		echo ('Invalid Topic!');
+		return;
+	}
+
+	$dbRecord = $dbResult->record();
+	$order_id = $dbRecord->getInt('order_id');
+	$topic = stripslashes($dbRecord->getString('topic'));
+
+	$parent_topic_id = $dbRecord->getInt('parent_topic_id');
+	$dbResult = $db->read('SELECT * FROM manual WHERE topic_id = ' . $db->escapeNumber($parent_topic_id));
 	if ($dbResult->hasRecord()) {
 		$dbRecord = $dbResult->record();
-		$parent_topic_id = $dbRecord->getInt('parent_topic_id');
-		$order_id = $dbRecord->getInt('order_id');
-		$topic = stripslashes($dbRecord->getString('topic'));
-
-		echo ('<table>');
-		echo ('<tr>');
-
-		// **************************
-		// **  PREVIOUS
-		// **************************
-		$dbResult = $db->read('SELECT * FROM manual WHERE parent_topic_id = ' . $db->escapeNumber($parent_topic_id) . ' AND order_id = ' . $db->escapeNumber($order_id - 1));
-
-		// no result?
-		if (!$dbResult->hasRecord()) {
-			$dbResult = $db->read('SELECT * FROM manual WHERE topic_id = ' . $db->escapeNumber($parent_topic_id));
-		}
-
-		echo ('<th width="32">');
-		if ($dbResult->hasRecord()) {
-			$dbRecord = $dbResult->record();
-			$previous_topic_id = $dbRecord->getInt('topic_id');
-			$previous_topic = stripslashes($dbRecord->getString('topic'));
-			echo ('<a href="/manual.php?' . $previous_topic_id . '"><img src="/images/help/previous.jpg" width="32" height="32" border="0"></a>');
-		} else {
-			echo ('<img src="/images/help/empty.jpg" width="32" height="32">');
-		}
-		echo ('</th>');
-
-		// **************************
-		// **  UP
-		// **************************
-		$dbResult = $db->read('SELECT * FROM manual WHERE topic_id = ' . $db->escapeNumber($parent_topic_id));
-		echo ('<th width="32">');
-		if ($dbResult->hasRecord()) {
-			$dbRecord = $dbResult->record();
-			$up_topic_id = $dbRecord->getInt('topic_id');
-			$up_topic = stripslashes($dbRecord->getString('topic'));
-			echo ('<a href="/manual.php?' . $up_topic_id . '"><img src="/images/help/up.jpg" width="32" height="32" border="0"></a>');
-		} else {
-			echo ('<img src="/images/help/empty.jpg" width="32" height="32">');
-		}
-		echo ('</th>');
-
-		// **************************
-		// **  NEXT
-		// **************************
-		$dbResult = $db->read('SELECT * FROM manual WHERE parent_topic_id = ' . $db->escapeNumber($topic_id) . ' AND order_id = 1');
-
-		if (!$dbResult->hasRecord()) {
-			$dbResult = $db->read('SELECT * FROM manual WHERE parent_topic_id = ' . $db->escapeNumber($parent_topic_id) . ' AND order_id = ' . $db->escapeNumber($order_id + 1));
-		}
-
-		$seenParentIDs = [0];
-		$curr_parent_topic_id = $parent_topic_id;
-		while (!$dbResult->hasRecord() && !in_array($curr_parent_topic_id, $seenParentIDs)) {
-			$seenParentIDs[] = $curr_parent_topic_id;
-			$dbResult2 = $db->read('SELECT * FROM manual WHERE topic_id = ' . $db->escapeNumber($parent_topic_id));
-			$dbRecord2 = $dbResult2->record();
-			$curr_order_id = $dbRecord2->getInt('order_id');
-			$curr_parent_topic_id = $dbRecord2->getInt('parent_topic_id');
-
-			$dbResult = $db->read('SELECT * FROM manual WHERE parent_topic_id = ' . $db->escapeNumber($parent_topic_id) . ' AND order_id = ' . $db->escapeNumber($curr_order_id + 1));
-		}
-
-		echo ('<th width="32">');
-		if ($dbResult->hasRecord()) {
-			$dbRecord = $dbResult->record();
-			$next_topic_id = $dbRecord->getInt('topic_id');
-			$next_topic = stripslashes($dbRecord->getString('topic'));
-			echo ('<a href="/manual.php?' . $next_topic_id . '"><img src="/images/help/next.jpg" width="32" height="32" border="0"></a>');
-		} else {
-			echo ('<img src="/images/help/empty.jpg" width="32" height="32">');
-		}
-		echo ('</th>');
-
-		echo ('<th width="100%" class="center" validn="middle" style="font-size:18pt;font-weight:bold;">' . get_numbering($topic_id) . $topic . '</th>');
-		echo ('<th width="32"><a href="/manual_toc.php"><img src="/images/help/contents.jpg" width="32" height="32" border="0"></a></th>');
-
-		echo ('</tr>');
-
-		echo ('<tr>');
-		echo ('<td colspan="5">');
-		if (isset($previous_topic_id) && $previous_topic_id > 0) {
-			echo ('<b>Previous:</b> <a href="/manual.php?' . $previous_topic_id . '">' . get_numbering($previous_topic_id) . $previous_topic . '</a>&nbsp;&nbsp;&nbsp;');
-		}
-		if (isset($up_topic_id) && $up_topic_id > 0) {
-			echo ('<b>Up:</b> <a href="/manual.php?' . $up_topic_id . '">' . get_numbering($up_topic_id) . $up_topic . '</a>&nbsp;&nbsp;&nbsp;');
-		}
-		if (isset($next_topic_id) && $next_topic_id > 0) {
-			echo ('<b>Next:</b> <a href="/manual.php?' . $next_topic_id . '">' . get_numbering($next_topic_id) . $next_topic . '</a>');
-		}
-		echo ('</tr>');
-
-		echo ('</table>');
-	} else {
-		echo ('Invalid Topic!');
+		$parent = [
+			'id' => $dbRecord->getInt('topic_id'),
+			'topic' => $dbRecord->getString('topic'),
+			'parent_topic_id' => $dbRecord->getInt('parent_topic_id'),
+			'order_id' => $dbRecord->getInt('order_id'),
+		];
 	}
+
+	echo ('<table>');
+	echo ('<tr>');
+
+	// **************************
+	// **  PREVIOUS
+	// **************************
+	$dbResult = $db->read('SELECT * FROM manual WHERE parent_topic_id = ' . $db->escapeNumber($parent_topic_id) . ' AND order_id = ' . $db->escapeNumber($order_id - 1));
+	echo ('<th width="32">');
+	if ($dbResult->hasRecord()) {
+		$dbRecord = $dbResult->record();
+		$previous = [
+			'id' => $dbRecord->getInt('topic_id'),
+			'topic' => stripslashes($dbRecord->getString('topic')),
+		];
+	} elseif (isset($parent)) {
+		$previous = $parent;
+	}
+
+	if (isset($previous)) {
+		echo ('<a href="/manual.php?' . $previous['id'] . '"><img src="/images/help/previous.jpg" width="32" height="32" border="0"></a>');
+	} else {
+		echo ('<img src="/images/help/empty.jpg" width="32" height="32">');
+	}
+	echo ('</th>');
+
+	// **************************
+	// **  UP
+	// **************************
+	echo ('<th width="32">');
+	if (isset($parent)) {
+		$up = $parent;
+		echo ('<a href="/manual.php?' . $up['id'] . '"><img src="/images/help/up.jpg" width="32" height="32" border="0"></a>');
+	} else {
+		echo ('<img src="/images/help/empty.jpg" width="32" height="32">');
+	}
+	echo ('</th>');
+
+	// **************************
+	// **  NEXT
+	// **************************
+	$dbResult = $db->read('SELECT * FROM manual WHERE parent_topic_id = ' . $db->escapeNumber($topic_id) . ' AND order_id = 1');
+
+	if (!$dbResult->hasRecord()) {
+		$dbResult = $db->read('SELECT * FROM manual WHERE parent_topic_id = ' . $db->escapeNumber($parent_topic_id) . ' AND order_id = ' . $db->escapeNumber($order_id + 1));
+	}
+
+	if (!$dbResult->hasRecord() && isset($parent)) {
+		$dbResult = $db->read('SELECT * FROM manual WHERE parent_topic_id = ' . $db->escapeNumber($parent['parent_topic_id']) . ' AND order_id = ' . $db->escapeNumber($parent['order_id'] + 1));
+	}
+
+	echo ('<th width="32">');
+	if ($dbResult->hasRecord()) {
+		$dbRecord = $dbResult->record();
+		$next = [
+			'id' => $dbRecord->getInt('topic_id'),
+			'topic' => stripslashes($dbRecord->getString('topic')),
+		];
+		echo ('<a href="/manual.php?' . $next['id'] . '"><img src="/images/help/next.jpg" width="32" height="32" border="0"></a>');
+	} else {
+		echo ('<img src="/images/help/empty.jpg" width="32" height="32">');
+	}
+	echo ('</th>');
+
+	echo ('<th width="100%" class="center" validn="middle" style="font-size:18pt;font-weight:bold;">' . get_numbering($topic_id) . $topic . '</th>');
+	echo ('<th width="32"><a href="/manual_toc.php"><img src="/images/help/contents.jpg" width="32" height="32" border="0"></a></th>');
+
+	echo ('</tr>');
+
+	echo ('<tr>');
+	echo ('<td colspan="5">');
+	if (isset($previous) && $previous['id'] > 0) {
+		echo ('<b>Previous:</b> <a href="/manual.php?' . $previous['id'] . '">' . get_numbering($previous['id']) . $previous['topic'] . '</a>&nbsp;&nbsp;&nbsp;');
+	}
+	if (isset($up) && $up['id'] > 0) {
+		echo ('<b>Up:</b> <a href="/manual.php?' . $up['id'] . '">' . get_numbering($up['id']) . $up['topic'] . '</a>&nbsp;&nbsp;&nbsp;');
+	}
+	if (isset($next) && $next['id'] > 0) {
+		echo ('<b>Next:</b> <a href="/manual.php?' . $next['id'] . '">' . get_numbering($next['id']) . $next['topic'] . '</a>');
+	}
+	echo ('</tr>');
+
+	echo ('</table>');
 }
 
 function echo_content($topic_id) {
