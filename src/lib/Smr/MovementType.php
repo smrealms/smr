3485d9a5babed695a631cfa9b5dd09dc0adcc545
Dan Hemberger <daniel.hemberger@gmail.com>
2022-06-12 00:55:22 -0700

    Convert MOVEMENT_* constants to an Enum
    This is a great use case for enums because we don't need to use it
    as an array key.

---
+++ src/lib/Smr/MovementType.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Smr;
+
+/**
+ * Enumerates the types of movements that players can take.
+ */
+enum MovementType {
+
+	case Walk;
+	case Jump;
+	case Warp;
+
+}
