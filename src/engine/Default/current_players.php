aa442a9ff965d68358dac798bf55b59b699db012
Dan Hemberger <daniel.hemberger@gmail.com>
2022-07-24 13:19:31 -0700

    current_players.php: use more sane variable names
    Rename variables so that it's more clear what they represent:
    
    * count_real_last_active -> count_active (i.e. w/ active session)
    
    * count_last_active -> count_moving (i.e. w/ recent CPL action)
    
    Also make some logical simplifications, such as there _always_ being
    a `count` record from a SQL query, and using `max` instead of an if-
    statement.

--- src/engine/Default/current_players.php
+++ src/engine/Default/current_players.php
@@ -9,43 +9,39 @@ $inactiveTime = Smr\Epoch::time() - TIME_BEFORE_INACTIVE;
 $template->assign('PageTopic', 'Current Players');
 $db = Smr\Database::getInstance();
 $db->write('DELETE FROM cpl_tag WHERE expires > 0 AND expires < ' . $db->escapeNumber(Smr\Epoch::time()));
+
 $dbResult = $db->read('SELECT count(*) count FROM active_session
 			WHERE last_accessed >= ' . $db->escapeNumber($inactiveTime) . ' AND
 				game_id = ' . $db->escapeNumber($player->getGameID()));
-$count_real_last_active = 0;
-if ($dbResult->hasRecord()) {
-	$count_real_last_active = $dbResult->record()->getInt('count');
-}
+$count_active = $dbResult->record()->getInt('count');
 
 $dbResult = $db->read('SELECT * FROM player
 		WHERE last_cpl_action >= ' . $db->escapeNumber($inactiveTime) . '
 			AND game_id = ' . $db->escapeNumber($player->getGameID()) . '
 		ORDER BY experience DESC, player_name DESC');
-$count_last_active = $dbResult->getNumRecords();
+$count_moving = $dbResult->getNumRecords();
 
 // fix it if some1 is using the logoff button
-if ($count_real_last_active < $count_last_active) {
-	$count_real_last_active = $count_last_active;
-}
+$count_active = max($count_active, $count_moving);
 
 // Get the summary text
 $summary = 'There ';
-if ($count_real_last_active != 1) {
-	$summary .= 'are ' . $count_real_last_active . ' players who have ';
+if ($count_active != 1) {
+	$summary .= 'are ' . $count_active . ' players who have ';
 } else {
 	$summary .= 'is 1 player who has ';
 }
 $summary .= 'accessed the server in the last ' . format_time(TIME_BEFORE_INACTIVE) . '.<br />';
 
-if ($count_last_active == 0) {
+if ($count_moving == 0) {
 	$summary .= 'No one was moving so your ship computer can\'t intercept any transmissions.';
 } else {
-	if ($count_last_active == $count_real_last_active) {
+	if ($count_moving == $count_active) {
 		$summary .= 'All ';
 	} else {
 		$summary .= 'A few ';
 	}
-	$summary .= 'of them were moving so your ship computer was able to intercept ' . pluralise($count_last_active, 'transmission') . '.';
+	$summary .= 'of them were moving so your ship computer was able to intercept ' . pluralise($count_moving, 'transmission') . '.';
 }
 
 $summary .= '<br />The traders listed in <span class="italic">italics</span> are still ranked as Newbie or Beginner.';
