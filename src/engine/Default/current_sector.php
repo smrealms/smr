smrealms/smr

commit 17c0bea3e60ecd546c5f867cf2fc2c6c322ac069

Remove useless LIMIT 1 in SQL queries
We were including `LIMIT 1` in many SQL queries that can only return
(and are _designed_ to only return) one record. In my opinion, using
`LIMIT 1` in this scenario is an anti-pattern, because it applies an
artificial constraint on the query.

There are some arguments for including it: specious ones regarding
performance (it is not faster), and ones that recommend it in the
name of safety (though I think this is approaching safety from the
wrong direction). Instead, I think `LIMIT 1` _at best_ will hide the
fact that you have an improper query if it is supposed to return one
record but would have returned more (had it not been for `LIMIT 1`).
The one query you do get will be silently erroneous or misleading.

src/engine/Default/current_sector.php
@@ -174,7 +174,7 @@ function checkForForceRefreshMessage(string &$msg): void {
 			$player = Smr\Session::getInstance()->getPlayer();
 
 			$forceRefreshMessage = '';
-			$dbResult = $db->read('SELECT refresh_at FROM sector_has_forces WHERE refresh_at >= ' . $db->escapeNumber(Smr\Epoch::time()) . ' AND sector_id = ' . $db->escapeNumber($player->getSectorID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND refresher = ' . $db->escapeNumber($player->getAccountID()) . ' ORDER BY refresh_at DESC LIMIT 1');
+			$dbResult = $db->read('SELECT refresh_at FROM sector_has_forces WHERE refresh_at >= ' . $db->escapeNumber(Smr\Epoch::time()) . ' AND sector_id = ' . $db->escapeNumber($player->getSectorID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND refresher = ' . $db->escapeNumber($player->getAccountID()));
 			if ($dbResult->hasRecord()) {
 				$remainingTime = $dbResult->record()->getInt('refresh_at') - Smr\Epoch::time();
 				$forceRefreshMessage = '<span class="green">REFRESH</span>: All forces will be refreshed in ' . $remainingTime . ' seconds.';

src/engine/Default/admin/comp_share.php
@@ -38,7 +38,7 @@ foreach ($dbResult->records() as $dbRecord) {
 	}
 
 	if ($rows > 1) {
-		$dbResult2 = $db->read('SELECT login FROM account WHERE account_id =' . $db->escapeNumber($currTabAccId) . ($skipUnusedAccs ? ' AND last_login > ' . $db->escapeNumber(Smr\Epoch::time() - 86400 * 30) : '') . ' LIMIT 1');
+		$dbResult2 = $db->read('SELECT login FROM account WHERE account_id =' . $db->escapeNumber($currTabAccId) . ($skipUnusedAccs ? ' AND last_login > ' . $db->escapeNumber(Smr\Epoch::time() - 86400 * 30) : ''));
 		if (!$dbResult2->hasRecord()) {
 			continue;
 		}

src/engine/Default/admin/unigen/game_create_processing.php
@@ -3,7 +3,7 @@
 $db = Smr\Database::getInstance();
 
 //first create the game
-$dbResult = $db->read('SELECT 1 FROM game WHERE game_name=' . $db->escapeString(Smr\Request::get('game_name')) . ' LIMIT 1');
+$dbResult = $db->read('SELECT 1 FROM game WHERE game_name=' . $db->escapeString(Smr\Request::get('game_name')));
 if ($dbResult->hasRecord()) {
 	create_error('That game name is already taken.');
 }

src/engine/Default/album_delete_processing.php
@@ -7,7 +7,7 @@ if (Smr\Request::get('action') == 'Yes') {
 	$db = Smr\Database::getInstance();
 	$db->write('DELETE
 				FROM album
-				WHERE account_id = ' . $db->escapeNumber($account->getAccountID()) . ' LIMIT 1');
+				WHERE account_id = ' . $db->escapeNumber($account->getAccountID()));
 
 	$db->write('DELETE
 				FROM album_has_comments

src/engine/Default/album_edit_processing.php
@@ -79,7 +79,7 @@ if ($dbResult->hasRecord()) {
 					last_changed = ' . $db->escapeNumber(Smr\Epoch::time()) . ',
 					approved = \'TBC\',
 					disabled = \'FALSE\'
-				WHERE account_id = ' . $db->escapeNumber($account->getAccountID()) . ' LIMIT 1');
+				WHERE account_id = ' . $db->escapeNumber($account->getAccountID()));
 } else {
 	// if he didn't upload a picture before
 	// we kick him out here

src/engine/Default/alliance_message_delete_processing.php
@@ -12,7 +12,7 @@ if (isset($var['reply_id'])) {
 				WHERE game_id = ' . $db->escapeNumber($player->getGameID()) . '
 				AND alliance_id = ' . $db->escapeNumber($alliance_id) . '
 				AND thread_id = ' . $db->escapeNumber($var['thread_id']) . '
-				AND reply_id = ' . $db->escapeNumber($var['reply_id']) . ' LIMIT 1');
+				AND reply_id = ' . $db->escapeNumber($var['reply_id']));
 	Page::create('skeleton.php', 'alliance_message_view.php', $var)->go();
 } else {
 	$db->write('DELETE FROM alliance_thread

src/engine/Default/alliance_mod.php
@@ -18,12 +18,12 @@ Menu::alliance($alliance->getAllianceID());
 // Check to see if an alliance op is scheduled
 // Display it for 1 hour past start time (late arrivals, etc.)
 $db = Smr\Database::getInstance();
-$dbResult = $db->read('SELECT time FROM alliance_has_op WHERE alliance_id=' . $db->escapeNumber($player->getAllianceID()) . ' AND game_id=' . $db->escapeNumber($player->getGameID()) . ' AND time > ' . $db->escapeNumber(Smr\Epoch::time() - 3600) . ' LIMIT 1');
+$dbResult = $db->read('SELECT time FROM alliance_has_op WHERE alliance_id=' . $db->escapeNumber($player->getAllianceID()) . ' AND game_id=' . $db->escapeNumber($player->getGameID()) . ' AND time > ' . $db->escapeNumber(Smr\Epoch::time() - 3600));
 if ($dbResult->hasRecord()) {
 	$template->assign('OpTime', $dbResult->record()->getInt('time'));
 
 	// Has player responded yet?
-	$dbResult2 = $db->read('SELECT response FROM alliance_has_op_response WHERE alliance_id=' . $db->escapeNumber($player->getAllianceID()) . ' AND ' . $player->getSQL() . ' LIMIT 1');
+	$dbResult2 = $db->read('SELECT response FROM alliance_has_op_response WHERE alliance_id=' . $db->escapeNumber($player->getAllianceID()) . ' AND ' . $player->getSQL());
 
 	$response = $dbResult2->hasRecord() ? $dbResult2->record()->getField('response') : null;
 	$responseHREF = Page::create('alliance_op_response_processing.php')->href();

src/engine/Default/bank_anon_detail.php
@@ -14,7 +14,7 @@ $db = Smr\Database::getInstance();
 $dbResult = $db->read('SELECT *
 			FROM anon_bank
 			WHERE anon_id=' . $db->escapeNumber($account_num) . '
-			AND game_id=' . $db->escapeNumber($player->getGameID()) . ' LIMIT 1');
+			AND game_id=' . $db->escapeNumber($player->getGameID()));
 
 if (!$dbResult->hasRecord()) {
 	create_error('This anonymous account does not exist!');

commit ea571ddf81714da34c058a4506ee42c932f8c588

current_sector.php: fix refresh all forces message
When there are multiple force stacks in a sector, using "Refresh All"
results in an error due to the `sector_has_forces` query returning more
than one record.

This bug was introduced in 17c0bea3e60, where we removed an important
"LIMIT 1" clause. The fix is to simply add that clause back.

src/engine/Default/current_sector.php
@@ -169,7 +169,7 @@ function checkForForceRefreshMessage(string &$msg): void {
 			$player = Smr\Session::getInstance()->getPlayer();
 
 			$forceRefreshMessage = '';
-			$dbResult = $db->read('SELECT refresh_at FROM sector_has_forces WHERE refresh_at >= ' . $db->escapeNumber(Smr\Epoch::time()) . ' AND sector_id = ' . $db->escapeNumber($player->getSectorID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND refresher = ' . $db->escapeNumber($player->getAccountID()));
+			$dbResult = $db->read('SELECT refresh_at FROM sector_has_forces WHERE refresh_at >= ' . $db->escapeNumber(Smr\Epoch::time()) . ' AND sector_id = ' . $db->escapeNumber($player->getSectorID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()) . ' AND refresher = ' . $db->escapeNumber($player->getAccountID()) . ' ORDER BY refresh_at DESC LIMIT 1');
 			if ($dbResult->hasRecord()) {
 				$remainingTime = $dbResult->record()->getInt('refresh_at') - Smr\Epoch::time();
 				$forceRefreshMessage = '<span class="green">REFRESH</span>: All forces will be refreshed in ' . $remainingTime . ' seconds.';