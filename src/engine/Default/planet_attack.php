3d51d8e63ecf088aa9e4f42bde4c293cf39368f4
Dan Hemberger <daniel.hemberger@gmail.com>
2022-08-19 19:05:18 -0700

    planet_attack.php: remove unused AlreadyDestroyed
    When attacking a planet, there is never a screen where you see that
    the planet is already destroyed. Instead, if it is destroyed, you
    simply get the option to land instead of attack.
    
    This must have been logic copied over from port_attack.php.

--- src/engine/Default/planet_attack.php
+++ src/engine/Default/planet_attack.php
@@ -5,12 +5,7 @@ $session = Smr\Session::getInstance();
 $var = $session->getCurrentVar();
 $player = $session->getPlayer();
 
-if (isset($var['results'])) {
-	$template->assign('FullPlanetCombatResults', $var['results']);
-	$template->assign('AlreadyDestroyed', false);
-} else {
-	$template->assign('AlreadyDestroyed', true);
-}
+$template->assign('FullPlanetCombatResults', $var['results']);
 $template->assign('MinimalDisplay', false);
 $template->assign('OverrideDeath', $player->isDead());
 $template->assign('Planet', SmrPlanet::getPlanet($player->getGameID(), $var['sector_id']));
--- src/templates/Default/engine/Default/includes/PlanetFullCombatResults.inc.php
+++ src/templates/Default/engine/Default/includes/PlanetFullCombatResults.inc.php
@@ -1,21 +1,14 @@
 <?php
-if (!$AlreadyDestroyed) {
-	if (!$MinimalDisplay) { ?>
-		<h1>Attacker Results</h1><br /><?php
-	}
-	$this->includeTemplate('includes/PlanetTraderTeamCombatResults.inc.php', ['TraderTeamCombatResults' => $FullPlanetCombatResults['Attackers'], 'MinimalDisplay' => $MinimalDisplay]);
-} elseif (!$MinimalDisplay) {
-	?><span class="bold">The planet is already destroyed.</span><?php
+if (!$MinimalDisplay) { ?>
+	<h1>Attacker Results</h1><br /><?php
 }
+$this->includeTemplate('includes/PlanetTraderTeamCombatResults.inc.php', ['TraderTeamCombatResults' => $FullPlanetCombatResults['Attackers'], 'MinimalDisplay' => $MinimalDisplay]);
 ?><br /><?php
 if (!$MinimalDisplay) { ?>
 	<br />
-	<img src="images/planetAttack.jpg" width="480" height="330" alt="Planet Attack" title="Planet Attack"><br /><?php
+	<img src="images/planetAttack.jpg" width="480" height="330" alt="Planet Attack" title="Planet Attack"><br />
+	<br />
+	<h1>Planet Results</h1><br /><?php
 }
-if (!$AlreadyDestroyed) {
-	if (!$MinimalDisplay) { ?>
-		<br />
-		<h1>Planet Results</h1><br /><?php
-	}
-	$this->includeTemplate('includes/PlanetCombatResults.inc.php', ['PlanetCombatResults' => $FullPlanetCombatResults['Planet'], 'MinimalDisplay' => $MinimalDisplay]);
-} ?>
+$this->includeTemplate('includes/PlanetCombatResults.inc.php', ['PlanetCombatResults' => $FullPlanetCombatResults['Planet'], 'MinimalDisplay' => $MinimalDisplay]);
+?>
