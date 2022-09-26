5933e5ba07088c16087876c9a7ddd29d604bf22b
Dan Hemberger <846186+hemberger@users.noreply.github.com>
2022-09-25 23:42:57 -0700

    Switch to docker compose plugin (#1436)
    The docker compose plugin is a native docker subcommand rather than
    a separate program written in Python.
    
    Use docker compose v2+ and the "Compose Specification" format of the
    configuration file instead of the legacy "Version 3" format. In this
    new format, the schema "version" element is deprecated.
    
    * For services that specify both a `build` and `image` elements (i.e.
      all the services that use the `local/smr` image), we were getting an
      error during `docker compose pull` (because the image only exists
      locally). Specifying `pull_policy: build` resulted in rebuilding the
      image whenever the service was _run_, so we settled on never pulling
      the image (`pull_policy: never`).
    
    * Even though the flyway service `command` worked properly with
      docker-compose 1.29, in v2.10.2 it was not being parsed correctly,
      and just returned the flyway help text (maybe something about the
      leading dash?). It works fine in the argument list style, however,
      so we switch to that. Docker-compose is _supposed_ to convert string
      commands to array syntax internally, so this looks like a bug in the
      new Docker-compose version.
    
    * Remove COMPOSE_DOCKER_CLI_BUILD environment variable. This is not
      needed in Docker Compose v2 (see
      https://docs.docker.com/compose/reference/envvars/#compose_docker_cli_buildx).

--- db/init_admin.sh
+++ db/init_admin.sh
@@ -5,4 +5,4 @@
 
 # Initialize the first account as an admin.
 # Will validate and grant "Manage Admins Permissions" permission.
-docker-compose exec -T mysql sh -c 'mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "use smr_live; REPLACE INTO account_has_permission (account_id, permission_id) VALUES (1, 1); UPDATE account SET validated=\"TRUE\" WHERE account_id=1;"'
+docker compose exec -T mysql sh -c 'mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "use smr_live; REPLACE INTO account_has_permission (account_id, permission_id) VALUES (1, 1); UPDATE account SET validated=\"TRUE\" WHERE account_id=1;"'
