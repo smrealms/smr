smrealms/smr

commit 31efc1bcee8f0473e911c76992f40883a4e7b4b7

shop_hardware_processing.php: add var typehint
PHPStan has no way of knowing the type of an object that we get from
the `$var` (at least until we refactor engine files to take vars as
parameters), so we need to explicitly typehint it.

Note that for local variables, this `@var` docblock typehint overrides
any other information PHPStan has about the variable.

src/engine/Default/shop_goods_processing.php
@@ -71,14 +71,18 @@ if ($transaction === TransactionType::Buy && $player->getCredits() < $bargain_pr
 $relations = $player->getRelation($port->getRaceID());
 
 if (!isset($var['ideal_price'])) {
-	$var['ideal_price'] = $port->getIdealPrice($good_id, $transaction, $amount, $relations);
+	$ideal_price = $port->getIdealPrice($good_id, $transaction, $amount, $relations);
+} else {
+	/** @var int $ideal_price */
+	$ideal_price = $var['ideal_price'];
 }
-$ideal_price = $var['ideal_price'];
 
 if (!isset($var['offered_price'])) {
-	$var['offered_price'] = $port->getOfferPrice($ideal_price, $relations, $transaction);
+	$offered_price = $port->getOfferPrice($ideal_price, $relations, $transaction);
+} else {
+	/** @var int $offered_price */
+	$offered_price = $var['offered_price'];
 }
-$offered_price = $var['offered_price'];
 
 // nothing should happen here but just to avoid / by 0
 if ($ideal_price == 0 || $offered_price == 0) {

src/engine/Default/message_view.php
@@ -7,20 +7,23 @@ $player = $session->getPlayer();
 
 Menu::messages();
 
+/** @var int $folderID */
+$folderID = $var['folder_id'];
+
 $db = Smr\Database::getInstance();
 $whereClause = 'WHERE game_id = ' . $db->escapeNumber($player->getGameID());
-if ($var['folder_id'] == MSG_SENT) {
+if ($folderID == MSG_SENT) {
 	$whereClause .= ' AND sender_id = ' . $db->escapeNumber($player->getAccountID()) . '
 					AND message_type_id = ' . $db->escapeNumber(MSG_PLAYER) . '
 					AND sender_delete = ' . $db->escapeBoolean(false);
 } else {
 	$whereClause .= ' AND account_id = ' . $db->escapeNumber($player->getAccountID()) . '
-					AND message_type_id = ' . $db->escapeNumber($var['folder_id']) . '
+					AND message_type_id = ' . $db->escapeNumber($folderID) . '
 					AND receiver_delete = ' . $db->escapeBoolean(false);
 }
 
 $messageBox = [];
-if ($var['folder_id'] == MSG_SENT) {
+if ($folderID == MSG_SENT) {
 	$messageBox['UnreadMessages'] = 0;
 } else {
 	$dbResult = $db->read('SELECT count(*) as count
@@ -30,7 +33,7 @@ if ($var['folder_id'] == MSG_SENT) {
 }
 $dbResult = $db->read('SELECT count(*) as count FROM message ' . $whereClause);
 $messageBox['TotalMessages'] = $dbResult->record()->getInt('count');
-$messageBox['Type'] = $var['folder_id'];
+$messageBox['Type'] = $folderID;
 
 $page = $var['page'] ?? 0;
 
@@ -49,7 +52,7 @@ if ($page == 0 && !USING_AJAX) {
 	$player->setMessagesRead($messageBox['Type']);
 }
 
-$messageBox['Name'] = Smr\Messages::getMessageTypeNames($var['folder_id']);
+$messageBox['Name'] = Smr\Messages::getMessageTypeNames($folderID);
 $template->assign('PageTopic', 'Viewing ' . $messageBox['Name']);
 
 if ($messageBox['Type'] == MSG_GLOBAL || $messageBox['Type'] == MSG_SCOUT) {
@@ -71,7 +74,7 @@ $messageBox['NumberMessages'] = $dbResult->getNumRecords();
 $messageBox['Messages'] = [];
 
 // Group scout messages if they wouldn't fit on a single page
-if ($var['folder_id'] == MSG_SCOUT && !isset($var['show_all']) && $messageBox['TotalMessages'] > $player->getScoutMessageGroupLimit()) {
+if ($folderID == MSG_SCOUT && !isset($var['show_all']) && $messageBox['TotalMessages'] > $player->getScoutMessageGroupLimit()) {
 	// get rid of all old scout messages (>48h)
 	$db->write('DELETE FROM message WHERE expire_time < ' . $db->escapeNumber(Smr\Epoch::time()) . ' AND message_type_id = ' . $db->escapeNumber(MSG_SCOUT));
 
@@ -84,12 +87,12 @@ if ($var['folder_id'] == MSG_SCOUT && !isset($var['show_all']) && $messageBox['T
 	$template->unassign('NextPageHREF'); // always displaying all scout messages?
 } else {
 	foreach ($dbResult->records() as $dbRecord) {
-		$messageBox['Messages'][] = displayMessage($dbRecord->getInt('message_id'), $dbRecord->getInt('account_id'), $dbRecord->getInt('sender_id'), $player->getGameID(), $dbRecord->getString('message_text'), $dbRecord->getInt('send_time'), $dbRecord->getBoolean('msg_read'), $var['folder_id'], $player->getAccount());
+		$messageBox['Messages'][] = displayMessage($dbRecord->getInt('message_id'), $dbRecord->getInt('account_id'), $dbRecord->getInt('sender_id'), $player->getGameID(), $dbRecord->getString('message_text'), $dbRecord->getInt('send_time'), $dbRecord->getBoolean('msg_read'), $folderID, $player->getAccount());
 	}
 }
 if (!USING_AJAX) {
 	$db->write('UPDATE message SET msg_read = \'TRUE\'
-				WHERE message_type_id = ' . $db->escapeNumber($var['folder_id']) . ' AND ' . $player->getSQL());
+				WHERE message_type_id = ' . $db->escapeNumber($folderID) . ' AND ' . $player->getSQL());
 }
 $template->assign('MessageBox', $messageBox);
 

src/engine/Default/planet_construction_processing.php
@@ -9,15 +9,19 @@ if (!$player->isLandedOnPlanet()) {
 }
 $planet = $player->getSectorPlanet();
 $action = $var['action'];
+
+/** @var int $constructionID */
+$constructionID = $var['construction_id'];
+
 if ($action == 'Build') {
 	// now start the construction
-	$planet->startBuilding($player, $var['construction_id']);
+	$planet->startBuilding($player, $constructionID);
 	$player->increaseHOF(1, ['Planet', 'Buildings', 'Started'], HOF_ALLIANCE);
 
-	$player->log(LOG_TYPE_PLANETS, 'Player starts a ' . $planet->getStructureTypes($var['construction_id'])->name() . ' on planet.');
+	$player->log(LOG_TYPE_PLANETS, 'Player starts a ' . $planet->getStructureTypes($constructionID)->name() . ' on planet.');
 
 } elseif ($action == 'Cancel') {
-	$planet->stopBuilding($var['construction_id']);
+	$planet->stopBuilding($constructionID);
 	$player->increaseHOF(1, ['Planet', 'Buildings', 'Stopped'], HOF_ALLIANCE);
 	$player->log(LOG_TYPE_PLANETS, 'Player cancels planet construction');
 }

src/engine/Default/shop_hardware_processing.php
@@ -8,6 +8,7 @@ $ship = $player->getShip();
 $action = Smr\Request::get('action');
 $amount = Smr\Request::getInt('amount');
 
+/** @var int $hardware_id */
 $hardware_id = $var['hardware_id'];
 $hardware_name = Globals::getHardwareName($hardware_id);
 $cost = Globals::getHardwareCost($hardware_id);

src/engine/Draft/alliance_pick_processing.php
@@ -4,16 +4,15 @@ $session = Smr\Session::getInstance();
 $var = $session->getCurrentVar();
 $player = $session->getPlayer();
 
-if (!is_numeric($var['PickedAccountID'])) {
-	create_error('You have to pick a player.');
-}
+/** @var int $pickedAccountID */
+$pickedAccountID = $var['PickedAccountID'];
 
 require_once(LIB . 'Draft/alliance_pick.inc.php');
 $teams = get_draft_teams($player->getGameID());
 if (!$teams[$player->getAccountID()]['CanPick']) {
 	create_error('You have to wait for others to pick first.');
 }
-$pickedPlayer = SmrPlayer::getPlayer($var['PickedAccountID'], $player->getGameID());
+$pickedPlayer = SmrPlayer::getPlayer($pickedAccountID, $player->getGameID());
 
 if ($pickedPlayer->isDraftLeader()) {
 	create_error('You cannot pick another leader.');

commit d603d9e27523c6e7f1ebb06cd791e9379ff5edd0

shop_goods_processing.php: fix container error
Fixes a bug introduced in 31efc1bcee that caused an error anytime a
trade was attempted, since `ideal_price` and `offered_price` were not
being properly added to containers.

src/engine/Default/shop_goods_processing.php
@@ -203,15 +203,11 @@ if ($stealing ||
 	} else {
 		$container = Page::create('shop_goods.php');
 	}
-	$container->addVar('ideal_price');
-	$container->addVar('offered_price');
 	$container['trade_msg'] = $tradeMessage;
 
 } else {
 	// does the trader try to outsmart us?
 	$container = Page::create('shop_goods_trade.php');
-	$container->addVar('ideal_price');
-	$container->addVar('offered_price');
 
 	require_once(LIB . 'Default/shop_goods.inc.php');
 	check_bargain_number($amount, $ideal_price, $offered_price, $bargain_price, $container, $player);
@@ -222,6 +218,9 @@ if ($stealing ||
 	$container['amount'] = $amount;
 	$container['bargain_price'] = $bargain_price;
 }
+
+$container['ideal_price'] = $ideal_price;
+$container['offered_price'] = $offered_price;
 
 // only take turns if they bargained
 if (!isset($container['number_of_bargains']) || $container['number_of_bargains'] != 1) {