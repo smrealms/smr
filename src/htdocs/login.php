705f36a3b5e576603c689c9b56fb340edda3c8ff
Dan Hemberger <daniel.hemberger@gmail.com>
2022-09-27 22:46:41 -0700

    Move login_story.inc.php into login.php
    The login game blurb doesn't really fit as part of an "engine" page,
    as it is just an array of strings. It could be a data class in `lib/`
    perhaps, but it makes the most sense living precisely where it is used
    in login.php.

--- src/htdocs/login.php
+++ src/htdocs/login.php
@@ -50,7 +50,15 @@ try {
 	$template->assign('GameNews', $gameNews);
 	unset($overrideGameID);
 
-	require_once(ENGINE . 'Default/login_story.inc.php');
+	// SMR game blurb
+	$story = [
+		'It is a time of great interstellar turmoil....',
+		'The decline of the Galactic Federation, which served for centuries as a glowing bastion of peace, freedom, and prosperity, has given way to a rising tide of corruption and organized crime. Trade lanes are unsafe even in the most well populated areas. Pirates disrupt the free flow of cash and goods to local economies. Organized gangs have laid claim to entire planets and control the most lucrative ports by threat of violence. Black market goods are sold in the open without fear.',
+		'Sensing that the Federation has lost its grip on power, the eight known races have split off and formed independent governments. The presidents and their councils guide the flow of weapons and trade in a bid to retain economic control and keep their people safe. Despite these efforts, violence and desperation continue to escalate.',
+		'This is the universe you find yourself in.',
+		'Space Merchant Realms is a game of speed, skill, and strategy. To make it to the top ranks, it takes a combination of leadership, persistence, courage, and cooperation. Will you quest for riches as a tradeship captain? Or seek fame and glory as an alliance fleet commander? Is the life of a pirate what you are after? Where will your destiny take you?',
+	];
+	$template->assign('Story', $story);
 
 	$template->assign('Body', 'login/login.php');
 	$template->display('login/skeleton.php');
