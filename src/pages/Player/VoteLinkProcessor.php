09d5a8ea40b6f8fdb9f0a76021984b303ca3e692
Dan Hemberger <daniel.hemberger@gmail.com>
2022-12-21 23:55:52 -0800

    VoteLinkProcessor: remove unnecessary else
    It is clearer to only put the error condition in the if-statement.

--- src/pages/Player/VoteLinkProcessor.php
+++ src/pages/Player/VoteLinkProcessor.php
@@ -14,15 +14,14 @@ class VoteLinkProcessor extends PlayerPageProcessor {
 	) {}
 
 	public function build(AbstractSmrPlayer $player): never {
-		if ($player->getGame()->hasStarted()) {
-			// Allow vote
-			$voteLink = new VoteLink($this->voteSite, $player->getAccountID(), $player->getGameID());
-			$voteLink->setClicked();
-			$voting = '<b><span class="red">v</span>o<span class="blue">t</span><span class="red">i</span>n<span class="blue">g</span></b>';
-			$message = "Thank you for $voting! You will receive bonus turns once your vote is processed.";
-		} else {
+		if (!$player->getGame()->hasStarted()) {
 			create_error('You cannot gain bonus turns until the game has started!');
 		}
+
+		$voteLink = new VoteLink($this->voteSite, $player->getAccountID(), $player->getGameID());
+		$voteLink->setClicked();
+		$voting = '<b><span class="red">v</span>o<span class="blue">t</span><span class="red">i</span>n<span class="blue">g</span></b>';
+		$message = "Thank you for $voting! You will receive bonus turns once your vote is processed.";
 
 		$container = new CurrentSector(message: $message);
 		$container->go();
