smrealms/smr

commit e280e9ad935671b0fefbe5e98b94b04f11533610

Convert engine files to Page classes
Page Classes
============

Page classes are constructed directly using the name of the class,
rather than identifying them by the engine filename, e.g.

    new CurrentSector();

instead of

    Page::create('current_sector.php');

Page class names mostly match the previous engine filenames (but with
CamelCase instead of snake_case). In some cases, the class names are a
bit different to improve alphabetic grouping or to clarify what it
actually does (e.g. MessageNotify -> MessageReport). As such, display
Pages no longer map 1:1 to template filenames; in the future we may
either rename the template files to match better, or possibly even
integrate them into the Page classes themselves.

Pages have been put into `Account`, `Player`, and `Admin` namespaces,
and inherit from the expected base classes based on the namespace.
Additional considerations about this arrangement:

* Some pages don't fit cleanly into these categories. For example, they
  will include player-related content if the session has a game;
  otherwise they are account-related only (see, e.g. `Preferences`).
  It's not clear how to handle these cases, but we default to using the
  `Account` namespace since it has the most generic interface.

* Some Pages don't consume all the interface arguments inherited from
  their base class (e.g. an `PlayerPage` may not actually need access
  to the `$player` object). This may change with further refactoring
  (e.g. if we pass the arguments along to the Template).

* Sub-namespaces have been created to help organize the massive list
  of pages into logical units (e.g. Planet, Bank, Bar, etc.).

Page Variables
==============

Page variables are now properties of the associated class, and are
accessed directly in the class scope rather than through the session
"var".

We have attempted to make most Page properties `readonly`, since it is
important for Pages to be unchanged once we create an SN for them

src/pages/Player/BetaFunctionsProcessor.php
@@ -1,16 +1,29 @@
 <?php declare(strict_types=1);
 
+namespace Smr\Pages\Player;
+
+use AbstractSmrPlayer;
+use Globals;
 use Smr\Database;
+use Smr\Page\PlayerPageProcessor;
 use Smr\Request;
 use Smr\SectorLock;
+use SmrPlanet;
+use SmrPort;
+use SmrSector;
+use SmrWeapon;
 
-		$session = Smr\Session::getInstance();
-		$var = $session->getCurrentVar();
-		$player = $session->getPlayer();
+class BetaFunctionsProcessor extends PlayerPageProcessor {
+
+	public function __construct(
+		private readonly string $func
+	) {}
+
+	public function build(AbstractSmrPlayer $player): never {
 		$ship = $player->getShip();
 		$sector = $player->getSector();
 
-		if ($var['func'] == 'Map') {
+		if ($this->func == 'Map') {
 			$account_id = $player->getAccountID();
 			$game_id = $player->getGameID();
 			// delete all entries from the player_visited_sector/port table
@@ -24,9 +37,9 @@ use Smr\SectorLock;
 				$port->addCachePort($account_id);
 			}
 
-		} elseif ($var['func'] == 'Money') {
+		} elseif ($this->func == 'Money') {
 			$player->setCredits(50000000);
-		} elseif ($var['func'] == 'Ship') {
+		} elseif ($this->func == 'Ship') {
 			$shipTypeID = Request::getInt('ship_type_id');
 			if ($shipTypeID <= 75 && $shipTypeID != 68) {
 				// assign the new ship
@@ -35,15 +48,15 @@ use Smr\SectorLock;
 				$ship->setTypeID($shipTypeID);
 				$ship->setHardwareToMax();
 			}
-		} elseif ($var['func'] == 'Weapon') {
+		} elseif ($this->func == 'Weapon') {
 			$weapon = SmrWeapon::getWeapon(Request::getInt('weapon_id'));
 			$amount = Request::getInt('amount');
 			for ($i = 1; $i <= $amount; $i++) {
 				$ship->addWeapon($weapon);
 			}
-		} elseif ($var['func'] == 'Uno') {
+		} elseif ($this->func == 'Uno') {
 			$ship->setHardwareToMax();
-		} elseif ($var['func'] == 'Warp') {
+		} elseif ($this->func == 'Warp') {
 			$sector_to = Request::getInt('sector_to');
 			if (!SmrSector::sectorExists($player->getGameID(), $sector_to)) {
 				create_error('Sector ID is not in any galaxy.');
@@ -55,25 +68,25 @@ use Smr\SectorLock;
 			$lock = SectorLock::getInstance();
 			$lock->release();
 			$lock->acquireForPlayer($player);
-		} elseif ($var['func'] == 'Turns') {
+		} elseif ($this->func == 'Turns') {
 			$player->setTurns(Request::getInt('turns'));
-		} elseif ($var['func'] == 'Exp') {
+		} elseif ($this->func == 'Exp') {
 			$exp = min(500000, Request::getInt('exp'));
 			$player->setExperience($exp);
-		} elseif ($var['func'] == 'Align') {
+		} elseif ($this->func == 'Align') {
 			$align = max(-500, min(500, Request::getInt('align')));
 			$player->setAlignment($align);
-		} elseif ($var['func'] == 'RemWeapon') {
+		} elseif ($this->func == 'RemWeapon') {
 			$ship->removeAllWeapons();
-		} elseif ($var['func'] == 'Hard_add') {
+		} elseif ($this->func == 'Hard_add') {
 			$type_hard = Request::getInt('type_hard');
 			$amount_hard = Request::getInt('amount_hard');
 			$ship->setHardware($type_hard, $amount_hard);
-		} elseif ($var['func'] == 'Relations') {
+		} elseif ($this->func == 'Relations') {
 			$amount = Request::getInt('amount');
 			$race = Request::getInt('race');
 			$player->setRelations($amount, $race);
-		} elseif ($var['func'] == 'Race_Relations') {
+		} elseif ($this->func == 'Race_Relations') {
 			$amount = Request::getInt('amount');
 			$race = Request::getInt('race');
 			if ($player->getRaceID() == $race) {
@@ -82,25 +95,28 @@ use Smr\SectorLock;
 			$db = Database::getInstance();
 			$db->write('UPDATE race_has_relation SET relation = ' . $db->escapeNumber($amount) . ' WHERE race_id_1 = ' . $db->escapeNumber($player->getRaceID()) . ' AND race_id_2 = ' . $db->escapeNumber($race) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()));
 			$db->write('UPDATE race_has_relation SET relation = ' . $db->escapeNumber($amount) . ' WHERE race_id_1 = ' . $db->escapeNumber($race) . ' AND race_id_2 = ' . $db->escapeNumber($player->getRaceID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()));
-		} elseif ($var['func'] == 'Race') {
+		} elseif ($this->func == 'Race') {
 			$race = Request::getInt('race');
 			$player->setRaceID($race);
-		} elseif ($var['func'] == 'planet_buildings') {
+		} elseif ($this->func == 'planet_buildings') {
 			$planet = $sector->getPlanet();
 			foreach ($planet->getMaxBuildings() as $id => $amount) {
 				$planet->setBuilding($id, $amount);
 			}
-		} elseif ($var['func'] == 'planet_defenses') {
+		} elseif ($this->func == 'planet_defenses') {
 			$planet = $sector->getPlanet();
 			$planet->setShields($planet->getMaxShields());
 			$planet->setCDs($planet->getMaxCDs());
 			$planet->setArmour($planet->getMaxArmour());
-		} elseif ($var['func'] == 'planet_stockpile') {
+		} elseif ($this->func == 'planet_stockpile') {
 			$planet = $sector->getPlanet();
 			foreach (Globals::getGoods() as $goodID => $good) {
 				$planet->setStockpile($goodID, SmrPlanet::MAX_STOCKPILE);
 			}
 		}
 
-		$container = Page::create('beta_functions.php');
+		$container = new BetaFunctions();
 		$container->go();
+	}
+
+}

commit 1362648af59ed208662db621613903e89a9c37b1

Break BetaFunctionsProcessor into separate pages
The BetaFunctions pages were fragile because they hard-coded a function
name (as a string) for each possible action, and those strings had to
match identically between display and processor pages.

The best solution seemed to be to break up the BetaFunctionsProcessor
page into separate pages for each function, grouped in a BetaFunctions
namespace. This solution has a bit of extra boilerplate for each new
Page file, but it is by far the most robust. (I had considered making
a BetaFunctions enum class to replace the `func` string, but this was
still too roundabout of a solution.)

Note: This also fixes an error where trying to set political relations
silently did nothing, since we didn't validate that they `func` string
actually matched anything, and the string was hard-coded differently
between the display and processor pages (`Race_relations` and
`Race_Relations` respectively). This error was introduced in e280e9ad9.

src/lib/Default/Globals.php
@@ -15,7 +15,7 @@ use Smr\Pages\Player\AllianceOptions;
 use Smr\Pages\Player\AllianceRoster;
 use Smr\Pages\Player\AttackPlayerProcessor;
 use Smr\Pages\Player\Bank\AllianceBank;
-use Smr\Pages\Player\BetaFunctions;
+use Smr\Pages\Player\BetaFunctions\BetaFunctions;
 use Smr\Pages\Player\BuyShipName;
 use Smr\Pages\Player\Chess\MatchList;
 use Smr\Pages\Player\Chess\MatchStartProcessor;

src/pages/Player/BetaFunctions/AddMoneyProcessor.php
@@ -1,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+
+class AddMoneyProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$player->setCredits(50000000);
+	}
+
+}

src/pages/Player/BetaFunctions/AddWeaponsProcessor.php
@@ -1,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+use Smr\Request;
+use SmrWeapon;
+
+class AddWeaponsProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$ship = $player->getShip();
+		$weapon = SmrWeapon::getWeapon(Request::getInt('weapon_id'));
+		$amount = Request::getInt('amount');
+		for ($i = 0; $i < $amount; $i++) {
+			$ship->addWeapon($weapon);
+		}
+	}
+
+}

src/pages/Player/BetaFunctions/BetaFunctions.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Smr\Pages\Player;
+namespace Smr\Pages\Player\BetaFunctions;
 
 use AbstractSmrPlayer;
 use Globals;
@@ -26,15 +26,15 @@ class BetaFunctions extends PlayerPage {
 		$template->assign('PageTopic', 'Beta Functions');
 
 		// let them map all
-		$container = new BetaFunctionsProcessor('Map');
+		$container = new RevealMapProcessor();
 		$template->assign('MapHREF', $container->href());
 
 		// let them get money
-		$container = new BetaFunctionsProcessor('Money');
+		$container = new AddMoneyProcessor();
 		$template->assign('MoneyHREF', $container->href());
 
 		//next time for ship
-		$container = new BetaFunctionsProcessor('Ship');
+		$container = new SetShipProcessor();
 		$template->assign('ShipHREF', $container->href());
 		$shipList = [];
 		foreach (SmrShipType::getAll() as $shipTypeID => $shipType) {
@@ -44,7 +44,7 @@ class BetaFunctions extends PlayerPage {
 		$template->assign('ShipList', $shipList);
 
 		//next weapons
-		$container = new BetaFunctionsProcessor('Weapon');
+		$container = new AddWeaponsProcessor();
 		$template->assign('AddWeaponHREF', $container->href());
 		$weaponList = [];
 		foreach (SmrWeaponType::getAllWeaponTypes() as $weaponTypeID => $weaponType) {
@@ -54,31 +54,31 @@ class BetaFunctions extends PlayerPage {
 		$template->assign('WeaponList', $weaponList);
 
 		//Remove Weapons
-		$container = new BetaFunctionsProcessor('RemWeapon');
+		$container = new RemoveWeaponsProcessor();
 		$template->assign('RemoveWeaponsHREF', $container->href());
 
 		//allow to get full hardware
-		$container = new BetaFunctionsProcessor('Uno');
+		$container = new RepairShipProcessor();
 		$template->assign('UnoHREF', $container->href());
 
 		//move whereever you want
-		$container = new BetaFunctionsProcessor('Warp');
+		$container = new SetSectorProcessor();
 		$template->assign('WarpHREF', $container->href());
 
 		//set turns
-		$container = new BetaFunctionsProcessor('Turns');
+		$container = new SetTurnsProcessor();
 		$template->assign('TurnsHREF', $container->href());
 
 		//set experience
-		$container = new BetaFunctionsProcessor('Exp');
+		$container = new SetExperienceProcessor();
 		$template->assign('ExperienceHREF', $container->href());
 
 		//Set alignment
-		$container = new BetaFunctionsProcessor('Align');
+		$container = new SetAlignmentProcessor();
 		$template->assign('AlignmentHREF', $container->href());
 
 		//add any type of hardware
-		$container = new BetaFunctionsProcessor('Hard_add');
+		$container = new SetHardwareProcessor();
 		$template->assign('HardwareHREF', $container->href());
 		$hardware = [];
 		foreach (Globals::getHardwareTypes() as $hardwareTypeID => $hardwareType) {
@@ -87,25 +87,25 @@ class BetaFunctions extends PlayerPage {
 		$template->assign('Hardware', $hardware);
 
 		//change personal relations
-		$container = new BetaFunctionsProcessor('Relations');
+		$container = new SetPersonalRelationsProcessor();
 		$template->assign('PersonalRelationsHREF', $container->href());
 
 		//change race relations
-		$container = new BetaFunctionsProcessor('Race_relations');
+		$container = new SetPoliticalRelationsProcessor();
 		$template->assign('RaceRelationsHREF', $container->href());
 
 		//change race
-		$container = new BetaFunctionsProcessor('Race');
+		$container = new SetRaceProcessor();
 		$template->assign('ChangeRaceHREF', $container->href());
 
 		if ($sector->hasPlanet()) {
-			$container = new BetaFunctionsProcessor('planet_buildings');
+			$container = new PlanetBuildingsProcessor();
 			$template->assign('MaxBuildingsHREF', $container->href());
 
-			$container = new BetaFunctionsProcessor('planet_defenses');
+			$container = new PlanetDefensesProcessor();
 			$template->assign('MaxDefensesHREF', $container->href());
 
-			$container = new BetaFunctionsProcessor('planet_stockpile');
+			$container = new PlanetStockpileProcessor();
 			$template->assign('MaxStockpileHREF', $container->href());
 		}
 	}

src/pages/Player/BetaFunctions/BetaFunctionsPageProcessor.php
@@ -1,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+use Smr\Page\PlayerPageProcessor;
+
+abstract class BetaFunctionsPageProcessor extends PlayerPageProcessor {
+
+	abstract protected function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void;
+
+	public function build(AbstractSmrPlayer $player): never {
+		$this->buildBetaFunctionsProcessor($player);
+
+		$container = new BetaFunctions();
+		$container->go();
+	}
+
+}

src/pages/Player/BetaFunctions/PlanetBuildingsProcessor.php
@@ -1,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+
+class PlanetBuildingsProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$planet = $player->getSector()->getPlanet();
+		foreach ($planet->getMaxBuildings() as $id => $amount) {
+			$planet->setBuilding($id, $amount);
+		}
+	}
+
+}

src/pages/Player/BetaFunctions/PlanetDefensesProcessor.php
@@ -1,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+
+class PlanetDefensesProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$planet = $player->getSector()->getPlanet();
+		$planet->setShields($planet->getMaxShields());
+		$planet->setCDs($planet->getMaxCDs());
+		$planet->setArmour($planet->getMaxArmour());
+	}
+
+}

src/pages/Player/BetaFunctions/PlanetStockpileProcessor.php
@@ -1,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+use Globals;
+use SmrPlanet;
+
+class PlanetStockpileProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$planet = $player->getSector()->getPlanet();
+		foreach (Globals::getGoods() as $goodID => $good) {
+			$planet->setStockpile($goodID, SmrPlanet::MAX_STOCKPILE);
+		}
+	}
+
+}

src/pages/Player/BetaFunctions/RemoveWeaponsProcessor.php
@@ -1,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+
+class RemoveWeaponsProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$ship = $player->getShip();
+		$ship->removeAllWeapons();
+	}
+
+}

src/pages/Player/BetaFunctions/RepairShipProcessor.php
@@ -1,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+
+class RepairShipProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$ship = $player->getShip();
+		$ship->setHardwareToMax();
+	}
+
+}

src/pages/Player/BetaFunctions/RevealMapProcessor.php
@@ -1,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+use Smr\Database;
+use SmrPort;
+
+class RevealMapProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$account_id = $player->getAccountID();
+		$game_id = $player->getGameID();
+		// delete all entries from the player_visited_sector/port table
+		$db = Database::getInstance();
+		$db->write('DELETE FROM player_visited_sector WHERE ' . $player->getSQL());
+
+		// add port infos
+		$dbResult = $db->read('SELECT * FROM port WHERE game_id = ' . $db->escapeNumber($game_id));
+		foreach ($dbResult->records() as $dbRecord) {
+			$port = SmrPort::getPort($game_id, $dbRecord->getInt('sector_id'), false, $dbRecord);
+			$port->addCachePort($account_id);
+		}
+	}
+
+}

src/pages/Player/BetaFunctions/SetAlignmentProcessor.php
@@ -1,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+use Smr\Request;
+
+class SetAlignmentProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$align = max(-500, min(500, Request::getInt('align')));
+		$player->setAlignment($align);
+	}
+
+}

src/pages/Player/BetaFunctions/SetExperienceProcessor.php
@@ -1,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+use Smr\Request;
+
+class SetExperienceProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$exp = min(500000, Request::getInt('exp'));
+		$player->setExperience($exp);
+	}
+
+}

src/pages/Player/BetaFunctions/SetHardwareProcessor.php
@@ -1,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+use Smr\Request;
+
+class SetHardwareProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$ship = $player->getShip();
+		$type_hard = Request::getInt('type_hard');
+		$amount_hard = Request::getInt('amount_hard');
+		$ship->setHardware($type_hard, $amount_hard);
+	}
+
+}

src/pages/Player/BetaFunctions/SetPersonalRelationsProcessor.php
@@ -1,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+use Smr\Request;
+
+class SetPersonalRelationsProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$amount = Request::getInt('amount');
+		$race = Request::getInt('race');
+		$player->setRelations($amount, $race);
+	}
+
+}

src/pages/Player/BetaFunctions/SetPoliticalRelationsProcessor.php
@@ -1,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+use Smr\Database;
+use Smr\Request;
+
+class SetPoliticalRelationsProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$amount = Request::getInt('amount');
+		$race = Request::getInt('race');
+		if ($player->getRaceID() == $race) {
+			create_error('You cannot change race relations with your own race.');
+		}
+		$db = Database::getInstance();
+		$db->write('UPDATE race_has_relation SET relation = ' . $db->escapeNumber($amount) . ' WHERE race_id_1 = ' . $db->escapeNumber($player->getRaceID()) . ' AND race_id_2 = ' . $db->escapeNumber($race) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()));
+		$db->write('UPDATE race_has_relation SET relation = ' . $db->escapeNumber($amount) . ' WHERE race_id_1 = ' . $db->escapeNumber($race) . ' AND race_id_2 = ' . $db->escapeNumber($player->getRaceID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()));
+	}
+
+}

src/pages/Player/BetaFunctions/SetRaceProcessor.php
@@ -1,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+use Smr\Request;
+
+class SetRaceProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$race = Request::getInt('race');
+		$player->setRaceID($race);
+	}
+
+}

src/pages/Player/BetaFunctions/SetSectorProcessor.php
@@ -1,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+use Smr\Request;
+use Smr\SectorLock;
+use SmrSector;
+
+class SetSectorProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$sector_to = Request::getInt('sector_to');
+		if (!SmrSector::sectorExists($player->getGameID(), $sector_to)) {
+			create_error('Sector ID is not in any galaxy.');
+		}
+		$player->setSectorID($sector_to);
+		$player->setLandedOnPlanet(false);
+		// Update sector lock
+		$player->update();
+		$lock = SectorLock::getInstance();
+		$lock->release();
+		$lock->acquireForPlayer($player);
+	}
+
+}

src/pages/Player/BetaFunctions/SetShipProcessor.php
@@ -1,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+use Smr\Request;
+
+class SetShipProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$ship = $player->getShip();
+		$shipTypeID = Request::getInt('ship_type_id');
+		if ($shipTypeID <= 75 && $shipTypeID != 68) {
+			// assign the new ship
+			$ship->decloak();
+			$ship->disableIllusion();
+			$ship->setTypeID($shipTypeID);
+			$ship->setHardwareToMax();
+		}
+	}
+
+}

src/pages/Player/BetaFunctions/SetTurnsProcessor.php
@@ -1,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Smr\Pages\Player\BetaFunctions;
+
+use AbstractSmrPlayer;
+use Smr\Request;
+
+class SetTurnsProcessor extends BetaFunctionsPageProcessor {
+
+	public function buildBetaFunctionsProcessor(AbstractSmrPlayer $player): void {
+		$player->setTurns(Request::getInt('turns'));
+	}
+
+}

src/pages/Player/BetaFunctionsProcessor.php
@@ -1,122 +1,0 @@
-<?php declare(strict_types=1);
-
-namespace Smr\Pages\Player;
-
-use AbstractSmrPlayer;
-use Globals;
-use Smr\Database;
-use Smr\Page\PlayerPageProcessor;
-use Smr\Request;
-use Smr\SectorLock;
-use SmrPlanet;
-use SmrPort;
-use SmrSector;
-use SmrWeapon;
-
-class BetaFunctionsProcessor extends PlayerPageProcessor {
-
-	public function __construct(
-		private readonly string $func
-	) {}
-
-	public function build(AbstractSmrPlayer $player): never {
-		$ship = $player->getShip();
-		$sector = $player->getSector();
-
-		if ($this->func == 'Map') {
-			$account_id = $player->getAccountID();
-			$game_id = $player->getGameID();
-			// delete all entries from the player_visited_sector/port table
-			$db = Database::getInstance();
-			$db->write('DELETE FROM player_visited_sector WHERE ' . $player->getSQL());
-
-			// add port infos
-			$dbResult = $db->read('SELECT * FROM port WHERE game_id = ' . $db->escapeNumber($game_id));
-			foreach ($dbResult->records() as $dbRecord) {
-				$port = SmrPort::getPort($game_id, $dbRecord->getInt('sector_id'), false, $dbRecord);
-				$port->addCachePort($account_id);
-			}
-
-		} elseif ($this->func == 'Money') {
-			$player->setCredits(50000000);
-		} elseif ($this->func == 'Ship') {
-			$shipTypeID = Request::getInt('ship_type_id');
-			if ($shipTypeID <= 75 && $shipTypeID != 68) {
-				// assign the new ship
-				$ship->decloak();
-				$ship->disableIllusion();
-				$ship->setTypeID($shipTypeID);
-				$ship->setHardwareToMax();
-			}
-		} elseif ($this->func == 'Weapon') {
-			$weapon = SmrWeapon::getWeapon(Request::getInt('weapon_id'));
-			$amount = Request::getInt('amount');
-			for ($i = 1; $i <= $amount; $i++) {
-				$ship->addWeapon($weapon);
-			}
-		} elseif ($this->func == 'Uno') {
-			$ship->setHardwareToMax();
-		} elseif ($this->func == 'Warp') {
-			$sector_to = Request::getInt('sector_to');
-			if (!SmrSector::sectorExists($player->getGameID(), $sector_to)) {
-				create_error('Sector ID is not in any galaxy.');
-			}
-			$player->setSectorID($sector_to);
-			$player->setLandedOnPlanet(false);
-			// Update sector lock
-			$player->update();
-			$lock = SectorLock::getInstance();
-			$lock->release();
-			$lock->acquireForPlayer($player);
-		} elseif ($this->func == 'Turns') {
-			$player->setTurns(Request::getInt('turns'));
-		} elseif ($this->func == 'Exp') {
-			$exp = min(500000, Request::getInt('exp'));
-			$player->setExperience($exp);
-		} elseif ($this->func == 'Align') {
-			$align = max(-500, min(500, Request::getInt('align')));
-			$player->setAlignment($align);
-		} elseif ($this->func == 'RemWeapon') {
-			$ship->removeAllWeapons();
-		} elseif ($this->func == 'Hard_add') {
-			$type_hard = Request::getInt('type_hard');
-			$amount_hard = Request::getInt('amount_hard');
-			$ship->setHardware($type_hard, $amount_hard);
-		} elseif ($this->func == 'Relations') {
-			$amount = Request::getInt('amount');
-			$race = Request::getInt('race');
-			$player->setRelations($amount, $race);
-		} elseif ($this->func == 'Race_Relations') {
-			$amount = Request::getInt('amount');
-			$race = Request::getInt('race');
-			if ($player->getRaceID() == $race) {
-				create_error('You cannot change race relations with your own race.');
-			}
-			$db = Database::getInstance();
-			$db->write('UPDATE race_has_relation SET relation = ' . $db->escapeNumber($amount) . ' WHERE race_id_1 = ' . $db->escapeNumber($player->getRaceID()) . ' AND race_id_2 = ' . $db->escapeNumber($race) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()));
-			$db->write('UPDATE race_has_relation SET relation = ' . $db->escapeNumber($amount) . ' WHERE race_id_1 = ' . $db->escapeNumber($race) . ' AND race_id_2 = ' . $db->escapeNumber($player->getRaceID()) . ' AND game_id = ' . $db->escapeNumber($player->getGameID()));
-		} elseif ($this->func == 'Race') {
-			$race = Request::getInt('race');
-			$player->setRaceID($race);
-		} elseif ($this->func == 'planet_buildings') {
-			$planet = $sector->getPlanet();
-			foreach ($planet->getMaxBuildings() as $id => $amount) {
-				$planet->setBuilding($id, $amount);
-			}
-		} elseif ($this->func == 'planet_defenses') {
-			$planet = $sector->getPlanet();
-			$planet->setShields($planet->getMaxShields());
-			$planet->setCDs($planet->getMaxCDs());
-			$planet->setArmour($planet->getMaxArmour());
-		} elseif ($this->func == 'planet_stockpile') {
-			$planet = $sector->getPlanet();
-			foreach (Globals::getGoods() as $goodID => $good) {
-				$planet->setStockpile($goodID, SmrPlanet::MAX_STOCKPILE);
-			}
-		}
-
-		$container = new BetaFunctions();
-		$container->go();
-	}
-
-}