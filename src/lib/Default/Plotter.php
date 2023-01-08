smrealms/smr

commit 7330d7aa1850bea91db9153db3fbdb6ad0ede772

Use str2int for validated int casting
In cases where we want to validate that a string is an int and then
cast it to an int, use `str2int`. (Note that this should not be used
for user input, unless we are HTML5 validation as well.)

In `AbstractSmrPlayer.php`, remove an unnecessary int cast.

src/lib/Default/Plotter.php
@@ -17,7 +17,7 @@ class Plotter {
 		}
 
 		// In all other cases, X is a numeric ID
-		$X = (int)$X;
+		$X = str2int($X);
 
 		// Helper function for plots to trade goods
 		$getGoodWithTransaction = function(int $goodID) use ($xType, $player) {

src/engine/Default/current_sector.php
@@ -180,10 +180,7 @@ function checkForAttackMessage(string &$msg): void {
 	$msg = str_replace('[ATTACK_RESULTS]', '', $msg, $contains);
 	if ($contains > 0) {
 		// $msg now contains only the log_id, if there is one
-		if (!is_numeric($msg)) {
-			throw new Exception('Improperly formatted attack message: ' . $msg);
-		}
-		$logID = (int)$msg;
+		$logID = str2int($msg);
 
 		$session = Smr\Session::getInstance();
 		$var = $session->getCurrentVar();

src/lib/Default/AbstractSmrPlayer.php
@@ -1834,7 +1834,7 @@ abstract class AbstractSmrPlayer {
 
 		$this->storedDestinations[] = [
 			'Label' => $label,
-			'SectorID' => (int)$sectorID,
+			'SectorID' => $sectorID,
 			'OffsetTop' => 1,
 			'OffsetLeft' => 1,
 		];

commit f241079e1f100380e829053ee1167c9ec1939a3e

Plotter: fix validation of getX input
We cannot pass an int value of `$X` to the `str2int` method, because it
only accepts strings. This regression was introduced in 7330d7aa1850,
and was causing an error when accepting missions (since that's the only
place we currently pass an int `$X`).

We may want to consider changing `str2int` to allow other types, e.g.
int or float or even mixed, as the implementation works for any type
(but then obviously we'd need to rename the function).

Fixes the PHPStan warning:

> Parameter #1 $val of function str2int expects string, int|string given.

src/lib/Default/Plotter.php
@@ -17,8 +17,10 @@ class Plotter {
 			return $X;
 		}
 
-		// In all other cases, X is a numeric ID
-		$X = str2int($X);
+		// In all other cases, X is either an int or a numeric string
+		if (is_string($X)) {
+			$X = str2int($X);
+		}
 
 		// Helper function for plots to trade goods
 		$getGoodWithTransaction = function(int $goodID) use ($xType, $player) {