c967bdda8f09200dfc69e23657b3a1091bfeba04
Dan Hemberger <daniel.hemberger@gmail.com>
2023-01-13 15:52:16 -0800

    PlanetList.inc.php: fix method capitalization
    PHP is case-insensitive, but we want to use consistent capitalization.
    
    Fixes PHPStan warning:
    
    > Call to method Smr\Planet::getTypeName() with incorrect case: getTypename

--- src/templates/Default/engine/Default/includes/PlanetList.inc.php
+++ src/templates/Default/engine/Default/includes/PlanetList.inc.php
@@ -23,7 +23,7 @@ if (count($Planets) > 0) { ?>
 			foreach ($Planets as $Planet) { ?>
 				<tr id="planet-<?php echo $Planet->getSectorID(); ?>" class="ajax">
 					<td class="noWrap">
-						<img src="<?php echo $Planet->getTypeImage(); ?>"  width="16" height="16" alt="" title="<?php echo $Planet->getTypename() . ': ' . $Planet->getTypeDescription(); ?>" /></td>
+						<img src="<?php echo $Planet->getTypeImage(); ?>"  width="16" height="16" alt="" title="<?php echo $Planet->getTypeName() . ': ' . $Planet->getTypeDescription(); ?>" /></td>
 					<td class="sort_name"><?php echo $Planet->getDisplayName(); ?></td>
 					<td class="sort_lvl center"><?php echo number_format($Planet->getLevel(), 2); ?></td>
 					<td class="sort_owner noWrap"><?php echo $Planet->getOwner()->getLinkedDisplayName(false); ?></td>
