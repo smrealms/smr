69f6d6ddb51a6bef3147c0b139df46ee1c468b18
Dan Hemberger <daniel.hemberger@gmail.com>
2023-01-20 13:39:32 -0800

    Add unique icons for each hardware shop
    All the (non-racial) hardware shops used the same icon, making it hard
    to distinguish between them. This change makes it possible to identify
    each one visually, without needing to check the image tooltip.
    
    I have tried to keep to the "hardware" theme as much as possible. They
    use the same color scheme (cyan with black border) and have that 1990s
    feel.
    
    Modified the "J" in "NIJ" on the Nijarin Beacon so that it matches the
    "J" in the Accelerated Systems. (Remove one pixel from the top serif.)

---
+++ db/patches/V1_6_68_06__hardware_icons.sql
@@ -0,0 +1,8 @@
+-- Customize hardware icons for each type
+UPDATE location_type SET location_image = 'images/ca.png' WHERE location_type_id = 602;
+UPDATE location_type SET location_image = 'images/hardware_J.png' WHERE location_type_id = 603;
+UPDATE location_type SET location_image = 'images/hardware_S.png' WHERE location_type_id = 604;
+UPDATE location_type SET location_image = 'images/hardware_C.png' WHERE location_type_id = 605;
+UPDATE location_type SET location_image = 'images/hardware_I.png' WHERE location_type_id = 606;
+UPDATE location_type SET location_image = 'images/hardware_D.png' WHERE location_type_id = 608;
+UPDATE location_type SET location_image = 'images/bdf.png' WHERE location_type_id = 609;
