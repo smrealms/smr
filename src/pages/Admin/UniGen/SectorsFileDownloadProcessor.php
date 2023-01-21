8a13bbf40954de69869384cc60e00127bdae883b
Dan Hemberger <daniel.hemberger@gmail.com>
2023-01-21 00:29:52 -0800

    SectorsFileDownloadProcessor: fix base class of UniGen version
    The UniGen version of this page must inherit from AccountPageProcessor,
    not PlayerPageProcessor, since the session doesn't have a Player in the
    admin pages.

--- src/pages/Admin/UniGen/SectorsFileDownloadProcessor.php
+++ src/pages/Admin/UniGen/SectorsFileDownloadProcessor.php
@@ -2,17 +2,17 @@
 
 namespace Smr\Pages\Admin\UniGen;
 
-use Smr\AbstractPlayer;
-use Smr\Page\PlayerPageProcessor;
+use Smr\Account;
+use Smr\Page\AccountPageProcessor;
 use Smr\SectorsFile;
 
-class SectorsFileDownloadProcessor extends PlayerPageProcessor {
+class SectorsFileDownloadProcessor extends AccountPageProcessor {
 
 	public function __construct(
 		private readonly int $gameID
 	) {}
 
-	public function build(AbstractPlayer $player): never {
+	public function build(Account $account): never {
 		SectorsFile::create($this->gameID, player: null, adminCreate: true);
 	}
 
