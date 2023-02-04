87fcd10ec342a9cfb5dabcdd9d5a8d05ab042887
Dan Hemberger <daniel.hemberger@gmail.com>
2023-02-04 10:31:46 -0800

    Add infrastructure for rector
    Rector is a refactoring tool. It can help reduce the manual effort
    needed for upgrading to new PHP versions or other major libraries.
    We add the basic infrastructure for its use, though it will take some
    time to get it to the point where we would consider using it in CI.
    
    Note: this has an issue where editing the files change the file owner
    to root, and then the files become read-only in the local filesystem.
    This is because the container is running as root, and rector does not
    do any special handling to help this situation (unlike, e.g., phpcbf).
    Fortunately, git will revert ownership when it modifies the file.

---
+++ rector.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+	$rectorConfig->paths([
+		__DIR__ . '/test',
+		__DIR__ . '/src',
+	]);
+};
