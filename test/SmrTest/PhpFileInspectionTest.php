83bf74b68ed7efd915aec226439d64f7afa19d20
Dan Hemberger <daniel.hemberger@gmail.com>
2023-02-20 14:29:14 -0800

    PhpFileInspectionTest: update to PHPLint v9 API

--- test/SmrTest/PhpFileInspectionTest.php
+++ test/SmrTest/PhpFileInspectionTest.php
@@ -2,21 +2,37 @@
 
 namespace SmrTest;
 
+use Overtrue\PHPLint\Command\LintCommand;
+use Overtrue\PHPLint\Configuration\ConsoleOptionsResolver;
+use Overtrue\PHPLint\Event\EventDispatcher;
+use Overtrue\PHPLint\Finder;
 use Overtrue\PHPLint\Linter;
 use PHPUnit\Framework\Attributes\CoversNothing;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Input\ArrayInput;
 
 #[CoversNothing]
 class PhpFileInspectionTest extends TestCase {
 
 	public function test_all_files_pass_phplint(): void {
-		$paths = [ROOT];
-		$excludes = ['vendor'];
-		$linter = new Linter($paths, $excludes, warning: true);
-		$linter->setProcessLimit(8); // multiprocessing
+		$dispatcher = new EventDispatcher([]);
 
-		// get errors
-		$errors = $linter->lint();
+		$arguments = [
+			'path' => [ROOT],
+			'--exclude' => ['vendor'],
+			'--no-configuration' => true,
+			'--warning' => true,
+			'--jobs' => 8,
+		];
+		$command = new LintCommand($dispatcher);
+		$input = new ArrayInput($arguments, $command->getDefinition());
+		$configResolver = new ConsoleOptionsResolver($input);
+
+		$finder = new Finder($configResolver);
+		$linter = new Linter($configResolver, $dispatcher);
+		$results = $linter->lintFiles($finder->getFiles());
+
+		$errors = $results->getFailures();
 		$this->assertEmpty($errors, print_r($errors, true));
 	}
 
