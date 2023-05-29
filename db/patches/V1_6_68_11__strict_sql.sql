41ff5d21c92ac7660f8865b84ea99bdf56c3a270
Dan Hemberger <daniel.hemberger@gmail.com>
2023-05-29 01:01:47 -0700

    Default `account.mail_banned` to 0
    This was the value being used to initialize this column in non-strict
    mode, and it is a sensible default because mail ban values less than
    the current epoch imply no ban.

--- db/patches/V1_6_68_11__strict_sql.sql
+++ db/patches/V1_6_68_11__strict_sql.sql
@@ -5,3 +5,5 @@ ALTER TABLE `player` MODIFY `last_port` int unsigned NOT NULL DEFAULT 0;
 
 ALTER TABLE `account` MODIFY `password_reset` char(32) DEFAULT NULL;
 UPDATE `account` SET `password_reset` = NULL WHERE `password_reset` = '';
+
+ALTER TABLE `account` MODIFY `mail_banned` int unsigned NOT NULL DEFAULT 0;
