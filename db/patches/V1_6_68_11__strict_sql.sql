37b6120af49067abae5ad47833d30e22813141ce
Dan Hemberger <daniel.hemberger@gmail.com>
2023-05-29 01:37:12 -0700

    Fix inserting `planet` rows with strict SQL
    * The `last_updated` column was never used and can be removed.
    
    * The `password` column can default to the empty string, since planet
      passwords will only be set once claimed by a player.

--- db/patches/V1_6_68_11__strict_sql.sql
+++ db/patches/V1_6_68_11__strict_sql.sql
@@ -9,3 +9,7 @@ UPDATE `account` SET `password_reset` = NULL WHERE `password_reset` = '';
 ALTER TABLE `account` MODIFY `mail_banned` int unsigned NOT NULL DEFAULT 0;
 
 ALTER TABLE `alliance` MODIFY `img_src` varchar(255) NOT NULL DEFAULT '';
+
+ALTER TABLE `planet` MODIFY `password` varchar(32) NOT NULL DEFAULT '';
+
+ALTER TABLE `planet` DROP COLUMN `last_updated`;
