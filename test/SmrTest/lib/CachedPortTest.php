smrealms/smr

commit f3a6d74c26b42317a9de1896398be118f6449e7f

Use prepared statements for database
Instead of manually escaping variables inline in SQL statements, we now
use prepared statements. These automatically take care of escaping, and
are generally considered a best practice. It's easier to read as well,
since we have a structured array of parameters instead of a long string
concatenation. This also enables SQL static analysis using phpstan-dba.

We switch the database access layer from MySQLi to Doctrine/DBAL with
PDO as the underlying driver. This change was made after consideration
of the following desirements:

1. Named parameters (much safer and more readable than `?`).

2. Emulated prepares (relays with the database once instead of twice,
   which is important for us because communication with the database
   is our biggest runtime expense).

3. Array escaping.

4. Ability to safely close the connection (for daemon processes).

MySQLi: 4
PDO: 1, 2
Doctrine\DBAL: 1, 2 (with PDO driver), 3, 4

In Doctrine\DBAL, parameter types are passed in a separate array to aid
the driver in properly escaping the values. Unfortunately, this doesn't
validate the PHP type; instead, it does an unsafe cast of the value to
that type (e.g. if the string "3ab" is passed where an int is expected,
then the int `3` will be used). For this reason, we will continue to
validate types manually using the escaping methods. Then we can map the
PHP types to SQL types automatically using `Database::parameterTypes`.

Note that the escaping methods no longer do any actual escaping. They
serve as a simple type check in most cases (to make sure we're passing
the type of value that we expect), but in some cases they convert a PHP
type into a corresponding SQL type (e.g. escapeBoolean converts from a
PHP bool to our TRUE/FALSE enum in SQL). In the future, these methods
will be renamed, but are retained here with misleading names to reduce
the magnitude of the diff.

In data classes (e.g. Account, Player, etc.), we change the SQL string

src/lib/Smr/Port.php
@@ -48,7 +48,9 @@ class Port {
 	public const RAZE_PAYOUT = 0.75; // fraction of base payout for razing
 	public const KILLER_RELATIONS_LOSS = 45; // relations lost by killer in PR
 
-	protected readonly string $SQL;
+	public const SQL = 'sector_id = :sector_id AND game_id = :game_id';
+	/** @var array{sector_id: int, game_id: int} */
+	public readonly array $SQLID;
 
 	protected int $shields;
 	protected int $combatDrones;
@@ -89,7 +91,10 @@ class Port {
 	public static function getGalaxyPorts(int $gameID, int $galaxyID, bool $forceUpdate = false): array {
 		$db = Database::getInstance();
 		// Use a left join so that we populate the cache for every sector
-		$dbResult = $db->read('SELECT port.* FROM port LEFT JOIN sector USING(game_id, sector_id) WHERE game_id = ' . $db->escapeNumber($gameID) . ' AND galaxy_id = ' . $db->escapeNumber($galaxyID));
+		$dbResult = $db->read('SELECT port.* FROM port LEFT JOIN sector USING(game_id, sector_id) WHERE game_id = :game_id AND galaxy_id = :galaxy_id', [
+			'game_id' => $db->escapeNumber($gameID),
+			'galaxy_id' => $db->escapeNumber($galaxyID),
+		]);
 		$galaxyPorts = [];
 		foreach ($dbResult->records() as $dbRecord) {
 			$sectorID = $dbRecord->getInt('sector_id');
@@ -111,13 +116,15 @@ class Port {
 
 	public static function removePort(int $gameID, int $sectorID): void {
 		$db = Database::getInstance();
-		$SQL = 'game_id = ' . $db->escapeNumber($gameID) . '
-		        AND sector_id = ' . $db->escapeNumber($sectorID);
-		$db->write('DELETE FROM port WHERE ' . $SQL);
-		$db->write('DELETE FROM port_has_goods WHERE ' . $SQL);
-		$db->write('DELETE FROM player_visited_port WHERE ' . $SQL);
-		$db->write('DELETE FROM player_attacks_port WHERE ' . $SQL);
-		$db->write('DELETE FROM port_info_cache WHERE ' . $SQL);
+		$SQLID = [
+			'game_id' => $db->escapeNumber($gameID),
+			'sector_id' => $db->escapeNumber($sectorID),
+		];
+		$db->write('DELETE FROM port WHERE ' . self::SQL, $SQLID);
+		$db->write('DELETE FROM port_has_goods WHERE ' . self::SQL, $SQLID);
+		$db->write('DELETE FROM player_visited_port WHERE ' . self::SQL, $SQLID);
+		$db->write('DELETE FROM player_attacks_port WHERE ' . self::SQL, $SQLID);
+		$db->write('DELETE FROM port_info_cache WHERE ' . self::SQL, $SQLID);
 		unset(self::$CACHE_PORTS[$gameID][$sectorID]);
 	}
 
@@ -148,10 +155,13 @@ class Port {
 	) {
 		$this->cachedTime = Epoch::time();
 		$db = Database::getInstance();
-		$this->SQL = 'sector_id = ' . $db->escapeNumber($sectorID) . ' AND game_id = ' . $db->escapeNumber($gameID);
+		$this->SQLID = [
+			'sector_id' => $db->escapeNumber($sectorID),
+			'game_id' => $db->escapeNumber($gameID),
+		];
 
 		if ($dbRecord === null) {
-			$dbResult = $db->read('SELECT * FROM port WHERE ' . $this->SQL);
+			$dbResult = $db->read('SELECT * FROM port WHERE ' . self::SQL, $this->SQLID);
 			if ($dbResult->hasRecord()) {
 				$dbRecord = $dbResult->record();
 			}
@@ -215,7 +225,7 @@ class Port {
 				$this->setCreditsToDefault();
 			}
 			$db = Database::getInstance();
-			$db->write('DELETE FROM player_attacks_port WHERE ' . $this->SQL);
+			$db->write('DELETE FROM player_attacks_port WHERE ' . self::SQL, $this->SQLID);
 		}
 	}
 
@@ -248,7 +258,7 @@ class Port {
 		}
 		if (!isset($this->goodAmounts)) {
 			$db = Database::getInstance();
-			$dbResult = $db->read('SELECT * FROM port_has_goods WHERE ' . $this->SQL . ' ORDER BY good_id ASC');
+			$dbResult = $db->read('SELECT * FROM port_has_goods WHERE ' . self::SQL . ' ORDER BY good_id ASC', $this->SQLID);
 			foreach ($dbResult->records() as $dbRecord) {
 				$goodID = $dbRecord->getInt('good_id');
 				$this->goodTransactions[$goodID] = $dbRecord->getStringEnum('transaction_type', TransactionType::class);
@@ -1176,10 +1186,21 @@ class Port {
 
 			$db->write('INSERT IGNORE INTO port_info_cache
 						(game_id, sector_id, port_info_hash, port_info)
-						VALUES (' . $db->escapeNumber($this->getGameID()) . ', ' . $db->escapeNumber($this->getSectorID()) . ', ' . $cacheHash . ', ' . $cache . ')');
+						VALUES (:game_id, :sector_id, :hash, :cache)', [
+				...$this->SQLID,
+				'hash' => $cacheHash,
+				'cache' => $cache,
+			]);
 
 			// We can't use the SQL member here because CachePorts don't have it
-			$db->write('UPDATE player_visited_port SET visited=' . $db->escapeNumber($this->getCachedTime()) . ', port_info_hash=' . $cacheHash . ' WHERE visited<=' . $db->escapeNumber($this->getCachedTime()) . ' AND account_id IN (' . $db->escapeArray($accountIDs) . ') AND sector_id=' . $db->escapeNumber($this->getSectorID()) . ' AND game_id=' . $db->escapeNumber($this->getGameID()) . ' LIMIT ' . count($accountIDs));
+			$db->write('UPDATE player_visited_port SET visited = :cached_time, port_info_hash = :port_info_hash WHERE visited <= :cached_time AND account_id IN (:account_ids) AND sector_id = :sector_id AND game_id = :game_id LIMIT :limit', [
+				'cached_time' => $db->escapeNumber($this->getCachedTime()),
+				'port_info_hash' => $cacheHash,
+				'sector_id' => $db->escapeNumber($this->getSectorID()),
+				'game_id' => $db->escapeNumber($this->getGameID()),
+				'account_ids' => $db->escapeArray($accountIDs),
+				'limit' => count($accountIDs),
+			]);
 
 			unset($cache);
 			return true;
@@ -1196,9 +1217,13 @@ class Port {
 			$dbResult = $db->read('SELECT visited, port_info
 						FROM player_visited_port
 						JOIN port_info_cache USING (game_id,sector_id,port_info_hash)
-						WHERE account_id = ' . $db->escapeNumber($accountID) . '
-							AND game_id = ' . $db->escapeNumber($gameID) . '
-							AND sector_id = ' . $db->escapeNumber($sectorID) . ' LIMIT 1');
+						WHERE account_id = :account_id
+							AND game_id = :game_id
+							AND sector_id = :sector_id LIMIT 1', [
+				'account_id' => $db->escapeNumber($accountID),
+				'game_id' => $db->escapeNumber($gameID),
+				'sector_id' => $db->escapeNumber($sectorID),
+			]);
 
 			if ($dbResult->hasRecord()) {
 				$dbRecord = $dbResult->record();
@@ -1241,38 +1266,40 @@ class Port {
 		if (!$this->cacheIsValid) {
 			$this->updateSectorPlayersCache();
 			// route_cache tells NPC's where they can trade
-			$db->write('DELETE FROM route_cache WHERE game_id=' . $db->escapeNumber($this->getGameID()));
+			$db->write('DELETE FROM route_cache WHERE game_id = :game_id', [
+				'game_id' => $db->escapeNumber($this->getGameID()),
+			]);
 		}
 
 		// If any fields in the `port` table have changed, update table
 		if ($this->hasChanged) {
+			$params = [
+				...$this->SQLID,
+				'experience' => $db->escapeNumber($this->getExperience()),
+				'shields' => $db->escapeNumber($this->getShields()),
+				'armour' => $db->escapeNumber($this->getArmour()),
+				'combat_drones' => $db->escapeNumber($this->getCDs()),
+				'level' => $db->escapeNumber($this->getLevel()),
+				'credits' => $db->escapeNumber($this->getCredits()),
+				'upgrade' => $db->escapeNumber($this->getUpgrade()),
+				'reinforce_time' => $db->escapeNumber($this->getReinforceTime()),
+				'attack_started' => $db->escapeNumber($this->getAttackStarted()),
+				'race_id' => $db->escapeNumber($this->getRaceID()),
+			];
 			if ($this->isNew === false) {
-				$db->write('UPDATE port SET experience = ' . $db->escapeNumber($this->getExperience()) .
-								', shields = ' . $db->escapeNumber($this->getShields()) .
-								', armour = ' . $db->escapeNumber($this->getArmour()) .
-								', combat_drones = ' . $db->escapeNumber($this->getCDs()) .
-								', level = ' . $db->escapeNumber($this->getLevel()) .
-								', credits = ' . $db->escapeNumber($this->getCredits()) .
-								', upgrade = ' . $db->escapeNumber($this->getUpgrade()) .
-								', reinforce_time = ' . $db->escapeNumber($this->getReinforceTime()) .
-								', attack_started = ' . $db->escapeNumber($this->getAttackStarted()) .
-								', race_id = ' . $db->escapeNumber($this->getRaceID()) . '
-								WHERE ' . $this->SQL);
+				$db->write('UPDATE port SET experience = :experience,
+								shields = :shields,
+								armour = :armour,
+								combat_drones = :combat_drones,
+								level = :level,
+								credits = :credits,
+								upgrade = :upgrade,
+								reinforce_time = :reinforce_time,
+								attack_started = :attack_started,
+								race_id = :race_id
+								WHERE ' . self::SQL, $params);
 			} else {
-				$db->insert('port', [
-					'game_id' => $db->escapeNumber($this->getGameID()),
-					'sector_id' => $db->escapeNumber($this->getSectorID()),
-					'experience' => $db->escapeNumber($this->getExperience()),
-					'shields' => $db->escapeNumber($this->getShields()),
-					'armour' => $db->escapeNumber($this->getArmour()),
-					'combat_drones' => $db->escapeNumber($this->getCDs()),
-					'level' => $db->escapeNumber($this->getLevel()),
-					'credits' => $db->escapeNumber($this->getCredits()),
-					'upgrade' => $db->escapeNumber($this->getUpgrade()),
-					'reinforce_time' => $db->escapeNumber($this->getReinforceTime()),
-					'attack_started' => $db->escapeNumber($this->getAttackStarted()),
-					'race_id' => $db->escapeNumber($this->getRaceID()),
-				]);
+				$db->insert('port', $params);
 				$this->isNew = false;
 			}
 			$this->hasChanged = false;
@@ -1285,7 +1312,12 @@ class Port {
 				continue;
 			}
 			$amount = $this->getGoodAmount($goodID);
-			$db->write('UPDATE port_has_goods SET amount = ' . $db->escapeNumber($amount) . ', last_update = ' . $db->escapeNumber(Epoch::time()) . ' WHERE ' . $this->SQL . ' AND good_id = ' . $db->escapeNumber($goodID));
+			$db->write('UPDATE port_has_goods SET amount = :amount, last_update = :last_update WHERE ' . self::SQL . ' AND good_id = :good_id', [
+				...$this->SQLID,
+				'good_id' => $db->escapeNumber($goodID),
+				'amount' => $db->escapeNumber($amount),
+				'last_update' => $db->escapeNumber(Epoch::time()),
+			]);
 			unset($this->goodAmountsChanged[$goodID]);
 		}
 
@@ -1294,8 +1326,7 @@ class Port {
 			if ($status === true) {
 				// add the good
 				$db->replace('port_has_goods', [
-					'game_id' => $db->escapeNumber($this->getGameID()),
-					'sector_id' => $db->escapeNumber($this->getSectorID()),
+					...$this->SQLID,
 					'good_id' => $db->escapeNumber($goodID),
 					'transaction_type' => $db->escapeString($this->getGoodTransaction($goodID)->value),
 					'amount' => $db->escapeNumber($this->getGoodAmount($goodID)),
@@ -1303,7 +1334,10 @@ class Port {
 				]);
 			} else {
 				// remove the good
-				$db->write('DELETE FROM port_has_goods WHERE ' . $this->SQL . ' AND good_id=' . $db->escapeNumber($goodID) . ';');
+				$db->write('DELETE FROM port_has_goods WHERE ' . self::SQL . ' AND good_id = :good_id', [
+					...$this->SQLID,
+					'good_id' => $db->escapeNumber($goodID),
+				]);
 			}
 			unset($this->goodTransactionsChanged[$goodID]);
 		}
@@ -1412,7 +1446,10 @@ class Port {
 		//get all players involved for HoF
 		$attackers = [];
 		$db = Database::getInstance();
-		$dbResult = $db->read('SELECT player.* FROM player_attacks_port JOIN player USING (game_id, account_id) WHERE game_id = ' . $db->escapeNumber($this->gameID) . ' AND player_attacks_port.sector_id = ' . $db->escapeNumber($this->sectorID) . ' AND time > ' . $db->escapeNumber(Epoch::time() - self::TIME_TO_CREDIT_RAID));
+		$dbResult = $db->read('SELECT player.* FROM player_attacks_port JOIN player USING (game_id, account_id) WHERE game_id = :game_id AND player_attacks_port.sector_id = :sector_id AND time > :credit_time', [
+			...$this->SQLID,
+			'credit_time' => $db->escapeNumber(Epoch::time() - self::TIME_TO_CREDIT_RAID),
+		]);
 		foreach ($dbResult->records() as $dbRecord) {
 			$attackers[] = Player::getPlayer($dbRecord->getInt('account_id'), $this->getGameID(), false, $dbRecord);
 		}

commit 72dcc90a42f3634fbd9c1a3e4982cb67f6d7e881

Port: fix use of incomplete cached Port
The following are not cached properties, and must either be set before
being used, or not used at all:

* `SQLID` should not be used (broken by f3a6d74c26b42317a).
* `isNew` should have a default value (broken by 0ec877aa5ba62).

These errors were identified by trying to share maps.

src/lib/Smr/Port.php
@@ -78,7 +78,7 @@ class Port {
 	protected bool $cacheIsValid = true;
 
 	protected bool $hasChanged = false;
-	protected bool $isNew;
+	protected bool $isNew = false;
 
 	public static function clearCache(): void {
 		self::$CACHE_PORTS = [];
@@ -168,7 +168,6 @@ class Port {
 		}
 
 		if ($dbRecord !== null) {
-			$this->isNew = false;
 			$this->shields = $dbRecord->getInt('shields');
 			$this->combatDrones = $dbRecord->getInt('combat_drones');
 			$this->armour = $dbRecord->getInt('armour');
@@ -1195,7 +1194,8 @@ class Port {
 			$db->write('INSERT IGNORE INTO port_info_cache
 						(game_id, sector_id, port_info_hash, port_info)
 						VALUES (:game_id, :sector_id, :hash, :cache)', [
-				...$this->SQLID,
+				'sector_id' => $db->escapeNumber($this->getSectorID()),
+				'game_id' => $db->escapeNumber($this->getGameID()),
 				'hash' => $cacheHash,
 				'cache' => $cache,
 			]);

test/SmrTest/lib/CachedPortTest.php
@@ -1,0 +1,73 @@
+<?php declare(strict_types=1);
+
+namespace SmrTest\lib;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use Smr\Container\DiContainer;
+use Smr\Epoch;
+use Smr\Port;
+use Smr\TransactionType;
+use SmrTest\BaseIntegrationSpec;
+
+#[CoversClass(Port::class)]
+class CachedPortTest extends BaseIntegrationSpec {
+
+	protected function tablesToTruncate(): array {
+		return ['player_visited_port', 'port_info_cache'];
+	}
+
+	protected function tearDown(): void {
+		Port::clearCache();
+		// Reset the DI container to avoid contaminating other tests
+		DiContainer::initialize(false);
+	}
+
+	public function test_addCachePorts(): void {
+		$account1 = 42;
+		$account2 = 43;
+
+		// Create an arbitrary Port
+		$sectorID = 2;
+		$gameID = 3;
+		$port1 = Port::createPort($gameID, $sectorID);
+		$port1->setCredits(1); // so that exists() is true
+		$port1->addPortGood(GOODS_ORE, TransactionType::Sell);
+
+		// Add a cached port from a Port
+		$added = $port1->addCachePorts([$account1]);
+		self::assertTrue($added);
+
+		// Check that the goods on the cached Port match the original
+		$cachedPort1 = Port::getCachedPort($gameID, $sectorID, $account1);
+		$expected = [GOODS_ORE => TransactionType::Sell];
+		self::assertSame($expected, $cachedPort1->getGoodTransactions());
+
+		// Add a cached port from a cached Port (test multiple accounts at once)
+		$added = $cachedPort1->addCachePorts([$account1, $account2]);
+		self::assertTrue($added);
+
+		// Create a "newer" version of the Port with a larger cache time
+		$t2 = Epoch::time() + 2;
+		$epoch = $this->createStub(Epoch::class);
+		$epoch->method('getTime')->willReturn($t2);
+		DiContainer::getContainer()->set(Epoch::class, $epoch);
+
+		Port::clearCache();
+		$port2 = Port::createPort($gameID, $sectorID);
+		$port2->setCredits(1); // so that exists() is true
+		self::assertSame($t2, $port2->getCachedTime());
+
+		// Adding with a newer Port does change the cache
+		$port2->addPortGood(GOODS_WOOD, TransactionType::Buy);
+		$port2->addCachePort($account2);
+		$cachedPort2 = Port::getCachedPort($gameID, $sectorID, $account2);
+		$expected = [GOODS_WOOD => TransactionType::Buy];
+		self::assertSame($expected, $cachedPort2->getGoodTransactions());
+
+		// Adding with an older Port does not change the cache
+		$port1->addCachePort($account2);
+		$cachedPort2 = Port::getCachedPort($gameID, $sectorID, $account2, true);
+		self::assertSame($expected, $cachedPort2->getGoodTransactions());
+	}
+
+}