smrealms/smr

commit c046eea6607183641e7332abc9e2b82b9266547c

Player: remove nullability of player comparisons
For the `equals` and `sameAlliance` methods, do not allow a null player
argument. While this may have been useful when Player getter methods
could return null, but now that failure to find a Player is an error,
we should not be accepting null for comparison of Player objects.

src/lib/Smr/AbstractPlayer.php
@@ -3034,51 +3034,51 @@ abstract class AbstractPlayer {
 		return false;
 	}
 
-	public function equals(self $otherPlayer = null): bool {
-		return $otherPlayer !== null && $this->getAccountID() === $otherPlayer->getAccountID() && $this->getGameID() === $otherPlayer->getGameID();
+	public function equals(self $otherPlayer): bool {
+		return $this->getAccountID() === $otherPlayer->getAccountID() && $this->getGameID() === $otherPlayer->getGameID();
 	}
 
-	public function sameAlliance(self $otherPlayer = null): bool {
-		return $this->equals($otherPlayer) || ($otherPlayer !== null && $this->getGameID() === $otherPlayer->getGameID() && $this->hasAlliance() && $this->getAllianceID() === $otherPlayer->getAllianceID());
+	public function sameAlliance(self $otherPlayer): bool {
+		return $this->getGameID() === $otherPlayer->getGameID() && $this->hasAlliance() && $this->getAllianceID() === $otherPlayer->getAllianceID();
 	}
 
-	public function sharedForceAlliance(self $otherPlayer = null): bool {
+	public function sharedForceAlliance(self $otherPlayer): bool {
 		return $this->sameAlliance($otherPlayer);
 	}
 
-	public function forceNAPAlliance(self $otherPlayer = null): bool {
+	public function forceNAPAlliance(self $otherPlayer): bool {
 		return $this->sameAlliance($otherPlayer);
 	}
 
-	public function planetNAPAlliance(self $otherPlayer = null): bool {
+	public function planetNAPAlliance(self $otherPlayer): bool {
 		return $this->sameAlliance($otherPlayer);
 	}
 
-	public function traderNAPAlliance(self $otherPlayer = null): bool {
+	public function traderNAPAlliance(self $otherPlayer): bool {
 		return $this->sameAlliance($otherPlayer);
 	}
 
-	public function traderMAPAlliance(self $otherPlayer = null): bool {
+	public function traderMAPAlliance(self $otherPlayer): bool {
 		return $this->traderAttackTraderAlliance($otherPlayer) && $this->traderDefendTraderAlliance($otherPlayer);
 	}
 
-	public function traderAttackTraderAlliance(self $otherPlayer = null): bool {
+	public function traderAttackTraderAlliance(self $otherPlayer): bool {
 		return $this->sameAlliance($otherPlayer);
 	}
 
-	public function traderDefendTraderAlliance(self $otherPlayer = null): bool {
+	public function traderDefendTraderAlliance(self $otherPlayer): bool {
 		return $this->sameAlliance($otherPlayer);
 	}
 
-	public function traderAttackForceAlliance(self $otherPlayer = null): bool {
+	public function traderAttackForceAlliance(self $otherPlayer): bool {
 		return $this->sameAlliance($otherPlayer);
 	}
 
-	public function traderAttackPortAlliance(self $otherPlayer = null): bool {
+	public function traderAttackPortAlliance(self $otherPlayer): bool {
 		return $this->sameAlliance($otherPlayer);
 	}
 
-	public function traderAttackPlanetAlliance(self $otherPlayer = null): bool {
+	public function traderAttackPlanetAlliance(self $otherPlayer): bool {
 		return $this->sameAlliance($otherPlayer);
 	}
 

src/pages/Account/HallOfFamePersonal.php
@@ -34,7 +34,6 @@ class HallOfFamePersonal extends AccountPage {
 	public function build(Account $account, Template $template): void {
 		$account_id = $this->hofAccountID;
 		$game_id = $this->gameID;
-		$player = null;
 
 		if (isset($game_id)) {
 			try {
@@ -66,7 +65,7 @@ class HallOfFamePersonal extends AccountPage {
 			if ($account->getAccountID() === $account_id) {
 				$allowedVis[] = HOF_ALLIANCE;
 				$allowedVis[] = HOF_PRIVATE;
-			} elseif (isset($hofPlayer) && $hofPlayer->sameAlliance($player)) {
+			} elseif (isset($hofPlayer, $player) && $hofPlayer->sameAlliance($player)) {
 				$allowedVis[] = HOF_ALLIANCE;
 			}
 			$categories = HallOfFame::getHofCategories($this, $allowedVis, $game_id, $account_id);

commit 01d508c12bd19575bb08a5835035bd9eb4e39dd3

Player: fix sameAlliance for players not in alliance
The Player::sameAlliance method is expected to return true when the
two players are the same player. This is basically just a shortcut for
avoiding an additional Player::equals check in some places. The issue
is that this was returning false if the player was not in an alliance.

This issue was introduced in c046eea660. It caused solo players to be
hostile to their own forces and to not be able to land on their own
planet.

src/lib/Smr/AbstractPlayer.php
@@ -3059,7 +3059,7 @@ abstract class AbstractPlayer {
 	}
 
 	public function sameAlliance(self $otherPlayer): bool {
-		return $this->getGameID() === $otherPlayer->getGameID() && $this->hasAlliance() && $this->getAllianceID() === $otherPlayer->getAllianceID();
+		return $this->equals($otherPlayer) || ($this->getGameID() === $otherPlayer->getGameID() && $this->hasAlliance() && $this->getAllianceID() === $otherPlayer->getAllianceID());
 	}
 
 	public function sharedForceAlliance(self $otherPlayer): bool {

test/SmrTest/lib/AbstractPlayerIntegrationTest.php
@@ -10,6 +10,7 @@ use Smr\Exceptions\PlayerNotFound;
 use Smr\Exceptions\UserError;
 use Smr\Game;
 use SmrTest\BaseIntegrationSpec;
+use SmrTest\TestUtils;
 
 #[CoversClass(AbstractPlayer::class)]
 class AbstractPlayerIntegrationTest extends BaseIntegrationSpec {
@@ -213,6 +214,31 @@ class AbstractPlayerIntegrationTest extends BaseIntegrationSpec {
 		// False if player is in a ship with too large an attack rating
 		$player1->setShipTypeID(SHIP_TYPE_MOTHER_SHIP);
 		self::assertFalse($player1->isNewbieCombatant());
+	}
+
+	public function test_sameAlliance(): void {
+		$player1 = AbstractPlayer::createPlayer(1, self::$gameID, 'test1', RACE_HUMAN, true);
+		$player2 = AbstractPlayer::createPlayer(2, self::$gameID, 'test2', RACE_HUMAN, true);
+
+		// True if the players are identical, and not in an alliance
+		self::assertTrue($player1->sameAlliance($player1));
+
+		// False if both players are not in an alliance
+		self::assertFalse($player1->sameAlliance($player2));
+
+		// True if the players are identical, and in an alliance
+		$setter1 = TestUtils::getPrivateMethod($player1, 'setAllianceID');
+		$setter1->invoke($player1, 42);
+		self::assertTrue($player1->sameAlliance($player1));
+
+		// True if both players are in the same alliance
+		$setter2 = TestUtils::getPrivateMethod($player2, 'setAllianceID');
+		$setter2->invoke($player2, 42);
+		self::assertTrue($player1->sameAlliance($player2));
+
+		// False if players are in different alliances
+		$setter2->invoke($player2, 43);
+		self::assertFalse($player1->sameAlliance($player2));
 	}
 
 }