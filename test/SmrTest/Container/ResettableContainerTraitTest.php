4a90daa01e27ee50ca59c9590be72b569ae8c958
Dan Hemberger <daniel.hemberger@gmail.com>
2024-07-28 12:38:19 -0700

    Upgrade to PHPUnit 11
    Address deprecation warnings:
    
    > Targeting a trait such as Smr\Container\ResettableContainerTrait with #[CoversClass] is deprecated, please refactor your test to use #[CoversTrait] instead.
    
    > returnValueMap() is deprecated and will be removed in PHPUnit 12. Use $double->willReturnMap() instead of $double->will($this->returnValueMap())

--- test/SmrTest/Container/ResettableContainerTraitTest.php
+++ test/SmrTest/Container/ResettableContainerTraitTest.php
@@ -2,12 +2,12 @@
 
 namespace SmrTest\Container;
 
-use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\CoversTrait;
 use PHPUnit\Framework\TestCase;
 use Smr\Container\ResettableContainer;
 use Smr\Container\ResettableContainerTrait;
 
-#[CoversClass(ResettableContainerTrait::class)]
+#[CoversTrait(ResettableContainerTrait::class)]
 class ResettableContainerTraitTest extends TestCase {
 
 	public function test_not_initialized_by_definition(): void {
