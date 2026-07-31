e88c8a19ec66467c25d9d3d02f1c8b2d1373e972
Dan Hemberger <daniel.hemberger@gmail.com>
2026-07-30 23:40:07 -0700

    album_delete_confirmation.php: fix HTML entity typo
    There is no `&nbps;` entity. Should have been `&nbsp;`.

--- src/templates/Default/engine/Default/album_delete_confirmation.php
+++ src/templates/Default/engine/Default/album_delete_confirmation.php
@@ -9,5 +9,5 @@
 Are you sure you want to delete your photo album entry and all comments added to it?<br />
 This action cannot be undone.<br /><br />
 
-<?php echo create_submit_link($ConfirmHref, 'Yes'); ?>&nbps;&nbsp;&nbsp;
+<?php echo create_submit_link($ConfirmHref, 'Yes'); ?>&nbsp;&nbsp;&nbsp;
 <?php echo create_submit_link($CancelHref, 'No');
